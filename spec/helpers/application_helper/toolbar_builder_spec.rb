1e8845e81f06b9f2c720580dd26781eee401276d
Erik Clarizio <eclarizi@redhat.com>
2016-10-21 08:56:40 -0700

    Add 'delete' to generic object configuration dropdown
    https://www.pivotaltracker.com/story/show/129980083
    (cherry picked from commit 619fe92ca778e663cadd80a7af1493c56c2fcc06)

--- spec/helpers/application_helper/toolbar_builder_spec.rb
+++ spec/helpers/application_helper/toolbar_builder_spec.rb
@@ -1190,6 +1190,19 @@ describe ApplicationHelper, "::ToolbarBuilder" do
             'function-data' => '{"eventType": "showEditForm"}'
           }
         )
+        expect(items[2]).to include(
+          :id      => "generic_object_definition_choice__generic_object_definition_delete",
+          :type    => :button,
+          :icon    => "pficon pficon-delete fa-lg",
+          :title   => "Delete this Generic Object Definition",
+          :text    => "Delete this Generic Object Definition",
+          :onwhen  => "1",
+          :enabled => false,
+          :data    => {
+            'function'      => 'sendDataWithRx',
+            'function-data' => '{"eventType": "deleteGenericObject"}'
+          }
+        )
       end
     end
   end
