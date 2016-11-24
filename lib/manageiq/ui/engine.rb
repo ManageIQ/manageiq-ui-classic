fba4cb10aa7703c3a95d4847adb658f89a4f51d3
Martin Povolny <mpovolny@redhat.com>
2016-11-24 10:58:17 +0100

    Add autoload path for controller mixins.

--- lib/manageiq/ui/engine.rb
+++ lib/manageiq/ui/engine.rb
@@ -1,6 +1,7 @@
 module Manageiq
   module Ui
     class Engine < ::Rails::Engine
+      config.autoload_paths << File.expand_path(File.join('..', '..', '..', '..', 'app', 'controllers', 'mixins'), __FILE__)
     end
   end
 end
