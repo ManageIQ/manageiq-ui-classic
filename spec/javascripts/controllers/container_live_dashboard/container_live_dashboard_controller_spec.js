5b3b4883ef9135a745eb6c6447a94fa24b74ae03
yaacov <kobi.zamir@gmail.com>
2017-01-03 13:28:31 +0200

    use controller as

--- spec/javascripts/controllers/container_live_dashboard/container_live_dashboard_controller_spec.js
+++ spec/javascripts/controllers/container_live_dashboard/container_live_dashboard_controller_spec.js
@@ -14,19 +14,17 @@ describe('containerLiveDashboardController', function() {
     });
   });
 
-  beforeEach(inject(function(_$httpBackend_, $rootScope, _$controller_, _pfViewUtils_) {
+  beforeEach(inject(function(_$httpBackend_, _$controller_, _pfViewUtils_) {
     pfViewUtils = _pfViewUtils_;
-    $scope = $rootScope.$new();
     $httpBackend = _$httpBackend_;
     $httpBackend.when('GET','/container_dashboard/data/42/?live=true&tenant=_ops&query=metric_tags').respond(mock_data);
     $httpBackend.when('GET','/container_dashboard/data/42/?live=true&tenant=_ops&query=metric_definitions&tags={}').respond(mock_metrics_data);
     $httpBackend.when('GET','/container_dashboard/data/42/?live=true&tenant=_ops&query=get_data&metric_id=hello1&limit=5&order=DESC').respond(mock_data1_data);
     $httpBackend.when('GET','/container_dashboard/data/42/?live=true&tenant=_ops&query=get_data&metric_id=hello2&limit=5&order=DESC').respond(mock_data2_data);
     $controller = _$controller_('containerLiveDashboardController', {
-        $scope: $scope,
         pfViewUtils: pfViewUtils
     });
-    $scope.refresh();
+    $controller.refresh();
     $httpBackend.flush();
   }));
 
@@ -37,34 +35,34 @@ describe('containerLiveDashboardController', function() {
 
   describe('loading page data', function() {
     it('should load tag list', function() {
-      expect($scope.filterConfig.fields.length).toBe(12);
+      expect($controller.filterConfig.fields.length).toBe(12);
     });
 
     it('should load metrics definitions', function() {
-      expect($scope.loadingMetrics).toBeDefined();
-      expect($scope.loadingMetrics).toBe(false);
-      expect($scope.items.length).toBe(2);
+      expect($controller.loadingMetrics).toBeDefined();
+      expect($controller.loadingMetrics).toBe(false);
+      expect($controller.items.length).toBe(2);
     });
   });
 
   describe('count increment', function() {    
     it('should increment the count', function() {
-      $scope.countIncrement();
-      expect($scope.timeFilter.range_count).toBe(2);
+      $controller.countIncrement();
+      expect($controller.timeFilter.range_count).toBe(2);
     });
 
     it('should decrement the count', function() {
-      $scope.timeFilter.range_count = 10;
-      $scope.countDecrement();
-      expect($scope.timeFilter.range_count).toBe(9);
+      $controller.timeFilter.range_count = 10;
+      $controller.countDecrement();
+      expect($controller.timeFilter.range_count).toBe(9);
     });
   });
 
   describe('check numeric formater', function() {    
     it('should fomrat numbers correctly', function() {
-      expect($scope.items[0].lastValues[1480424640000]).toBe("10.00");
-      expect($scope.items[0].lastValues[1480424630000]).toBe("1.01k");
-      expect($scope.items[0].lastValues[1480424620000]).toBe("20.00t");
+      expect($controller.items[0].lastValues[1480424640000]).toBe("10.00");
+      expect($controller.items[0].lastValues[1480424630000]).toBe("1.01k");
+      expect($controller.items[0].lastValues[1480424620000]).toBe("20.00t");
     });
   });
 });
