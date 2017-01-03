161080ab8376b3a69336baf03a67ecd83f4dc499
Eric Hayes <eric@erichayes.net>
2017-01-03 13:17:53 -0800

    WIP converting rbac_tree to tree builder

--- app/controllers/ops_controller/ops_rbac.rb
+++ app/controllers/ops_controller/ops_rbac.rb
@@ -954,13 +954,20 @@ module OpsController::OpsRbac
   def rbac_role_get_details(id)
     @edit = nil
     @record = @role = MiqUserRole.find_by_id(from_cid(id))
-    @role_features = @role.feature_identifiers.sort
-    @features_tree = rbac_build_features_tree
+    @rbac_menu_tree = build_rbac_menu_tree
   end
 
-  def rbac_build_features_tree
+  def build_rbac_menu_tree
     @role = @sb[:typ] == "copy" ? @record.dup : @record if @role.nil? # if on edit screen use @record
-    TreeBuilder.convert_bs_tree(OpsController::RbacTree.build(@role, @role_features, !@edit.nil?)).to_json
+    # TreeBuilder.convert_bs_tree(OpsController::RbacTree.build(@role, @role_features, !@edit.nil?)).to_json
+    TreeBuilderOpsRbacMenu.new(
+      "features_tree",
+      "features",
+      @sb,
+      true,
+      role: @role,
+      editable: @edit.present?
+    )
   end
 
   # Set form variables for role edit
@@ -1176,7 +1183,7 @@ module OpsController::OpsRbac
     @edit[:current] = copy_hash(@edit[:new])
 
     @role_features = @record.feature_identifiers.sort
-    @features_tree = rbac_build_features_tree
+    @rbac_menu_tree = build_rbac_menu_tree
   end
 
   # Get array of total set of features from the children of selected features
