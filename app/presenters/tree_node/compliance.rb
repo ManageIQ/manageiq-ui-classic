aec003d9bb0d2ce2a2246856323a089fd78ea9db
Dávid Halász <dhalasz@redhat.com>
2017-01-05 11:38:42 +0100

    Use the newly added ViewHelper in TreeNode:: subclasses

--- app/presenters/tree_node/compliance.rb
+++ app/presenters/tree_node/compliance.rb
@@ -1,9 +1,9 @@
 module TreeNode
   class Compliance < Node
     set_attribute(:title) do
-      capture do
-        concat content_tag(:strong, "#{_('Compliance Check on')}: ")
-        concat format_timezone(@object.timestamp, Time.zone, 'gtl')
+      ViewHelper.capture do
+        ViewHelper.concat ViewHelper.content_tag(:strong, "#{_('Compliance Check on')}: ")
+        ViewHelper.concat format_timezone(@object.timestamp, Time.zone, 'gtl')
       end
     end
 
--- app/presenters/tree_node/miq_policy.rb
+++ app/presenters/tree_node/miq_policy.rb
@@ -3,9 +3,9 @@ module TreeNode
     set_attribute(:image) { "100/miq_policy_#{@object.towhat.downcase}#{@object.active ? '' : '_inactive'}.png" }
     set_attribute(:title) do
       if @options[:tree] == :policy_profile_tree
-        capture do
-          concat content_tag(:strong, "#{ui_lookup(:model => @object.towhat)} #{@object.mode.titleize}: ")
-          concat ERB::Util.html_escape(@object.description)
+        ViewHelper.capture do
+          ViewHelper.concat ViewHelper.content_tag(:strong, "#{ui_lookup(:model => @object.towhat)} #{@object.mode.titleize}: ")
+          ViewHelper.concat ERB::Util.html_escape(@object.description)
         end
       else
         @object.description
