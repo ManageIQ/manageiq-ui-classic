98ec3dec53720adfed70e6537c93477cbcfc6bba
Marcel Hild <hild@b4mad.net>
2017-01-09 09:46:53 +0100

    dont require shared specs from miq core but use autorequired from ui spec_helper

--- spec/controllers/cloud_subnet_controller_spec.rb
+++ spec/controllers/cloud_subnet_controller_spec.rb
@@ -1,5 +1,3 @@
-require Rails.root.join('spec/shared/controllers/shared_examples_for_cloud_subnet_controller')
-
 describe CloudSubnetController do
   include_examples :shared_examples_for_cloud_subnet_controller, %w(openstack azure google)
 
--- spec/controllers/security_group_controller_spec.rb
+++ spec/controllers/security_group_controller_spec.rb
@@ -1,5 +1,3 @@
-require Rails.root.join('spec/shared/controllers/shared_examples_for_security_group_controller')
-
 describe SecurityGroupController do
   include_examples :shared_examples_for_security_group_controller, %w(openstack azure google)
 
