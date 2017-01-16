2e88fd18132082afef3048a8948bad078f83a79b
Chris Arcand <chris@chrisarcand.com>
2017-01-16 13:19:19 -0600

    Namespace ViewHelper properly

--- spec/support/view_spec_helper.rb
+++ spec/support/view_spec_helper.rb
@@ -1,30 +1,33 @@
-module ViewSpecHelper
-  def show_element(resp, id)
-    set_element_visibility(resp, id, true)
-  end
+module Spec
+  module Support
+    module ViewSpecHelper
+      def show_element(resp, id)
+        set_element_visibility(resp, id, true)
+      end
 
-  def hide_element(resp, id)
-    set_element_visibility(resp, id, false)
-  end
+      def hide_element(resp, id)
+        set_element_visibility(resp, id, false)
+      end
 
-  private
+      private
 
-  def set_element_visibility(resp, id, visible)
-    c = Capybara.string(resp)
-    node = c.find(id, :visible => :all)
-    node.native["style"] = node.native["style"].gsub(/display\s*:[^;]+/, "display:#{visible ? "show" : "none"}")
-    c.native.to_s
-  end
+      def set_element_visibility(resp, id, visible)
+        c = Capybara.string(resp)
+        node = c.find(id, :visible => :all)
+        node.native["style"] = node.native["style"].gsub(/display\s*:[^;]+/, "display:#{visible ? "show" : "none"}")
+        c.native.to_s
+      end
 
-  def set_controller_for_view(controller_name)
-    controller.request.path_parameters[:controller] = controller_name
-  end
+      def set_controller_for_view(controller_name)
+        controller.request.path_parameters[:controller] = controller_name
+      end
 
-  def set_controller_for_view_to_be_restful
-    allow(controller).to receive(:restful?).and_return(true)
-  end
+      def set_controller_for_view_to_be_restful
+        allow(controller).to receive(:restful?).and_return(true)
+      end
 
-  def set_controller_for_view_to_be_nonrestful
-    allow(controller).to receive(:restful?).and_return(false)
+      def set_controller_for_view_to_be_nonrestful
+        allow(controller).to receive(:restful?).and_return(false)
+      end
+    end
   end
-end
