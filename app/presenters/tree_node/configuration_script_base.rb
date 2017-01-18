e44dd267be50c3c7956b9ccd9873e5e130097f3f
Jason Frey <fryguy9@gmail.com>
2017-01-18 15:45:51 -0500

    Fix issue with change in base class of ConfigurationScript

--- app/presenters/tree_node/configuration_script_base.rb
+++ app/presenters/tree_node/configuration_script_base.rb
@@ -1,5 +1,5 @@
 module TreeNode
-  class ConfigurationScript < Node
+  class ConfigurationScriptBase < Node
     set_attribute(:icon, 'product product-template')
     set_attribute(:tooltip) { _("Ansible Tower Job Template: %{name}") % {:name => @object.name} }
   end
