253e27d6499df0ddeb81efd9fea9c5a38893cd6f
Dávid Halász <dhalasz@redhat.com>
2017-01-18 13:06:30 +0100

    Unified the layout for VNC/SPICE remote consoles
    - Created a common HAML view for both console types
    - Separate JS files with preprocessed dependencies
    - Removed the old HAML views for VNC/SPICE
    - Removed some dead code in the related controller

---
+++ app/assets/javascripts/remote_consoles/spice.js
@@ -0,0 +1,32 @@
+//= require jquery
+//= require spice-html5-bower/spiceHTML5/spicearraybuffer
+//= require spice-html5-bower
+//= require_tree ../locale
+//= require gettext/all
+
+$(function() {
+  var host = window.location.hostname;
+  var encrypt = window.location.protocol === 'https:';
+  var port = encrypt ? 443 : 80;
+  if (window.location.port) {
+    port = window.location.port;
+  }
+
+  $('#ctrlaltdel').click(sendCtrlAltDel);
+
+  var spice = new SpiceMainConn({
+    uri: (encrypt ? 'wss://' : 'ws://') + host + ':' + port + '/' + $('#remote-console').attr('data-url'),
+    screen_id: "remote-console",
+    password: $('#remote-console').attr('data-secret'),
+    onerror: function(e) {
+      spice.stop();
+      $('#connection-status').removeClass('label-success label-warning').addClass('label-danger');
+      $('#connection-status').text(__('Disconnected'));
+      console.error("SPICE", e);
+    },
+    onsuccess: function() {
+      $('#connection-status').removeClass('label-danger label-warning').addClass('label-success');
+      $('#connection-status').text(__('Connected'));
+    },
+  });
+});
