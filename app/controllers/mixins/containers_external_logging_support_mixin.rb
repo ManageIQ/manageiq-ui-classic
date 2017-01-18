f2b074829fde1bcdd67a666a4a1faf078c56600d
Erez Freiberger <efreiber@redhat.com>
2017-01-18 11:39:31 +0200

    Adding external logging link

---
+++ app/controllers/mixins/containers_external_logging_support_mixin.rb
@@ -0,0 +1,18 @@
+module ContainersExternalLoggingSupportMixin
+  def launch_external_logging_support
+    record = self.class.model.find(params[:id])
+    ems = record.ext_management_system
+    route_name = ems.external_logging_route_name
+    logging_route = ContainerRoute.find_by(:name => route_name, :ems_id => ems.id) if route_name
+    if logging_route
+      scheme = URI.parse(logging_route.host_name).scheme || "https"
+      url = "#{scheme}://#{logging_route.host_name}#{record.external_logging_path}"
+      javascript_open_window(url)
+    else
+      javascript_flash(:text        => _("A route named '#{route_name}' is configured to connect to the " \
+                                          "external logging server but it doesn't exist"),
+                       :severity    => :error,
+                       :spinner_off => true)
+    end
+  end
+end
