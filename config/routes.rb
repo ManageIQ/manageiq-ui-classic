8cc0b82abfa6d1a5b169b779e635bbf5fb82e166
Gilles Dubreuil <gilles@redhat.com>
2017-01-19 16:27:16 +1100

    Adds missing route for wait_for_task

--- config/routes.rb
+++ config/routes.rb
@@ -1533,6 +1533,7 @@ Rails.application.routes.draw do
         tag_edit_form_field_changed
         tagging_edit
         update
+        wait_for_task
       ) +
         adv_search_post +
         compare_post +
