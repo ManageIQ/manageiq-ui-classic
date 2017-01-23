bdb3d5a47e0510f6787a996d8f5b5fe504eaa9f7
Martin Hradil <mhradil@redhat.com>
2017-01-23 20:17:12 +0000

    Static spec - remove offending allow_any_instance_of - no longer needed

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,9 +1,5 @@
 describe "/static/*" do
   context "foo" do
-    before :each do
-      allow_any_instance_of(ApplicationController).to receive(:set_user_time_zone)
-    end
-
     it "returns test template" do
       get "/static/test"
       expect(response.status).to eq(200)
