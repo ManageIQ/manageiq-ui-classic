21b48545f7a74461e9ffd4473d161f4e67e9314c
Martin Hradil <mhradil@redhat.com>
2017-01-23 19:28:03 +0000

    Move ui-specific tests from manageiq
    this is the counterpart to https://github.com/ManageIQ/manageiq/pull/13627

---
+++ spec/routing/support_routing_spec.rb
@@ -0,0 +1,9 @@
+describe "routes for AvailabilityZoneController" do
+  let(:controller_name) { "support" }
+
+  describe "#index" do
+    it "routes with GET" do
+      expect(get("/#{controller_name}")).to route_to("#{controller_name}#index")
+    end
+  end
+end
