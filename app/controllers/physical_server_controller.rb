1a8658238bbddde0224d002ad02d1cb9af14abe0
Walter Alves <walter.arruda.alves@gmail.com>
2017-01-30 15:19:54 -0300

    Configure Physical server actions
    Configuring show and show_list actions in physical_server_controller.rb.

--- app/controllers/physical_server_controller.rb
+++ app/controllers/physical_server_controller.rb
@@ -1,6 +1,6 @@
 class PhysicalServerController  < ApplicationController
   include Mixins::GenericListMixin
-  include Mixins::GenericShowMixin
+#  include Mixins::GenericShowMixin
 
 
 
@@ -18,7 +18,41 @@ class PhysicalServerController  < ApplicationController
     @table_name ||= "physical_servers"
   end
 
+  def get_session_data
+    @title  = _("Physical Servers")
+  end
 
+  def collect_data(server_id)
+    PhysicalServerService.new(server_id, self).all_data
+  end
+
+  def set_session_data
+    session[:layout] = @layout
+  end
+
+
+  def show_list
+    process_show_list
+  end
+
+
+  def show
+    @display = params[:display] || "main" unless pagination_or_gtl_request?
+  
+    @ph_server = @record = identity_record(params[:id])
+    return if record_no_longer_exists?(@ph_server, 'PhysicalServer')
+
+    case @display
+
+    when "main"
+      get_tagdata(@ph_server)
+      drop_breadcrumb({:name => _("Physical Server"), :url => "/physical_server/show_list?page=#{@current_page}&refresh=y"}, true)
+      drop_breadcrumb({:name => _("%{name} (Summary)") % {:name => @ph_server.name, :url => "physical_server/show/#{@ph_server.id}"}})
+      @show_type = "main"
+
+    end
+
+  end
 
 
 
