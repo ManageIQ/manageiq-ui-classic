0deded466501ea3eb1b45bdb0af117e872fb077b
Jeffrey Phillips <jephilli@redhat.com>
2017-02-01 10:54:38 -0500

    Add unit tests for alerts service and controllers

---
+++ spec/javascripts/controllers/alerts_center/alerts_overview_controller_spec.js
@@ -0,0 +1,101 @@
+describe('alertsOverviewController', function() {
+  var $scope, $controller, alertsCenterService;
+  var adminResponse, operatorResponse, existingUsersResponse, providersResponse, tagsResponse, alertsResponse;
+
+  beforeEach(module('alertsCenter'));
+
+  beforeEach(inject(function(_$rootScope_, _$controller_, _alertsCenterService_) {
+    $scope = _$rootScope_.$new();
+    alertsCenterService = _alertsCenterService_;
+
+    alertsCenterService.refreshInterval = -1;
+    adminResponse = getJSONFixture('alerts_center/admin_user_response.json');
+    operatorResponse = getJSONFixture('alerts_center/operator_user_response.json');
+    existingUsersResponse = getJSONFixture('alerts_center/existing_users_response.json');
+    providersResponse = getJSONFixture('alerts_center/providers_response.json');
+    tagsResponse = getJSONFixture('alerts_center/tags_response.json');
+    alertsResponse = getJSONFixture('alerts_center/alerts_response.json');
+
+    fakeGetAlerts = function() {
+      alertsCenterService.currentUser = adminResponse.identity;
+      alertsCenterService.existingUsers = existingUsersResponse.resources;
+      alertsCenterService.providers = providersResponse.resources;
+      alertsCenterService.tags = tagsResponse.resources;
+
+      return Promise.resolve(alertsResponse);
+    };
+
+    spyOn(alertsCenterService, 'updateAlertsData').and.callFake(fakeGetAlerts);
+
+    $controller = _$controller_('alertsOverviewController',
+      {
+        $scope: $scope,
+        alertsCenterService: alertsCenterService
+      }
+    );
+  }));
+
+  afterEach(function() {
+  });
+
+  describe('data loads successfully and', function() {
+    it('shows the correct summary item cards', function(done) {
+      expect($controller.loadingDone).toBeFalsy();
+
+      setTimeout(function () {
+        expect($controller.loadingDone).toBeTruthy();
+        expect($controller.groups.length).toBe(3);
+        expect($controller.displayFilters.length).toBe(1);
+        expect($controller.categories.length).toBe(1);
+        done();
+      });
+    });
+
+    it('shows the correct items in the summary cards', function(done) {
+      setTimeout(function () {
+        expect($controller.groups.length).toBe(3);
+        expect($controller.groups[0].hasItems).toBeTruthy();
+        expect($controller.groups[0].itemsList.length).toBe(1);
+        expect($controller.groups[0].itemsList[0].danger.length).toBe(1);
+        expect($controller.groups[0].itemsList[0].info.length).toBe(0);
+        expect($controller.groups[0].itemsList[0].warning.length).toBe(0);
+
+        expect($controller.groups[1].hasItems).toBeTruthy();
+        expect($controller.groups[1].itemsList.length).toBe(1);
+        expect($controller.groups[1].itemsList[0].danger.length).toBe(0);
+        expect($controller.groups[1].itemsList[0].info.length).toBe(1);
+        expect($controller.groups[1].itemsList[0].warning.length).toBe(1);
+
+        expect($controller.groups[2].hasItems).toBeTruthy();
+        expect($controller.groups[2].itemsList.length).toBe(1);
+        expect($controller.groups[2].itemsList[0].danger.length).toBe(1);
+        expect($controller.groups[2].itemsList[0].info.length).toBe(0);
+        expect($controller.groups[2].itemsList[0].warning.length).toBe(0);
+
+        done();
+      });
+    });
+
+    it('filters out cards appropriately', function(done) {
+      setTimeout(function () {
+        $controller.filterConfig.appliedFilters = [{id: 'severityCount', value: 'Error'}];
+        $controller.filterChange();
+
+        expect($controller.groups.length).toBe(3);
+        expect($controller.groups[0].hasItems).toBeTruthy();
+        expect($controller.groups[1].hasItems).toBeFalsy();
+        expect($controller.groups[2].hasItems).toBeTruthy();
+
+        $controller.filterConfig.appliedFilters = [{id: 'name', value: '1'}];
+        $controller.filterChange();
+
+        expect($controller.groups.length).toBe(3);
+        expect($controller.groups[0].hasItems).toBeFalsy();
+        expect($controller.groups[1].hasItems).toBeTruthy();
+        expect($controller.groups[2].hasItems).toBeFalsy();
+
+        done();
+      });
+    });
+  });
+});
