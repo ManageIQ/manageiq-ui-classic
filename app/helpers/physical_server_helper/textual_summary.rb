02f6998ad8bd8ffdeba8724a26bd18101a61589b
Walter Alves <walter.arruda.alves@gmail.com>
2017-02-01 17:57:20 -0300

    Show more properties in Physicals server Summary
    Improving physical server summary screen information.

--- app/helpers/physical_server_helper/textual_summary.rb
+++ app/helpers/physical_server_helper/textual_summary.rb
@@ -2,8 +2,7 @@ module PhysicalServerHelper::TextualSummary
 
 
   def textual_group_properties
-  #   %i(name hostname productName manufacturer machineType serialNumber uuid)
-  %i(name)
+    %i(name model productName manufacturer  machineType serialNumber  uuid)
   end
 
   def textual_group_relationships
@@ -19,35 +18,39 @@ module PhysicalServerHelper::TextualSummary
   # end
 
   def textual_host
-    #TODO (walteraa) use physical->virtual relationship
-    {:label => _("Host"), :value => 0}
+    #TODO (walteraa) use physical->virtual relationship to show host TAG
+    {:label => _("Host"), :value => "2YSAGD6236235DFASD123"}
   end
 
   def textual_name
     {:label => _("Server name"), :value => @record.name }
   end
 
-  # def textual_productName
-  #   {:label => _("Product Name"), :value => @record.productName }
-  # end
-  #
-  # def textual_manufacturer
-  #   {:label => _("Manufacturer"), :value => @record.manufacturer }
-  # end
-  #
-  #
-  # def textual_machineType
-  #   {:label =>_("Machine Type"), :value =>  @record.machineType }
-  # end
-  #
-  #
-  # def textual_serialNumber
-  #   {:label => _("Serial Number"), :value => @record.serialNumber }
-  # end
-  #
-  # def textual_uuid
-  #   {:label => _("UUID"), :value => @record.uuid }
-  #
-  # end
+   def textual_productName
+     {:label => _("Product Name"), :value => @record.productName }
+   end
+  
+   def textual_manufacturer
+     {:label => _("Manufacturer"), :value => @record.manufacturer }
+   end
+  
+  
+   def textual_machineType
+     {:label =>_("Machine Type"), :value =>  @record.machineType }
+   end
+  
+  
+   def textual_serialNumber
+     {:label => _("Serial Number"), :value => @record.serialNumber }
+   end
+  
+   def textual_uuid
+     {:label => _("UUID"), :value => @record.uuid }
+  
+   end
+
+   def textual_model
+      {:label =>  _("Model"), :value  =>  @record.model}
+   end
     
 end
