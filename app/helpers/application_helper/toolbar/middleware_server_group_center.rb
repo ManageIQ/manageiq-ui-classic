a9a7f7ae6dba94dfc17130ba193738377dfc6294
Jirka Kremser <jkremser@redhat.com>
2017-02-02 14:33:06 +0100

    Middelware server group power ops: toolbar buttons

--- app/helpers/application_helper/toolbar/middleware_server_group_center.rb
+++ app/helpers/application_helper/toolbar/middleware_server_group_center.rb
@@ -16,6 +16,71 @@ class ApplicationHelper::Toolbar::MiddlewareServerGroupCenter < ApplicationHelpe
                    ]
                  ),
                ])
+  button_group('middleware_server_operations', [
+    select(
+      :middleware_server_power_choice,
+      'fa fa-power-off fa-lg',
+      t = N_('Power'),
+      t,
+      :items => [
+        button(
+          :middleware_server_group_restart,
+          nil,
+          N_('Restart Servers in this Server Group'),
+          N_('Restart Server Group'),
+          :image   => 'restart',
+          :confirm => N_('Do you want to restart this server group?'),
+        ),
+        separator,
+        button(
+          :middleware_server_group_stop,
+          nil,
+          N_('Stop Servers in this Server Group'),
+          N_('Stop Server Group'),
+          :image   => 'power_off',
+          :data  => {'toggle'        => 'modal',
+                     'target'        => '#modal_param_div',
+                     'function'      => 'sendDataWithRx',
+                     'function-data' => '{"type": "mwServerOps", "operation": "stop", "timeout": 10}'},
+        ),
+        button(
+          :middleware_server_group_suspend,
+          nil,
+          N_('Suspend Server in this Server Group'),
+          N_('Suspend Server Group'),
+          :image => 'suspend',
+          :data  => {'toggle'        => 'modal',
+                     'target'        => '#modal_param_div',
+                     'function'      => 'sendDataWithRx',
+                     'function-data' => '{"type": "mwServerOps", "operation": "suspend", "timeout": 10}'},
+        ),
+        button(
+          :middleware_server_group_resume,
+          nil,
+          N_('Resume Servers in this Server Group'),
+          N_('Resume Server Group'),
+          :image   => 'resume',
+          :confirm => N_('Do you want to resume this server group?'),
+        ),
+        button(
+          :middleware_server_group_reload,
+          nil,
+          N_('Reload Servers in this Server Group'),
+          N_('Reload Server Group'),
+          :confirm => N_('Do you want to trigger a reload of this server group?'),
+          :image   => 'guest_restart',
+        ),
+        button(
+          :middleware_server_group_start,
+          nil,
+          N_('Start Servers in this Server Group'),
+          N_('Start Server Group'),
+          :image   => 'start',
+          :confirm => N_('Do you want to trigger a start of this server group?'),
+        ),
+      ]
+    ),
+  ])
   button_group('middleware_server_deployments', [
                  select(
                    :middleware_server_deployments_choice,
