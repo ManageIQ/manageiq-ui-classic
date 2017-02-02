787529db11bf783cb624945fa77ef6916745107d
Dávid Halász <dhalasz@redhat.com>
2017-02-02 20:55:20 +0100

    Moved TreeNode::MiqAction fonticon selection logic to a decorator

--- app/presenters/tree_node/miq_action.rb
+++ app/presenters/tree_node/miq_action.rb
@@ -14,41 +14,8 @@ module TreeNode
         ev = ::MiqEventDefinition.find_by_id(ApplicationRecord.uncompress_id(event_id))
 
         p.action_result_for_event(@object, ev) ? "pficon pficon-ok" : "pficon pficon-error-circle-o"
-      elsif @object.action_type == "default"
-        "product product-action"
       else
-        case @object.action_type
-        when 'assign_scan_profile'
-          'fa fa-list-ul'
-        when 'create_snapshot'
-          'fa fa-camera'
-        when 'custom_automation'
-          'fa fa-recycle'
-        when 'delete_snapshots_by_age'
-          'fa fa-camera'
-        when 'email'
-          'fa fa-envelope-o'
-        when 'evaluate_alerts'
-          'pficon pficon-warning-triangle-o'
-        when 'inherit_parent_tags'
-          'fa fa-tags'
-        when 'reconfigure_cpus'
-          'fa pficon-cpu'
-        when 'reconfigure_memory'
-          'pficon pficon-memory'
-        when 'remove_tags'
-          'fa fa-tags'
-        when 'script'
-          'fa fa-file-text-o'
-        when 'set_custom_attribute'
-          'product product-attribute'
-        when 'snmp_trap'
-          'fa fa fa-envelope-o'
-        when 'tag'
-          'fa fa-tag'
-        when 'vm_collect_running_processes'
-          'fa fa-cogs'
-        end
+        @object.decorate.fonticon
       end
     end
   end
