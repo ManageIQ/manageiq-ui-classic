f04e68c084d7968cb085cf0382b58590222aaabd
Martin Povolny <mpovolny@redhat.com>
2017-02-09 01:09:17 +0100

    TextualSummary: fix table view used in security groups.

--- app/helpers/textual_table.rb
+++ app/helpers/textual_table.rb
@@ -1,9 +1,9 @@
-TextualTable = Struct.new(:title, :values, :labels) do
+TextualTable = Struct.new(:title, :rows, :labels) do
   def template
     'shared/summary/textual_table'
   end
 
   def locals
-    {:title => title, :values => values, :labels => labels}
+    {:title => title, :rows => rows, :labels => labels}
   end
 end
