0df54cda9dc30e79b0cee3269036d93f12294182
Aparna Karve <akarve@redhat.com>
2017-02-10 14:21:05 -0800

    Highlights of this commit --
    angularForm() -- A function that returns the angularForm object
    from a scope where it exists
    
    modelCopy() -- A function that returns the modelCopy object from
    a scope where it exists
    
    model() -- A function that returns the model object from a scope
    where it exists
    
    With more complex forms that have nested controllers like the
    `CredentialsController` the scope gets nested differently in
    different situations. The above functions consider all those
    nesting possibilities and return a valid scope to play with to
    the rest of the functions in this directive
    
    Drawbacks: Makes an assumption that `controllerAs` always
    uses `vm`. But I guess we can chalk it up the nomenclature
    protocol that we have been using for our Angular forms
    across the board.

--- app/assets/javascripts/directives/checkchange.js
+++ app/assets/javascripts/directives/checkchange.js
@@ -5,77 +5,113 @@ ManageIQ.angular.app.directive('checkchange', ['miqService', function(miqService
       scope['formchange_' + ctrl.$name] = elem[0].name
       scope['elemType_' + ctrl.$name] = attr.type;
 
-      if (angular.isDefined(scope.modelCopy)) {
+      var angularForm = function() {
+        if (scope.$parent.vm && scope.$parent.vm.angularForm) {
+          return scope.$parent.vm.angularForm;
+        } else if (scope.vm && scope.vm.angularForm) {
+          return scope.vm.angularForm;
+        } else if (scope.$parent.$parent && scope.$parent.$parent.angularForm) {
+          return scope.$parent.$parent.angularForm;
+        } else {
+          return scope.angularForm;
+        }
+      };
+
+      var modelCopy = function() {
+        if (scope.$parent.vm && scope.$parent.vm.modelCopy) {
+          return scope.$parent.vm.modelCopy;
+        } else if (scope.vm && scope.vm.modelCopy) {
+          return scope.vm.modelCopy;
+        } else if (scope.$parent.$parent && scope.$parent.$parent.modelCopy) {
+          return scope.$parent.$parent.modelCopy;
+        } else {
+          return scope.modelCopy;
+        }
+      };
+
+      var model = function() {
+        if (scope.$parent.vm && scope.$parent.vm.model) {
+          return scope.$parent.vm[scope.$parent.vm.model];
+        } else if (scope.vm && scope.vm.model) {
+          return scope.vm[scope.vm.model];
+        } else if (scope.$parent.$parent && scope.$parent.$parent.model) {
+          return scope.$parent.$parent[scope.$parent.$parent.model];
+        } else {
+          return scope[scope.model];
+        }
+      };
+
+      if (angular.isDefined(modelCopy())) {
         scope.$watch(attr.ngModel, function () {
           if (scope['elemType_' + ctrl.$name] == "date" || _.isDate(ctrl.$modelValue)) {
-            viewModelDateComparison(scope, ctrl);
+            viewModelDateComparison(scope, ctrl, modelCopy(), angularForm());
           } else {
-            viewModelComparison(scope, ctrl);
+            viewModelComparison(scope, ctrl, modelCopy(), model(), angularForm());
           }
-          if (scope.angularForm.$pristine)
-            checkForOverallFormPristinity(scope, ctrl);
+          if (angularForm().$pristine)
+            checkForOverallFormPristinity(scope, ctrl, modelCopy(), model(), angularForm());
         });
       }
 
       ctrl.$parsers.push(function(value) {
         miqService.miqFlashClear();
 
-        if (value == scope.modelCopy[ctrl.$name]) {
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
+        if (value == modelCopy()[ctrl.$name]) {
+          angularForm()[scope['formchange_' + ctrl.$name]].$setPristine();
         }
-        if (scope.angularForm[scope['formchange_' + ctrl.$name]].$pristine) {
-          checkForOverallFormPristinity(scope, ctrl);
+        if (angularForm()[scope['formchange_' + ctrl.$name]].$pristine) {
+          checkForOverallFormPristinity(scope, ctrl, modelCopy(), model(), angularForm());
         }
-        scope.angularForm[scope['formchange_' + ctrl.$name]].$setTouched();
+        angularForm()[scope['formchange_' + ctrl.$name]].$setTouched();
         return value;
       });
 
-      if(scope.angularForm.$pristine)
-        scope.angularForm.$setPristine();
+      if(angularForm().$pristine)
+        angularForm().$setPristine();
     }
   }
 }]);
 
-var viewModelComparison = function(scope, ctrl) {
-  if ((Array.isArray(scope.modelCopy[ctrl.$name]) &&
-       angular.equals(scope[scope.model][ctrl.$name], scope.modelCopy[ctrl.$name])) ||
-       ctrl.$viewValue == scope.modelCopy[ctrl.$name]) {
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
-    scope.angularForm.$pristine = true;
+var viewModelComparison = function(scope, ctrl, modelCopy, model, angularForm) {
+  if ((Array.isArray(modelCopy[ctrl.$name]) &&
+       angular.equals(model[ctrl.$name], modelCopy[ctrl.$name])) ||
+       ctrl.$viewValue == modelCopy[ctrl.$name]) {
+    angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
+    angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
+    angularForm.$pristine = true;
   } else {
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
-    scope.angularForm.$pristine = false;
+    angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
+    angularForm.$pristine = false;
   }
 };
 
-var viewModelDateComparison = function(scope, ctrl) {
+var viewModelDateComparison = function(scope, ctrl, modelCopy, angularForm) {
   var modelDate = (ctrl.$modelValue != undefined) ? moment(ctrl.$modelValue) : null;
-  var copyDate = (scope.modelCopy[ctrl.$name] != undefined) ? moment(scope.modelCopy[ctrl.$name]) : null;
+  var copyDate = (modelCopy[ctrl.$name] != undefined) ? moment(modelCopy[ctrl.$name]) : null;
 
   if((modelDate && copyDate && (modelDate.diff(copyDate, 'days') == 0)) || (!modelDate && !copyDate)){
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
-    scope.angularForm.$pristine = true;
+    angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
+    angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
+    angularForm.$pristine = true;
   } else {
-    scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
-    scope.angularForm.$pristine = false;
+    angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
+    angularForm.$pristine = false;
   }
 };
 
-var checkForOverallFormPristinity = function(scope, ctrl) {
+var checkForOverallFormPristinity = function(scope, ctrl, modelCopy, model, angularForm) {
   // don't do anything before the model and modelCopy are actually initialized
-  if (! ('modelCopy' in scope) || ! scope.modelCopy || ! scope.model || ! (scope.model in scope))
+  if (! modelCopy || ! model)
     return;
 
-  var modelCopyObject = _.cloneDeep(scope.modelCopy);
+  var modelCopyObject = _.cloneDeep(modelCopy);
   delete modelCopyObject[ctrl.$name];
 
-  var modelObject = _.cloneDeep(scope[scope.model]);
+  var modelObject = _.cloneDeep(model);
   delete modelObject[ctrl.$name];
 
-  scope.angularForm.$pristine = angular.equals(modelCopyObject, modelObject);
+  angularForm.$pristine = angular.equals(modelCopyObject, modelObject);
 
-  if (scope.angularForm.$pristine)
-    scope.angularForm.$setPristine();
+  if (angularForm.$pristine)
+    angularForm.$setPristine();
 };
