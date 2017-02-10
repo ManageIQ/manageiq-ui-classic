c559e684fd9c07da20be5762f36f4800038ab956
Aparna Karve <akarve@redhat.com>
2017-02-10 13:33:08 -0800

    Revert changes from b2c57a8 and dbb5005
    The more complex forms with nested controllers like credentials
    will not work with the above commits.
    This directive can get tricky, therefore it's best to apply new
    ideas on a clean slate, hence reverting those commits

--- app/assets/javascripts/directives/checkchange.js
+++ app/assets/javascripts/directives/checkchange.js
@@ -5,14 +5,7 @@ ManageIQ.angular.app.directive('checkchange', ['miqService', function(miqService
       scope['formchange_' + ctrl.$name] = elem[0].name
       scope['elemType_' + ctrl.$name] = attr.type;
 
-      var model = function() {
-        return scope.$eval(scope.angularForm.model || scope.model);
-      };
-      var modelCopy = function() {
-        return scope.$eval(scope.angularForm.modelCopy || "modelCopy");
-      };
-
-      if (modelCopy()) {
+      if (angular.isDefined(scope.modelCopy)) {
         scope.$watch(attr.ngModel, function () {
           if (scope['elemType_' + ctrl.$name] == "date" || _.isDate(ctrl.$modelValue)) {
             viewModelDateComparison(scope, ctrl);
@@ -27,7 +20,7 @@ ManageIQ.angular.app.directive('checkchange', ['miqService', function(miqService
       ctrl.$parsers.push(function(value) {
         miqService.miqFlashClear();
 
-        if (value == modelCopy()[ctrl.$name]) {
+        if (value == scope.modelCopy[ctrl.$name]) {
           scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
         }
         if (scope.angularForm[scope['formchange_' + ctrl.$name]].$pristine) {
@@ -37,53 +30,52 @@ ManageIQ.angular.app.directive('checkchange', ['miqService', function(miqService
         return value;
       });
 
-      if (scope.angularForm.$pristine)
+      if(scope.angularForm.$pristine)
         scope.angularForm.$setPristine();
-
-      var viewModelComparison = function(scope, ctrl) {
-        if ((Array.isArray(modelCopy()[ctrl.$name]) &&
-          angular.equals(model()[ctrl.$name], modelCopy()[ctrl.$name])) ||
-          ctrl.$viewValue == modelCopy()[ctrl.$name]) {
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
-          scope.angularForm.$pristine = true;
-        } else {
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
-          scope.angularForm.$pristine = false;
-        }
-      };
-
-      var viewModelDateComparison = function(scope, ctrl) {
-        var modelDate = (ctrl.$modelValue != undefined) ? moment(ctrl.$modelValue) : null;
-        var copyDate = (modelCopy()[ctrl.$name] != undefined) ? moment(modelCopy()[ctrl.$name]) : null;
-
-        if((modelDate && copyDate && (modelDate.diff(copyDate, 'days') == 0)) || (!modelDate && !copyDate)){
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
-          scope.angularForm.$pristine = true;
-        } else {
-          scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
-          scope.angularForm.$pristine = false;
-        }
-      };
-
-      var checkForOverallFormPristinity = function(scope, ctrl) {
-        // don't do anything before the model and modelCopy are actually initialized
-        if (!model() || !modelCopy())
-          return;
-
-        var modelCopyObject = _.cloneDeep(modelCopy());
-        delete modelCopyObject[ctrl.$name];
-
-        var modelObject = _.cloneDeep(model());
-        delete modelObject[ctrl.$name];
-
-        scope.angularForm.$pristine = angular.equals(modelCopyObject, modelObject);
-
-        if (scope.angularForm.$pristine)
-          scope.angularForm.$setPristine();
-      };
     }
   }
 }]);
 
+var viewModelComparison = function(scope, ctrl) {
+  if ((Array.isArray(scope.modelCopy[ctrl.$name]) &&
+       angular.equals(scope[scope.model][ctrl.$name], scope.modelCopy[ctrl.$name])) ||
+       ctrl.$viewValue == scope.modelCopy[ctrl.$name]) {
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
+    scope.angularForm.$pristine = true;
+  } else {
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
+    scope.angularForm.$pristine = false;
+  }
+};
+
+var viewModelDateComparison = function(scope, ctrl) {
+  var modelDate = (ctrl.$modelValue != undefined) ? moment(ctrl.$modelValue) : null;
+  var copyDate = (scope.modelCopy[ctrl.$name] != undefined) ? moment(scope.modelCopy[ctrl.$name]) : null;
+
+  if((modelDate && copyDate && (modelDate.diff(copyDate, 'days') == 0)) || (!modelDate && !copyDate)){
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setPristine();
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setUntouched();
+    scope.angularForm.$pristine = true;
+  } else {
+    scope.angularForm[scope['formchange_' + ctrl.$name]].$setDirty();
+    scope.angularForm.$pristine = false;
+  }
+};
+
+var checkForOverallFormPristinity = function(scope, ctrl) {
+  // don't do anything before the model and modelCopy are actually initialized
+  if (! ('modelCopy' in scope) || ! scope.modelCopy || ! scope.model || ! (scope.model in scope))
+    return;
+
+  var modelCopyObject = _.cloneDeep(scope.modelCopy);
+  delete modelCopyObject[ctrl.$name];
+
+  var modelObject = _.cloneDeep(scope[scope.model]);
+  delete modelObject[ctrl.$name];
+
+  scope.angularForm.$pristine = angular.equals(modelCopyObject, modelObject);
+
+  if (scope.angularForm.$pristine)
+    scope.angularForm.$setPristine();
+};
