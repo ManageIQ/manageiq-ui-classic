bc1e2b83bb1864e4b9937cc0a1707bd40a7c5081
PanSpagetka <rknaur@redhat.com>
2017-02-13 15:09:03 +0100

    Address comments

--- app/helpers/application_helper/button/host_miq_request_new.rb
+++ app/helpers/application_helper/button/host_miq_request_new.rb
@@ -2,8 +2,7 @@ class ApplicationHelper::Button::HostMiqRequestNew < ApplicationHelper::Button::
   needs :@record
 
   def disabled?
-    count = PxeServer.all.size
-    @error_message = N_("No PXE Servers are available for Host provisioning") if count <= 0
+    @error_message = N_("No PXE Servers are available for Host provisioning") if PxeServer.count <= 0
     @error_message = N_("This Host can not be provisioned because the MAC address is not known") unless @record.mac_address
     @error_message.present?
   end
