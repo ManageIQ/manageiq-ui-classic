d9bf334159e5a15210d0ff064f770f74e963e88e
Martin Povolny <mpovolny@redhat.com>
2017-02-14 15:22:33 +0100

    Introduce MoreShowActions for deduplication od #show.

---
+++ app/controllers/mixins/more_show_actions.rb
@@ -0,0 +1,48 @@
+module Mixins
+  module MoreShowActions
+    def show_timeline
+      @showtype = "timeline"
+      session[:tl_record_id] = params[:id] if params[:id]
+      @lastaction = "show_timeline"
+      @timeline = @timeline_filter = true
+      tl_build_timeline # Create the timeline report
+      drop_breadcrumb(:name => _("Timelines"),
+                      :url  => "/#{controller_name}/show/#{@record.id}" \
+                               "?refresh=n&display=timeline")
+    end
+
+    def show_performance
+      @showtype = "performance"
+      display_name = @record.respond_to?(:evm_display_name) ? @record.evm_display_name : @record.name
+      drop_breadcrumb(:name => _("%{name} Capacity & Utilization") % {:name => display_name},
+                      :url  => "/#{controller_name}/show/#{@record.id}" \
+                               "?display=#{@display}&refresh=n")
+      perf_gen_init_options # Initialize options, charts are generated async
+    end
+
+    def show_compliance_history
+      count = params[:count] ? params[:count].to_i : 10
+      update_session_for_compliance_history(count)
+      drop_breadcrumb_for_compliance_history(count)
+      @showtype = @display
+    end
+
+    def update_session_for_compliance_history(count)
+      @ch_tree = TreeBuilderComplianceHistory.new(:ch_tree, :ch, @sb, true, @record)
+      session[:ch_tree] = @ch_tree.tree_nodes
+      session[:tree_name] = "ch_tree"
+      session[:squash_open] = (count == 1)
+    end
+
+    def drop_breadcrumb_for_compliance_history(count)
+      if count == 1
+        drop_breadcrumb(:name => _("%{name} (Latest Compliance Check)") % {:name => @record.name},
+                        :url  => "/#{controller_name}/show/#{@record.id}?display=#{@display}&refresh=n")
+      else
+        drop_breadcrumb(
+          :name => _("%{name} (Compliance History - Last %{number} Checks)") % {:name => @record.name, :number => count},
+          :url  => "/#{controller_name}/show/#{@record.id}?display=#{@display}&refresh=n")
+      end
+    end
+  end
+end
