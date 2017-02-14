15af4b068028256b36d94f741eb1e41fc45f86a4
Federico Simoncelli <fsimonce@redhat.com>
2017-02-14 12:20:47 +0100

    Enable container start pages
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- spec/controllers/dashboard_controller/start_url_spec.rb
+++ spec/controllers/dashboard_controller/start_url_spec.rb
@@ -7,19 +7,6 @@ describe DashboardController do
       stub_user(:features => :all)
     end
 
-    context 'ems_container_show_list' do
-      subject { subj.call('ems_container_show_list') }
-
-      it 'should return true for containers start pages when product flag is set' do
-        stub_settings(:product => {:containers => true})
-        is_expected.to be_truthy
-      end
-
-      it 'should return false for containers start pages when product flag is not set' do
-        is_expected.to be_falsey
-      end
-    end
-
     it 'should return true for host start page' do
       expect(subj.call('host_show_list')).to be_truthy
     end
