2883cad093de4cd518f7e24c8042d7ed6d8c8599
Šimon Lukašík <isimluk@fedoraproject.org>
2017-02-15 09:27:23 +0100

    Remove commented-out code from ancient times

--- lib/report_formatter/html.rb
+++ lib/report_formatter/html.rb
@@ -41,8 +41,6 @@ module ReportFormatter
       end
       output << '<tbody>'
 
-      # table data
-      #     save_val = nil
       row = 0
       unless mri.table.nil?
         row_limit = mri.rpt_options && mri.rpt_options[:row_limit] ? mri.rpt_options[:row_limit] : 0
