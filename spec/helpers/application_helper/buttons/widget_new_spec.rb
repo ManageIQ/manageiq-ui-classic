eb40b66d3187d6fc087ad7173212ec50bad14959
Attila Vecerek <avecerek@redhat.com>
2017-02-17 12:40:18 +0100

    Edit existing specs of _new and _discover button classes

--- spec/helpers/application_helper/buttons/widget_new_spec.rb
+++ spec/helpers/application_helper/buttons/widget_new_spec.rb
@@ -1,8 +1,13 @@
 describe ApplicationHelper::Button::WidgetNew do
   let(:view_context) { setup_view_context_with_sandbox({}) }
-  subject { described_class.new(view_context, {}, {}, {}) }
+  let(:lastaction) { '' }
+  let(:display) { '' }
+  let(:x_node) { 'not_root' }
+  subject { described_class.new(view_context, {}, {'lastaction' => lastaction, 'display' => display}, {}) }
 
   before { allow(view_context).to receive(:x_node).and_return(x_node) }
+
+  it_behaves_like 'a _new or _discover button'
 
   describe '#visible?' do
     context 'when x_node == root' do
@@ -10,7 +15,6 @@ describe ApplicationHelper::Button::WidgetNew do
       it { expect(subject.visible?).to be_falsey }
     end
     context 'when x_node != root' do
-      let(:x_node) { 'not_root' }
       it { expect(subject.visible?).to be_truthy }
     end
   end
