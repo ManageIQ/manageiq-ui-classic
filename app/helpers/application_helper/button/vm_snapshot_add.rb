b9d8d60169e41e1149ab8da7d89a38316ed119fc
Gaurav Aradhye <garadhye@redhat.com>
2017-02-21 11:58:57 -0500

    Review changes

--- app/helpers/application_helper/button/vm_snapshot_add.rb
+++ app/helpers/application_helper/button/vm_snapshot_add.rb
@@ -3,7 +3,7 @@ class ApplicationHelper::Button::VmSnapshotAdd < ApplicationHelper::Button::Basi
     @error_message = if records_and_role_allows? && !@active
                        _('Select the Active snapshot to create a new snapshot for this VM')
                      else
-                       @record.unsupported_reason(:create_snapshot)
+                       @record.unsupported_reason(:snapshot_create)
                      end
     @error_message.present?
   end
@@ -11,6 +11,6 @@ class ApplicationHelper::Button::VmSnapshotAdd < ApplicationHelper::Button::Basi
   private
 
   def records_and_role_allows?
-    @record.supports_create_snapshot?
+    @record.supports_snapshot_create?
   end
 end
