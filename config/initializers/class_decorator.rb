900e2a15c9f31bda3add3658e7e12ef9b3e917fe
Martin Hradil <mhradil@redhat.com>
2017-02-23 15:16:40 +0000

    MiqDecorator - move the logic to find the right decorator class to MiqDecorator.for
    so that MiqDecorator.for(Klass) returns KlassDecorator, if it exists

--- config/initializers/class_decorator.rb
+++ config/initializers/class_decorator.rb
@@ -1,22 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   class << self
     def decorate
-      @_decorator ||= decorator_for(self)
-    end
-
-    def decorator_for(klass)
-      decorator = nil
-
-      while decorator.nil? && klass != ApplicationRecord
-        decorator = "#{klass.name}Decorator".safe_constantize
-        klass = klass.superclass
-      end
-
-      decorator
+      @_decorator ||= MiqDecorator.for(self)
     end
   end
 
   def decorate
-    self.class.decorator_for(self.class).new(self)
+    @_decorator ||= MiqDecorator.for(self.class).new(self)
   end
 end
--- lib/miq_decorator.rb
+++ lib/miq_decorator.rb
@@ -1,2 +1,14 @@
 class MiqDecorator < SimpleDelegator
+  class << self
+    def for(klass)
+      decorator = nil
+
+      while decorator.nil? && klass != ApplicationRecord && !klass.nil?
+        decorator = "#{klass.name}Decorator".safe_constantize
+        klass = klass.superclass
+      end
+
+      decorator
+    end
+  end
 end
