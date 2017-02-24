e17ec0a2337b817b559ce4f7a1ae7ee5677e9c7f
Attila Vecerek <avecerek@redhat.com>
2017-02-24 15:12:40 +0100

    Remove gtl buttons from #disable_button

--- app/helpers/application_helper/toolbar_builder.rb
+++ app/helpers/application_helper/toolbar_builder.rb
@@ -403,7 +403,6 @@ class ApplicationHelper::ToolbarBuilder
   # Determine if a button should be disabled. Returns either boolean or
   # string message with explanation of reason for disabling
   def disable_button(id)
-    return true if @gtl_type && id.starts_with?("view_") && id.ends_with?(@gtl_type)  # GTL view buttons
     if id == 'vm_vnc_console' && @record.vendor == 'vmware' &&
        ExtManagementSystem.find_by(:id => @record.ems_id).api_version.to_f >= 6.5
       return N_("VNC consoles are unsupported on VMware ESXi 6.5 and later.")
