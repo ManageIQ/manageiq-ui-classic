37569382aa938f6231cf220fa5a695b9dfda39fd
Robert Čížek <swi2@rejpal31.mendelu.cz>
2017-02-24 10:42:01 +0100

    Added mixin for session into vm_controller
    [WIP] VM_controller session in generic_session_mixin
    
    [WIP]completed vm_controller to be compatible with GenericSessionMixin
    
    [WIP]vm_controller and generic session mixin update
    
    [WIP]vm_controller added private keyword

--- app/controllers/vm_controller.rb
+++ app/controllers/vm_controller.rb
@@ -6,6 +6,8 @@ class VmController < ApplicationController
   include VmCommon # common methods for vm controllers
   include VmRemote # methods for VM remote access
 
+  include Mixins::GenericSessionMixin
+
   def index
     session[:vm_type] = nil             # Reset VM type if coming in from All tab
     redirect_to :action => 'show_list'
@@ -20,26 +22,36 @@ class VmController < ApplicationController
   private ####
 
   def get_session_data
-    @title          = _("Virtual Machines")
-    @layout         = "vm"
-    @lastaction     = session[:vm_lastaction]
-    @showtype       = session[:vm_showtype]
-    @filters        = session[:vm_filters]
-    @catinfo        = session[:vm_catinfo]
-    @display        = session[:vm_display]
+
+
+
+
+
+
+
+
+
+
     @polArr         = session[:polArr] || ""           # current tags in effect
     @policy_options = session[:policy_options] || ""
   end
 
   def set_session_data
-    session[:vm_lastaction]   = @lastaction
-    session[:vm_showtype]     = @showtype
-    session[:miq_compressed]  = @compressed unless @compressed.nil?
-    session[:miq_exists_mode] = @exists_mode unless @exists_mode.nil?
-    session[:vm_filters]      = @filters
-    session[:vm_catinfo]      = @catinfo
-    session[:vm_display]      = @display unless @display.nil?
+
+
+
+
+
+
+
+
+
+
     session[:polArr]          = @polArr unless @polArr.nil?
     session[:policy_options]  = @policy_options unless @policy_options.nil?
+  end
+
+  def title
+    _("Virtual Machines")
   end
 end
