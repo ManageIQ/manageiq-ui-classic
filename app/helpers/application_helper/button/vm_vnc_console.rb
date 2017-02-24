327eee2ee30b76017fa07932d8194fa20a482bfe
Attila Vecerek <avecerek@redhat.com>
2017-02-24 19:16:45 +0100

    Extend VmVncConsole#disabled? implementation to also check the record's vendor and api version

--- app/helpers/application_helper/button/vm_vnc_console.rb
+++ app/helpers/application_helper/button/vm_vnc_console.rb
@@ -2,12 +2,22 @@ class ApplicationHelper::Button::VmVncConsole < ApplicationHelper::Button::VmCon
   needs :@record
 
   def visible?
-    return console_supports_type?('VNC') if @record.vendor == 'vmware'
+    return console_supports_type?('VNC') if vmware?
     @record.console_supported?('vnc')
   end
 
   def disabled?
-    @error_message = _('The web-based VNC console is not available because the VM is not powered on') unless on?
+    @error_message = if vmware? && !supported_vendor_api?
+                       _('VNC consoles are unsupported on VMware ESXi 6.5 and later.')
+                     elsif !on?
+                       _('The web-based VNC console is not available because the VM is not powered on')
+                     end
     @error_message.present?
+  end
+
+  private
+
+  def unsupported_vendor_api
+    6.5
   end
 end
