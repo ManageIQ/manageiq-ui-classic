29ad7cb7dd24391e2cba2587ee8c93725cb34c4e
Robert Čížek <swi2@rejpal31.mendelu.cz>
2017-02-24 10:42:01 +0100

    Added mixin for session into vm_controller

--- app/controllers/vm_controller.rb
+++ app/controllers/vm_controller.rb
@@ -6,6 +6,8 @@ class VmController < ApplicationController
   include VmCommon # common methods for vm controllers
   include VmRemote # methods for VM remote access
 
+  include GenericSessionMixin
+
   def index
     session[:vm_type] = nil             # Reset VM type if coming in from All tab
     redirect_to :action => 'show_list'
