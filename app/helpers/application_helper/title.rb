2250763baeb788b10a56a32c7cc92e52fa9f7b43
Martin Povolny <mpovolny@redhat.com>
2017-03-01 10:35:47 +0100

    Title: refactor if/elses to case.

--- app/helpers/application_helper/title.rb
+++ app/helpers/application_helper/title.rb
@@ -17,64 +17,66 @@ module ApplicationHelper
     def title_from_layout(layout)
       title = product_title
 
-      if layout.blank?  # no layout, leave title alone
-      elsif ["configuration", "dashboard", "chargeback", "about"].include?(layout)
-        title += ": #{layout.titleize}"
-      elsif @layout == "ems_cluster"
-        title += ": #{title_for_clusters}"
-      elsif @layout == "host"
-        title += ": #{title_for_hosts}"
-      # Specific titles for certain layouts
-      elsif layout == "miq_server"
-        title += _(": Servers")
-      elsif layout == "usage"
-        title += _(": VM Usage")
-      elsif layout == "scan_profile"
-        title += _(": Analysis Profiles")
-      elsif layout == "miq_policy_rsop"
-        title += _(": Policy Simulation")
-      elsif layout == "all_ui_tasks"
-        title += _(": All UI Tasks")
-      elsif layout == "my_ui_tasks"
-        title += _(": My UI Tasks")
-      elsif layout == "rss"
-        title += _(": RSS")
-      elsif layout == "storage_manager"
-        title += _(": Storage - Storage Managers")
-      elsif layout == "ops"
-        title += _(": Configuration")
-      elsif layout == "provider_foreman"
-        title += _(": Configuration Management")
-      elsif layout == "pxe"
-        title += _(": PXE")
-      elsif layout == "explorer"
-        title += ": #{controller_model_name(params[:controller])} Explorer"
-      elsif layout == "vm_cloud"
-        title += _(": Instances")
-      elsif layout == "vm_infra"
-        title += _(": Virtual Machines")
-      elsif layout == "vm_or_template"
-        title += _(": Workloads")
-      # Specific titles for groups of layouts
-      elsif layout.starts_with?("miq_ae_")
-        title += _(": Automation")
-      elsif layout.starts_with?("miq_policy")
-        title += _(": Control")
-      elsif layout.starts_with?("miq_capacity")
-        title += _(": Optimize")
-      elsif layout.starts_with?("miq_request")
-        title += _(": Requests")
-      elsif layout == "manageiq/providers/ansible_tower/automation_manager/playbook"
-        title += ": Playbooks (Ansible Tower)"
-      elsif layout == "manageiq/providers/automation_manager/authentication"
-        title += ": Credentials"
-      elsif layout == "configuration_script_source"
-        title += ": Repositories"
-      # Assume layout is a table name and look up the plural version
-      else
-        title += ": #{ui_lookup(:tables => layout)}"
-      end
-      title
+      return title if layout.blank?  # no layout, leave title alone
+
+      title + case layout
+              when "configuration", "dashboard", "chargeback", "about"
+                ": #{layout.titleize}"
+              when "ems_cluster"
+                ": #{title_for_clusters}"
+              when "host"
+                ": #{title_for_hosts}"
+              # Specific titles for certain layouts
+              when "miq_server"
+                _(": Servers")
+              when "usage"
+                _(": VM Usage")
+              when "scan_profile"
+                _(": Analysis Profiles")
+              when "miq_policy_rsop"
+                _(": Policy Simulation")
+              when "all_ui_tasks"
+                _(": All UI Tasks")
+              when "my_ui_tasks"
+                _(": My UI Tasks")
+              when "rss"
+                _(": RSS")
+              when "storage_manager"
+                _(": Storage - Storage Managers")
+              when "ops"
+                _(": Configuration")
+              when "provider_foreman"
+                _(": Configuration Management")
+              when "pxe"
+                _(": PXE")
+              when "explorer"
+                ": #{controller_model_name(params[:controller])} Explorer"
+              when "vm_cloud"
+                _(": Instances")
+              when "vm_infra"
+                _(": Virtual Machines")
+              when "vm_or_template"
+                _(": Workloads")
+
+              # Specific titles for groups of layouts
+              when /^miq_ae_/
+                _(": Automation")
+              when /^miq_policy/
+                _(": Control")
+              when /^miq_capacity/
+                _(": Optimize")
+              when /^miq_request/
+                _(": Requests")
+              when "manageiq/providers/ansible_tower/automation_manager/playbook"
+                ": Playbooks (Ansible Tower)"
+              when "manageiq/providers/automation_manager/authentication"
+                ": Credentials"
+              when "configuration_script_source"
+                ": Repositories"
+              else
+                # Assume layout is a table name and look up the plural version
+                ": #{ui_lookup(:tables => layout)}"
+              end
     end
   end
 end
