1be02ad5c89e007a13fae8862baea3f852670bdd
Attila Večerek <veat24@gmail.com>
2017-03-01 16:52:07 +0100

    Rename class to represent a more general entity

--- app/helpers/application_helper/button/logs.rb
+++ app/helpers/application_helper/button/logs.rb
@@ -1,4 +1,4 @@
-class ApplicationHelper::Button::CollectLogs < ApplicationHelper::Button::Basic
+class ApplicationHelper::Button::Logs < ApplicationHelper::Button::Basic
   def calculate_properties
     super
     self[:prompt] = @record.try(:log_file_depot).try(:requires_support_case?)
