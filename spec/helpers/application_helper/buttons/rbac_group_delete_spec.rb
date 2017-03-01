780c7460e5f70a14f1264d19f83e34a3391bf5e6
Attila Večerek <veat24@gmail.com>
2017-03-01 14:52:22 +0100

    Create spec examples for RbacGroupDelete and RbacGroupEdit button classes

---
+++ spec/helpers/application_helper/buttons/rbac_group_delete_spec.rb
@@ -0,0 +1,3 @@
+describe ApplicationHelper::Button::RbacGroupDelete do
+  it_behaves_like 'an rbac_group_action button', 'deleted'
+end
---
+++ spec/support/examples_group/shared_examples_for_rbac_group_buttons.rb
@@ -0,0 +1,17 @@
+shared_examples 'an rbac_group_action button' do |action|
+  let(:view_context) { setup_view_context_with_sandbox({}) }
+  let(:button) { described_class.new(view_context, {}, {'record' => record}, {}) }
+
+  describe '#calculate_properties' do
+    before { button.calculate_properties }
+
+    context 'record is read-only' do
+      let(:record) { FactoryGirl.create(:miq_group, :system_type) }
+      it_behaves_like 'a disabled button', "This Group is Read Only and can not be #{action}"
+    end
+    context 'record is writable' do
+      let(:record) { FactoryGirl.create(:miq_group) }
+      it_behaves_like 'an enabled button'
+    end
+  end
+end
