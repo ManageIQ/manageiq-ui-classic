cfaa4d2b394f137c3970409b3c03b355b015ec14
Attila Vecerek <avecerek@redhat.com>
2017-03-02 15:56:28 +0100

    Create spec examples for VmPerf button class

---
+++ spec/support/examples_group/shared_examples_for_performance_buttons.rb
@@ -0,0 +1,17 @@
+shared_examples_for 'a performance button' do |message|
+  describe '#calculate_properties' do
+    before do
+      record.metric_rollups << metric_rollup if metric_rollup
+      button.calculate_properties
+    end
+
+    context 'when performance data has not been collected' do
+      let(:metric_rollup) { nil }
+      it_behaves_like 'a disabled button', "No Capacity & Utilization data has been collected for this #{message}"
+    end
+    context 'when performance data are available' do
+      let(:metric_rollup) { FactoryGirl.create(:metric_rollup_storage_hr) }
+      it_behaves_like 'an enabled button'
+    end
+  end
+end
