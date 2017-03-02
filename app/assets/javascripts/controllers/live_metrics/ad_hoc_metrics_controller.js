ca15e505875d905a7137b4ef73006dd06f1f1535
yaacov <kobi.zamir@gmail.com>
2017-03-02 22:14:35 +0200

    id may include r, relax the id regex

--- app/assets/javascripts/controllers/live_metrics/ad_hoc_metrics_controller.js
+++ app/assets/javascripts/controllers/live_metrics/ad_hoc_metrics_controller.js
@@ -11,7 +11,7 @@
     // get the pathname and remove trailing / if exist
     var pathname = $window.location.pathname.replace(/\/$/, '');
 
-    dash.providerId = '/' + (/^\/[^\/]+\/(\d+)$/.exec(pathname)[1]);
+    dash.providerId = '/' + (/^\/[^\/]+\/([r\d]+)$/.exec(pathname)[1]);
     dash.tenant = '_ops';
     dash.minBucketDurationInSecondes = 20 * 60;
     dash.max_metrics = 1000;
