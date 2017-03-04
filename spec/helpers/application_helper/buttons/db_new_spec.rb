6184ab678bc62ef0e7eadf96dcadafe7c98367c6
Attila Večerek <veat24@gmail.com>
2017-03-04 19:36:46 +0100

    Create spec examples for DbNew button class

---
+++ spec/helpers/application_helper/buttons/db_new_spec.rb
@@ -0,0 +1,18 @@
+describe ApplicationHelper::Button::DbNew do
+  let(:view_context) { setup_view_context_with_sandbox({}) }
+  let(:dashboard_count) { 9 }
+  let(:widgetsets) { Array.new(dashboard_count) { |_i| FactoryGirl.create(:miq_widget_set) } }
+  let(:button) { described_class.new(view_context, {}, {'widgetsets' => widgetsets}, {}) }
+
+  describe '#calculate_properties' do
+    before { button.calculate_properties }
+
+    context 'when dashboard group is full' do
+      let(:dashboard_count) { 10 }
+      it_behaves_like 'a disabled button', 'Only 10 Dashboards are allowed for a group'
+    end
+    context 'when dashboard group has still room for a new dashboard' do
+      it_behaves_like 'an enabled button'
+    end
+  end
+end
