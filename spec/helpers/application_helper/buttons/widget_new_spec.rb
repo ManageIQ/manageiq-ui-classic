b46013339fdee3e86739907dd67eecfcfb3b5916
Attila Večerek <veat24@gmail.com>
2017-03-04 20:27:18 +0100

    Edit spec files affected by the changes made in shared examples for *_new and *_discover buttons

--- spec/helpers/application_helper/buttons/widget_new_spec.rb
+++ spec/helpers/application_helper/buttons/widget_new_spec.rb
@@ -3,19 +3,20 @@ describe ApplicationHelper::Button::WidgetNew do
   let(:lastaction) { '' }
   let(:display) { '' }
   let(:x_node) { 'not_root' }
-  subject { described_class.new(view_context, {}, {'lastaction' => lastaction, 'display' => display}, {}) }
+  let(:button) { described_class.new(view_context, {}, {'lastaction' => lastaction, 'display' => display}, {}) }
 
   before { allow(view_context).to receive(:x_node).and_return(x_node) }
 
   it_behaves_like 'a _new or _discover button'
 
   describe '#visible?' do
+    subject { button.visible? }
     context 'when x_node == root' do
       let(:x_node) { 'root' }
-      it { expect(subject.visible?).to be_falsey }
+      it { expect(subject).to be_falsey }
     end
     context 'when x_node != root' do
-      it { expect(subject.visible?).to be_truthy }
+      it { expect(subject).to be_truthy }
     end
   end
 end
