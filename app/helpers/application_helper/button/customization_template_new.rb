c08f2c143e30e3222534ccaae6d6ac5fbe420138
Attila Večerek <veat24@gmail.com>
2017-03-04 19:09:48 +0100

    Implement CustomizationTemplateNew#disabled?

--- app/helpers/application_helper/button/customization_template_new.rb
+++ app/helpers/application_helper/button/customization_template_new.rb
@@ -4,4 +4,11 @@ class ApplicationHelper::Button::CustomizationTemplateNew < ApplicationHelper::B
   def visible?
     !sub_list_view_screen? && !system?
   end
+
+  def disabled?
+    if @pxe_image_types_count <= 0
+      @error_message = _('No System Image Types available, Customization Template cannot be added')
+    end
+    @error_message.present?
+  end
 end
