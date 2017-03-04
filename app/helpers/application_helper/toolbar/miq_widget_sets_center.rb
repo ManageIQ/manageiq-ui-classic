764d4c5f7afdfdf5a3d1f342b07cd2d032d0cadc
Attila Večerek <veat24@gmail.com>
2017-03-04 19:38:54 +0100

    Assign db_new button to its previously created class

--- app/helpers/application_helper/toolbar/miq_widget_sets_center.rb
+++ app/helpers/application_helper/toolbar/miq_widget_sets_center.rb
@@ -11,7 +11,7 @@ class ApplicationHelper::Toolbar::MiqWidgetSetsCenter < ApplicationHelper::Toolb
           'pficon pficon-add-circle-o fa-lg',
           t = N_('Add a new Dashboard'),
           t,
-          :klass => ApplicationHelper::Button::ButtonNewDiscover),
+          :klass => ApplicationHelper::Button::DbNew),
         separator,
         button(
           :db_seq_edit,
