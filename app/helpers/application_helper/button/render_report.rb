ab9a7555aea560a099665c06694f47e2e207ea51
Attila Večerek <veat24@gmail.com>
2017-03-05 16:05:41 +0100

    Implement RenderReport and RenderReportPdf button classes

---
+++ app/helpers/application_helper/button/render_report.rb
@@ -0,0 +1,3 @@
+class ApplicationHelper::Button::RenderReport < ApplicationHelper::Button::Basic
+  include ApplicationHelper::Button::Mixins::RenderReportMixin
+end
---
+++ app/helpers/application_helper/button/render_report_pdf.rb
@@ -0,0 +1,3 @@
+class ApplicationHelper::Button::RenderReportPdf < ApplicationHelper::Button::Pdf
+  include ApplicationHelper::Button::Mixins::RenderReportMixin
+end
