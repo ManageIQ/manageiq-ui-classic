f0f3778be0955b7fed5e9f41256f178893e1faa1
Attila Vecerek <avecerek@redhat.com>
2017-03-06 20:47:51 +0100

    Review respone: Add missing

--- app/helpers/application_helper/button/ae_copy_simulate.rb
+++ app/helpers/application_helper/button/ae_copy_simulate.rb
@@ -2,6 +2,7 @@ class ApplicationHelper::Button::AeCopySimulate < ApplicationHelper::Button::But
   def disabled?
     if @resolve[:button_class].blank?
       @error_message = _('Object attribute must be specified to copy object details for use in a Button')
+      @error_message.present?
     end
   end
 end
