bc364808e36deb0f79b27ba9c47ac316da31044b
lgalis <lgalis@redhat.com>
2017-03-08 01:33:07 -0500

    Added spec

--- spec/javascripts/controllers/catalog/catalog_item_form_controller_spec.js
+++ spec/javascripts/controllers/catalog/catalog_item_form_controller_spec.js
@@ -59,6 +59,134 @@ describe('catalogItemFormController', function() {
     });
   });
 
+  describe('#playbookChanged', function() {
+    describe('when the provisioning playbook changes', function() {
+      beforeEach(function() {
+        $scope.vm = {};
+        $scope.vm.catalogItemModel = {
+          name: 'catalogItemName',
+          description: 'catalogItemDescription',
+          service_template_catalog_id: 10000000000012,
+          display: true,
+          prov_type: 'generic_ansible_playbook',
+          type: 'ServiceTemplateAnsiblePlaybook',
+          config_info: {
+            provision: {
+              dialog_id: '10000000000031',
+              repository_id: undefined,
+              playbook_id: 10000000000493,
+              credential_id: 10000000000090,
+              hosts: undefined,
+              network_credential_id: 10000000000091,
+              cloud_credential_id: 10000000000092
+            }
+          },
+          provisioning_repository_id: '',
+          provisioning_playbook_id: '',
+          provisioning_machine_credential_id: '',
+          provisioning_network_credential_id: '',
+          provisioning_cloud_credential_id: '',
+          provisioning_key: '',
+          provisioning_value: '',
+          provisioning_variables: {},
+          provisioning_editMode: false,
+          provisioning_cloud_type: '',
+          retirement_repository_id: '',
+          retirement_playbook_id: '',
+          retirement_machine_credential_id: '',
+          retirement_network_credential_id: '',
+          retirement_cloud_credential_id: '',
+          retirement_inventory: 'localhost',
+          retirement_dialog_existing: 'existing',
+          retirement_dialog_id: '',
+          retirement_dialog_name: '',
+          retirement_key: '',
+          retirement_value: '',
+          retirement_variables: {},
+          retirement_editMode: false,
+          retirement_cloud_type: '',
+          cloud_types: ["Amazon", "Azure", "Google", "Openstack", "Vmware"]
+        }
+        $scope.vm.provisioning_machine_credentials = {};
+        $scope.vm.provisioning_network_credentials = {};
+        $scope.vm.provisioning_cloud_credentials = {};
+      });
+
+      it('resets the provisioning credentials', function() {
+        //$scope.vm.playbookChanged('provisioning', 10000000000600 );
+        expect($scope.vm.catalogItemModel.provisioning_machine_credential_id).toEqual('');
+        expect($scope.vm.catalogItemModel.provisioning_cloud_credential_id).toEqual('');
+        expect($scope.vm.catalogItemModel.provisioning_cloud_type).toEqual('');
+        expect($scope.vm['provisioning_cloud_credentials']).toEqual({});
+        expect($scope.vm['provisioning_network_credentials']).toEqual({});
+        expect($scope.vm['provisioning_machine_credentials']).toEqual({});
+      });
+    });
+
+    describe('when the retirement playbook changes', function() {
+      beforeEach(function() {
+        $scope.vm = {};
+        $scope.vm.catalogItemModel = {
+          name: 'catalogItemName',
+          description: 'catalogItemDescription',
+          service_template_catalog_id: 10000000000012,
+          display: true,
+          prov_type: 'generic_ansible_playbook',
+          type: 'ServiceTemplateAnsiblePlaybook',
+          config_info: {
+            retire: {
+              dialog_id: '10000000000031',
+              repository_id: undefined,
+              playbook_id: 10000000000693,
+              credential_id: 10000000000190,
+              hosts: undefined,
+              network_credential_id: 10000000000191,
+              cloud_credential_id: 10000000000192,
+            }
+          },
+          provisioning_repository_id: '',
+          provisioning_playbook_id: '',
+          provisioning_machine_credential_id: '',
+          provisioning_network_credential_id: '',
+          provisioning_cloud_credential_id: '',
+          provisioning_key: '',
+          provisioning_value: '',
+          provisioning_variables: {},
+          provisioning_editMode: false,
+          provisioning_cloud_type: '',
+          retirement_repository_id: '',
+          retirement_playbook_id: '',
+          retirement_machine_credential_id: '',
+          retirement_network_credential_id: '',
+          retirement_cloud_credential_id: '',
+          retirement_inventory: 'localhost',
+          retirement_dialog_existing: 'existing',
+          retirement_dialog_id: '',
+          retirement_dialog_name: '',
+          retirement_key: '',
+          retirement_value: '',
+          retirement_variables: {},
+          retirement_editMode: false,
+          retirement_cloud_type: '',
+          cloud_types: ["Amazon", "Azure", "Google", "Openstack", "Vmware"]
+        }
+        $scope.vm.retirement_machine_credentials = {};
+        $scope.vm.retirement_network_credentials = {};
+        $scope.vm.retirement_cloud_credentials = {};
+      });
+
+      it('resets the retirement credentials', function() {
+        //playbookChanged('provisioning', 10000000000600 );
+        expect($scope.vm.catalogItemModel['retirement_machine_credential_id']).toEqual('');
+        expect($scope.vm.catalogItemModel['retirement_cloud_credential_id']).toEqual('');
+        expect($scope.vm.catalogItemModel['retirement_cloud_type']).toEqual('');
+        expect($scope.vm['retirement_cloud_credentials']).toEqual({});
+        expect($scope.vm['retirement_network_credentials']).toEqual({});
+        expect($scope.vm['retirement_machine_credentials']).toEqual({});
+      });
+    });
+  });
+
   describe('#cancelClicked', function() {
     beforeEach(function() {
       $scope.angularForm = {
