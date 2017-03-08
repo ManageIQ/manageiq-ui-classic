9a940b45f2530db913e51895e83c9a7156a45602
Attila Vecerek <avecerek@redhat.com>
2017-03-08 14:33:12 +0100

    Review response: Add missing  and turn the code to be more readable

--- app/helpers/application_helper/button/report_only.rb
+++ app/helpers/application_helper/button/report_only.rb
@@ -1,10 +1,10 @@
 class ApplicationHelper::Button::ReportOnly < ApplicationHelper::Button::RenderReport
   def disabled?
-    if @record.nil?
-      super
-    elsif @record.class == MiqReportResult
+    return super if @record.nil?
+    if @record.class == MiqReportResult
       @error_message = _('No records found for this report') unless report_records?
     end
+    @error_message.present?
   end
 
   private
