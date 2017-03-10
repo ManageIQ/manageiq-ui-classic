37e6928f04811900e8532069f3aa4cb0b26601fc
Martin Povolny <mpovolny@redhat.com>
2017-03-10 14:57:53 +0100

    Fix rubocop issues in shared custom button specs.

--- spec/shared/controllers/shared_example_for_custom_buttons_spec.rb
+++ spec/shared/controllers/shared_example_for_custom_buttons_spec.rb
@@ -33,10 +33,8 @@ shared_examples 'explorer controller with custom buttons' do
     controller.instance_variable_set(:@explorer, true)
     controller.instance_variable_set(
       :@sb,
-      {
-        :active_tree => "my_tree",
-        :trees       => {"my_tree" => {:active_node => 'root'}}
-      }
+      :active_tree => "my_tree",
+      :trees       => {"my_tree" => {:active_node => 'root'}}
     )
 
     text = controller.custom_toolbar_filename
@@ -49,10 +47,8 @@ shared_examples 'explorer controller with custom buttons' do
     controller.instance_variable_set(:@record, true)
     controller.instance_variable_set(
       :@sb,
-      {
-        :active_tree => "my_tree",
-        :trees       => {"my_tree" => {:active_node => 'v-1r35'}}
-      }
+      :active_tree => "my_tree",
+      :trees       => {"my_tree" => {:active_node => 'v-1r35'}}
     )
 
     text = controller.custom_toolbar_filename
@@ -65,14 +61,11 @@ shared_examples 'explorer controller with custom buttons' do
     controller.instance_variable_set(:@record, true)
     controller.instance_variable_set(
       :@sb,
-      {
-        :active_tree => "my_tree",
-        :trees       => {"my_tree" => {:active_node => 'v-1r35'}}
-      }
+      :active_tree => "my_tree",
+      :trees       => {"my_tree" => {:active_node => 'v-1r35'}}
     )
 
     text = controller.custom_toolbar_filename
     expect(text).to eq("custom_buttons_tb")
   end
 end
-
