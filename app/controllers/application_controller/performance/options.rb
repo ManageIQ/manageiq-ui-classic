ManageIQ/manageiq-ui-classic

commit ffeebc60744229f3ac78a2214d9c64800cdb351a

Refactor: Extract method: set_dates

app/controllers/application_controller/performance/options.rb
@@ -62,6 +62,49 @@ module ApplicationController::Performance
       end
     end
 
+    def set_dates(start_date, end_date, allow_interval_override)
+      tz = time_profile_tz || self.tz # Use time profile tz or chosen tz, if no profile tz
+      self.sdate = start_date.in_time_zone(tz)
+      self.edate = end_date.in_time_zone(tz)
+      self.sdate_daily = sdate.hour.zero? ? sdate : sdate + 1.day
+      self.edate_daily = edate.hour < 23 ? edate - 1.day : edate
+
+      if typ == 'Daily' && edate_daily < sdate_daily
+        self.no_daily = true
+        self.typ = 'Hourly' if allow_interval_override
+      else
+        self.no_daily = false
+      end
+
+      if hourly_date.present? &&
+         (hourly_date.to_date < sdate.to_date || hourly_date.to_date > edate.to_date || # it is out of range
+         (typ == 'Hourly' && time_profile && !time_profile_days.include?(hourly_date.to_date.wday))) # or out of profile
+        self.hourly_date = nil
+      end
+      if daily_date.present? &&
+         (daily_date.to_date < sdate_daily.to_date || daily_date.to_date > edate_daily.to_date)
+        options[:daily_date] = nil
+      end
+      self.hourly_date ||= [edate.month, edate.day, edate.year].join('/')
+      self.daily_date  ||= [edate_daily.month, edate_daily.day, edate_daily.year].join('/')
+
+      if typ == 'Hourly' && time_profile # If hourly and profile in effect, set hourly date to a valid day in profile
+        self.skip_days = (1..7).to_a.delete_if do |d|
+          # time_profile_days has 0 for sunday, skip_days needs 7 for sunday
+          time_profile_days.include?(d % 7)
+        end
+
+        hdate = hourly_date.to_date                                       # Start at the currently set hourly date
+        6.times do                                                        # Go back up to 6 days (try each weekday)
+          break if time_profile_days.include?(hdate.wday)                 # If weekday is in the profile, use it
+          hdate -= 1.day                                                  # Drop back 1 day and try again
+        end
+        self.hourly_date = [hdate.month, hdate.day, hdate.year].join('/') # Set the new hourly date
+      else
+        self.skip_days = nil
+      end
+    end
+
     def cats # category pulldown for tag charts
       return unless %w(EmsCluster Host Storage AvailabilityZone HostAggregate).include?(model)
       self[:cats] ||=

app/controllers/application_controller/performance.rb
@@ -162,7 +162,6 @@ module ApplicationController::Performance
   # Correct any date that is out of the date/range or not allowed in a profile
   def perf_set_or_fix_dates(options, allow_interval_override = true)
     # Get start/end dates in selected timezone
-    tz = options[:time_profile_tz] || options[:tz]  # Use time profile tz or chosen tz, if no profile tz
     s, e = @perf_record.first_and_last_capture('hourly')
     if s.nil?
       s, e = @perf_record.first_and_last_capture('realtime')
@@ -174,47 +173,7 @@ module ApplicationController::Performance
       options[:typ] = "realtime"
       options[:no_rollups] = true
     end
-    sdate = s.in_time_zone(tz)
-    edate = e.in_time_zone(tz)
-    options[:sdate] = sdate
-    options[:edate] = edate
-
-    sdate_daily = sdate.hour == 00 ? sdate : sdate + 1.day
-    options[:sdate_daily] = sdate_daily
-    edate_daily = edate.hour < 23 ? edate - 1.day : edate
-    options[:edate_daily] = edate_daily
-    # check if Daily report was manualy chosen in UI
-    if options[:typ] == "Daily" && edate_daily < sdate_daily
-      options[:no_daily] = true
-      options[:typ] = "Hourly" if allow_interval_override
-    else
-      options[:no_daily] = false
-    end
-
-    if options[:hourly_date].present? && # Need to clear hourly date if not nil so it will be reset below if
-       (options[:hourly_date].to_date < sdate.to_date || options[:hourly_date].to_date > edate.to_date || # it is out of range
-         (options[:typ] == "Hourly" && options[:time_profile] && !options[:time_profile_days].include?(options[:hourly_date].to_date.wday))) # or not in profile
-      options[:hourly_date] = nil
-    end
-    if options[:daily_date].present? &&
-       (options[:daily_date].to_date < sdate_daily.to_date || options[:daily_date].to_date > edate_daily.to_date)
-      options[:daily_date] = nil
-    end
-    options[:hourly_date] ||= [edate.month, edate.day, edate.year].join("/")
-    options[:daily_date] ||= [edate_daily.month, edate_daily.day, edate_daily.year].join("/")
-
-    if options[:typ] == "Hourly" && options[:time_profile]              # If hourly and profile in effect, set hourly date to a valid day in the profile
-      options[:skip_days] = skip_days_from_time_profile(options[:time_profile_days])
-
-      hdate = options[:hourly_date].to_date                             # Start at the currently set hourly date
-      6.times do                                                        # Go back up to 6 days (try each weekday)
-        break if options[:time_profile_days].include?(hdate.wday)       # If weekday is in the profile, use it
-        hdate -= 1.day                                                  # Drop back 1 day and try again
-      end
-      options[:hourly_date] = [hdate.month, hdate.day, hdate.year].join("/")  # Set the new hourly date
-    else
-      options[:skip_days] = nil
-    end
+    @perf_options.set_dates(s, e, allow_interval_override)
   end
 
   def skip_days_from_time_profile(time_profile_days)

commit c95a2c643eaca90bde99e18cab44304390864135

Corrected bug introduced in commit ffeebc60

app/controllers/application_controller/performance/options.rb
@@ -83,7 +83,7 @@ module ApplicationController::Performance
       end
       if daily_date.present? &&
          (daily_date.to_date < sdate_daily.to_date || daily_date.to_date > edate_daily.to_date)
-        options[:daily_date] = nil
+        self.daily_date = nil
       end
       self.hourly_date ||= [edate.month, edate.day, edate.year].join('/')
       self.daily_date  ||= [edate_daily.month, edate_daily.day, edate_daily.year].join('/')