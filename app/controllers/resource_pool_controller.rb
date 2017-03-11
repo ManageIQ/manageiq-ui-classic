aa00ff7318124bf2ff94db3742ddc785b1e98c52
Eric Hayes <eric@erichayes.net>
2017-03-10 22:48:47 -0800

    Button method can be removed from ResourcePoolController

--- app/controllers/resource_pool_controller.rb
+++ app/controllers/resource_pool_controller.rb
@@ -13,37 +13,16 @@ class ResourcePoolController < ApplicationController
     %w(vms descendant_vms all_vms resource_pools)
   end
 
-  # handle buttons pressed on the button bar
-  def button
-    generic_button_setup
-
-    handle_tag_presses(params[:pressed])
-    handle_button_pressed(params[:pressed])
-
-    handle_sub_item_presses(params[:pressed]) do |pfx|
-      process_vm_buttons(pfx) unless params[:pressed].ends_with?("_tag")
-
-      return if button_control_transferred?(params[:pressed])
-
-      unless button_has_redirect_suffix?(params[:pressed])
-        set_refresh_and_show
-      end
-    end
-
-    return if response && performed?
-
-    check_if_button_is_implemented
-
-    if button_has_redirect_suffix?(params[:pressed])
-      render_or_redirect_partial_for(params[:pressed])
-    elsif button_replace_gtl_main?
-      replace_gtl_main_div
-    else
-      render_flash
-    end
-  end
-
   private
+
+  def textual_group_list
+    [%i(properties relationships), %i(configuration smart_management)]
+  end
+  helper_method :textual_group_list
+
+  def button_sub_item_display_values
+    %w(all_vms vms resource_pools)
+  end
 
   def handled_buttons
     %w(resource_pool_delete resource_pool_protect)
@@ -56,15 +35,6 @@ class ResourcePoolController < ApplicationController
 
   def handle_resource_pool_protect
     assign_policies(ResourcePool)
-  end
-
-  def textual_group_list
-    [%i(properties relationships), %i(configuration smart_management)]
-  end
-  helper_method :textual_group_list
-
-  def button_sub_item_display_values
-    %w(all_vms vms resource_pools)
   end
 
   menu_section :inf
