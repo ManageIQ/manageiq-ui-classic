ManageIQ/manageiq-ui-classic

commit 417d3f127a60b649bd719d92bcb63b85cde73af3

rubocop issues: tree_builder_catalogs_class.rb
== app/presenters/tree_builder_catalogs_class.rb ==
R: 28:  3: Assignment Branch Condition size for x_get_tree_aset_kids is
too high. [27/15]
R: 28:  3: Cyclomatic complexity for x_get_tree_aset_kids is too high.
[8/6]
R: 28:  3: Perceived complexity for x_get_tree_aset_kids is too high.
[10/7]
C: 32:  7: Convert if nested inside else to elsif.
(transferred from ManageIQ/manageiq@b791d615950ac9531678ea224ea2855c4badb17e)

app/presenters/tree_builder_catalogs_class.rb
@@ -25,20 +25,23 @@ class TreeBuilderCatalogsClass < TreeBuilder
     end
   end
 
+  def get_tree_aset_kids_for_nil_id(object, count_only)
+    count_only ? get_custom_buttons(object).count : get_custom_buttons(object).sort_by { |a| a.name.downcase }
+  end
+
+  def button_order?(object)
+    object[:set_data] && object[:set_data][:button_order]
+  end
+
   def x_get_tree_aset_kids(object, count_only)
-    if count_only
-      object.id.nil? ? get_custom_buttons(object).count : object.members.count
+    if object.id.nil?
+      get_tree_aset_kids_for_nil_id(object, count_only)
+    elsif count_only
+      object.members.count
     else
-      if object.id.nil?
-        get_custom_buttons(object).sort_by { |a| a.name.downcase }
-      else
-        # need to show button nodes in button order that they were saved in
-        button_order = object[:set_data] && object[:set_data][:button_order] ? object[:set_data][:button_order] : nil
-        objects = []
-        Array(button_order).each do |bidx|
-          object.members.each { |b| objects.push(b) if bidx == b.id && !objects.include?(b) }
-        end
-        objects
+      button_order = button_order?(object) ? object[:set_data][:button_order] : nil
+      Array(button_order).each_with_object([]) do |bidx, arr|
+        object.members.each { |b| arr.push(b) if bidx == b.id && !objects.include?(b) }
       end
     end
   end

commit eea932acb3d5b0995f2813575ee69729becaf2cf

Corrected bug introduced in commit 417d3f12

app/presenters/tree_builder_catalogs_class.rb
@@ -41,7 +41,7 @@ class TreeBuilderCatalogsClass < TreeBuilder
     else
       button_order = button_order?(object) ? object[:set_data][:button_order] : nil
       Array(button_order).each_with_object([]) do |bidx, arr|
-        object.members.each { |b| arr.push(b) if bidx == b.id && !objects.include?(b) }
+        object.members.each { |b| arr.push(b) if bidx == b.id && !arr.include?(b) }
       end
     end
   end