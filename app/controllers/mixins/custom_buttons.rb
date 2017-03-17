896b77b4e7bdc2d8bd019665ad5f5d642c961c6c
Roman Blanco <rblanco@redhat.com>
2017-03-17 14:32:05 +0100

    Grouped same methods in one mixin

--- app/controllers/mixins/custom_buttons.rb
+++ app/controllers/mixins/custom_buttons.rb
@@ -1,6 +1,35 @@
 module Mixins::CustomButtons
   extend ActiveSupport::Concern
 
+  def get_tree_aset_kids_for_nil_id(object, count_only)
+    count_only ? get_custom_buttons(object).count : get_custom_buttons(object).sort_by { |a| a.name.downcase }
+  end
+
+  def buttons_ordered?(object)
+    !!(object[:set_data] && object[:set_data][:button_order])
+  end
+
+  def x_get_tree_aset_kids(object, count_only)
+    if object.id.nil?
+      get_tree_aset_kids_for_nil_id(object, count_only)
+    elsif count_only
+      object.members.count
+    else
+      button_order = buttons_ordered?(object) ? object[:set_data][:button_order] : nil
+      Array(button_order).each_with_object([]) do |bidx, arr|
+        object.members.each { |b| arr.push(b) if bidx == b.id && !arr.include?(b) }
+      end
+    end
+  end
+
+  def get_custom_buttons(object)
+    # FIXME: don't we have a method for the splits?
+    # FIXME: cannot we ask for the null parent using Arel?
+    CustomButton.buttons_for(object.name.split('|').last.split('-').last).select do |uri|
+      uri.parent.nil?
+    end
+  end
+
   def custom_toolbar?
     return nil unless self.class.instance_eval { @custom_buttons }
 
--- app/presenters/tree_builder_catalogs_class.rb
+++ app/presenters/tree_builder_catalogs_class.rb
@@ -1,4 +1,5 @@
 class TreeBuilderCatalogsClass < TreeBuilder
+  include Mixins::CustomButtons
   has_kids_for CustomButtonSet, [:x_get_tree_aset_kids]
 
   private
@@ -9,40 +10,13 @@ class TreeBuilderCatalogsClass < TreeBuilder
     when :stcat
       return count_only_or_objects(count_only, objects)
     when :sandt
-      return count_only_or_objects(count_only,
-                                   objects.unshift(ServiceTemplateCatalog.new(:name        => 'Unassigned',
-                                                                              :description => 'Unassigned Catalogs')),
-                                   nil)
-    end
-  end
-
-  # TODO: De-duplicate the following methods from tree_builder_buttons.rb
-  def get_custom_buttons(object)
-    # FIXME: don't we have a method for the splits?
-    # FIXME: cannot we ask for the null parent using Arel?
-    CustomButton.buttons_for(object.name.split('|').last.split('-').last).select do |uri|
-      uri.parent.nil?
-    end
-  end
-
-  def get_tree_aset_kids_for_nil_id(object, count_only)
-    count_only ? get_custom_buttons(object).count : get_custom_buttons(object).sort_by { |a| a.name.downcase }
-  end
-
-  def buttons_ordered?(object)
-    !!(object[:set_data] && object[:set_data][:button_order])
-  end
-
-  def x_get_tree_aset_kids(object, count_only)
-    if object.id.nil?
-      get_tree_aset_kids_for_nil_id(object, count_only)
-    elsif count_only
-      object.members.count
-    else
-      button_order = buttons_ordered?(object) ? object[:set_data][:button_order] : nil
-      Array(button_order).each_with_object([]) do |bidx, arr|
-        object.members.each { |b| arr.push(b) if bidx == b.id && !arr.include?(b) }
-      end
+      return count_only_or_objects(
+        count_only,
+        objects.unshift(ServiceTemplateCatalog.new(
+          :name        => 'Unassigned',
+          :description => 'Unassigned Catalogs')),
+         nil
+      )
     end
   end
 end
