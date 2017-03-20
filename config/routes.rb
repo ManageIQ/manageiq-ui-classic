ca99b62bed310f1cd9ed6c704190b348915d54b6
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-03-20 20:52:57 -0300

    [Routes] Adds routes to physical_server 1. Adding routes to physical_server in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1263,6 +1263,24 @@ Rails.application.routes.draw do
                save_post
     },
 
+    :physical_server    =>  {
+      :get  =>  %w(
+        download_data
+        perf_top_chart
+        protect
+        show_list
+        show
+      ) + compare_get,
+
+      :post   =>  %w(
+        button
+        show_list
+        create
+        update
+
+      )
+    },
+
     :ems_physical_infra_dashboard      => {
       :get => %w(
         show
