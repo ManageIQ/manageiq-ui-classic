913401629193e8b5178dbebc9498b42c78d5d90a
Jozef Zigmund <jzigmund@redhat.com>
2017-03-21 15:07:21 +0100

    Use the new methods in views and helpers

--- app/helpers/host_helper.rb
+++ app/helpers/host_helper.rb
@@ -1,4 +1,11 @@
 module HostHelper
   include_concern 'TextualSummary'
   include_concern 'ComplianceSummaryHelper'
+
+  def get_host_info
+    devices = get_devices
+    os_info = get_os_info
+    vmm_info = get_vmm_info
+    [devices, os_info, vmm_info]
+  end
 end
