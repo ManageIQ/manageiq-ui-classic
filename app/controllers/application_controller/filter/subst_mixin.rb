079a293f7eefc8cad69dd996a54a0f2c0e6c01a4
Šimon Lukašík <isimluk@fedoraproject.org>
2017-03-21 14:22:51 +0100

    Fix unneessary spacing rubocop is crying mom

--- app/controllers/application_controller/filter/subst_mixin.rb
+++ app/controllers/application_controller/filter/subst_mixin.rb
@@ -22,7 +22,7 @@ module ApplicationController::Filter::SubstMixin
       @exp_token ||= 0
       @exp_token += 1
       exp[:token] = @exp_token
-      exp_table.push(quick_search ? "NOT" : ["NOT", @exp_token])  # No token if building quick search exp
+      exp_table.push(quick_search ? "NOT" : ["NOT", @exp_token])             # No token if building quick search exp
       exp_table.push("(") unless %w(and or).include?(exp["not"].keys.first)  # No parens if and/or under me
       exp_table += exp_build_table(exp["not"], quick_search)
       exp_table.push(")") unless %w(and or).include?(exp["not"].keys.first)  # No parens if and/or under me
@@ -30,9 +30,9 @@ module ApplicationController::Filter::SubstMixin
       @exp_token ||= 0
       @exp_token += 1
       exp[:token] = @exp_token
-      if exp["???"]                             # Found a new expression part
+      if exp["???"]                                   # Found a new expression part
         exp_table.push(["???", @exp_token])
-        exp_context[@expkey][:exp_token] = @exp_token         # Save the token value for the view
+        exp_context[@expkey][:exp_token] = @exp_token # Save the token value for the view
         exp_context[:edit_exp] = copy_hash(exp)       # Save the exp part for the view
         exp_context[@expkey].update_from_exp_tree(exp_context[:edit_exp]) # Set the fields for a new exp part
       elsif quick_search # Separate out the user input fields if doing a quick search
@@ -43,7 +43,7 @@ module ApplicationController::Filter::SubstMixin
         else
           exp_table.push(human_exp)
         end
-      else            # Not quick search, add token to the expression
+      else # Not quick search, add token to the expression
         exp_table.push([MiqExpression.to_human(exp), @exp_token])
       end
     end
@@ -62,9 +62,9 @@ module ApplicationController::Filter::SubstMixin
       if tokens[token]                # Only atoms included in tokens will have user input
         value = tokens[token][:value] # Get the user typed value
         if tokens[token][:value_type] == :bytes
-          value += ".#{tokens[token][:suffix] || "bytes"}"  # For :bytes type, add in the suffix
+          value += ".#{tokens[token][:suffix] || "bytes"}" # For :bytes type, add in the suffix
         end
-        exp[key]["value"] = value     # Replace the exp value with the proper qs value
+        exp[key]["value"] = value # Replace the exp value with the proper qs value
       end
     end
   end
@@ -94,7 +94,7 @@ module ApplicationController::Filter::SubstMixin
       exp        = exp_find_by_token(orig_exp, token)
       first_exp  = exp[exp.keys.first]
 
-      if first_exp.key?("field")  # Base token settings on exp type
+      if first_exp.key?("field") # Base token settings on exp type
         field = exp[exp.keys.first]["field"]
         acc[token][:field]      = field
         acc[token][:value_type] = MiqExpression.get_col_info(field)[:format_sub_type]
