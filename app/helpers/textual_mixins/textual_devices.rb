8c66e96754c4b8c4c9a33455dfadda9dbb516885
Jozef Zigmund <jzigmund@redhat.com>
2017-03-24 19:39:13 +0100

    Refactor 'get_devices' method

--- app/helpers/textual_mixins/textual_devices.rb
+++ app/helpers/textual_mixins/textual_devices.rb
@@ -1,4 +1,17 @@
 module TextualMixins::TextualDevices
+  Device = Struct.new(:name, :description, :units, :icon) do
+    def description_value(record)
+      method = self.description
+      self.description = if record.hardware.respond_to?(method)
+                            value = record.hardware.send(method).to_s
+                            value += " #{units}" if value.present? && units
+                            value
+                         else
+                            nil
+                         end
+    end
+  end
+
   def textual_devices
     devices = get_devices
     h = {:label    => _("Devices"),
@@ -12,146 +25,94 @@ module TextualMixins::TextualDevices
     h
   end
 
+  def number_of_devices
+    #return just number of them
+  end
+
+  def processor_description
+    Device.new(_("Processors"),
+               _("%{total_cores} (%{num_sockets} Sockets x %{num_cores} Cores)") %
+                                                                        {:total_cores => @record.cpu_total_cores,
+                                                                         :num_sockets => @record.num_cpu,
+                                                                         :num_cores => @record.cpu_cores_per_socket},
+               nil, :processor)
+  end
+
+  def cpu_attributes
+    devices = [[_("CPU Type"), :cpu_type, nil, :processor],
+       [_("CPU Speed"), :cpu_speed,  _("MHz"), :processor],
+       [_("Memory"), :memory_mb, _("MB"), :memory]].map do |attribute|
+         device = Device.new(*attribute)
+         device.description_value(@record)
+         device
+       end
+  end
+
+  def disks_attributes
+    disks = []
+    return disks if @record.hardware.disks.empty?
+
+    # HDDs
+    disks = @record.hardware.hard_disks.map do |disk|
+      ctrl_type = disk.controller_type.upcase
+      location = disk.location
+      size = disk.size
+      prov = disk.used_percent_of_provisioned
+      device_name = _("Hard Disk (%{controller_type} %{location}), Size %{size}, Percent Used Provisioned Space %{space}") % {:controller_type => ctrl_type,
+                   :location => location,
+                   :size => size,
+                   :space => prov}
+      description = _("%{filename}, Mode: %{mode}") % {:filename => disk.filename, :mode => disk.mode}
+      Device.new(device_name, description, nil, :disk)
+    end
+
+    #Floppies
+    floppies = @record.hardware.floppies
+    if floppies.present?
+     disks += floppies.map do |floppy|
+        name = floppy.controller_type.upcase
+        connection = _("Connected at Power On = %{connect}") % {:connect => floppy.start_connected.to_s}
+        location = floppy.location
+        device_name = _("Floppy %{name} %{location} %{connection}") % {:name => name, :connection => connection, :location => location}
+        Device.new(device_name, nil, nil, :floppy)
+      end
+    end
+
+    #CD-ROMS
+    cdroms = @record.hardware.cdroms
+    if cdroms.present?
+      disks += cdroms.map do |cd|
+        name = cd.controller_type.upcase
+        connection = _("Connected at Power On = %{connect}") % {:connect => cd.start_connected.to_s}
+        location = cd.location
+        device_name = _("CD-ROM (%{name} %{location}), %{connection}") % {:name => name, :location => location, :connection => connection}
+        Device.new(device_name,nil, nil, :cdrom)
+      end
+    end
+    disks.compact
+  end
+
+  def network_attributes
+    networks = []
+    return networks if @record.hardware.ports.empty?
+    @record.hardware.ports.map do |port|
+      name = "#{port.controller_type.capitalize} #{port.device_name}"
+      location = port.location
+      address = port.address
+      filename = port.filename
+      autodetect = port.auto_detect ? "" : _("Default Adapter")
+      desc = "#{location}, #{address}, #{filename}, #{autodetect}"
+      Device.new(name, desc, nil, port.device_type)
+    end
+  end
+
   def get_devices
     devices = []
     return devices unless @record.try(:hardware)
-    if @record.hardware.cpu_total_cores
-      cpu_details =
-        if @record.num_cpu && @record.cpu_cores_per_socket
-          " (#{pluralize(@record.num_cpu, 'socket')} x #{pluralize(@record.cpu_cores_per_socket, 'core')})"
-        else
-          ""
-        end
-
-      devices.push(:device      => _("Processors"),
-                   :description => "#{@record.hardware.cpu_total_cores}#{cpu_details}",
-                   :icon        => "processor")
-    end
-
-    devices.push(:device      => _("CPU Type"),
-                 :description => @record.hardware.cpu_type,
-                 :icon        => "processor") if @record.hardware.cpu_type
-    devices.push(:device      => _("CPU Speed"),
-                 :description => "#{@record.hardware.cpu_speed} MHz",
-                 :icon        => "processor") if @record.hardware.cpu_speed
-    devices.push(:device      => _("Memory"),
-                 :description => "#{@record.hardware.memory_mb} MB",
-                 :icon        => "memory") if @record.hardware.memory_mb
-
-    # Add disks to the device array
-    unless @record.hardware.disks.nil?
-      @record.hardware.disks.each do |disk|
-        # relying on to_s to force nils into ""
-        loc = disk.location.to_s
-
-        # default device is controller_type
-        dev = disk.controller_type ? "#{disk.controller_type} #{loc}" : ""
-
-        # default description is filename
-        desc = disk.filename.to_s
-
-        # default icon prefix is device_name
-        icon = disk.device_name.to_s
-
-        conn = disk.start_connected ? _(", Connect at Power On = Yes") : _(", Connect at Power On = No")
-
-        # Customize disk entries by type
-        if disk.device_type == "cdrom-raw"
-          dev = _("CD-ROM (IDE %{location})%{connection}") % {:location => loc, :connection => conn}
-          icon = "cdrom"
-        elsif disk.device_type == "atapi-cdrom"
-          dev = _("ATAPI CD-ROM (IDE %{location})%{connection}") % {:location => loc, :connection => conn}
-          icon = "cdrom"
-        elsif disk.device_type == "cdrom-image"
-          dev = _("CD-ROM Image (IDE %{location})%{connection}") % {:location => loc, :connection => conn}
-          icon = "cdrom"
-        elsif disk.device_type == "disk"
-          icon = "disk"
-          if disk.controller_type == "ide"
-            dev = _("Hard Disk (IDE %{location})") % {:location => loc}
-          elsif disk.controller_type == "scsi"
-            dev = _("Hard Disk (SCSI %{location})") % {:location => loc}
-            icon = "scsi"
-          end
-          unless disk.size.nil?
-            dev += _(", Size: %{number}") % {:number => number_to_human_size(disk.size, :precision => 2)}
-          end
-          unless disk.size_on_disk.nil?
-            dev += _(", Size on disk: %{number}") % {:number => number_to_human_size(disk.size_on_disk,
-                                                                                     :precision => 2)}
-          end
-          unless disk.used_percent_of_provisioned.nil?
-            dev += _(", Percent Used Provisioned Space: %{number}%%") %
-              {:number => disk.used_percent_of_provisioned.to_s}
-          end
-          desc += _(", Mode: %{mode}") % {:mode => disk.mode} unless disk.mode.nil?
-        elsif disk.device_type == "ide"
-          dev = _("Hard Disk (IDE %{location})") % {:location => loc}
-          unless disk.size.nil?
-            dev += _(", Size: %{number}") % {:number => number_to_human_size(disk.size, :precision => 2)}
-          end
-          unless disk.size_on_disk.nil?
-            dev += _(", Size on disk: %{number}") % {:number => number_to_human_size(disk.size_on_disk, :precision => 2)}
-          end
-          unless disk.used_percent_of_provisioned.nil?
-            dev += _(", Percent Used Provisioned Space: %{number}%%") %
-              {:number => disk.used_percent_of_provisioned.to_s}
-          end
-          desc += _(", Mode: %{mode}") % {:mode => disk.mode} unless disk.mode.nil?
-          icon = "disk"
-        elsif ["scsi", "scsi-hardDisk"].include?(disk.device_type)
-          dev = _("Hard Disk (SCSI %{location})") % {:location => loc}
-          unless disk.size.nil?
-            dev += _(", Size: %{number}") % {:number => number_to_human_size(disk.size, :precision => 2)}
-          end
-          unless disk.size_on_disk.nil?
-            dev += _(", Size on disk: %{number}") % {:number => number_to_human_size(disk.size_on_disk,
-                                                                                     :precision => 2)}
-          end
-          unless disk.used_percent_of_provisioned.nil?
-            dev += _(", Percent Used Provisioned Space: %{number}%%") %
-              {:number => disk.used_percent_of_provisioned.to_s}
-          end
-          desc += _(", Mode: %{mode}") % {:mode => disk.mode} unless disk.mode.nil?
-          icon = "scsi"
-        elsif disk.device_type == "scsi-passthru"
-          dev = _("Generic SCSI (%{location})") % {:location => loc}
-          icon = "scsi"
-        elsif disk.device_type == "floppy"
-          dev += conn
-          icon = "floppy"
-        end
-        # uppercase the first character of the device name and description
-        dev = dev[0..0].upcase + dev[1..-1].to_s
-        desc = desc[0..0].upcase + desc[1..-1].to_s
-
-        devices.push(:device      => dev,
-                     :description => desc,
-                     :icon        => icon)
-      end
-    end
-
-    # Add ports to the device array
-    unless @record.hardware.ports.nil?
-      @record.hardware.ports.each do |port|
-        loc = port.location.nil? ? "" : port.location
-        loc = loc.strip == "0" ? "" : loc.next
-        dev = port.controller_type << " " << loc
-        desc = port.filename.nil? ? "" : port.filename
-        icon = port.device_type
-        # Customize port entries by type
-        if port.device_type == "sound"
-          dev = "Audio"
-          desc = port.auto_detect.nil? ? "" : _("Default Adapter")
-        end
-        # uppercase the first character of the device name and description
-        dev = dev[0..0].upcase + dev[1..-1]
-        desc = desc[0..0].upcase + desc[1..-1] if desc.length > 0
-        devices.push(:device      => dev,
-                     :description => desc,
-                     :icon        => icon)
-      end
-    end
+    devices << processor_description
+    devices += cpu_attributes
+    devices += disks_attributes
+    devices += network_attributes
     devices
   end
 end
