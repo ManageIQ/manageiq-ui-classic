b2148abac56654a31c4e6631ce1336748df10cbf
Tomas Jakubek <tmsjkbk@gmail.com>
2017-03-24 09:03:51 +0100

    Replace scope with vm in RetirementFormCtrl

--- app/assets/javascripts/controllers/retirement/retirement_form_controller.js
+++ app/assets/javascripts/controllers/retirement/retirement_form_controller.js
@@ -1,12 +1,13 @@
 ManageIQ.angular.app.controller('retirementFormController', ['$http', '$scope', 'objectIds', 'miqService', function($http, $scope, objectIds, miqService) {
-  $scope.objectIds = objectIds;
-  $scope.retirementInfo = {
+  var vm = this;
+  vm.objectIds = objectIds;
+  vm.retirementInfo = {
     retirementDate: null,
     retirementWarning: ''
   };
-  $scope.datepickerStartDate = new Date();
-  $scope.modelCopy = _.extend({}, $scope.retirementInfo);
-  $scope.model = 'retirementInfo';
+  vm.datepickerStartDate = new Date();
+  vm.modelCopy = _.extend({}, vm.retirementInfo);
+  vm.model = 'retirementInfo';
 
   if (objectIds.length == 1) {
     $http.get('retirement_info/' + objectIds[0])
@@ -14,25 +15,25 @@ ManageIQ.angular.app.controller('retirementFormController', ['$http', '$scope', 
       .catch(miqService.handleFailure);
   }
 
-  $scope.cancelClicked = function() {
+  vm.cancelClicked = function() {
     miqService.sparkleOn();
     miqService.miqAjaxButton('retire?button=cancel');
   };
 
-  $scope.saveClicked = function() {
+  vm.saveClicked = function() {
     miqService.sparkleOn();
     miqService.miqAjaxButton('retire?button=save',
-                             {'retire_date': $scope.retirementInfo.retirementDate,
-                              'retire_warn': $scope.retirementInfo.retirementWarning});
+                             {'retire_date': vm.retirementInfo.retirementDate,
+                              'retire_warn': vm.retirementInfo.retirementWarning});
   };
 
   function getRetirementInfoFormData(response) {
     var data = response.data;
 
     if (data.retirement_date != null) {
-      $scope.retirementInfo.retirementDate = moment.utc(data.retirement_date, 'MM-DD-YYYY').toDate();
+      vm.retirementInfo.retirementDate = moment.utc(data.retirement_date, 'MM-DD-YYYY').toDate();
     }
-    $scope.retirementInfo.retirementWarning = data.retirement_warning || '';
-    $scope.modelCopy = _.extend({}, $scope.retirementInfo);
+    vm.retirementInfo.retirementWarning = data.retirement_warning || '';
+    vm.modelCopy = _.extend({}, vm.retirementInfo);
   }
 }]);
