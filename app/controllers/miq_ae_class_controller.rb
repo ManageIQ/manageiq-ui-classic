e57d94d23042a7147d41c9240ece99dd570fffb7
Martin Povolny <mpovolny@redhat.com>
2017-03-24 21:11:49 +0100

    MiqEeClass: class_fields updates should do replace, not replace_html.

--- app/controllers/miq_ae_class_controller.rb
+++ app/controllers/miq_ae_class_controller.rb
@@ -1268,7 +1268,7 @@ class MiqAeClassController < ApplicationController
     @changed = (@edit[:new] != @edit[:current])
     render :update do |page|
       page << javascript_prologue
-      page.replace_html("class_fields_div", :partial => "class_fields")
+      page.replace("class_fields_div", :partial => "class_fields")
       page << javascript_for_miq_button_visibility(@changed)
       page << "miqSparkle(false);"
     end
@@ -1282,7 +1282,7 @@ class MiqAeClassController < ApplicationController
     @dtype_combo_xml = build_dtype_options
     render :update do |page|
       page << javascript_prologue
-      page.replace_html("class_fields_div", :partial => "class_fields")
+      page.replace("class_fields_div", :partial => "class_fields")
       page << javascript_for_miq_button_visibility(@changed)
       page << "miqSparkle(false);"
     end
@@ -1302,7 +1302,7 @@ class MiqAeClassController < ApplicationController
     @changed = (@edit[:new] != @edit[:current])
     render :update do |page|
       page << javascript_prologue
-      page.replace_html("class_fields_div", :partial => "class_fields")
+      page.replace("class_fields_div", :partial => "class_fields")
       page << javascript_for_miq_button_visibility(@changed)
       page << "miqSparkle(false);"
     end
