afc2ce7feffc1e94235ab70f2b22e91681dd467b
Martin Hradil <mhradil@redhat.com>
2017-03-28 13:09:39 +0000

    bower: use manageiq-ui-classic/vendor instead of spec/manageiq/vendor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+Rails.application.config.assets.paths << ManageIQ::UI::Classic::Engine.root.join('vendor', 'assets', 'bower_components')
 
 Rails.application.config.assets.precompile += %w(
   codemirror/modes/*.js
