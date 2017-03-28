6c0cca49901d24cc1bf1177dd488d65bc74f6a7f
Martin Hradil <mhradil@redhat.com>
2017-03-28 13:02:49 +0000

    Merge config/initializers/assets from manageiq
    we already had it here, but we had the rest in manageiq .. moving to ui-classic, merging the files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,15 @@
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
 Rails.application.config.assets.precompile += %w(
+  codemirror/modes/*.js
+  codemirror/themes/*.css
+  jquery
+  jquery-1.8/jquery.js
+  jquery_overrides.js
+  noVNC/web-socket-js/WebSocketMain.swf
+  novnc-rails
   remote_consoles/*.js
+  spice-html5-bower
+  spice-html5-bower/spiceHTML5/spicearraybuffer.js
+  vmrc.css
 )
