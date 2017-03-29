61f8ff439809dc2db5ef2389a05b0f833056b622
Martin Hradil <mhradil@redhat.com>
2017-03-29 13:18:34 +0000

    initializer/assets - merge initializer from engine.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,9 @@
 Rails.application.config.assets.paths << ManageIQ::UI::Classic::Engine.root.join('vendor', 'assets', 'bower_components')
 
+Rails.application.config.assets.precompile << proc do |filename, path|
+  path =~ %r{app/assets} && !%w(.js .css).include?(File.extname(filename))
+end
+
 Rails.application.config.assets.precompile += %w(
   codemirror/modes/*.js
   codemirror/themes/*.css
