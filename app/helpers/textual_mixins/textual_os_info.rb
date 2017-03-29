17a1845787f30535ad3d9d1538effb9aa88517c5
Jozef Zigmund <jzigmund@redhat.com>
2017-03-29 19:51:20 +0200

    Refactor 'get_os_info' method

--- app/helpers/textual_mixins/textual_os_info.rb
+++ app/helpers/textual_mixins/textual_os_info.rb
@@ -1,4 +1,7 @@
 module TextualMixins::TextualOsInfo
+  OsInfo = Struct.new(:osinfo, :description)
+  AccountPolicy = Struct.new(:field, :description)
+
   def textual_osinfo
     h = {:label => _("Operating System")}
     product_name = @record.product_name
@@ -20,42 +23,37 @@ module TextualMixins::TextualOsInfo
     h
   end
 
-  def get_os_info
-    unless @record.operating_system.nil?
-      os_info = []   # This will be an array of hashes to allow the rhtml to pull out each field by name
-      account_policy = []   # This will be an array of hashes to allow the rhtml to pull out each field by name for account policy
-      # add OS entry to the array
-      os_info.push(:osinfo      => _("Operating System"),
-                   :description => @record.operating_system.product_name) unless @record.operating_system.product_name.nil?
-      os_info.push(:osinfo      => _("Service Pack"),
-                   :description => @record.operating_system.service_pack) unless @record.operating_system.service_pack.nil?
-      os_info.push(:osinfo      => _("Product ID"),
-                   :description => @record.operating_system.productid) unless @record.operating_system.productid.nil?
-      os_info.push(:osinfo      => _("Version"),
-                   :description => @record.operating_system.version) unless @record.operating_system.version.nil?
-      os_info.push(:osinfo      => _("Build Number"),
-                   :description => @record.operating_system.build_number) unless @record.operating_system.build_number.nil?
-      os_info.push(:osinfo      => _("System Type"),
-                   :description => @record.operating_system.bitness.to_s + "-bit OS") unless @record.operating_system.bitness.nil?
-      account_policy.push(:field       => _("Password History"),
-                           :description => @record.operating_system.pw_hist) unless @record.operating_system.pw_hist.nil?
-      account_policy.push(:field       => _("Max Password Age"),
-                           :description => @record.operating_system.max_pw_age) unless @record.operating_system.max_pw_age.nil?
-      account_policy.push(:field       => _("Min Password Age"),
-                           :description => @record.operating_system.min_pw_age) unless @record.operating_system.min_pw_age.nil?
-      account_policy.push(:field       => _("Min Password Length"),
-                           :description => @record.operating_system.min_pw_len) unless @record.operating_system.min_pw_len.nil?
-      account_policy.push(:field       => _("Password Complex"),
-                           :description => @record.operating_system.pw_complex) unless @record.operating_system.pw_complex.nil?
-      account_policy.push(:field       => _("Password Encrypt"),
-                           :description => @record.operating_system.pw_encrypt) unless @record.operating_system.pw_encrypt.nil?
-      account_policy.push(:field       => _("Lockout Threshold"),
-                           :description => @record.operating_system.lockout_threshold) unless @record.operating_system.lockout_threshold.nil?
-      account_policy.push(:field       => _("Lockout Duration"),
-                           :description => @record.operating_system.lockout_duration) unless @record.operating_system.lockout_duration.nil?
-      account_policy.push(:field       => _("Reset Lockout Counter"),
-                           :description => @record.operating_system.reset_lockout_counter) unless @record.operating_system.reset_lockout_counter.nil?
+  def os_info_details
+    os_info = []
+    if @record.operating_system.present?
+      os_info = {:product_name => _("Operating System"), :service_pack => _("Service Pack"),
+                 :productid => _("Product ID"), :version => _("Version"), :build_number => _("Build Number"),
+                 :bitness => _("System Type")}.map do |method, title|
+                   value = @record.operating_system.send(method)
+                   value ? OsInfo.new(title, value) : next
+                end
     end
+    os_info.compact
+  end
+
+  def account_policy_details
+    policies = []
+    if @record.operating_system.present?
+      policies = {:pw_hist => _("Password History"), :max_pw_age => _("Max Password Age"),
+                  :min_pw_age => _("Min Password Age"), :min_pw_len => _("Min Password Length"),
+                  :pw_complex => _("Password Complex"), :pw_encrypt => _("Password Encrypt"),
+                  :lockout_threshold => _("Lockout Threshold"), :lockout_duration => _("Lockout Duration"),
+                  :reset_lockout_counter => _("Reset Lockout Counter")}.map do |method, title|
+                    value = @record.operating_system.send(method)
+                    value ? AccountPolicy.new(title, value) : next
+                  end
+    end
+    policies.compact
+  end
+
+  def get_os_info
+    os_info = os_info_details
+    account_policy = account_policy_details
     [os_info, account_policy]
   end
 end
