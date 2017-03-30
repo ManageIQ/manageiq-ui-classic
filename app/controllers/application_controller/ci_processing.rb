5ce542291a9d09ef6825cef41b9fa8b205f12408
Martin Povolny <mpovolny@redhat.com>
2017-03-30 15:16:50 +0200

    Cleanup manager_button_operation.

--- app/controllers/application_controller/ci_processing.rb
+++ app/controllers/application_controller/ci_processing.rb
@@ -1979,22 +1979,14 @@ module ApplicationController::CiProcessing
   end
 
   def manager_button_operation(method, display_name)
-    items = []
-    if params[:id]
-      if params[:id].nil? || !ExtManagementSystem.where(:id => params[:id]).exists?
-        add_flash(_("%{record} no longer exists") % {:record => ui_lookup(:table => controller_name)}, :error)
-      else
-        items.push(find_id_with_rbac(ExtManagementSystem, params[:id]))
-      end
-    else
-      items = find_checked_ids_with_rbac(ExtManagementSystem)
-    end
+    items = params[:id] ? [params[:id]] : find_checked_items
 
     if items.empty?
       add_flash(_("No providers were selected for %{task}") % {:task  => display_name}, :error)
-    else
-      process_managers(items, method) unless items.empty? && !flash_errors?
+      return
     end
+
+    process_managers(items, method)
   end
 
   def process_managers(managers, task)
