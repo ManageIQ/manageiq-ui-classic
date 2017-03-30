03bdee15e77c90115b2f059355984dbb863f2d09
Jozef Zigmund <jzigmund@redhat.com>
2017-03-30 16:29:55 +0200

    Rename method 'get_host_info' to 'host_info'

--- app/helpers/host_helper.rb
+++ app/helpers/host_helper.rb
@@ -2,7 +2,7 @@ module HostHelper
   include_concern 'TextualSummary'
   include_concern 'ComplianceSummaryHelper'
 
-  def get_host_info
+  def host_info
     devices = devices_details
     os_info = os_info_details
     vmm_info = vmm_info_details
