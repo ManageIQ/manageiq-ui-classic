3faa0b0c1e85d54d76835ca5961616dc0074c442
Martin Maroši <marvusm.mmi@gmail.com>
2017-03-31 10:33:37 +0200

    upadate tests

--- spec/javascripts/controllers/automation_manager/automation_manager_form_controller_spec.js
+++ spec/javascripts/controllers/automation_manager/automation_manager_form_controller_spec.js
@@ -1,5 +1,5 @@
 describe('automationManagerFormController', function() {
-  var $scope, $controller, $httpBackend, miqService;
+  var $scope, $httpBackend, miqService, vm;
 
   beforeEach(module('ManageIQ'));
 
@@ -22,7 +22,8 @@ describe('automationManagerFormController', function() {
 
     $httpBackend = _$httpBackend_;
     $httpBackend.whenGET('/automation_manager/form_fields/new').respond(automationManagerFormResponse);
-    $controller = _$controller_('automationManagerFormController', {
+    vm = _$controller_('automationManagerFormController as vm', {
+
       $scope: $scope,
       automationManagerFormId: 'new',
       miqService: miqService
@@ -38,25 +39,25 @@ describe('automationManagerFormController', function() {
   describe('initialization', function() {
     describe('when the automationManagerFormId is new', function() {
       it('sets the name to blank', function () {
-        expect($scope.automationManagerModel.name).toEqual('');
+        expect(vm.automationManagerModel.name).toEqual('');
       });
       it('sets the zone to blank', function () {
-        expect($scope.automationManagerModel.zone).toEqual('foo_zone');
+        expect(vm.automationManagerModel.zone).toEqual('foo_zone');
       });
       it('sets the url to blank', function () {
-        expect($scope.automationManagerModel.url).toEqual('');
+        expect(vm.automationManagerModel.url).toEqual('');
       });
       it('sets the verify_ssl to blank', function () {
-        expect($scope.automationManagerModel.verify_ssl).toBeFalsy();
+        expect(vm.automationManagerModel.verify_ssl).toBeFalsy();
       });
       it('sets the log_userid to blank', function () {
-        expect($scope.automationManagerModel.log_userid).toEqual('');
+        expect(vm.automationManagerModel.log_userid).toEqual('');
       });
       it('sets the log_password to blank', function () {
-        expect($scope.automationManagerModel.log_password).toEqual('');
+        expect(vm.automationManagerModel.log_password).toEqual('');
       });
       it('sets the log_verify to blank', function () {
-        expect($scope.automationManagerModel.log_verify).toEqual('');
+        expect(vm.automationManagerModel.log_verify).toEqual('');
       });
     });
 
@@ -71,7 +72,8 @@ describe('automationManagerFormController', function() {
 
       beforeEach(inject(function(_$controller_) {
         $httpBackend.whenGET('/automation_manager/form_fields/12345').respond(automationManagerFormResponse);
-        $controller = _$controller_('automationManagerFormController',
+        vm = _$controller_('automationManagerFormController as vm',
+
           {
             $scope: $scope,
             automationManagerFormId: '12345',
@@ -81,25 +83,25 @@ describe('automationManagerFormController', function() {
       }));
 
       it('sets the name to the value returned from http request', function () {
-        expect($scope.automationManagerModel.name).toEqual('Ansible');
+        expect(vm.automationManagerModel.name).toEqual('Ansible');
       });
       it('sets the zone to the value returned from the http request', function () {
-        expect($scope.automationManagerModel.zone).toEqual('My Test Zone');
+        expect(vm.automationManagerModel.zone).toEqual('My Test Zone');
       });
       it('sets the url to the value returned from http request', function () {
-        expect($scope.automationManagerModel.url).toEqual('10.10.10.10');
+        expect(vm.automationManagerModel.url).toEqual('10.10.10.10');
       });
       it('sets the verify_ssl to the value returned from http request', function () {
-        expect($scope.automationManagerModel.verify_ssl).toBeTruthy();
+        expect(vm.automationManagerModel.verify_ssl).toBeTruthy();
       });
       it('sets the log_userid to the value returned from http request', function () {
-        expect($scope.automationManagerModel.log_userid).toEqual('admin');
+        expect(vm.automationManagerModel.log_userid).toEqual('admin');
       });
       it('sets the log_password to the value returned from http request', function () {
-        expect($scope.automationManagerModel.log_password).toEqual(miqService.storedPasswordPlaceholder);
+        expect(vm.automationManagerModel.log_password).toEqual(miqService.storedPasswordPlaceholder);
       });
       it('sets the log_verify to the value returned from http request', function () {
-        expect($scope.automationManagerModel.log_verify).toEqual(miqService.storedPasswordPlaceholder);
+        expect(vm.automationManagerModel.log_verify).toEqual(miqService.storedPasswordPlaceholder);
       });
     });
   });
