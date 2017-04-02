e49d75d6b336581284f77c93cca99c66af19271c
yaacov <yzamir@redhat.com>
2017-04-02 14:49:56 +0300

    Add last values to metrics definitions
    use lowdash for unique chart id
    
    make the prefix less ugly
    
    Make data calls parallel on ManageIQ side

--- app/assets/javascripts/controllers/live_metrics/util/metrics-parse-url-factroy.js
+++ app/assets/javascripts/controllers/live_metrics/util/metrics-parse-url-factroy.js
@@ -36,6 +36,6 @@ angular.module('miq.util').factory('metricsParseUrlFactory', function() {
     dash.tenantList = [];
     dash.minBucketDurationInSecondes = parseInt(dash.params.bucket, 10) || 20 * 60;
     dash.max_metrics = parseInt(dash.params.max_metrics, 10) || 10000;
-    dash.items_per_page = parseInt(dash.params.items_per_page, 10) || 10;
+    dash.items_per_page = parseInt(dash.params.items_per_page, 10) || 8;
   };
 });
