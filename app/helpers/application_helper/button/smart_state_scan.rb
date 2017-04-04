ed6570638cb8060724c7d118e45953ddcb675008
yaacov <yzamir@redhat.com>
2017-04-04 17:24:16 +0300

    Anable configuration button for container images

--- app/helpers/application_helper/button/smart_state_scan.rb
+++ app/helpers/application_helper/button/smart_state_scan.rb
@@ -1,12 +1,18 @@
 class ApplicationHelper::Button::SmartStateScan < ApplicationHelper::Button::Basic
-  needs :@record
-
   def check_smart_roles
     my_zone = MiqServer.my_server.my_zone
     MiqServer::ServerSmartProxy::SMART_ROLES.each do |role|
       unless MiqServer.all.any? { |s| s.has_active_role?(role) && (s.my_zone == my_zone) }
         @error_message = _("There is no server with the #{role} role enabled")
       end
+    end
+  end
+
+  def calculate_properties
+    super
+    if disabled?
+      self[:onwhen] = nil
+      self[:title] = @error_message
     end
   end
 
