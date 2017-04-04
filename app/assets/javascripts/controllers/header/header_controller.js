ef2eb3034c5635eae095c8850a153bb1e0fbd94e
Martin Hradil <mhradil@redhat.com>
2017-04-04 11:02:58 +0000

    $scope.$on('destroy') should be '$destroy'
    'destroy' would need to be a custom event
    the intent behind that code was definitely the $destroy event which happens when destroyin the scope

--- app/assets/javascripts/controllers/header/header_controller.js
+++ app/assets/javascripts/controllers/header/header_controller.js
@@ -46,7 +46,7 @@ function HeaderCtrl($scope, eventNotifications, $timeout) {
 
   eventNotifications.registerObserverCallback(refresh);
 
-  $scope.$on('destroy', destroy);
+  $scope.$on('$destroy', destroy);
 
   refresh();
 }
