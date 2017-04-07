9736954fde484d9238d0b4daab931e2770553b92
Pettan <panhammer@seznam.cz>
2017-04-07 10:26:32 +0200

    Formatting fixed

--- spec/controllers/ansible_repository_controller_spec.rb
+++ spec/controllers/ansible_repository_controller_spec.rb
@@ -1,5 +1,4 @@
 describe AnsibleRepositoryController do
-
   include CompressedIds
 
   before do
@@ -10,23 +9,22 @@ describe AnsibleRepositoryController do
   end
 
   context "show" do
-     subject do
-       get :show, :params => {:id => @repository.id}
-     end
+    subject do
+      get :show, :params => {:id => @repository.id}
+    end
 
-     it "render specific repostitory" do
-       is_expected.to have_http_status 200
-     end
-   end
-
+    it "render specific repostitory" do
+      is_expected.to have_http_status 200
+    end
+  end
 
   context "showList" do
-     subject do
-       get :show_list
-     end
+    subject do
+      get :show_list
+    end
 
-     it "render list of repositories" do
-       is_expected.to have_http_status 200
-     end
-   end
+    it "render list of repositories" do
+      is_expected.to have_http_status 200
+    end
+  end
 end
