bd9d2d458031c6cb9d7b916bce9a65b65dcb5cca
Zita Nemeckova <znemecko@redhat.com>
2017-04-11 14:57:54 +0200

    Replace checkchange with form-changed
    Instead of checking individual inputs for change check whole form for changes

---
+++ app/assets/javascripts/directives/form_changed.js
@@ -0,0 +1,19 @@
+ManageIQ.angular.app.directive('formChanged', function() {
+  return {
+    require: "form",
+    link: function(scope, elem, attr, ctrl) {
+      var model = function() {
+        return scope.$eval(ctrl.model || scope.model);
+      };
+      var modelCopy = function() {
+        return scope.$eval(ctrl.modelCopy || "modelCopy");
+      };
+      scope.$watchCollection(ctrl.model || scope.model, function() {
+        if (angular.equals(model(), modelCopy())) {
+          ctrl.$setPristine();
+        }
+      });
+    },
+  };
+});
+
