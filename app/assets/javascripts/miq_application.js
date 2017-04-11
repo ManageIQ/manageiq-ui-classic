e6059ecd37a9cc154f66c0ef0927e853b4190e2a
Martin Hradil <mhradil@redhat.com>
2017-04-11 16:55:15 +0000

    Topology header toolbar - work around implicit browser click on enter
    For some reason, pressing enter in an input triggers a *click* event on the immediately following button. (Maybe something to do with a form without any input type=submit?)
    
    Thus, and given the restrictions on this particular template, working around the issue by providing an invisible button which does the same thing as form submit does.
    
    (Could have been just a `%button.hidden` by itself, except all `%button` elements in toolbars have a miqToolbarOnClick as the click handler.)
    
    The change in `miqHideSearchClearButton` is to find the right button even if it is not an immediate sibling.

--- app/assets/javascripts/miq_application.js
+++ app/assets/javascripts/miq_application.js
@@ -1579,7 +1579,7 @@ function miqHideSearchClearButton() {
   // Show the clear button upon entering text in the search input
   $(".search-pf .has-clear .form-control").keyup(function () {
     var t = $(this);
-    t.next('button').toggle(Boolean(t.val()));
+    t.nextAll('button.clear').toggle(Boolean(t.val()));
   });
   // Upon clicking the clear button, empty the entered text and hide the clear button
   $(".search-pf .has-clear .clear").click(function () {
