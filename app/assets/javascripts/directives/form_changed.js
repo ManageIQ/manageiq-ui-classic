85cc4091068e385e704a3a5b4ac853ba9efa8197
Zita Nemeckova <znemecko@redhat.com>
2017-04-12 13:43:32 +0200

    Key Pair form has form-changed instead of checkchange

--- app/assets/javascripts/directives/form_changed.js
+++ app/assets/javascripts/directives/form_changed.js
@@ -1,12 +1,12 @@
 ManageIQ.angular.app.directive('formChanged', function() {
   return {
-    require: "form",
-    link: function(scope, elem, attr, ctrl) {
+    require: 'form',
+    link: function(scope, _elem, _attr, ctrl) {
       var model = function() {
         return scope.$eval(ctrl.model || scope.model);
       };
       var modelCopy = function() {
-        return scope.$eval(ctrl.modelCopy || "modelCopy");
+        return scope.$eval(ctrl.modelCopy || 'modelCopy');
       };
       scope.$watchCollection(ctrl.model || scope.model, function() {
         if (angular.equals(model(), modelCopy())) {
