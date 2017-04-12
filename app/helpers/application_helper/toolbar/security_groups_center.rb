566e0516082d966f7c05dd3e0e753a4516673d3f
Aparna Karve <akarve@redhat.com>
2017-04-12 16:15:58 -0700

    Fix Rubocop

--- app/helpers/application_helper/toolbar/security_groups_center.rb
+++ app/helpers/application_helper/toolbar/security_groups_center.rb
@@ -13,7 +13,8 @@ class ApplicationHelper::Toolbar::SecurityGroupsCenter < ApplicationHelper::Tool
             'pficon pficon-add-circle-o fa-lg',
             t = N_('Add a new Security Group'),
             t,
-            :klass => ApplicationHelper::Button::SecurityGroupNew),
+            :klass => ApplicationHelper::Button::SecurityGroupNew
+          ),
           separator,
           # TODO: Uncomment until cross controllers show_list issue fully in place
           # https://github.com/ManageIQ/manageiq/pull/12551
