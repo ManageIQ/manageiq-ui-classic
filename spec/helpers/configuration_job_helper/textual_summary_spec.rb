8133028c689e6c29854ce2de745f6bf44db3e5a4
Dan Clarizio <dclarizi@redhat.com>
2017-04-13 13:01:45 -0700

    Merge pull request #1029 from lgalis/fix_textual_provider_svg_and_controller_for_ansible_job
    Fix the controller and svg in textual provider for Ansible Tower Jobs
    (cherry picked from commit a965a4fa00f2e384df3c924d0252bfa7408d0181)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1442770
    https://bugzilla.redhat.com/show_bug.cgi?id=1442767

---
+++ spec/helpers/configuration_job_helper/textual_summary_spec.rb
@@ -0,0 +1,14 @@
+describe ConfigurationJobHelper::TextualSummary do
+  include CompressedIds
+  include ApplicationHelper
+
+  let(:zone) { EvmSpecHelper.local_miq_server.zone }
+  let(:automation_provider) { FactoryGirl.create(:provider_ansible_tower, :name => "ansibletest", :url => "test", :zone => zone) }
+
+  it "#textual_provider" do
+    manager = ManageIQ::Providers::AnsibleTower::AutomationManager.find_by(:provider_id => automation_provider.id)
+    @record = FactoryGirl.create(:ansible_tower_job, :ext_management_system => manager)
+    expect(textual_provider[:image]).to eq("svg/vendor-ansible.svg")
+    expect(textual_provider[:link]).to eq("/automation_manager/explorer/at-#{to_cid(manager.id)}")
+  end
+end
