07af0a84421bb07ec3b9ce81737cb34b9543a8aa
Zita Nemeckova <znemecko@redhat.com>
2017-04-19 15:29:47 +0200

    Remove children from hash nodes
    Remove subsection that was used to keep information about nodes children. Store data in hash not in array of arrays of arrays.

--- app/presenters/tree_builder_timelines.rb
+++ app/presenters/tree_builder_timelines.rb
@@ -3,11 +3,34 @@ class TreeBuilderTimelines < TreeBuilder
 
   # Needs to be true only for first two levels. Move to better place later
   def override(node, object, _pid, _options)
+    if object.kind_of?(MiqReport)
+      node[:tooltip] = _("Report: %{name}") % {:name => object.name}
+    end
     node[:expand] = object[:expand] if object[:expand].present?
   end
 
-  def initialize(name, type, sandbox, build = true, menu:)
-    @menu = menu
+  # {'first level' => {'second level' => [MiqReports]}}
+  def reports
+    @rep = MiqReport.all.sort_by { |r| [r.rpt_type, r.filename.to_s, r.name] }
+    @rep.reject! { |r| r.timeline.nil? }
+    @rep.each_with_object({}) do |report, reports|
+      next if report.template_type != "report" && !report.template_type.blank?
+      first, second = if report.rpt_group == 'Custom'
+                        [@title, 'Custom']
+                      else
+                        report.rpt_group.split('-').collect(&:strip)
+                      end
+      if reports.fetch_path(first, second)
+        reports[first][second].push(report)
+      else
+        reports.store_path(first, second, [report])
+      end
+    end
+  end
+
+  def initialize(name, type, sandbox, build, params)
+    @title = params[:title]
+    @reports = reports
     super(name, type, sandbox, build)
   end
 
@@ -35,46 +58,34 @@ class TreeBuilderTimelines < TreeBuilder
   end
 
   def x_get_tree_roots(count_only, _options)
-    menus = @menu.map do |item|
-      title = item.first
+    kids = @reports.keys.sort.map do |item|
       {
-        :text        => title,
-        :id          => "r__#{title}",
+        :text        => item,
+        :id          => item,
         :icon        => 'pficon pficon-folder-close',
-        :tip         => title,
+        :tip         => item,
         :cfmeNoClick => true,
         :expand      => true,
-        :subsections => item.last
       }
     end
-    count_only_or_objects(count_only, menus.compact)
+    count_only_or_objects(count_only, kids)
   end
 
   def x_get_tree_hash_kids(parent, count_only)
-    subsection_reports = MiqReport.where(:name => parent[:subsections].select {|r| r.kind_of? String})
-                           .pluck(:name, :id).to_h
-    subsections = parent[:subsections].map do |item|
-      if item.kind_of?(Array)
-        title = item.first
-        {
-          :text        => title,
-          :id          => "p__#{title}",
-          :icon        => 'pficon pficon-folder-close',
-          :tip         => _("Group: %{:name}" % {:name => title}),
-          :expand      => false,
-          :cfmeNoClick => true,
-          :subsections => item.last
-        }
-      elsif subsection_reports[item]
-        {
-          :text        => item,
-          :id          => "#{subsection_reports[item]}__#{item}",
-          :icon        => "fa fa-arrow-circle-o-right",
-          :tip         => _("Report: %{:name}" % {:name => item}),
-          :subsections => []
-        }
-      end
-     end
-    count_only_or_objects(count_only, subsections.compact)
+    kids = if @reports.fetch_path(parent[:text])
+             @reports[parent[:text]].keys.map do |item|
+               {
+                 :text        => item,
+                 :id          => item,
+                 :icon        => 'pficon pficon-folder-close',
+                 :tip         => _("Group: %{name}") % {:name => item},
+                 :expand      => false,
+                 :cfmeNoClick => true,
+               }
+             end
+           else
+             @reports.values.select { |item| item.key?(parent[:text]) }.first[parent[:text]]
+           end
+    count_only_or_objects(count_only, kids)
   end
 end
