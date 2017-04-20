b74f12a320a9e4b2a5e2aecd0b2c4eda228a5e73
Zita Nemeckova <znemecko@redhat.com>
2017-04-20 13:21:17 +0200

    Add custom compare function to deal with empty fields
    Originally empty field is not added to model and modelCopy. If that field is modified later it's only added to model. At that point model and modelCopy will never be same(unless reset is pushed). So compare function that deals with it is added. It adds missing fields to modelCopy from model. Then it compares it with premise undefined == null == ''.

--- app/assets/javascripts/directives/form_changed.js
+++ app/assets/javascripts/directives/form_changed.js
@@ -5,11 +5,36 @@ ManageIQ.angular.app.directive('formChanged', function() {
       var model = function() {
         return scope.$eval(ctrl.model || scope.model);
       };
+
       var modelCopy = function() {
         return scope.$eval(ctrl.modelCopy || 'modelCopy');
       };
+
+      var empty = function(object) {
+        return object === undefined || object === '' || object === null;
+      };
+
+      var compare = function(original, copy, key) {
+        // add missing keys in copy from original so recursion works
+        if (_.isObject(copy) && _.isObject(original)) {
+          _.difference(Object.keys(original), Object.keys(copy)).forEach(function(k) {
+            copy[k] = undefined;
+          });
+        }
+        // don't compare ng stuff TODO remove in Angular 2/4
+        if (key !== undefined && key[0] === '$') {
+          return true;
+        }
+        if (empty(original) && empty(copy)) {
+          return true;
+        }
+        // use default compare
+        return undefined;
+      };
+
       scope.$watchCollection(ctrl.model || scope.model, function() {
-        if (angular.equals(model(), modelCopy())) {
+        // TODO in lodash 4 it's _.isEqualWith
+        if (_.isEqual(model(), modelCopy(), compare)) {
           ctrl.$setPristine();
         }
       });
