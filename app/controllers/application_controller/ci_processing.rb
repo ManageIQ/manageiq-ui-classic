d8f65f1b96366dc016cb686d90b468812d727b88
Milan Zázrivec <mzazrivec@redhat.com>
2017-04-24 10:05:06 +0200

    Merge pull request #1101 from AparnaKarve/bz1442112_no_flash_retiring_service
    Fixed no flash message issue while retiring a service
    (cherry picked from commit 431296483b057834ff0e825ab1970c3c1df0e97d)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1444968

--- app/controllers/application_controller/ci_processing.rb
+++ app/controllers/application_controller/ci_processing.rb
@@ -2076,7 +2076,6 @@ module ApplicationController::CiProcessing
   end
   alias_method :instance_retire_now, :retirevms_now
   alias_method :vm_retire_now, :retirevms_now
-  alias_method :service_retire_now, :retirevms_now
   alias_method :orchestration_stack_retire_now, :retirevms_now
 
   def check_compliance_vms
