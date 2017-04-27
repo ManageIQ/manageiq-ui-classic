6469905d853bcfe4e85e00b2fc9887ba0476894a
Martin Povolny <mpovolny@redhat.com>
2017-04-27 14:47:11 +0200

    Settings/server flash messages fixes: update tests.

--- spec/controllers/ops_controller_spec.rb
+++ spec/controllers/ops_controller_spec.rb
@@ -329,7 +329,7 @@ describe OpsController do
         allow(controller).to receive(:check_privileges).and_return(true)
         allow(controller).to receive(:assert_privileges).and_return(true)
         seed_session_trees('ops', :settings_tree, 'root')
-        expect(controller).to receive(:render_to_string).with(any_args).twice
+        expect(controller).to receive(:render_to_string).with(any_args).exactly(3).times
         post :change_tab, :params => {:tab_id => tab}
       end
 
