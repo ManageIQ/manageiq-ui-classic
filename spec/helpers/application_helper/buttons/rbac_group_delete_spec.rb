1874bb029eaf9877fb4899aa3972cb558bf3b15d
Attila Večerek <veat24@gmail.com>
2017-04-28 11:12:32 +0200

    Create and use shared context for Rbac Group buttons

--- spec/helpers/application_helper/buttons/rbac_group_delete_spec.rb
+++ spec/helpers/application_helper/buttons/rbac_group_delete_spec.rb
@@ -1,3 +1,12 @@
+require 'shared/helpers/application_helper/buttons/rbac_group'
+
 describe ApplicationHelper::Button::RbacGroupDelete do
-  it_behaves_like 'an rbac_group_action button', 'deleted'
+  include_context 'ApplicationHelper::Button::RbacGroup'
+
+  describe '#calculate_properties' do
+    before { subject.calculate_properties }
+
+    include_examples 'ApplicationHelper::Button::RbacGroup read-only record with restriction', 'deleted'
+    include_examples 'ApplicationHelper::Button::RbacGroup writable record'
+  end
 end
--- spec/helpers/application_helper/buttons/rbac_group_edit_spec.rb
+++ spec/helpers/application_helper/buttons/rbac_group_edit_spec.rb
@@ -1,3 +1,12 @@
+require 'shared/helpers/application_helper/buttons/rbac_group'
+
 describe ApplicationHelper::Button::RbacGroupEdit do
-  it_behaves_like 'an rbac_group_action button', 'edited'
+  include_context 'ApplicationHelper::Button::RbacGroup'
+
+  describe '#calculate_properties' do
+    before { subject.calculate_properties }
+
+    include_examples 'ApplicationHelper::Button::RbacGroup read-only record with restriction', 'edited'
+    include_examples 'ApplicationHelper::Button::RbacGroup writable record'
+  end
 end
