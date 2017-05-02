67980fc59a61f4bdecc80d70dda5615df2a7609f
Eric Hayes <eric@erichayes.net>
2017-05-02 16:08:38 -0700

    Check feature parent in Menu::Items for when role has “everything under” feature.
    https://bugzilla.redhat.com/show_bug.cgi?id=1442796

--- app/presenters/tree_node/menu/item.rb
+++ app/presenters/tree_node/menu/item.rb
@@ -4,12 +4,24 @@ module TreeNode
       set_attribute(:key)      { "#{@options[:node_id_prefix]}__#{@object.feature}" }
       set_attribute(:title)    { _(details[:name]) }
       set_attribute(:tooltip)  { _(details[:description]) || _(details[:name]) }
-      set_attribute(:selected) { @options[:features].include?(@object.feature) }
+      set_attribute(:selected) { parent_selected? || self_selected? }
 
       private
 
       def details
         ::MiqProductFeature.obj_features[@object.feature].try(:[], :feature).try(:details)
+      end
+
+      def feature_parent
+        ::MiqProductFeature.obj_features[::MiqProductFeature.feature_parent(@object.feature)][:feature]
+      end
+
+      def parent_selected?
+        @options[:features].include?(feature_parent.identifier)
+      end
+
+      def self_selected?
+        @options[:features].include?(@object.feature)
       end
     end
   end
