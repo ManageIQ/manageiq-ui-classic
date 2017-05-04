5a2adc451c8d8ad7a481740a012fcb79f5f03bed
Dávid Halász <dhalasz@redhat.com>
2017-05-04 14:37:20 +0200

    Declare fonticon classes instead of chars in UiServiceMixin

--- app/services/ui_service_mixin.rb
+++ app/services/ui_service_mixin.rb
@@ -1,44 +1,45 @@
 module UiServiceMixin
   def icons
     {
-      :AvailabilityZone        => {:type => "glyph", :icon => "\uE911", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-zone
-      :ContainerReplicator     => {:type => "glyph", :icon => "\uE624", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-replicator
-      :ContainerProject        => {:type => "glyph", :icon => "\uE905", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-project
-      :ContainerGroup          => {:type => "glyph", :icon => "\uF1B3", :fontfamily => "FontAwesome"},             # fa-cubes
-      :ContainerNode           => {:type => "glyph", :icon => "\uE621", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-container-node
-      :ContainerService        => {:type => "glyph", :icon => "\uE61E", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-service
-      :ContainerRoute          => {:type => "glyph", :icon => "\uE625", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-route
-      :Container               => {:type => "glyph", :icon => "\uF1B2", :fontfamily => "FontAwesome"},             # fa-cube
-      :EmsCluster              => {:type => "glyph", :icon => "\uE620", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-cluster
-      :Host                    => {:type => "glyph", :icon => "\uE600", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-screen
-      :Vm                      => {:type => "glyph", :icon => "\uE90f", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-virtual-machine
-      :MiddlewareDatasource    => {:type => "glyph", :icon => "\uF1C0", :fontfamily => "FontAwesome"},             # fa-database
-      :MiddlewareDeployment    => {:type => "glyph", :icon => "\uF0f6", :fontfamily => "FontAwesome"},             # fa-file-text-o
-      :MiddlewareDeploymentEar => {:type => "glyph", :icon => "\uE626", :fontfamily => "icomoon"},                 # product-file-ear-o
-      :MiddlewareDeploymentWar => {:type => "glyph", :icon => "\uE627", :fontfamily => "icomoon"},                 # product-file-war-o
-      :MiddlewareDomain        => {:type => "glyph", :icon => "\uE919", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-domain
-      :MiddlewareMessaging     => {:type => "glyph", :icon => "\uF0EC", :fontfamily => "FontAwesome"},             # fa-exchange (placeholder)
-      :MiddlewareServerGroup   => {:type => "glyph", :icon => "\uE91a", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-server-group
-      :Kubernetes              => {:type => "image", :icon => provider_icon(:Kubernetes)},
-      :Openshift               => {:type => "image", :icon => provider_icon(:Openshift)},
-      :CloudSubnet             => {:type => "glyph", :icon => "\uE909", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-network
-      :NetworkRouter           => {:type => "glyph", :icon => "\uE625", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-route
-      :SecurityGroup           => {:type => "glyph", :icon => "\uE903", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-cloud-security
-      :FloatingIp              => {:type => "glyph", :icon => "\uF041", :fontfamily => "FontAwesome"},             # fa-map-marker
-      :CloudNetwork            => {:type => "glyph", :icon => "\uE62c", :fontfamily => "IcoMoon"},                 # product-cloud_network
-      :CloudTenant             => {:type => "glyph", :icon => "\uE904", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-cloud-tenant
-      :LoadBalancer            => {:type => "glyph", :icon => "\uE637", :fontfamily => "IcoMoon"},                 # product-load_balancer
-      :Tag                     => {:type => "glyph", :icon => "\uF02b", :fontfamily => "FontAwesome"},             # fa-tag
-      :PhysicalServer          => {:type => "glyph", :icon => "\uE91a", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-server-group
-      :Openstack               => {:type => "image", :icon => provider_icon(:Openstack)},
+      :AvailabilityZone        => {:type => "glyph", :class => 'pficon pficon-zone'},
+      :CloudNetwork            => {:type => "glyph", :class => 'product product-cloud_network'},
+      :CloudSubnet             => {:type => "glyph", :class => 'pficon pficon-network'},
+      :CloudTenant             => {:type => "glyph", :class => 'pficon pficon-cloud-tenant'},
+      :Container               => {:type => "glyph", :class => 'fa fa-cube'},
+      :ContainerGroup          => {:type => "glyph", :class => 'fa fa-cubes'},
+      :ContainerNode           => {:type => "glyph", :class => 'pficon pficon-container-node'},
+      :ContainerProject        => {:type => "glyph", :class => 'pficon pficon-project'},
+      :ContainerReplicator     => {:type => "glyph", :class => 'pficon pficon-replicator'},
+      :ContainerRoute          => {:type => "glyph", :class => 'pficon pficon-route'},
+      :ContainerService        => {:type => "glyph", :class => 'pficon pficon-service'},
+      :EmsCluster              => {:type => "glyph", :class => 'pficon pficon-cluster'},
+      :FloatingIp              => {:type => "glyph", :class => 'fa fa-map-marker'},
+      :Host                    => {:type => "glyph", :class => 'pficon pficon-screen'},
+      :LoadBalancer            => {:type => "glyph", :class => 'product product-load_balancer'},
+      :MiddlewareDatasource    => {:type => "glyph", :class => 'fa fa-database'},
+      :MiddlewareDeployment    => {:type => "glyph", :class => 'fa fa-file-text-o'},
+      :MiddlewareDeploymentEar => {:type => "glyph", :class => 'product product-file-ear-o'},
+      :MiddlewareDeploymentWar => {:type => "glyph", :class => 'product product-file-war-o'},
+      :MiddlewareDomain        => {:type => "glyph", :class => 'pficon pficon-domain'},
+      :MiddlewareMessaging     => {:type => "glyph", :class => 'fa fa-exchange (placeholder)'},
+      :MiddlewareServerGroup   => {:type => "glyph", :class => 'pficon pficon-server-group'},
+      :NetworkRouter           => {:type => "glyph", :class => 'pficon pficon-route'},
+      :PhysicalServer          => {:type => "glyph", :class => 'pficon pficon-server-group'},
+      :SecurityGroup           => {:type => "glyph", :class => 'pficon pficon-cloud-security'},
+      :Tag                     => {:type => "glyph", :class => 'fa fa-tag'},
+      :Vm                      => {:type => "glyph", :class => 'pficon pficon-virtual-machine'},
+
       :Amazon                  => {:type => "image", :icon => provider_icon(:Amazon)},
       :Azure                   => {:type => "image", :icon => provider_icon(:Azure)},
       :Google                  => {:type => "image", :icon => provider_icon(:Google)},
-      :Microsoft               => {:type => "image", :icon => provider_icon(:Microsoft)},
-      :Redhat                  => {:type => "image", :icon => provider_icon(:Redhat)},
-      :Vmware                  => {:type => "image", :icon => provider_icon(:Vmware)},
+      :Kubernetes              => {:type => "image", :icon => provider_icon(:Kubernetes)},
       :Lenovo                  => {:type => "image", :icon => provider_icon(:Lenovo)},
+      :Microsoft               => {:type => "image", :icon => provider_icon(:Microsoft)},
       :Nuage                   => {:type => "image", :icon => provider_icon(:Nuage_Network)},
+      :Openshift               => {:type => "image", :icon => provider_icon(:Openshift)},
+      :Openstack               => {:type => "image", :icon => provider_icon(:Openstack)},
+      :Redhat                  => {:type => "image", :icon => provider_icon(:Redhat)},
+      :Vmware                  => {:type => "image", :icon => provider_icon(:Vmware)}
     }
   end
 
