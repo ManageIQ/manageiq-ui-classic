2680f530ebfc29424b3e0449ca110224d6f5fddd
lgalis <lgalis@redhat.com>
2017-05-05 16:59:21 -0400

    Display summary tags for the ansible playbook services

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -137,7 +137,7 @@ class ServiceController < ApplicationController
 
   def textual_group_list
     if @record.type == "ServiceAnsiblePlaybook"
-      [%i(properties), %i(lifecycle)]
+      [%i(properties), %i(lifecycle tags)]
     else
       [%i(properties lifecycle relationships miq_custom_attributes), %i(vm_totals tags)]
     end
