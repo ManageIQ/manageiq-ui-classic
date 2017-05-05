28ce324a4d163a0f264e72f242768e7e57696e22
Dan Vybiral <xvybira5@mendelu.cz>
2017-05-05 09:42:22 +0200

    Added more tests.

--- spec/javascripts/controllers/cloud/cloud_tenant_form_controller_spec.js
+++ spec/javascripts/controllers/cloud/cloud_tenant_form_controller_spec.js
@@ -3,25 +3,24 @@ describe('cloudTenantFormController', function() {
 
   beforeEach(module('ManageIQ'));
 
-  beforeEach(inject(function(_$httpBackend_, $rootScope, _$controller_, _miqService) {
-    miqService = _miqService;
+  beforeEach(inject(function(_$httpBackend_, $rootScope, _$controller_, _miqService_) {
+    miqService = _miqService_;
 
     spyOn(miqService, 'showButtons');
     spyOn(miqService, 'hideButtons');
     spyOn(miqService, 'miqAjaxButton');
-    spyOn(miqService, 'sparkleOn');
-    spyOn(miqService, 'sparkleOff');
-    spyOn(postService, 'cancelOperation');
-    spyOn(postService, 'saveRecord');
-    spyOn(postService, 'createRecord');
+    spyOn(miqService, 'miqFlash');
 
     scope = $rootScope.$new();
 
+    var mock_data = { name: 'test', ems_id: 1 };
+
     $httpBackend = _$httpBackend_;
     $httpBackend.when('GET','/cloud_tenant/show_list').respond(mock_data);
-    $controller = _$controller_('cloudTenantFormController',
-      {$scope: scope});
-    $httpBackend.flush();
+    $controller = _$controller_('cloudTenantFormController as vm',{
+      $scope: scope,
+      miqService: miqService,
+      cloudTenantFormId: 'new'});
   }));
 
   describe('#cancelClicked', function() {
@@ -29,18 +28,49 @@ describe('cloudTenantFormController', function() {
       $scope.angularForm = {
         $setPristine: function (value){}
       };
-      setTimeout($scope.cancelClicked);
+      setTimeout($controller.cancelClicked);
     });
 
-    it('delegates to miqService.cancelOperation', function(done) {
+    it('delegates to miqService.miqAjaxButton', function(done) {
       setTimeout(function () {
-        var msg = "Edit of Cloud tenant item cloudTenantModel was cancelled by the user";
-        expect(miqService.cancelOperation).toHaveBeenCalledWith(redirectUrl + "?", msg);
+        var redirectUrl = '/cloud_tenant/create/new' + '?button=cancel';
+        expect(miqService.miqAjaxButton).toHaveBeenCalledWith(redirectUrl);
         done();
       });
     });
   });
 
+  describe('#saveClicked', function() {
+    beforeEach(function() {
+      $scope.angularForm = {
+          $setPristine: function (value){}
+      };
+      setTimeout($controller.saveClicked);
+    });
+
+    it('delegates to miqService.miqAjaxButton', function(done) {
+      setTimeout(function () {
+          var redirectUrl = 'create/new' + '?button=add';
+          expect(miqService.miqAjaxButton).toHaveBeenCalledWith(redirectUrl, $controller.cloudTenantModel, { complete: false });
+          done();
+      });
+    });
+  });
 
 
+  describe('#resetClicked', function() {
+    beforeEach(function() {
+      $scope.angularForm = {
+          $setPristine: function (value){}
+      };
+      setTimeout($controller.resetClicked);
+    });
+
+    it('warns the user with flash', function(done) {
+      setTimeout(function () {
+          expect(miqService.miqFlash).toHaveBeenCalled();
+          done();
+      });
+    });
+  });
 });
