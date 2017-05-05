6fff955fe3bac95eb7e2fb2c5bbeccb435fa1b83
Martina Škantárová <skantarova.martina@gmail.com>
2017-05-05 09:27:45 +0200

    updating test and codeckimate issues solved

--- app/assets/javascripts/controllers/floating_ip/floating_ip_form_controller.js
+++ app/assets/javascripts/controllers/floating_ip/floating_ip_form_controller.js
@@ -1,17 +1,17 @@
 ManageIQ.angular.app.controller('floatingIpFormController', ['$http', '$scope', 'floatingIpFormId', 'miqService', function($http, $scope, floatingIpFormId, miqService) {
   var vm = this;
-    vm.floatingIpModel = { name: '' };
-    vm.formId = floatingIpFormId;
-    vm.afterGet = false;
-    vm.modelCopy = angular.copy( vm.floatingIpModel );
-    vm.model = "floatingIpModel";
+  vm.floatingIpModel = { name: '' };
+  vm.formId = floatingIpFormId;
+  vm.afterGet = false;
+  vm.modelCopy = angular.copy( vm.floatingIpModel );
+  vm.model = "floatingIpModel";
 
   ManageIQ.angular.scope = vm;
 
   if (floatingIpFormId == 'new') {
-      vm.floatingIpModel.name = "";
-      vm.floatingIpModel.description = "";
-      vm.newRecord = true;
+    vm.floatingIpModel.name = "";
+    vm.floatingIpModel.description = "";
+    vm.newRecord = true;
   } else {
     miqService.sparkleOn();
 
@@ -20,51 +20,51 @@ ManageIQ.angular.app.controller('floatingIpFormController', ['$http', '$scope', 
       .catch(miqService.handleFailure);
   }
 
-    $scope.addClicked = function() {
-    var url = 'create/new' + '?button=add';
-    miqService.miqAjaxButton(url, vm.floatingIpModel, { complete: false });
-  };
+  $scope.addClicked = function() {
+      var url = 'create/new' + '?button=add';
+      miqService.miqAjaxButton(url, vm.floatingIpModel, { complete: false });
+    };
 
-    $scope.cancelClicked = function() {
-    if (floatingIpFormId == 'new') {
+  $scope.cancelClicked = function() {
+      if (floatingIpFormId == 'new') {
       var url = '/floating_ip/create/new' + '?button=cancel';
     } else {
       var url = '/floating_ip/update/' + floatingIpFormId + '?button=cancel';
     }
-    miqService.miqAjaxButton(url);
+      miqService.miqAjaxButton(url);
   };
 
-    $scope.saveClicked = function() {
-    var url = '/floating_ip/update/' + floatingIpFormId + '?button=save';
-    miqService.miqAjaxButton(url, vm.floatingIpModel, { complete: false });
-  };
+  $scope.saveClicked = function() {
+      var url = '/floating_ip/update/' + floatingIpFormId + '?button=save';
+      miqService.miqAjaxButton(url, vm.floatingIpModel, { complete: false });
+    };
 
-    $scope.resetClicked = function() {
-        vm.floatingIpModel = angular.copy( vm.modelCopy );
-    $scope.angularForm.$setPristine(true);
-    miqService.miqFlash("warn", "All changes have been reset");
-  };
+  $scope.resetClicked = function() {
+      vm.floatingIpModel = angular.copy( vm.modelCopy );
+      $scope.angularForm.$setPristine(true);
+      miqService.miqFlash("warn", "All changes have been reset");
+    };
 
-    vm.filterNetworkManagerChanged = function(id) {
-    miqService.sparkleOn();
-    $http.get('/floating_ip/networks_by_ems/' + id)
+  vm.filterNetworkManagerChanged = function(id) {
+      miqService.sparkleOn();
+      $http.get('/floating_ip/networks_by_ems/' + id)
       .then(getNetworkByEmsFormData)
       .catch(miqService.handleFailure);
-  };
+    };
 
   function getFloatingIpFormData(response) {
     var data = response.data;
 
-      vm.afterGet = true;
-      vm.floatingIpModel.network_port_ems_ref = data.network_port_ems_ref;
-      vm.modelCopy = angular.copy( vm.floatingIpModel );
+    vm.afterGet = true;
+    vm.floatingIpModel.network_port_ems_ref = data.network_port_ems_ref;
+    vm.modelCopy = angular.copy( vm.floatingIpModel );
     miqService.sparkleOff();
   }
 
   function getNetworkByEmsFormData(response) {
     var data = response.data;
 
-      vm.available_networks = data.available_networks;
+    vm.available_networks = data.available_networks;
     miqService.sparkleOff();
   }
 }]);
--- spec/javascripts/controllers/floatingIpFormController_spec.js
+++ spec/javascripts/controllers/floatingIpFormController_spec.js
@@ -1,5 +1,66 @@
 /**
  * Created by swi2 on 31/03/2017.
  */
+describe('floatingIpFormController', function() {
+  var $http,$scope, $controller, floatingIpFormId,miqService,vm;
+
+  beforeEach(module('ManageIQ'));
+
+  beforeEach(inject(function($rootScope,$http, _$controller_, miqService, ) {
+    spyOn(miqService, 'miqAjaxButton');
+    spyOn(miqService, 'miqFlash');
+    spyOn(miqService, 'sparkleOn');
+    spyOn(miqService, 'sparkleOff');
+    $scope = $rootScope.$new();
+    $scope.vm = {};
+    $scope.vm.floatingIpModel = {
+      name:                         'floatingIpName',
+      description:                  'floatingIpDescription'
+    };
+
+    $controller = _$controller_('floatingIpFormController', {
+      $scope: $scope,
+      floatingIpFormId: 1000000000001
+    });
+
+  }));
+
+  describe('#saveClicked', function() {
+    beforeEach(function() {
+      setTimeout($scope.saveClicked);
+    });
+
+    it('delegates to saveRecord', function(done) {
+      setTimeout(function() {
+        expect(miqService.miqAjaxButton).toHaveBeenCalledWith(
+          '/floating_ip/update/1000000000001?button=save',
+          $scope.vm.floatingIpModel,
+          { complete: false }
+        );
+        done();
+      });
+    });
+  });
+
+  describe('#resetClicked', function() {
+    beforeEach(function() {
+      $scope.vm.floatingIpModel.name = 'floatingIpName';
+      $scope.angularForm = {
+        $setPristine: function (value){},
+        $setUntouched: function (value){},
+      };
+      setTimeout($scope.resetClicked);
+    });
+
+    it('warn, All changes have been reset', function(done) {
+      setTimeout(function() {
+        expect($scope.vm.floatingIpModel.name).toEqual('floatingIpName');
+        done();
+      });
+    });
+  });
 
 
+
+
+});
