baf89446d42a9d5632a8bb2cc173e372c44d8ec2
Pettan <panhammer@seznam.cz>
2017-05-05 09:00:49 +0200

    Fixed typos and used lazy loading

--- spec/controllers/ansible_repository_controller_spec.rb
+++ spec/controllers/ansible_repository_controller_spec.rb
@@ -2,34 +2,33 @@ describe AnsibleRepositoryController do
   include CompressedIds
 
   before do
-    @repository = FactoryGirl.create(:embedded_ansible_configuration_script_source)
     EvmSpecHelper.create_guid_miq_server_zone
     login_as FactoryGirl.create(:user_admin)
   end
 
   context "show" do
-    subject do
-      get :show, :params => {:id => @repository.id}
-    end
+    let(:repository) { FactoryGirl.create(:embedded_ansible_configuration_script_source) }
+    subject { get :show, :params => {:id => repository.id} }
     render_views
+
     it "return correct http response code" do
       is_expected.to have_http_status 200
     end
     it "render view for specific repostitory" do
-      is_expected.to render_template(:partial => "layouts/extual_groups_generic")
+      is_expected.to render_template(:partial => "layouts/_textual_groups_generic")
     end
   end
 
   context "showList" do
-    subject do
-      get :show_list, :params => {}
-    end
+    let(:repository) { FactoryGirl.create(:embedded_ansible_configuration_script_source) }
+    subject { get :show_list, :params => {} }
     render_views
+
     it "return correct http response code" do
       is_expected.to have_http_status 200
     end
     it "render view for list of repositories" do
-      is_expected.to render_template(:partial => "layouts/gtl")
+      is_expected.to render_template(:partial => "layouts/_gtl")
     end
   end
 end
