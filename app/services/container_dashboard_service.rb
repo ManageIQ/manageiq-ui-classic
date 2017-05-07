a43f9c6f55744b6e8e55cfefd36d1c1ba2a059ab
yaacov <yzamir@redhat.com>
2017-05-07 16:33:24 +0300

    protect from null

--- app/services/container_dashboard_service.rb
+++ app/services/container_dashboard_service.rb
@@ -98,8 +98,11 @@ class ContainerDashboardService
   end
 
   def alerts
+    errors = 0
+    warnings = 0
+
     relation = @ems ? @ems.miq_alert_statuses : MiqAlertStatus.where.not(:ems_id => nil)
-    warnings, errors = relation.group(:severity).count.values_at(*%w(warning error))
+    warnings, errors = relation.group(:severity).count.values_at(*%w(warning error)) if relation.present?
 
     errors_struct = errors > 0 ? {:iconClass => "pficon pficon-error-circle-o", :count => errors} : nil
     warnings_struct = warnings > 0 ? {:iconClass => "pficon pficon-warning-triangle-o", :count => warnings} : nil
