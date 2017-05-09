74e8450610dda2ea00f7a8d3fb2a715c22280b97
Dávid Halász <dhalasz@redhat.com>
2017-05-09 15:38:22 +0200

    Add some new tests for the TopologyService

--- spec/services/topology_service_spec.rb
+++ spec/services/topology_service_spec.rb
@@ -32,4 +32,100 @@ describe TopologyService do
       expect(subject.build_link(source, target)).to eq(:source => source, :target => target)
     end
   end
+
+  describe '#build_kinds' do
+    let(:kinds) { %i(Host Vm) }
+    it 'returns with hash where the keys are kinds' do
+      described_class.instance_variable_set(:@kinds, kinds)
+
+      expect(subject.build_kinds.keys).to eq(kinds)
+    end
+  end
+
+  describe '#entity_id' do
+    let(:host) { FactoryGirl.create(:host) }
+
+    it 'output begins with class name' do
+      expect(subject.entity_id(host)).to start_with(host.class.to_s)
+    end
+
+    it 'output ends with compressed id' do
+      expect(subject.entity_id(host)).to end_with(host.compressed_id)
+    end
+  end
+
+  describe '#build_topology' do
+    let(:providers) { subject.instance_variable_set(:@providers, []) }
+
+    it 'output is rendered in the expected format' do
+      allow(providers).to receive(:includes).and_return([])
+      described_class.instance_variable_set(:@kinds, [])
+
+      topology = subject.build_topology
+
+      expect(topology).to have_key(:items)
+      expect(topology).to have_key(:relations)
+      expect(topology).to have_key(:kinds)
+      expect(topology).to have_key(:icons)
+    end
+  end
+
+  describe '#map_to_graph' do
+    let(:provider) { FactoryGirl.create(:ext_management_system) }
+    let(:tag) { FactoryGirl.create(:tag) }
+    let(:vm) { FactoryGirl.create(:vm, :ext_management_system => provider, :tags => [tag]) }
+    let(:graph) { {:vms => {:tags => nil}} }
+
+    before do
+      allow(subject).to receive(:build_entity_data) { |input| input }
+    end
+
+    it 'calls the graph items as methods recursively' do
+      expect(provider).to receive(:vms).and_return([vm])
+      expect(vm).to receive(:tags).and_return([tag])
+      subject.map_to_graph([provider], graph)
+    end
+
+    it 'returns with the list of objects and the links between' do
+      allow(provider).to receive(:vms).and_return([vm])
+      allow(vm).to receive(:tags).and_return([tag])
+      output = subject.map_to_graph([provider], graph)
+
+      expect(output.first.values).to eq([provider, vm, tag])
+      expect(output.last.length).to eq(2)
+    end
+  end
+
+  describe '#build_entity_relationships' do
+    let(:output) { subject.build_entity_relationships(input) }
+
+    context 'input is a symbol' do
+      let(:input) { :sym }
+      it 'returns with Input => nil' do
+        expect(output).to have_key(input.capitalize)
+        expect(output[input.capitalize]).to be_nil
+      end
+    end
+
+    context 'input is an array of symbols' do
+      let(:input) { [:s1, :s2] }
+
+      it 'array values are matching capitalized hash keys with nil as values' do
+        expect(output.keys).to eq(input.map(&:capitalize))
+        expect(output.values.compact).to eq([])
+      end
+    end
+
+    context 'input is a hash with symbols' do
+      let(:input) { {:s1 => :v1, :s2 => :v2} }
+
+      it 'returns with hash with capitalized keys from input' do
+        expect(output.keys).to eq(input.keys.map(&:capitalize))
+      end
+
+      it 'recursively calls itself on the input hash values' do
+        expect(output.values.reduce({}, :merge).keys).to eq(input.values.map(&:capitalize))
+      end
+    end
+  end
 end
