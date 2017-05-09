7c9b34ab06b4c0e7ec9df7d2147bd8cafe26cc62
Martin Hradil <himdel@seznam.cz>
2017-05-09 17:25:29 +0000

    Automation manager form - fix whitespace and sparkle
    Sparkleoff would get called right after the request, making the whole sparkle thing useless.

--- app/assets/javascripts/controllers/automation_manager/automation_manager_form_controller.js
+++ app/assets/javascripts/controllers/automation_manager/automation_manager_form_controller.js
@@ -1,5 +1,6 @@
 ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$scope', 'automationManagerFormId', 'miqService', function($http, $scope, automationManagerFormId, miqService) {
   var vm = this;
+  
   vm.automationManagerModel = {
     name: '',
     url: '',
@@ -18,8 +19,10 @@ ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$s
 
   ManageIQ.angular.scope = vm;
 
+  miqService.sparkleOn();
   if (automationManagerFormId === 'new') {
-    vm.newRecord  = true;
+    vm.newRecord = true;
+
     vm.automationManagerModel.name = '';
     vm.automationManagerModel.url = '';
     vm.automationManagerModel.verify_ssl = false;
@@ -32,39 +35,44 @@ ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$s
       .catch(miqService.handleFailure);
   } else {
     vm.newRecord = false;
-    miqService.sparkleOn();
+
     $http.get('/automation_manager/form_fields/' + automationManagerFormId)
       .then(getAutomationManagerFormDataComplete)
       .catch(miqService.handleFailure);
-    miqService.sparkleOff();
   }
 
   function getAutomationManagerNewFormDataComplete(response) {
     var data = response.data;
-    vm.afterGet = true;
+    
     vm.automationManagerModel.zone = data.zone;
-    vm.modelCopy  = angular.copy(vm.automationManagerModel);
+
+    vm.modelCopy = angular.copy(vm.automationManagerModel);
+    vm.afterGet = true;
+    miqService.sparkleOff();
   }
 
   function getAutomationManagerFormDataComplete(response) {
     var data = response.data;
-    vm.afterGet = true;
 
     vm.automationManagerModel.name = data.name;
     vm.automationManagerModel.zone = data.zone;
     vm.automationManagerModel.url = data.url;
     vm.automationManagerModel.verify_ssl = data.verify_ssl == "1";
-
     vm.automationManagerModel.log_userid = data.log_userid;
 
     if (vm.automationManagerModel.log_userid != '') {
       vm.automationManagerModel.log_password = vm.automationManagerModel.log_verify = miqService.storedPasswordPlaceholder;
     }
+
     vm.modelCopy = angular.copy(vm.automationManagerModel);
+    vm.afterGet = true;
+    miqService.sparkleOff();
   }
+
   vm.canValidateBasicInfo = function() {
     return vm.isBasicInfoValid();
   };
+
   vm.isBasicInfoValid = function() {
     return $scope.angularForm.url.$valid &&
       $scope.angularForm.log_userid.$valid &&
@@ -74,6 +82,7 @@ ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$s
 
   var automationManagerEditButtonClicked = function(buttonName, serializeFields) {
     miqService.sparkleOn();
+
     var url = '/automation_manager/edit/' + automationManagerFormId + '?button=' + buttonName;
     if (serializeFields === undefined) {
       miqService.miqAjaxButton(url);
@@ -81,10 +90,12 @@ ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$s
       miqService.miqAjaxButton(url, serializeFields);
     }
   };
+
   vm.cancelClicked = function() {
     automationManagerEditButtonClicked('cancel');
     $scope.angularForm.$setPristine(true);
   };
+
   vm.resetClicked = function() {
     $scope.$broadcast('resetClicked');
     vm.automationManagerModel = angular.copy(vm.modelCopy);
@@ -96,6 +107,7 @@ ManageIQ.angular.app.controller('automationManagerFormController', ['$http', '$s
     automationManagerEditButtonClicked('save', true);
     $scope.angularForm.$setPristine(true);
   };
+
   vm.addClicked = function() {
     vm.saveClicked();
   };
