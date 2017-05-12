dccc3afcb7786933106fbd924d8095e13623aca8
Harpreet Kataria <hkataria@redhat.com>
2017-05-12 09:08:00 -0400

    Merge pull request #1338 from ZitaNemeckova/bz1445683
    Only Host under Cluster have no checkbox
    (cherry picked from commit c89220faeddbf55044af993875a1ca8e7a8d8c22)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1460265

--- app/presenters/tree_builder_belongs_to_hac.rb
+++ app/presenters/tree_builder_belongs_to_hac.rb
@@ -9,7 +9,7 @@ class TreeBuilderBelongsToHac < TreeBuilder
     if [ExtManagementSystem, EmsCluster, Datacenter, EmsFolder, ResourcePool].any? { |klass| object.kind_of?(klass) }
       node[:select] = options.key?(:selected) && options[:selected].include?("#{object.class.name}_#{object[:id]}")
     end
-    node[:hideCheckbox] = true if object.kind_of?(Host)
+    node[:hideCheckbox] = true if object.kind_of?(Host) && object.ems_cluster_id.present?
     node[:cfmeNoClick] = true
     node[:checkable] = options[:checkable_checkboxes] if options.key?(:checkable_checkboxes)
   end
