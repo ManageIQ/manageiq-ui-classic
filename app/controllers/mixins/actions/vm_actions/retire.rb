e4426d5f4f4593a3754cfe39952186fe55c3228d
Martin Hradil <mhradil@redhat.com>
2017-05-17 14:56:56 +0000

    Remove retire_date and retire_warn from session
    never read :)

--- app/controllers/mixins/actions/vm_actions/retire.rb
+++ app/controllers/mixins/actions/vm_actions/retire.rb
@@ -97,12 +97,6 @@ module Mixins
           build_targets_hash(@retireitems)
           @view = get_db_view(kls)              # Instantiate the MIQ Report view object
           @view.table = MiqFilter.records2table(@retireitems, @view.cols + ['id'])
-          if @retireitems.length == 1 && !@retireitems[0].retires_on.nil?
-            t = @retireitems[0].retires_on                                         # Single VM, set to current time
-            w = @retireitems[0].retirement_warn if @retireitems[0].retirement_warn # Single VM, get retirement warn
-          end
-          session[:retire_date] = "#{t.month}/#{t.day}/#{t.year}" unless t.nil?
-          session[:retire_warn] = w
           @in_a_form = true
           @edit ||= {}
           @edit[:object_ids] = @retireitems
