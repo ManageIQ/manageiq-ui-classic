a1c09b2f3e28be16719e583d1ecc7b7304be3bf5
PanSpagetka <rknaur@redhat.com>
2017-05-19 15:34:04 +0200

    Add GenericShowMixin to CloudObjectStoreObject

--- app/controllers/cloud_object_store_object_controller.rb
+++ app/controllers/cloud_object_store_object_controller.rb
@@ -7,6 +7,7 @@ class CloudObjectStoreObjectController < ApplicationController
   include Mixins::GenericListMixin
   include Mixins::GenericSessionMixin
   include Mixins::GenericFormMixin
+  include Mixins::GenericShowMixin
 
   def breadcrumb_name(_model)
     ui_lookup(:tables => "cloud_object_store_object")
@@ -19,35 +20,6 @@ class CloudObjectStoreObjectController < ApplicationController
     process_cloud_object_storage_buttons(params[:pressed])
 
     delete_action if params[:pressed].ends_with?("delete")
-  end
-
-  def show
-    @display = params[:display] || "main" unless pagination_or_gtl_request?
-    @showtype = @display
-    @lastaction = "show"
-    @object_store_object = @record = identify_record(params[:id])
-    return if record_no_longer_exists?(@object_store_object)
-
-    @gtl_url = "/show"
-    drop_breadcrumb(
-      {
-        :name => ui_lookup(:tables => "cloud_objects"),
-        :url  => "/cloud_object_store_object/show_list?page=#{@current_page}&refresh=y"
-      },
-      true
-    )
-    case @display
-    when "main", "summary_only"
-      get_tagdata(@object_store_object)
-      drop_breadcrumb(
-        :name => _("%{name} (Summary)") % {:name => @object_store_object.key.to_s},
-        :url  => "/cloud_object_store_object/show/#{@object_store_object.id}"
-      )
-      @showtype = "main"
-      set_summary_pdf_data if @display == "summary_only"
-    end
-
-    replace_gtl_main_div if pagination_request?
   end
 
   def title
