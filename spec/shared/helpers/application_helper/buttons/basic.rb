a74c094f6a634f3588691b2de90db92efb4caa97
Attila Večerek <veat24@gmail.com>
2017-05-23 18:42:19 +0200

    Move button creation into the subject block as there are no more references to button

--- spec/shared/helpers/application_helper/buttons/basic.rb
+++ spec/shared/helpers/application_helper/buttons/basic.rb
@@ -1,7 +1,6 @@
 shared_context 'ApplicationHelper::Button::Basic' do
   let(:view_context) { setup_view_context_with_sandbox(sandbox) }
-  let(:button) { described_class.new(view_context, {}, instance_data, props) }
-  subject { button }
+  subject { described_class.new(view_context, {}, instance_data, props) }
 end
 
 shared_examples 'ApplicationHelper::Button::Basic visible' do
