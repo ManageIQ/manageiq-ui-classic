6225a3dc932c6193e70afd84d79a901c978d6093
lpichler <lpichler@redhat.com>
2017-05-25 14:49:30 +0200

    Let AR model generate the query

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,10 +2,6 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    ExtManagementSystem.where(:type => "ManageIQ::Providers::Openstack::CloudManager").collect { |ems|
-      if ems.respond_to?(:network_manager) && ems.network_manager
-        ems.network_manager
-      end
-    }.compact
+    ManageIQ::Providers::Openstack::NetworkManager.all
   end
 end
