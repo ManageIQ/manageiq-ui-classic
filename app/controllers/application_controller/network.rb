4986c5ce38f971ebd9f9c436211008b3025e6500
lpichler <lpichler@redhat.com>
2017-05-25 14:50:54 +0200

    Don’t pass whole piano when you want play just few tones

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,6 +2,6 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).all
+    Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name)
   end
 end
