e3c9c3bde9b7699354f5ac208ab2a8143539af00
Martin Povolny <mpovolny@redhat.com>
2017-05-25 10:50:06 +0200

    Merge pull request #1428 from h-kataria/suspend_role_button_fix
    Fixed condition in disabled? method
    (cherry picked from commit 89fa2d4217859863362fbc8bfc9aa2d938980adb)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1460366

--- spec/helpers/application_helper/buttons/role_suspend_spec.rb
+++ spec/helpers/application_helper/buttons/role_suspend_spec.rb
@@ -78,6 +78,17 @@ describe ApplicationHelper::Button::RoleSuspend do
         button.calculate_properties
         expect(button[:title]).to eq("Only active Roles on active Servers can be suspended")
       end
+
+      # enable button when record is active and max concurrent is not 1
+      it "enables the suspend role button " do
+        allow(@record).to receive(:active).and_return(true)
+        allow(@record.server_role).to receive(:max_concurrent).and_return(2)
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        allow(view_context).to receive(:x_node).and_return('z-1r23')
+        expect(button.disabled?).to be_falsey
+        button.calculate_properties
+      end
     end
   end
 end
