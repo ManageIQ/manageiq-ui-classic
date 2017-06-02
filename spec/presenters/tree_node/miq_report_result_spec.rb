622b95aa6a370c03e2ef8e927eeaf2556190859f
lpichler <lpichler@redhat.com>
2017-06-02 17:11:17 +0200

    Fix spec to use correct status of miq_task
    Node icon have not to be error icon,
    we need to add report to miq_report_result,
    for now I am adding just empty report.

--- spec/presenters/tree_node/miq_report_result_spec.rb
+++ spec/presenters/tree_node/miq_report_result_spec.rb
@@ -2,7 +2,8 @@ require 'shared/presenters/tree_node/common'
 
 describe TreeNode::MiqReportResult do
   subject { described_class.new(object, nil, {}) }
-  let(:object) { FactoryGirl.create(:miq_report_result) }
+
+  let(:object) { FactoryGirl.create(:miq_report_result, :report => {}) }
 
   include_examples 'TreeNode::Node#key prefix', 'rr-'
   include_examples 'TreeNode::Node#icon', 'fa fa-arrow-right'
