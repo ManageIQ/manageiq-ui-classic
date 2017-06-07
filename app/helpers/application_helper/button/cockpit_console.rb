b31810b495c345b153964ddba8022b812582c167
Brian McLaughlin <bmclaugh@redhat.com>
2017-06-06 22:35:13 -0400

    Refactoring based on feedback
    https://bugzilla.redhat.com/show_bug.cgi?id=1447100

--- app/helpers/application_helper/button/cockpit_console.rb
+++ app/helpers/application_helper/button/cockpit_console.rb
@@ -4,7 +4,7 @@ class ApplicationHelper::Button::CockpitConsole < ApplicationHelper::Button::Bas
   def disabled?
     record_type = @record.respond_to?(:current_state) ? _('VM') : _('Container Node')
     @error_message = _("The web-based console is not available because the %{record_type} is not powered on" % {:record_type => record_type}) unless on?
-    @error_message = _('The web-based console is not available because the Windows platform is not supported') unless platform_supported? && @record.respond_to?(:current_state)
+    @error_message = _('The web-based console is not available because the Windows platform is not supported') unless platform_supported?
     @error_message.present?
   end
 
@@ -16,6 +16,6 @@ class ApplicationHelper::Button::CockpitConsole < ApplicationHelper::Button::Bas
   end
 
   def platform_supported?
-    @record.platform.downcase != 'windows' if @record.respond_to?(:current_state)
+    @record.respond_to?(:current_state) && @record.platform.downcase != 'windows'
   end
 end
