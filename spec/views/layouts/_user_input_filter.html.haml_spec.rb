cc96efdc1747f248ecae2cd4cc1219f70e9ba3a7
lgalis <lgalis@redhat.com>
2017-06-28 10:27:16 -0400

    Spec for user input boolean dropdown

---
+++ spec/views/layouts/_user_input_filter.html.haml_spec.rb
@@ -0,0 +1,14 @@
+describe "layouts/_user_input_filter.html.haml" do
+  context 'for boolean user input field' do
+    before do
+      @edit = {:qs_tokens => { 1 => {:value => nil, :field => 'ManageIQ::Providers::InfraManager::Template-active', :value_type => :boolean}}}
+      @qs_exp_table = ["Template:Active =", [:user_input, 1]]
+    end
+
+    it 'shows a dropdown field' do
+      set_controller_for_view("host")
+      render
+      expect(rendered).to include("<option value=\"false\">False</option>\n<option value=\"true\">True</option></select>")
+    end
+  end
+end
