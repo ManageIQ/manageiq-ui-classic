ed71400db51590cb7d7ae5c0bbb89ce23fbfdc4b
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-17 17:26:35 +0200

    added some mandatory properties to components

--- app/assets/javascripts/components/verify_button.js
+++ app/assets/javascripts/components/verify_button.js
@@ -1,10 +1,12 @@
 ManageIQ.angular.app.component('verifyButton', {
   bindings: {
-
+    validate: '&',
+    enabled: '<',
   },
   controllerAs: 'vm',
   controller: ['$scope', function($scope) {
     $scope.__ = __;
+    var vm = this;
   }],
   templateUrl: '/static/verify_button.html.haml'
 });
