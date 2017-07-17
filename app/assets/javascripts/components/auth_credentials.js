7aa10c6bd9d095e1b6e1e5bfe3c456a97ac7193d
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-17 15:03:52 +0200

    basic required component function

--- app/assets/javascripts/components/auth_credentials.js
+++ app/assets/javascripts/components/auth_credentials.js
@@ -1,11 +1,45 @@
 ManageIQ.angular.app.component('authCredentials', {
   bindings: {
-    model: '=',
-    options: '<',
-    type: '<',
     newRecord: '<',
-    reset: '=',
-    deleteFromModel: '=',
-    storedPasswordPlaceholder: '<',
+    modelCopy: '<',
+    /**
+    * setuserId should be in diagnostic databse form controller
+    */
+    setUserId: '&'
   },
-}
+  controllerAs: 'vm',
+  controller: ['$scope', function($scope) {
+    $scope.__ = __;
+
+    var vm = this;
+
+    this.$onInit = function() {
+      this.bChangeStoredPassword = this.newRecord;
+      this.bCancelPasswordChange = this.newRecord;
+    };
+
+    this.changeStoredPassword = function() {
+      vm.bChangeStoredPassword = true;
+      vm.bCancelPasswordChange = false;
+    };
+    this.cancelPasswordChange = function() {
+      if (vm.bChangeStoredPassword) {
+        vm.bCancelPasswordChange = true;
+        vm.bChangeStoredPassword = false;
+      }
+    };
+
+    this.showChangePasswordLinks = function(index) {
+      return ! vm.newRecord && vm.modelCopy[index] !== '';
+    };
+
+    this.showVerify = function(userid) {
+      return vm.newRecord || (! vm.showChangePasswordLinks(userid)) || vm.bChangeStoredPassword;
+    };
+    $scope.$on('resetClicked', function(_e) {
+      vm.cancelPasswordChange();
+    });
+  }],
+  templateUrl: '/static/auth_credentials.html.haml',
+
+});
