8c78efa735b2f71e8ecb7942aa49cb32c98ee51a
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-17 16:50:02 +0200

    created verify button component and template

---
+++ app/assets/javascripts/components/verify_button.js
@@ -0,0 +1,10 @@
+ManageIQ.angular.app.component('verifyButton', {
+  bindings: {
+
+  },
+  controllerAs: 'vm',
+  controller: ['$scope', function($scope) {
+    $scope.__ = __;
+  }],
+  templateUrl: '/static/verify_button.html.haml'
+});
