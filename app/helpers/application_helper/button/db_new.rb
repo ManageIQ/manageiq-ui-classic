ce174afc947bb59b5f459d6602258cde86e971b8
Adrian Toth <adtoth@redhat.com>
2017-07-18 15:45:46 +0200

    Definition of constant MAX_DASHBOARD_COUNT has been added to class DbNew

--- app/helpers/application_helper/button/db_new.rb
+++ app/helpers/application_helper/button/db_new.rb
@@ -1,4 +1,7 @@
 class ApplicationHelper::Button::DbNew < ApplicationHelper::Button::ButtonNewDiscover
+
+  MAX_DASHBOARD_COUNT = 10
+
   def disabled?
     if @widgetsets.length >= MAX_DASHBOARD_COUNT
       @error_message = _('Only %{dashboard_count} Dashboards are allowed for a group') %
