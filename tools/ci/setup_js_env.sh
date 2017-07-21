0771c061f03b5d0f6db41402e0b7ba18552c1524
Martin Hradil <mhradil@redhat.com>
2017-07-21 11:38:03 +0000

    travis - install yarn if check_yarn fails

--- tools/ci/setup_js_env.sh
+++ tools/ci/setup_js_env.sh
@@ -19,3 +19,6 @@ else
   [ $STATUS = 0 ] || exit 1
   [ -d vendor/assets/bower_components ] || exit 1
 fi
+
+# make sure yarn is installed, in the right version
+bundle exec rake webpacker:check_yarn || npm install -g yarn
