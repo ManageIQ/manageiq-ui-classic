fef0ef36331ffb2a53e38ad4069c44619c82ba40
Josejulio Martínez <jmartine@redhat.com>
2017-07-25 12:52:58 -0500

    HAWKULAR-1164 - Stop an EAP domain  Allows middleware domains to be stopped.

--- app/helpers/application_helper/toolbar/middleware_domain_center.rb
+++ app/helpers/application_helper/toolbar/middleware_domain_center.rb
@@ -16,4 +16,22 @@ class ApplicationHelper::Toolbar::MiddlewareDomainCenter < ApplicationHelper::To
                    ]
                  ),
                ])
+  button_group('middleware_domain_operations', [
+                 select(
+                   :middleware_server_power_choice,
+                   'fa fa-power-off fa-lg',
+                   t = N_('Power'),
+                   t,
+                   :items => [
+                     button(
+                       :middleware_domain_stop,
+                       nil,
+                       N_('Stop this Domain'),
+                       N_('Stop Domain'),
+                       :image   => 'power_off',
+                       :confirm => N_('Do you want to stop this domain?')
+                     )
+                   ]
+                 ),
+               ])
 end
