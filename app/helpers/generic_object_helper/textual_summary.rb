f5dc66f022d3b699c55a64d152d1a3058ce2dc1b
Aparna Karve <akarve@redhat.com>
2017-07-26 16:33:46 -0700

    Use TextualMultilabel in lieu of TextualListview
    for better table formatting

--- app/helpers/generic_object_helper/textual_summary.rb
+++ app/helpers/generic_object_helper/textual_summary.rb
@@ -10,27 +10,17 @@ module GenericObjectHelper::TextualSummary
   end
 
   def textual_group_attribute_details_list
-    TextualListview.new_from_hash(textual_attribute_details_list)
-  end
-
-  def textual_attribute_details_list
-    {
-      :title     => _("Attributes (#{@record.property_attributes.count})"),
-      :headers   => [_("Name"), _("Value")],
-      :col_order => %w(name value),
-      :value     => attributes_array
-    }
+    TextualMultilabel.new(
+      _("Attributes (#{@record.property_attributes.count})"),
+      :additional_table_class => "table-fixed",
+      :labels                 => [_("Name"), _("Value")],
+      :values                 => attributes_array
+    )
   end
 
   def attributes_array
-    attributes = []
-    @record.property_attributes.each do |row|
-      attributes.push({
-        :title    => row[0],
-        :name     => row[0],
-        :value    => row[1],
-      })
+    @record.property_attributes.each do |var|
+      [var[0], var[1]]
     end
-    attributes
   end
 end
