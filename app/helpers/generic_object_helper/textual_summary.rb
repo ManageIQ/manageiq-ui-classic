b8f85119f8857c46bddf2530b79fa88b5ced5ff4
Aparna Karve <akarve@redhat.com>
2017-07-26 16:38:21 -0700

    Add Created and Updated to Summary screen

--- app/helpers/generic_object_helper/textual_summary.rb
+++ app/helpers/generic_object_helper/textual_summary.rb
@@ -2,11 +2,19 @@ module GenericObjectHelper::TextualSummary
   include TextualMixins::TextualName
 
   def textual_group_properties
-    TextualGroup.new(_("Properties"), %i(name))
+    TextualGroup.new(_("Properties"), %i(name created updated))
   end
 
   def textual_name
     {:label => _("Name"), :value => @record.name}
+  end
+
+  def textual_created
+    {:label => _("Created"), :value => format_timezone(@record.created_at)}
+  end
+
+  def textual_updated
+    {:label => _("Updated"), :value => format_timezone(@record.updated_at)}
   end
 
   def textual_group_attribute_details_list
