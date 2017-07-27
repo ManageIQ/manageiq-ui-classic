79f1349e325a57ac154e1e4f31fff4638616cc55
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-27 12:01:09 +0200

    added validation check to authCredentialsController and showing in ems for testing purposes

--- app/assets/javascripts/components/auth_credentials.js
+++ app/assets/javascripts/components/auth_credentials.js
@@ -24,6 +24,8 @@ ManageIQ.angular.app.component('authCredentials', {
     * setuserId should be in diagnostic databse form controller
     */
     setUserId: '&',
+    checkAuthentication: '<',
+    authStatus: '<',
   },
   controllerAs: 'vm',
   controller: ['$scope', function($scope) {
@@ -34,15 +36,20 @@ ManageIQ.angular.app.component('authCredentials', {
     this.$onInit = function() {
       this.bChangeStoredPassword = this.newRecord;
       this.bCancelPasswordChange = this.newRecord;
+      console.log('auth model: ', vm.model);
     };
 
     this.changeStoredPassword = function() {
       vm.bChangeStoredPassword = true;
+      vm.model[vm.prefix + '_password'] = '';
       vm.bCancelPasswordChange = false;
     };
     this.cancelPasswordChange = function() {
       if (vm.bChangeStoredPassword) {
         vm.bCancelPasswordChange = true;
+        Object.assign(vm.model, {
+          [vm.prefix + '_password']: vm.modelCopy[vm.prefix + '_password'],
+        });
         vm.bChangeStoredPassword = false;
       }
     };
