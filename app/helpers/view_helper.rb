fa2af5cc10f27cdbc1df29931526e942a59a26cb
Adrian Toth <adtoth@redhat.com>
2017-07-27 10:58:41 +0200

    constant PDF_PAGE_SIZES was added to ViewHelper

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -9,6 +9,23 @@ module ViewHelper
   MAX_DESC_LEN = 255
   MAX_HOSTNAME_LEN = 255
 
+  # PDF page sizes
+  PDF_PAGE_SIZES = {
+    "a0"            => N_("A0 - 841mm x 1189mm"),
+    "a1"            => N_("A1 - 594mm x 841mm"),
+    "a2"            => N_("A2 - 420mm x 594mm"),
+    "a3"            => N_("A3 - 297mm x 420mm"),
+    "a4"            => N_("A4 - 210mm x 297mm (default)"),
+    "US-Letter"     => N_("US Letter - 8.5in x 11.0in"),
+    "US-Legal"      => N_("US Legal - 8.5in x 14.0in"),
+    "US-Executive"  => N_("US Executive - 7.25in x 10.5in"),
+    "US-Ledger"     => N_("US Ledger - 17.0in x 11.0in"),
+    "US-Tabloid"    => N_("US Tabloid - 11.0in x 17.0in"),
+    "US-Government" => N_("US Government - 8.0in x 11.0in"),
+    "US-Statement"  => N_("US Statement - 5.5in x 8.5in"),
+    "US-Folio"      => N_("US Folio - 8.5in x 13.0in")
+  }
+
   class << self
     def concat_tag(*args, &block)
       concat content_tag(*args, &block)
