a67159366fd91e5be98d139ad3128c3a92889d05
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-27 09:05:52 +0200

    added static validation button hints

--- app/assets/javascripts/components/auth_credentials.js
+++ app/assets/javascripts/components/auth_credentials.js
@@ -11,6 +11,7 @@ ManageIQ.angular.app.component('authCredentials', {
     validate: '<',
     validateUrl: '@',
     restful: '<',
+    verifyTitleOff: '@',
     /**
     * is used in vrifyButton component for restfull validation, may be obsolete
     */
--- app/assets/javascripts/components/verify_button.js
+++ app/assets/javascripts/components/verify_button.js
@@ -5,11 +5,23 @@ ManageIQ.angular.app.component('verifyButton', {
     validateUrl: '<',
     restful: '<',
     valtype: '<',
+    verifyTitleOff: '@',
+    verifyTitleOn: '@',
   },
   controllerAs: 'vm',
   controller: ['$scope', function($scope) {
     $scope.__ = __;
     var vm = this;
+
+    this.$onInit = function() {
+      if (! vm.verifyTitleOff) {
+        vm.verifyTitleOff = 'test';
+        console.log('test button');
+      }
+      if (! vm.verifyTitleOn) {
+        vm.verifyTitleOn = 'test on';
+      }
+    };
     vm.chooseValidation = function() {
       if (vm.restful) {
         vm.validate({target: '.validate_button:visible'}, vm.valtype, true);
