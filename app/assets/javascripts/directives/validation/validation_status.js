a0f8e89fcc20cfdabe569c166a9c2d4191c7ada5
Martin Maroši <marvusm.mmi@gmail.com>
2017-07-31 13:06:27 +0200

    uncomment function call

--- app/assets/javascripts/directives/validation/validation_status.js
+++ app/assets/javascripts/directives/validation/validation_status.js
@@ -1,16 +1,15 @@
 ManageIQ.angular.app.directive('validationStatus', ['$rootScope', function($rootScope) {
   return {
     require: 'ngModel',
-    link: function (scope, elem, attrs, ctrl) {
-      ctrl.$validators.validationRequired = function (modelValue, viewValue) {
+    link: function(scope, elem, attrs, ctrl) {
+      ctrl.$validators.validationRequired = function(modelValue, viewValue) {
         if (viewValue !== undefined && viewValue === true) {
-          //scope.postValidationModelRegistry(attrs.prefix);
+          scope.postValidationModelRegistry(attrs.prefix);
           return true;
-        } else {
-          $rootScope.$broadcast('setErrorOnTab', {tab: attrs.prefix});
-          return false;
         }
+        $rootScope.$broadcast('setErrorOnTab', {tab: attrs.prefix});
+        return false;
       };
-    }
-  }
+    },
+  };
 }]);
