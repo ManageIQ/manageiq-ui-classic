93feec31899944484e52fd4621c1420664af4c28
Aparna Karve <akarve@redhat.com>
2017-08-01 14:15:00 -0700

    Add more details in Generic Object Class Summary screen
    Attributes, Associations and Methods -- attributes that define
    the Generic Object Class

--- app/helpers/generic_object_definition_helper/textual_summary.rb
+++ app/helpers/generic_object_definition_helper/textual_summary.rb
@@ -1,6 +1,22 @@
 module GenericObjectDefinitionHelper::TextualSummary
   include TextualMixins::TextualName
 
+  def textual_group_properties
+    TextualGroup.new(_("Properties"), %i(name created updated))
+  end
+
+  def textual_name
+    {:label => _("Name"), :value => @record.name}
+  end
+
+  def textual_created
+    {:label => _("Created"), :value => format_timezone(@record.created_at)}
+  end
+
+  def textual_updated
+    {:label => _("Updated"), :value => format_timezone(@record.updated_at)}
+  end
+
   def textual_group_relationships
     TextualGroup.new(_("Relationships"), %i(generic_objects))
   end
@@ -13,5 +29,44 @@ module GenericObjectDefinitionHelper::TextualSummary
                :title => _('Show all Instances'))
     end
     h
+  end
+
+  def textual_group_attribute_details_list
+    TextualMultilabel.new(
+      _("Attributes (#{@record.property_attributes.count})"),
+      :additional_table_class => "table-fixed",
+      :labels                 => [_("Name"), _("Type")],
+      :values                 => record_properties(:property_attributes)
+    )
+  end
+
+  def textual_group_association_details_list
+    TextualMultilabel.new(
+      _("Associations (#{@record.property_associations.count})"),
+      :additional_table_class => "table-fixed",
+      :labels                 => [_("Name"), _("Class")],
+      :values                 => record_properties(:property_associations)
+    )
+  end
+
+  def textual_group_method_details_list
+    TextualMultilabel.new(
+      _("Methods (#{@record.property_methods.count})"),
+      :additional_table_class => "table-fixed",
+      :labels                 => [_("Name")],
+      :values                 => methods_array
+    )
+  end
+
+  def record_properties(property)
+    @record.send(property).each do |var|
+      [var[0], var[1]]
+    end
+  end
+
+  def methods_array
+    @record.property_methods.collect do |var|
+      [var]
+    end
   end
 end
