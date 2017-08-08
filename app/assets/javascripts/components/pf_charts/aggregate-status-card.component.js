e9b3ee84a9086c1144c5d18b970234fcf47f0db8
Harpreet Kataria <hkataria@redhat.com>
2017-08-08 09:11:07 -0400

    Added Angularized PF aggregate charts that consume API endpoints.
    https://www.pivotaltracker.com/story/show/147962059

---
+++ app/assets/javascripts/components/pf_charts/aggregate-status-card.component.js
@@ -0,0 +1,18 @@
+angular.module( 'patternfly.card' ).component('pfAggregateStatusCard', {
+  bindings: {
+    status: '=',
+    showTopBorder: '@?',
+    altLayout: '@?',
+    layout: '@?'
+  },
+  templateUrl: '/static/pf_charts/aggregate-status-card.html.haml',
+  controller: function () {
+    'use strict';
+    var vm = this;
+    vm.$onInit = function () {
+      vm.shouldShowTopBorder = (vm.showTopBorder === 'true');
+      vm.isAltLayout = (vm.altLayout === 'true' || vm.layout === 'tall');
+      vm.isMiniLayout = (vm.layout === 'mini');
+    };
+  }
+});
