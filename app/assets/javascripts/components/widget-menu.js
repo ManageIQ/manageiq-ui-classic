c36f53a65809d6b7373e676183529ad577443ab8
Zita Nemeckova <znemecko@redhat.com>
2017-08-09 14:21:27 +0200

    Fix HTML
    Add missing spaces to make HTML readible

--- app/assets/javascripts/components/widget-menu.js
+++ app/assets/javascripts/components/widget-menu.js
@@ -6,9 +6,11 @@ ManageIQ.angular.app.component('widgetMenu', {
   controller: ['$http', 'miqService', function($http, miqService) {
     var vm = this;
     vm.widgetMenuModel = {shortcuts: []};
+
     vm.shortcutsMissing = function() {
       return vm.widgetMenuModel.shortcuts.length === 0;
     };
+
     this.$onInit = function() {
       $http.get('/dashboard/widget_menu_data/' + vm.id)
         .then(function(response) { vm.widgetMenuModel = response.data; })
@@ -17,21 +19,21 @@ ManageIQ.angular.app.component('widgetMenu', {
     };
   }],
   template: [
-    '<div class="mc" id={{vm.div_id}} ng-style="{ \'display\' : (vm.widgetMenuModel.minimized === true) ? \'none\' : \'\' }">',
-    '<table class="table table-hover">',
-    '<tbody>',
-    '<div ng-if="vm.shortcutsMissing()">',
+    '<div class="mc" id="{{vm.div_id}}" ng-class="{ hidden: vm.widgetMenuModel.minimized }">',
+    '  <table class="table table-hover">',
+    '    <tbody>',
+    '      <div ng-if="vm.shortcutsMissing()">',
     __('No shortcuts are authorized for this user, contact your Administrator'),
-    '</div>',
-    '<tr ng-if="!vm.shortcutsMissing()" ng-repeat="shortcut in vm.widgetMenuModel.shortcuts">',
-    '<td>',
-    '<a title="' + __("Click to go this location") + '" href="{{shortcut.href}}">',
+    '      </div>',
+    '      <tr ng-if="!vm.shortcutsMissing()" ng-repeat="shortcut in vm.widgetMenuModel.shortcuts">',
+    '        <td>',
+    '          <a title="' + __("Click to go this location") + '" href="{{shortcut.href}}">',
     '{{shortcut.description}}',
-    '</a>',
-    '</td>',
-    '</tr>',
-    '</tbody>',
-    '</table>',
+    '          </a>',
+    '        </td>',
+    '      </tr>',
+    '    </tbody>',
+    '  </table>',
     '</div>',
   ].join("\n"),
 });
