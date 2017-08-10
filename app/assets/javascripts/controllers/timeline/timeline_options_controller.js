51d560bdb5cdd1d3828e346cf3726723141d4ac1
Martin Maroši <marvusm.mmi@gmail.com>
2017-08-10 12:32:55 +0200

    Replaced var with let and arrow functions

--- app/assets/javascripts/controllers/timeline/timeline_options_controller.js
+++ app/assets/javascripts/controllers/timeline/timeline_options_controller.js
@@ -1,7 +1,7 @@
 ManageIQ.angular.app.controller('timelineOptionsController', ['$http', '$scope', 'miqService', 'url', 'categories', function($http, $scope, miqService, url, categories) {
-  var vm = this;
+  let vm = this;
 
-  var init = function() {
+  const init = () => {
     vm.reportModel = {
       tl_show: 'timeline',
       tl_categories: ['Power Activity'],
@@ -22,26 +22,26 @@ ManageIQ.angular.app.controller('timelineOptionsController', ['$http', '$scope',
     vm.availableCategories = categories;
   };
 
-  vm.eventTypeUpdated = function() {
+  vm.eventTypeUpdated = () => {
     vm.reportModel.tl_categories = [];
   };
 
-  vm.countDecrement = function() {
+  vm.countDecrement = () => {
     if (vm.reportModel.tl_range_count > 1) {
       vm.reportModel.tl_range_count--;
     }
   };
 
-  vm.countIncrement = function() {
+  vm.countIncrement = () => {
     vm.reportModel.tl_range_count++;
   };
 
-  vm.applyButtonClicked = function() {
+  vm.applyButtonClicked = () => {
     if (vm.reportModel.tl_categories.length === 0) {
       return;
     }
 
-        // process selections
+    // NOTE: process selection
     if (vm.reportModel.tl_timerange === 'days') {
       vm.reportModel.tl_typ = 'Hourly';
       vm.reportModel.tl_days = vm.reportModel.tl_range_count;
@@ -53,16 +53,16 @@ ManageIQ.angular.app.controller('timelineOptionsController', ['$http', '$scope',
         vm.reportModel.tl_days = vm.reportModel.tl_range_count * 30;
       }
     }
-
-    var selectedDay = moment(vm.reportModel.tl_date),
-      startDay = selectedDay.clone(),
-      endDay = selectedDay.clone();
+    // NOTE: moment is not immutable yet
+    let selectedDay = moment(vm.reportModel.tl_date);
+    let startDay = selectedDay.clone();
+    let endDay = selectedDay.clone();
 
     if (vm.reportModel.tl_timepivot === 'starting') {
       endDay.add(vm.reportModel.tl_days, 'days').toDate();
       vm.reportModel.miq_date = endDay.format('MM/DD/YYYY');
     } else if (vm.reportModel.tl_timepivot === 'centered') {
-      var enddays = Math.ceil(vm.reportModel.tl_days / 2);
+      let enddays = Math.ceil(vm.reportModel.tl_days / 2);
       startDay.subtract(enddays, 'days').toDate();
       endDay.add(enddays, 'days').toDate();
       vm.reportModel.miq_date = endDay.format('MM/DD/YYYY');
