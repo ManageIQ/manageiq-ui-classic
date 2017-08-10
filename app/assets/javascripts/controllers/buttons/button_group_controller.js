8efcbf0f105174548216da1ee93142e7e75266f6
Martin Maroši <marvusm.mmi@gmail.com>
2017-08-10 14:23:22 +0200

    initial commit

--- app/assets/javascripts/controllers/buttons/button_group_controller.js
+++
@@ -1,6 +0,0 @@
-ManageIQ.angular.app.controller('buttonGroupController', ['$scope', 'miqService', function($scope, miqService) {
-  var init = function() {
-    $scope.saveable = miqService.saveable;
-  };
-  init();
-}]);
