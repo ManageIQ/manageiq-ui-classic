fa7f62e1afb279a78b04f27f7a32f0f297bc02df
Aparna Karve <akarve@redhat.com>
2017-08-11 00:20:06 -0700

    Pass :assign_to to set the `checkable_checkboxes` properties
    Also other checkbox related properties for Alert Profile
    Assignment will be set.
    This fixes Alert Profile assignment for ems_folders
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1479944

--- app/presenters/tree_builder_belongs_to_hac.rb
+++ app/presenters/tree_builder_belongs_to_hac.rb
@@ -18,6 +18,7 @@ class TreeBuilderBelongsToHac < TreeBuilder
     @edit = params[:edit]
     @group = params[:group]
     @selected = params[:selected]
+    @assign_to = params[:assign_to]
     # need to remove tree info
     TreeState.new(sandbox).remove_tree(name)
     super(name, type, sandbox, build)
@@ -29,17 +30,26 @@ class TreeBuilderBelongsToHac < TreeBuilder
     {:full_ids             => true,
      :add_root             => false,
      :lazy                 => false,
-     :checkable_checkboxes => @edit.present?,
+     :checkable_checkboxes => @edit.present? || @assign_to.present?,
      :selected             => @selected}
   end
 
   def set_locals_for_render
     locals = super
+
+    oncheck, check_url = if @assign_to
+                           ["miqOnCheckHandler", "/miq_policy/alert_profile_assign_changed/"]
+                         elsif @edit
+                           ["miqOnCheckUserFilters", "/ops/rbac_group_field_changed/#{group_id}___"]
+                         else
+                           [nil, "/ops/rbac_group_field_changed/#{group_id}___"]
+                         end
+
     locals.merge!(:id_prefix         => 'hac_',
-                  :check_url         => "/ops/rbac_group_field_changed/#{group_id}___",
-                  :oncheck           => @edit ? "miqOnCheckUserFilters" : nil,
+                  :check_url         => check_url,
+                  :oncheck           => oncheck,
                   :checkboxes        => true,
-                  :highlight_changes => true,
+                  :highlight_changes => @assign_to ? false : true,
                   :onclick           => false)
   end
 
