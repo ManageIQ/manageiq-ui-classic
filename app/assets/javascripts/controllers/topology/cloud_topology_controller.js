e13b251fbafc76b950c9631c1fbd3325b8b6a1b2
Martin Maroši <marvusm.mmi@gmail.com>
2017-08-14 09:47:51 +0200

    replaced scope with vm in topology data variables

--- app/assets/javascripts/controllers/topology/cloud_topology_controller.js
+++ app/assets/javascripts/controllers/topology/cloud_topology_controller.js
@@ -2,20 +2,22 @@ angular.module('ManageIQ').controller('cloudTopologyController', CloudTopologyCt
 CloudTopologyCtrl.$inject = ['$scope', '$http', '$interval', '$location', 'topologyService', 'miqService'];
 
 function CloudTopologyCtrl($scope, $http, $interval, $location, topologyService, miqService) {
+  var vm = this;
+
   ManageIQ.angular.scope = $scope;
   miqHideSearchClearButton();
-  var self = this;
-  $scope.vs = null;
+  vm.vs = null;
   var icons = null;
 
   var d3 = window.d3;
-  $scope.d3 = d3;
+  // NOTE: for search
+  vm.d3 = d3;
 
-  topologyService.mixinContextMenu(this, $scope);
+  topologyService.mixinContextMenu(vm, vm);
 
   $scope.refresh = function() {
     var id;
-    if ($location.absUrl().match("show/$") || $location.absUrl().match("show$")) {
+    if ($location.absUrl().match('show/$') || $location.absUrl().match('show$')) {
       id = '';
     } else {
       id = '/' + (/cloud_topology\/show\/(\d+)/.exec($location.absUrl())[1]);
@@ -31,144 +33,147 @@ function CloudTopologyCtrl($scope, $http, $interval, $location, topologyService,
   function getCloudTopologyFormDataComplete(response) {
     var data = response.data;
 
-    var currentSelectedKinds = $scope.kinds;
+    var currentSelectedKinds = vm.kinds;
 
-    $scope.items = data.data.items;
-    $scope.relations = data.data.relations;
-    $scope.kinds = data.data.kinds;
+    vm.items = data.data.items;
+    vm.relations = $scope.relations = data.data.relations;
+    // NOTE: $scope.kind is required by kubernetes-topology-icon
+    vm.kinds = $scope.kinds = data.data.kinds;
     icons = data.data.icons;
 
-    if (currentSelectedKinds && (Object.keys(currentSelectedKinds).length !== Object.keys($scope.kinds).length)) {
-      $scope.kinds = currentSelectedKinds;
+    if (currentSelectedKinds && (Object.keys(currentSelectedKinds).length !== Object.keys(vm.kinds).length)) {
+      vm.kinds = vm.kinds = currentSelectedKinds;
     }
   }
 
-  $scope.checkboxModel = {
-    value: false
+  vm.checkboxModel = {
+    value: false,
   };
 
-  $scope.legendTooltip = __("Click here to show/hide entities of this type");
+  vm.legendTooltip = __('Click here to show/hide entities of this type');
 
-  $('input#box_display_names').click(topologyService.showHideNames($scope));
+  $('input#box_display_names').click(topologyService.showHideNames(vm));
   $scope.refresh();
   var promise = $interval($scope.refresh, 1000 * 60 * 3);
-
+// NOTE: cannot move $on events to scope until angular2
   $scope.$on('$destroy', function() {
     $interval.cancel(promise);
   });
 
-  $scope.$on("render", function(ev, vertices, added) {
+  $scope.$on('render', function(ev, vertices, added) {
     /*
      * We are passed two selections of <g> elements:
      * vertices: All the elements
      * added: Just the ones that were added
      */
 
-    added.attr("class", function(d) {
+    added.attr('class', function(d) {
       return d.item.kind;
     });
 
-    added.append("circle")
-      .attr("r", function(d) {
-        return self.getDimensions(d).r;
+    added.append('circle')
+      .attr('r', function(d) {
+        return vm.getDimensions(d).r;
       })
       .attr('class', function(d) {
         return topologyService.getItemStatusClass(d);
       })
-      .on("contextmenu", function(d) {
-        self.contextMenu(this, d);
+      .on('contextmenu', function(d) {
+        vm.contextMenu(this, d);
+        vm.contextMenu(this, d);
       });
 
-    added.append("title");
+    added.append('title');
 
-    added.on("dblclick", function(d) {
-      return self.dblclick(d);
+    added.on('dblclick', function(d) {
+      return vm.dblclick(d);
     });
 
-    added.append("image")
-      .attr("xlink:href", function (d) {
-        var iconInfo = self.getIcon(d);
-        switch(iconInfo.type) {
+    added.append('image')
+      .attr('xlink:href', function(d) {
+        var iconInfo = vm.getIcon(d);
+        switch (iconInfo.type) {
           case 'image':
             return iconInfo.icon;
-          case "glyph":
+          case 'glyph':
+            return null;
+          default:
             return null;
         }
       })
-      .attr("height", function(d) {
-        var iconInfo = self.getIcon(d);
-        if (iconInfo.type != 'image') {
+      .attr('height', function(d) {
+        var iconInfo = vm.getIcon(d);
+        if (iconInfo.type !== 'image') {
           return 0;
         }
         return 40;
       })
-      .attr("width", function(d) {
-        var iconInfo = self.getIcon(d);
-        if (iconInfo.type != 'image') {
+      .attr('width', function(d) {
+        var iconInfo = vm.getIcon(d);
+        if (iconInfo.type !== 'image') {
           return 0;
         }
         return 40;
       })
-      .attr("y", function(d) {
-        return self.getDimensions(d).y;
+      .attr('y', function(d) {
+        return vm.getDimensions(d).y;
       })
-      .attr("x", function(d) {
-        return self.getDimensions(d).x;
+      .attr('x', function(d) {
+        return vm.getDimensions(d).x;
       })
-      .on("contextmenu", function(d) {
-        self.contextMenu(this, d);
+      .on('contextmenu', function(d) {
+        vm.contextMenu(this, d);
       });
 
-    added.append("text")
+    added.append('text')
       .each(function(d) {
-        var iconInfo = self.getIcon(d);
-        if (iconInfo.type != 'glyph')
+        var iconInfo = vm.getIcon(d);
+        if (iconInfo.type !== 'glyph') {
           return;
-
+        }
         /* global fontIconChar */
         var fonticon = fontIconChar(iconInfo.class);
         $(this).text(fonticon.char)
-          .attr("class", "glyph")
+          .attr('class', 'glyph')
           .attr('font-family', fonticon.font);
       })
 
-      .attr("y", function(d) {
-        return self.getDimensions(d).y;
+      .attr('y', function(d) {
+        return vm.getDimensions(d).y;
       })
-      .attr("x", function(d) {
-        return self.getDimensions(d).x;
+      .attr('x', function(d) {
+        return vm.getDimensions(d).x;
       })
-      .on("contextmenu", function(d) {
-        self.contextMenu(this, d);
+      .on('contextmenu', function(d) {
+        vm.contextMenu(this, d);
       });
 
-    added.append("text")
-      .attr("x", 26)
-      .attr("y", 24)
+    added.append('text')
+      .attr('x', 26)
+      .attr('y', 24)
       .text(function(d) {
         return d.item.name;
       })
       .attr('class', function() {
-         var class_name = "attached-label";
-         if ($scope.checkboxModel.value) {
-           return class_name + ' visible';
-         } else {
-           return class_name;
-         }
+        var className = 'attached-label';
+        if (vm.checkboxModel.value) {
+          return className + ' visible';
+        }
+        return className;
       });
 
-    added.selectAll("title").text(function(d) {
-      return topologyService.tooltip(d).join("\n");
+    added.selectAll('title').text(function(d) {
+      return topologyService.tooltip(d).join('\n');
     });
 
-    $scope.vs = vertices;
+    vm.vs = vertices;
 
     /* Don't do default rendering */
     ev.preventDefault();
   });
 
-  this.getIcon = function getIcon(d) {
-    switch(d.item.kind) {
+  vm.getIcon = function getIcon(d) {
+    switch (d.item.kind) {
       case 'CloudManager':
         return icons[d.item.display_kind];
       default:
@@ -176,22 +181,25 @@ function CloudTopologyCtrl($scope, $http, $interval, $location, topologyService,
     }
   };
 
-  this.getDimensions = function getDimensions(d) {
+  vm.getDimensions = function getDimensions(d) {
     var defaultDimensions = topologyService.defaultElementDimensions();
     switch (d.item.kind) {
-      case "CloudManager":
+      case 'CloudManager':
         return { x: -20, y: -20, r: 28 };
-      case "AvailabilityZone":
-      case "Tag":
+      case 'AvailabilityZone':
+      case 'Tag':
         return { x: defaultDimensions.x, y: defaultDimensions.y, r: 13 };
-      case "CloudTenant":
+      case 'CloudTenant':
         return { x: defaultDimensions.x, y: defaultDimensions.y, r: 19 };
-      case "Vm":
+      case 'Vm':
         return { x: defaultDimensions.x, y: defaultDimensions.y, r: 21 };
       default:
         return defaultDimensions;
     }
   };
 
-  topologyService.mixinSearch($scope);
+  topologyService.mixinSearch(vm);
+  // NOTE: need for searching in miq-toolbar
+  $scope.searchNode = vm.searchNode;
+  $scope.resetSearch = vm.resetSearch;
 }
