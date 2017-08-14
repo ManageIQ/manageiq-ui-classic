43f7ff97c1866a2380befbf5d4c0764ee19ee040
Zita Nemeckova <znemecko@redhat.com>
2017-08-14 10:32:47 +0200

    Remove rss from old widget

--- app/assets/javascripts/components/widget-rss.js
+++ app/assets/javascripts/components/widget-rss.js
@@ -6,18 +6,20 @@ ManageIQ.angular.app.component('widgetRss', {
   controller: ['$http', 'miqService', '$sce', function($http, miqService, $sce) {
     var vm = this;
     vm.widgetRssModel = {};
+
     this.$onInit = function() {
       $http.get('/dashboard/widget_rss_data/' + vm.id)
-        .then(function(response) { vm.widgetRssModel.content = $sce.getTrustedHtml(response.data.content); })
+        .then(function(response) { vm.widgetRssModel.content = $sce.trustAsHtml(response.data.content); })
         .catch(miqService.handleFailure);
       vm.div_id = "dd_w" + vm.id + "_box";
     };
+
     vm.contentPresent = function() {
       return vm.widgetRssModel.content !== undefined;
     };
   }],
   template: [
-    '<div class="mc" id="{{vm.div_id}}"  ng-class="{ hidden: vm.widgetReportModel.minimized }">',
+    '<div class="mc" id="{{vm.div_id}}"  ng-class="{ hidden: vm.widgetRssModel.minimized }">',
     '  <div class="blank-slate-pf " style="padding: 10px" ng-if="!vm.contentPresent()">',
     '    <div class="blank-slate-pf-icon">',
     '      <i class="fa fa-cog">',
