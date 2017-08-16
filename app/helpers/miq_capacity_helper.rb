43a723eb24ec08c251bec2e0c0d3d35e1ea05981
Adrian Toth <adtoth@redhat.com>
2017-08-16 13:42:15 +0200

    Prefix "MiqCapacityHelper" was added to every occurrence of TARGET_TYPE_CHOICES and include MiqCapacityHelper to ApplicationHelper

--- app/helpers/miq_capacity_helper.rb
+++ app/helpers/miq_capacity_helper.rb
@@ -1,9 +1,7 @@
 module MiqCapacityHelper
-
   # Choices for Target options show pulldown
   TARGET_TYPE_CHOICES = {
     "EmsCluster" => N_("Clusters"),
     "Host"       => N_("Hosts")
   }.freeze
-
 end
