ea816762d372db82d61ed5b62246d14a4fcf73d1
Adrian Toth <adtoth@redhat.com>
2017-08-16 13:41:42 +0200

    Definition of constant TARGET_TYPE_CHOICES was moved to ViewHelper

---
+++ app/helpers/miq_capacity_helper.rb
@@ -0,0 +1,9 @@
+module MiqCapacityHelper
+
+  # Choices for Target options show pulldown
+  TARGET_TYPE_CHOICES = {
+    "EmsCluster" => N_("Clusters"),
+    "Host"       => N_("Hosts")
+  }.freeze
+
+end
