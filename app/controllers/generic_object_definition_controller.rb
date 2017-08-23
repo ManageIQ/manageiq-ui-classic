79429bc7adf5675d97214312f3722746271888a7
Aparna Karve <akarve@redhat.com>
2017-08-23 13:56:23 -0700

    Routes, templates and controller methods for `new` and `edit`

--- app/controllers/generic_object_definition_controller.rb
+++ app/controllers/generic_object_definition_controller.rb
@@ -10,6 +10,7 @@ class GenericObjectDefinitionController < ApplicationController
   include Mixins::GenericShowMixin
 
   menu_section :automate
+  toolbar :generic_object_definition
 
   def self.display_methods
     %w(generic_objects)
@@ -21,6 +22,26 @@ class GenericObjectDefinitionController < ApplicationController
 
   def self.model
     GenericObjectDefinition
+  end
+
+  def button
+    if params[:pressed] == 'generic_object_definition_new'
+      javascript_redirect :action => 'new'
+    elsif params[:pressed] == 'generic_object_definition_edit'
+      javascript_redirect :action => 'edit'
+    end
+  end
+
+  def new
+    assert_privileges('generic_object_definition_new')
+    drop_breadcrumb(:name => _("Add a new Generic Object Class"), :url => "/generic_object_definition/new")
+    @in_a_form = true
+  end
+
+  def edit
+    assert_privileges('generic_object_definition_edit')
+    drop_breadcrumb(:name => _("Edit Generic Object Class"), :url => "/generic_object_definition/edit")
+    @in_a_form = true
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2080,15 +2080,20 @@ Rails.application.routes.draw do
       :get => %w(
         download_data
         download_summary_pdf
+        edit
+        new
         show
         show_list
       ),
       :post => %w(
+        button
         create_del
+        edit
         exp_button
         exp_changed
         exp_token_pressed
         listnav_search_selected
+        new
         update_del
         quick_search
         show_list
