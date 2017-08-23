ffa7f082703814409d963bf37b3cd25adeca37da
Aparna Karve <akarve@redhat.com>
2017-08-23 13:37:50 -0700

    Add Toolbars for Generic Object Classes

--- app/helpers/application_helper/toolbar/generic_object_definition_center.rb
+++ app/helpers/application_helper/toolbar/generic_object_definition_center.rb
@@ -1,45 +1,23 @@
 class ApplicationHelper::Toolbar::GenericObjectDefinitionCenter < ApplicationHelper::Toolbar::Basic
   button_group('generic_object_definition', [
     select(
-      :generic_object_definition_choice,
+      :generic_object_definition_configuration,
       'fa fa-cog fa-lg',
       title = N_('Configuration'),
       title,
       :items => [
         button(
-          :generic_object_definition_create,
-          'pficon pficon-add-circle-o fa-lg',
-          title = N_('Create a new Generic Object Definition'),
-          title,
-          :data => {
-            'function'      => 'sendDataWithRx',
-            'function-data' => '{"eventType": "showAddForm"}'
-          }
-        ),
-        button(
           :generic_object_definition_edit,
           'pficon pficon-edit fa-lg',
-          title = N_('Edit this Generic Object Definition'),
-          title,
-          :onwhen  => "1",
-          :enabled => false,
-          :data    => {
-            'function'      => 'sendDataWithRx',
-            'function-data' => '{"eventType": "showEditForm"}'
-          }
+          t = N_('Edit this Generic Object Class'),
+          t,
         ),
         button(
           :generic_object_definition_delete,
           'pficon pficon-delete fa-lg',
-          title = N_('Delete this Generic Object Definition'),
-          title,
-          :onwhen  => "1",
-          :enabled => false,
-          :confirm => N_("Are you sure you want to delete this Generic Object Definition?"),
-          :data    => {
-            'function'      => 'sendDataWithRx',
-            'function-data' => '{"eventType": "deleteGenericObject"}'
-          }
+          t = N_('Remove this Generic Object Classes from Inventory'),
+          t,
+          :confirm => N_("Warning: This Generic Object Class will be permanently removed!"),
         )
       ]
     )
