a62e39448a98ba61cbfbe8f234e96ef5c5029bd5
Mike Thompson <mithomps@redhat.com>
2017-08-24 17:19:16 -0700

    PoC: Initial evm-relationship angular2 component.

---
+++ app/javascript/evm_relationship/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'evm-relationship',
+  template: `<h1>Evm Relationship: {{name}}</h1>`
+})
+export class AppComponent {
+  name = 'Brandy!';
+}
---
+++ app/javascript/evm_relationship/index.ts
@@ -0,0 +1,7 @@
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+// Halo world
+platformBrowserDynamic().bootstrapModule(AppModule);
