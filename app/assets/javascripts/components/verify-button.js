ed7a11cdcc81f901fca11d2b83c026eafbea70db
Martin Maroši <marvusm.mmi@gmail.com>
2017-08-28 09:48:12 +0200

    added verifyButton component

---
+++ app/assets/javascripts/components/verify-button.js
@@ -0,0 +1,24 @@
+ManageIQ.angular.app.component('verifyButton', {
+  bindings: {
+    validate: '<',
+    enabled: '<',
+    validateUrl: '<',
+    restful: '<',
+    valtype: '<',
+    buttonLabels: '<',
+  },
+  controllerAs: 'vm',
+  controller: ['$scope', function($scope) {
+    $scope.__ = __;
+    var vm = this;
+
+    vm.chooseValidation = function() {
+      if (vm.restful) {
+        vm.validate({target: '.validate_button:visible'}, vm.valtype, true);
+      } else {
+        vm.validate(vm.validateUrl);
+      }
+    };
+  }],
+  templateUrl: '/static/verify_button.html.haml',
+});
