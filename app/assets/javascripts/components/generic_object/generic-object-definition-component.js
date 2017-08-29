402d16b599e85bd271f98174a0b453605de30633
Aparna Karve <akarve@redhat.com>
2017-08-29 09:55:00 -0700

    Table for adding multiple Attributes

--- app/assets/javascripts/components/generic_object/generic-object-definition-component.js
+++ app/assets/javascripts/components/generic_object/generic-object-definition-component.js
@@ -17,9 +17,20 @@ function genericObjectDefinitionFormController(API, miqService) {
     vm.saveable = miqService.saveable;
     vm.afterGet = false;
 
+    vm.attributeTableHeaders = [__("Name"), __("Type"), "", ""];
+
+    vm.types = [
+      {id: "integer", name: "integer"},
+      {id: "string", name: "string"},
+      {id: "boolean", name: "boolean"},
+      {id: "datetime", name: "datetime"},
+    ];
+
     vm.genericObjectDefinitionModel = {
       name: '',
       description: '',
+      attribute_names: [],
+      attribute_types: [],
     };
 
     if (vm.recordId) {
@@ -38,6 +49,13 @@ function genericObjectDefinitionFormController(API, miqService) {
   // private functions
   function getGenericObjectDefinitionFormData(response) {
     Object.assign(vm.genericObjectDefinitionModel, response);
+
+    _.forEach(vm.genericObjectDefinitionModel.properties.attributes, function(value, key) {
+      vm.genericObjectDefinitionModel.attribute_names.push(key);
+      vm.genericObjectDefinitionModel.attribute_types.push(value);
+    });
+
+    vm.noOfRows = _.size(vm.genericObjectDefinitionModel.attribute_names);
 
     vm.afterGet = true;
     vm.modelCopy = angular.copy( vm.genericObjectDefinitionModel );
