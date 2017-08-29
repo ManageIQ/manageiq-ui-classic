8182c095dca4e1aba06b2ffd02d7c39c70ba45c1
Martin Hradil <himdel@seznam.cz>
2017-08-29 09:48:30 +0000

    Merge pull request #2024 from matobet/error-messages
    v2v: Disable VM transform button when op not possible
    (cherry picked from commit c2ad1a96ed786a560776465e8264a88bca8e7789)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1512661

--- app/helpers/application_helper/button/transform_vm_button.rb
+++ app/helpers/application_helper/button/transform_vm_button.rb
@@ -6,7 +6,22 @@ class ApplicationHelper::Button::TransformVmButton < ApplicationHelper::Button::
   end
 
   def disabled?
+    @error_message = _('No suitable target infra provider exists') unless destination_exists?
+    @error_message = _('Can only transform VMs in down state') unless vm_down?
+    @error_message = _('Cannot transform VMs with snapshots') if vm_has_snapshots?
+    @error_message.present?
+  end
+
+  def destination_exists?
     # Is there a provider that supports import?
-    EmsInfra.all.select(&:validate_import_vm).empty?
+    !EmsInfra.all.select(&:validate_import_vm).empty?
+  end
+
+  def vm_down?
+    @record.state == 'off'
+  end
+
+  def vm_has_snapshots?
+    !@record.snapshots.empty?
   end
 end
