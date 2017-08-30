e11e7cf5b667fc8693cad4754ab95d20a8ca2e21
drew uhlmann <drewuhlmann@gmail.com>
2017-08-30 16:57:02 -0400

    Adds error on dialog field association circular reference catch

--- app/controllers/miq_ae_customization_controller.rb
+++ app/controllers/miq_ae_customization_controller.rb
@@ -56,6 +56,8 @@ class MiqAeCustomizationController < ApplicationController
         add_flash(_("Error: the file uploaded is not of the supported format"), :error)
       rescue DialogImportValidator::ParsedNonDialogYamlError
         add_flash(_("Error during upload: incorrect Dialog format, only service dialogs can be imported"), :error)
+      rescue DialogImportValidator::DialogFieldAssociationCircularReferenceError
+        add_flash(_("Error during upload: the dialog fields to be imported contain circular association references"), :error)
       rescue DialogImportValidator::InvalidDialogFieldTypeError
         add_flash(_("Error during upload: one of the DialogField types is not supported"), :error)
       end
