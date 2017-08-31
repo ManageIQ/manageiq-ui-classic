40827f9aa2eaf66cdd01e38ce903d5ac13136711
Roman Blanco <rblanco@redhat.com>
2017-08-31 16:57:58 +0200

    Moved validation methods to an Angular service
    ui-components repository: https://github.com/ManageIQ/ui-components/pull/137

--- app/assets/javascripts/controllers/dialog_editor/dialog_editor_controller.js
+++ app/assets/javascripts/controllers/dialog_editor/dialog_editor_controller.js
@@ -1,4 +1,4 @@
-ManageIQ.angular.app.controller('dialogEditorController', ['$window', 'API', 'miqService', 'DialogEditor', 'dialogId', function($window, API, miqService, DialogEditor, dialogId) {
+ManageIQ.angular.app.controller('dialogEditorController', ['$window', 'API', 'miqService', 'DialogEditor', 'DialogValidation', 'dialogId', function($window, API, miqService, DialogEditor, DialogValidation, dialogId) {
   var vm = this;
 
   if (dialogId === 'new') {
@@ -27,11 +27,11 @@ ManageIQ.angular.app.controller('dialogEditorController', ['$window', 'API', 'mi
   function init(dialog) {
     DialogEditor.setData(dialog);
     vm.dialog = dialog;
+    vm.DialogValidation = DialogValidation;
   }
 
   vm.saveDialogDetails = saveDialogDetails;
   vm.dismissChanges = dismissChanges;
-  vm.dialogIsValid = dialogIsValid;
 
   var beingCloned = null; // hack that solves recursion problem for cloneDeep
   function customizer(value) {
@@ -99,56 +99,6 @@ ManageIQ.angular.app.controller('dialogEditorController', ['$window', 'API', 'mi
       + dialogId,
       {action: action, resource: dialogData}
     ).then(saveSuccess, saveFailure);
-  }
-
-  function dialogIsValid() {
-    var validators = {
-      dialog: [
-        dialog => ({ status: ! _.isEmpty(dialog.label),
-                     errorMessage: __('Dialog needs to have a label') }),
-        dialog => ({ status: dialog.dialog_tabs.length > 0,
-                     errorMessage: __('Dialog needs to have at least one tab') })
-      ],
-      tabs: [
-        tab => ({ status: ! _.isEmpty(tab.label),
-                  errorMessage: __('Dialog tab needs to have a name') }),
-        tab => ({ status: tab.dialog_groups.length > 0,
-                  errorMessage: __('Dialog tab needs to have at least one box') })
-      ],
-      groups: [
-        group => ({ status: ! _.isEmpty(group.label),
-                    errorMessage: __('Dialog box needs to have a name') }),
-        group => ({ status: group.dialog_fields.length > 0,
-                    errorMessage: __('Dialog box needs to have at least one element') })
-      ],
-      fields: [
-        field => ({ status: ! _.isEmpty(field.name),
-                    errorMessage: __('Dialog element needs to have a name') }),
-        field => ({ status: ! _.isEmpty(field.label),
-                    errorMessage: __('Dialog element needs to have a label') })
-      ]
-    };
-
-    let invalid = [];
-    let validate = function (f, item) {
-      let validation = f(item);
-      if (!validation.status) {
-        invalid.push({ message: validation.errorMessage, item: item });
-      }
-      return validation.status;
-    };
-
-    return _.every(DialogEditor.data.content, dialog =>
-      _.every(validators.dialog, f => validate(f, dialog)) &&
-      _.every(dialog.dialog_tabs, tab =>
-        _.every(validators.tabs, f => validate(f, tab)) &&
-        _.every(tab.dialog_groups, group =>
-          _.every(validators.groups, f => validate(f, group)) &&
-          _.every(group.dialog_fields, field =>
-            _.every(validators.fields, f => validate(f, field)))
-        )
-      )
-    );
   }
 
   function dismissChanges() {
