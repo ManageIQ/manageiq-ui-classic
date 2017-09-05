b8cfd2e4a640c97ad7c7cc6ae8bb282a7e1544e3
Dávid Halász <dhalasz@redhat.com>
2017-09-05 11:36:23 +0200

    Reorganize TreeView component initialization on explorer listnavs

--- app/assets/javascripts/controllers/tree_view_controller.js
+++ app/assets/javascripts/controllers/tree_view_controller.js
@@ -6,28 +6,33 @@
     var vm = this;
     vm.$http = $http;
     vm.selectedNodes = {};
-  };
+    vm.data = {};
 
-  TreeViewController.prototype.initSelected = function(tree, node) {
-    this.selectedNodes[tree] = this.selectedNodes[tree] || { key: node };
-  };
+    vm.initSelected = function(tree, node) {
+      vm.selectedNodes[tree] = vm.selectedNodes[tree] || { key: node };
+    };
 
-  TreeViewController.prototype.lazyLoad = function(node, name, url) {
-    var vm = this;
-    return new Promise(function(resolve) {
-      vm.$http.post(url, {
-        id: node.key,
-        tree: name,
-        mode: 'all',
-      }).then(function(response) {
-        resolve(response.data);
+    vm.initData = function(tree, data, selected) {
+      vm.data[tree] = vm.data[tree] || data;
+      vm.selectedNodes[tree] = vm.selectedNodes[tree] || { key: selected };
+    };
+
+    vm.lazyLoad = function(node, name, url) {
+      return new Promise(function(resolve) {
+        vm.$http.post(url, {
+          id: node.key,
+          tree: name,
+          mode: 'all',
+        }).then(function(response) {
+          resolve(response.data);
+        });
       });
-    });
-  };
+    };
 
-  TreeViewController.prototype.nodeSelect = function(node, path) {
-    var url = path + '?id=' + encodeURIComponent(node.key.split('__')[0]);
-    miqJqueryRequest(url, {beforeSend: true});
+    vm.nodeSelect = function(node, path) {
+      var url = path + '?id=' + encodeURIComponent(node.key.split('__')[0]);
+      miqJqueryRequest(url, {beforeSend: true});
+    };
   };
 
   TreeViewController.$inject = ['$http'];
