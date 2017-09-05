f67b0d1bed0e4625ec083b4c0f31671e5a6d3058
Ladislav Smola <lsmola@redhat.com>
2017-09-05 08:17:43 +0200

    Fixed based_volumes sublist for the new report_data function
    Fixed based_volumes sublist for the new report_data function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -215,6 +215,7 @@ module ApplicationHelper
   MODEL_STRING = {
     "all_vms"                                => VmOrTemplate,
     "all_miq_templates"                      => MiqTemplate,
+    "based_volumes"                          => CloudVolume,
     "instances"                              => Vm,
     "images"                                 => MiqTemplate,
     "groups"                                 => Account,
@@ -231,7 +232,8 @@ module ApplicationHelper
     "users"          => "users",
     "event_logs"     => "event_logs",
     "OsProcess"      => "processes",
-    "scan_histories" => "scan_histories"
+    "scan_histories" => "scan_histories",
+    "based_volumes"  => "based_volumes"
   }.freeze
 
   def model_to_report_data
