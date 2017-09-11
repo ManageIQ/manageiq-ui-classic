1a0a186caf7302c4593f25b802baf7236faf7a8e
Patrik Kománek <pkomanek@redhat.com>
2017-09-11 14:28:01 +0200

    Adding automateTreeSelectPicker dialog options to show only methods

--- app/helpers/automate_tree_helper.rb
+++ app/helpers/automate_tree_helper.rb
@@ -1,19 +1,18 @@
 module AutomateTreeHelper
   def at_tree_select_toggle(edit_key)
-    build_ae_tree(:automate, :automate_tree)
+    edit_key == :method ? build_ae_tree(:ae_methods, :automate_tree) : build_ae_tree(:automate, :automate_tree)
     render :update do |page|
       page << javascript_prologue
       tree_close = proc do
         @edit[:ae_tree_select] = false
         @changed = (@edit[:new] != @edit[:current])
         @changed = @edit[:new][:override_source] if params[:controller] == "miq_ae_class" &&
-                                                    @edit[:new][:namespace].nil?
+                                                    @edit[:new][:namespace].nil? && edit_key != :method
         page << javascript_hide("ae_tree_select_div")
         page << javascript_hide("blocker_div")
         page << javascript_for_miq_button_visibility(@changed)
         page << "miqSparkle(false);"
       end
-
       case params[:button]
       when 'submit'
         if @edit[:include_domain_prefix].nil?
@@ -22,9 +21,11 @@ module AutomateTreeHelper
             @edit[:automate_tree_selected_path] = selected_path.slice(selected_path.index('/', 1), selected_path.length)
           end
         end
-        @edit[:new][@edit[:ae_field_typ]] = @edit[:active_id]
-        page << set_element_visible("#{edit_key}_div", true)
-        @edit[:new][edit_key] = @edit[:automate_tree_selected_path]
+        if edit_key != :method
+          @edit[:new][@edit[:ae_field_typ]] = @edit[:active_id]
+          page << set_element_visible("#{edit_key}_div", true)
+          @edit[:new][edit_key] = @edit[:automate_tree_selected_path]
+        end
         if @edit[:new][edit_key]
           page << "$('##{edit_key}').val('#{@edit[:new][edit_key]}');"
           page << "$('##{edit_key}').prop('title', '#{@edit[:new][edit_key]}');"
@@ -34,7 +35,21 @@ module AutomateTreeHelper
           @edit[:include_domain_prefix] = nil
           @edit[:domain_prefix_check] = nil
         end
-        page.replace("form_div", :partial => "copy_objects_form") if params[:controller] == "miq_ae_class"
+        if edit_key == :method
+          @edit[:new][:embedded_methods] ||= []
+          if !@edit[:new][:embedded_methods].include?(@edit[:automate_tree_selected_path])
+            if @edit[:new][:embedded_methods].count >= 20
+              #add_flash(_("You can have only 20 embedded methods at most"), :warning)
+            elsif MiqAeMethod.find_by_id(@edit[:ae_method_id]).fqname == @edit[:automate_tree_selected_path]
+              #add_flash(_("fqname"), :warning)
+            else
+              @edit[:new][:embedded_methods].push(@edit[:automate_tree_selected_path])
+              page.replace("embedded_methods_div", :partial => "embedded_methods")
+            end
+          end
+        else
+          page.replace("form_div", :partial => "copy_objects_form") if params[:controller] == "miq_ae_class"
+        end
         tree_close.call
 
       when 'cancel'
@@ -74,7 +89,9 @@ module AutomateTreeHelper
         page << "$('#automate_div').addClass('modal fade in');"
         @edit[:ae_tree_select] = true
         type = @edit[:ae_field_typ] || params[:typ]
-        @edit[:current][:selected] = @edit[:new][:selected].nil? ? "" : @edit[:new][:selected]
+        if edit_key!= :method
+          @edit[:current][:selected] = @edit[:new][:selected].nil? ? "" : @edit[:new][:selected]
+        end
         unless @edit[:new][type].nil?
           @edit[:new][:selected] = @edit[:new][type]
           if x_node(:automate_tree)
@@ -89,17 +106,20 @@ module AutomateTreeHelper
     id = from_cid(parse_nodetype_and_id(params[:id]).last)
     if params[:id].start_with?("aei-")
       record = MiqAeInstance.find_by_id(id)
-    elsif params[:id].start_with?("aen-") && controller_name == "miq_ae_class"
+    elsif params[:id].start_with?("aen-") && controller_name == "miq_ae_class" && edit_key != :method
       record = MiqAeNamespace.find_by_id(id)
       record = nil if record.domain?
+    elsif params[:id].start_with?("aem-") && controller_name == "miq_ae_class"
+      record = MiqAeMethod.find_by_id(id)
+    end
+    if edit_key != :method
+      @edit[:new][edit_key] = @edit[edit_key] if @edit[:new][edit_key].nil?
+      @edit[:current][:selected] = @edit[:new][:selected].nil? ? "" : @edit[:new][:selected]
+      @edit[:new][:selected] = params[:id]
     end
 
-    @edit[:new][edit_key] = @edit[edit_key] if @edit[:new][edit_key].nil?
-    @edit[:current][:selected] = @edit[:new][:selected].nil? ? "" : @edit[:new][:selected]
-    @edit[:new][:selected] = params[:id]
-
     if record
-      @edit[:automate_tree_selected_path] = controller_name == "miq_ae_class" ? record.fqname_sans_domain : record.fqname
+      @edit[:automate_tree_selected_path] = controller_name == "miq_ae_class" && edit_key != :method ? record.fqname_sans_domain : record.fqname
       # save selected id in edit until save button is pressed
       @edit[:active_id] = params[:id]
       @changed = @edit[:new][edit_key] != @edit[:automate_tree_selected_path]
