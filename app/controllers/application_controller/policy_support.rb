7fe06992c925146b40bc364c05f7b60b95e53389
lgalis <lgalis@redhat.com>
2017-09-18 09:12:03 -0400

    Change to use array of words in condition

--- app/controllers/application_controller/policy_support.rb
+++ app/controllers/application_controller/policy_support.rb
@@ -58,7 +58,7 @@ module ApplicationController::PolicySupport
         @sb[:action] = nil
       end
       if @edit[:explorer]
-        @edit = nil if params[:button] == "cancel" || params[:button] == "save"
+        @edit = nil if %w(cancel save).include?(params[:button])
         replace_right_cell
       else
         @edit = nil                                       # Clear out the session :edit hash
