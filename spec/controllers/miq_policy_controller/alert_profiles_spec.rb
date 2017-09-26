a941269a5323fb66a3b02ce9b1ff01a016962816
Martin Povolny <mpovolny@redhat.com>
2017-09-26 10:10:17 +0200

    Add specs for alert_profile delete.

--- spec/controllers/miq_policy_controller/alert_profiles_spec.rb
+++ spec/controllers/miq_policy_controller/alert_profiles_spec.rb
@@ -50,5 +50,26 @@ describe MiqPolicyController do
         expect(controller.send(:flash_errors?)).not_to be_truthy
       end
     end
+
+    context '#alert_profile_delete' do
+      before :each do
+        @ap = FactoryGirl.create(:miq_alert_set)
+        controller.instance_variable_set(:@sb, :trees => {:alert_profile_tree => {:active_node => "xx-Vm_ap-#{@ap.id}"}}, :active_tree => :alert_profile_tree)
+        allow(controller).to receive(:replace_right_cell)
+      end
+
+      it 'Refuses to delete a non-existent profile' do
+        controller.instance_variable_set(:@_params, :id => @ap.id+1, :button => 'delete')
+        controller.alert_profile_delete
+        expect(assigns(:flash_array).first[:message]).to include("Alert Profile no longer exists")
+      end
+
+      it 'Deletes an existing profile' do
+        controller.instance_variable_set(:@_params, :id => @ap.id, :button => 'delete')
+        expect(controller).to receive(:process_alert_profiles).with([@ap.id], 'destroy').and_return(nil)
+        controller.alert_profile_delete
+        expect(controller.send(:flash_errors?)).not_to be_truthy
+      end
+    end
   end
 end
