e1661c8b6c961275f85e6dc14cd6bf8e6bb4e2f5
Alona Kaplan <alkaplan@redhat.com>
2017-10-01 12:05:20 +0300

    Adding ovirt's 'network manager' to the 'cloud network' 'provider choices'

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,6 +2,8 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name, :parent_ems_id)
+    openstack_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name, :parent_ems_id)
+    redhat_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Redhat::NetworkManager).select(:id, :name, :parent_ems_id)
+    openstack_network_manager + redhat_network_manager
   end
 end
