19e63e6a5971e3a37acfd3b264fb42dac3967f34
lpichler <lpichler@redhat.com>
2017-10-02 06:54:25 +0200

    Network manager is deriving name from parent manager now
    and it needs parent_ems_id as well in
    app/models/manageiq/providers/network_manager.rb:87

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,6 +2,6 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name)
+    Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name, :parent_ems_id)
   end
 end
