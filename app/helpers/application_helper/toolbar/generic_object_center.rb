a5e4fdc1e0efa3801cb3d8972e07b73c6d311298
lgalis <lgalis@redhat.com>
2017-10-03 15:58:55 -0400

    Add toolbar tagging buttons for generic object instances

---
+++ app/helpers/application_helper/toolbar/generic_object_center.rb
@@ -0,0 +1,17 @@
+class ApplicationHelper::Toolbar::GenericObjectCenter < ApplicationHelper::Toolbar::Basic
+  button_group('generic_object_policy', [
+    select(
+      :generic_object_policy_choice,
+      'fa fa-shield fa-lg',
+      t = N_('Policy'),
+      t,
+      :items => [
+        button(
+          :generic_object_tag,
+          'pficon pficon-edit fa-lg',
+          N_('Edit Tags for this Generic Object Instance'),
+          N_('Edit Tags')),
+      ]
+    ),
+  ])
+end
