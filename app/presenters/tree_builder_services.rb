9619cc41ddcec3f4e6961c0b680132518fe2c7d2
Hilda Stastna <hstastna@redhat.com>
2017-10-09 17:45:27 +0200

    Display saved filters in accordion
    Display the new, saved filters in the tree,
    under Global or My Filters in accordion.

--- app/presenters/tree_builder_services.rb
+++ app/presenters/tree_builder_services.rb
@@ -47,11 +47,31 @@ class TreeBuilderServices < TreeBuilder
 
   def x_get_tree_custom_kids(object, count_only, _options)
     services = Rbac.filtered(Service.where(:retired => object[:id] != 'asrv', :display => true))
-    if count_only
+    if %w(global my).include?(object[:id]) # Get My Filters and Global Filters
+      count_only_or_objects(count_only, x_get_search_results(object, _options[:leaf]))
+    elsif count_only
       services.size
     else
       MiqPreloader.preload(services.to_a, :picture)
       Service.arrange_nodes(services.sort_by { |n| [n.ancestry.to_s, n.name.downcase] })
     end
+  end
+
+  def x_get_search_results(object, leaf)
+    case object[:id]
+    when "global" # Global filters
+      x_get_global_filter_search_results(leaf)
+    when "my"     # My filters
+      x_get_my_filter_search_results(leaf)
+    end
+  end
+
+  def x_get_global_filter_search_results(leaf)
+    MiqSearch.where(:db => leaf).visible_to_all.sort_by { |a| a.description.downcase }
+  end
+
+  def x_get_my_filter_search_results(leaf)
+    MiqSearch.where(:db => leaf, :search_type => "user", :search_key => User.current_user.userid)
+             .sort_by { |a| a.description.downcase }
   end
 end
