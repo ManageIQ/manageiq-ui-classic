755fba3a8f791b26770c5095618b910ab3bfda12
Gilles Dubreuil <gilles@redhat.com>
2017-10-11 14:41:30 +1100

    Cloud_Network: No value option for network types

--- app/controllers/cloud_network_controller.rb
+++ app/controllers/cloud_network_controller.rb
@@ -246,7 +246,9 @@ class CloudNetworkController < ApplicationController
     # TODO: uncomment once form contains this field
     # options[:port_security_enabled] = params[:port_security_enabled] if params[:port_security_enabled]
     options[:qos_policy_id] = params[:qos_policy_id] if params[:qos_policy_id]
-    options[:provider_network_type] = params[:provider_network_type] if params[:provider_network_type]
+    if params[:provider_network_type] && !params[:provider_network_type].empty?
+      options[:provider_network_type] = params[:provider_network_type]
+    end
     options[:provider_physical_network] = params[:provider_physical_network] if params[:provider_physical_network]
     options[:provider_segmentation_id] = params[:provider_segmentation_id] if params[:provider_segmentation_id]
     options[:cloud_tenant] = find_record_with_rbac(CloudTenant, params[:cloud_tenant_id]) if params[:cloud_tenant_id]
