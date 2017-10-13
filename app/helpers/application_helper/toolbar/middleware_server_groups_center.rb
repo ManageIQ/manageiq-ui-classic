aa7d8cdc778d823dcfd844fabb8965b30b963d34
Martin Hradil <mhradil@redhat.com>
2017-10-13 12:20:16 +0000

    Toolbars (m*) - add send_checked when url_parms =~ _div
    Currently, our toolbar JS code sends a list of checked items exactly when `url_parms` contains a string ending with _div.
    
    This is not the only use of url_parms, so making the behaviour explicit by creating the new attribute.
    
    ---
    
    this bit deals with toolbars starting with M
    
    ```
    perl -i -npe 's/^(\s*):url_parms.*_div.*/$&\n$1:send_checked => true,/' app/helpers/application_helper/toolbar/m*_center.rb
    git st | sed -e '1d' -e 's/^ M //' | while read fn; do rockets.pl "$fn" > tmp.pl ; perl -i tmp.pl "$fn" ; rm tmp.pl ; done
    ```

--- app/helpers/application_helper/toolbar/middleware_server_groups_center.rb
+++ app/helpers/application_helper/toolbar/middleware_server_groups_center.rb
@@ -13,9 +13,10 @@ class ApplicationHelper::Toolbar::MiddlewareServerGroupsCenter < ApplicationHelp
                        'pficon pficon-edit fa-lg',
                        N_('Edit Tags for these Middleware Server Groups'),
                        N_('Edit Tags'),
-                       :url_parms => "main_div",
-                       :enabled   => false,
-                       :onwhen    => "1+",
+                       :url_parms    => "main_div",
+                       :send_checked => true,
+                       :enabled      => false,
+                       :onwhen       => "1+",
                      ),
                    ]
                  ),
