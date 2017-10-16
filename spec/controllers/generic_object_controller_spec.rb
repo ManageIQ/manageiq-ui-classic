d4d01da16d3e34194ef7a7a9c0a9ac9ed9d973e4
Aparna Karve <akarve@redhat.com>
2017-10-16 14:55:04 -0700

    spec for displaying a nested list of GO associations

--- spec/controllers/generic_object_controller_spec.rb
+++ spec/controllers/generic_object_controller_spec.rb
@@ -14,6 +14,31 @@ describe GenericObjectController do
       get :show, :params => {:id => generic_obj.id}
     end
     it { expect(response.status).to eq(200) }
+
+    it 'displays Generic Object association in the nested display list' do
+      generic_obj_defn = FactoryGirl.create(
+        :generic_object_definition,
+        :name       => "test_definition",
+        :properties => {
+          :attributes   => {
+            :flag       => "boolean",
+            :data_read  => "float",
+            :max_number => "integer",
+            :server     => "string",
+            :s_time     => "datetime"
+          },
+          :associations => {"cp" => "ManageIQ::Providers::CloudManager", "vms" => "Vm"},
+          :methods      => %w(some_method)
+        }
+      )
+      generic_obj = FactoryGirl.create(:generic_object, :generic_object_definition_id => generic_obj_defn.id)
+      
+      get :show, :params => { :display => "cp", :id => generic_obj.id }
+      expect(response.status).to eq(200)
+
+      get :show, :params => { :display => "vms", :id => generic_obj.id }
+      expect(response.status).to eq(200)
+    end
   end
 
   describe "#show_list" do
