ecc7d3f83f854c88ad6a2d0b40db799ed3a1c37a
Aparna Karve <akarve@redhat.com>
2017-10-20 01:21:14 -0700

    Add conditions to `visible?` for GOD Edit button
    taking into account cases for Custom Buttons, Groups and Actions

--- app/helpers/application_helper/button/generic_object_definition.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationHelper::Button::GenericObjectDefinition < ApplicationHelper::Button::Basic
-  def visible?
-    if @display == 'generic_objects'
-      false
-    else
-      true
-    end
-  end
-end
