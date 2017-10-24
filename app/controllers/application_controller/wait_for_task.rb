c1905693017f37bd62ae4a57b4054d526ab25913
Martin Hradil <mhradil@redhat.com>
2017-10-24 19:12:10 +0000

    initiate_wait_for_task - support :rx_action
    similar to action, except:
      * it always gets called with the task instance as a parameter
      * it returns a Hash
    
      * `wait_for_task_rx` is always the actual action and handles the wrapping

--- app/controllers/application_controller/wait_for_task.rb
+++ app/controllers/application_controller/wait_for_task.rb
@@ -37,6 +37,7 @@ module ApplicationController::WaitForTask
   #
   # :task_id => id of task to wait for
   # :action  => 'action_to_call' -- action to be called when the task finishes
+  # :rx_action => 'method_to_call' -- a method to create a RxJs message
   #
   def initiate_wait_for_task(options = {})
     task_id = options[:task_id]
@@ -50,7 +51,31 @@ module ApplicationController::WaitForTask
     # override method to be called, when the task is done
     session[:async][:params][:action] = options[:action] if options.key?(:action)
 
+    if options.key?(:rx_action)
+      raise "Unsupported combination" if options.key?(:action)
+
+      session[:async][:params][:action] = 'wait_for_task_rx'
+      session[:async][:params][:rx_action] = options[:rx_action]
+    end
+
     browser_refresh_task(task_id)
   end
   private :initiate_wait_for_task
+
+  # used for any task with rx_action
+  def wait_for_task_rx
+    task_id = params[:task_id]
+    rx_action = session[:async][:params][:rx_action]
+
+    task = MiqTask.find(task_id)
+    result = send(rx_action, task)
+    raise "Non-hash rx_action return" unless result.kind_of?(Hash)
+
+    # FIXME: move this to ExplorerPresenter
+    render :update do |page|
+      page << javascript_prologue
+      page << "sendDataWithRx(#{result.to_json});"
+    end
+  end
+  private :wait_for_task_rx
 end
