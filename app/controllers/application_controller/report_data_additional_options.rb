d3de62f80ba87cb606b414353033c4c01c22a18c
Martin Povolny <mpovolny@redhat.com>
2017-10-25 15:45:48 +0200

    ReportDataAdditionOptions: fix: save calculated value.

--- app/controllers/application_controller/report_data_additional_options.rb
+++ app/controllers/application_controller/report_data_additional_options.rb
@@ -13,7 +13,7 @@ class ApplicationController
       additional_options = new()
       additional_options.named_scope = options[:named_scope]
       additional_options.gtl_dbname = options[:gtl_dbname]
-      additional_options.model = with_model(options[:model]) if options[:model]
+      additional_options.with_model(options[:model]) if options[:model]
       additional_options.match_via_descendants = options[:match_via_descendants]
       additional_options.parent_id = options[:parent].id if options[:parent]
       additional_options.association = options[:association]
@@ -23,7 +23,7 @@ class ApplicationController
     end
 
     def with_model(curr_model)
-      (curr_model.kind_of? String) ? curr_model : curr_model.name
+      self.model = (curr_model.kind_of? String) ? curr_model : curr_model.name
     end
   end
 end
