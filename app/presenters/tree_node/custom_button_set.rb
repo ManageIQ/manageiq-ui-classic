6b4020041de82e79018d4152bd643eca189328ee
Aparna Karve <akarve@redhat.com>
2017-10-26 14:39:20 -0700

    Mark the Custom Button Groups with (Group)

--- app/presenters/tree_node/custom_button_set.rb
+++ app/presenters/tree_node/custom_button_set.rb
@@ -1,7 +1,7 @@
 module TreeNode
   class CustomButtonSet < Node
     set_attribute(:text) do
-      if @options[:type] == :sandt
+      if @options[:type] == :sandt || @options[:type] == :generic_object_definitions_tree
         _("%{button_group_name} (Group)") % {:button_group_name => @object.name.split("|").first}
       else
         @object.name.split("|").first
