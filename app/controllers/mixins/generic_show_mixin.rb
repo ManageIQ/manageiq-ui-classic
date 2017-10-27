a408c2e9802d5a451a40a319412b3d0a5011a06f
Sam Lucidi <slucidi@redhat.com>
2017-10-27 16:21:27 -0400

    Pass named_scope through nested_list to get_view

--- app/controllers/mixins/generic_show_mixin.rb
+++ app/controllers/mixins/generic_show_mixin.rb
@@ -184,6 +184,7 @@ module Mixins
       view_options[:association] = options[:association] if options.key?(:association)
       view_options[:parent_method] = options[:parent_method] if options.key?(:parent_method)
       view_options[:where_clause] = options[:where_clause] if options.key?(:where_clause)
+      view_options[:named_scope] = options[:named_scope] if options.key?(:named_scope)
 
       @view, @pages = get_view(model, view_options)
       @showtype = @display
