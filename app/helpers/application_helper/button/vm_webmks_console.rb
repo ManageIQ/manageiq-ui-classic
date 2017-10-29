35eea08cb4fb2658ef64e5cd1977307c790c336e
Brian McLaughlin <bmclaugh@redhat.com>
2017-10-28 21:22:46 -0400

    Set minimum vSphere API version for WebMKS consoles
    https://bugzilla.redhat.com/show_bug.cgi?id=1483936

--- app/helpers/application_helper/button/vm_webmks_console.rb
+++ app/helpers/application_helper/button/vm_webmks_console.rb
@@ -7,8 +7,18 @@ class ApplicationHelper::Button::VmWebmksConsole < ApplicationHelper::Button::Vm
   end
 
   def disabled?
-    @error_message = _("The web-based WebMKS console is not available because the required libraries aren't installed") unless webmks_assets_provided?
-    @error_message = _('The web-based WebMKS console is not available because the VM is not powered on') unless on?
+    canned_msg = _('The web-based WebMKS console is not available because')
+    @error_message = _("%{canned_msg} the required libraries aren't installed" % {:canned_msg => canned_msg}) unless webmks_assets_provided?
+    @error_message = _("%{canned_msg} the VM is not powered on" % {:canned_msg => canned_msg}) unless on?
+    @error_message = _("%{canned_msg} the VM does not support the minimum required vSphere API version." % {:canned_msg => canned_msg}) unless supported_vendor_api?
     @error_message.present?
+  end
+
+  def supported_vendor_api?
+    ExtManagementSystem.find(@record.ems_id).api_version.to_f >= min_required_api_version
+  end
+
+  def min_required_api_version
+    6.0
   end
 end
