9ed2cd475e7a815850a1e980579b01b63a28c5f5
Jason Frey <fryguy9@gmail.com>
2017-10-31 01:25:02 -0400

    Remove bower setup for CI

--- tools/ci/setup_js_env.sh
+++ tools/ci/setup_js_env.sh
@@ -1,22 +1,4 @@
-which bower || npm install -g bower
-
-# Check if the bower cache is valid, otherwise delete it
-if ! cmp --silent bower.json vendor/assets/bower_components/bower.json; then
-  rm -rf vendor/assets/bower_components
-fi
-
-if [ -d vendor/assets/bower_components ]; then
-  # Using bower_components from cache
-  echo "bower assets installed... moving on."
-else
-  bower install --allow-root -F --config.analytics=false
-  STATUS=$?
-  echo bower exit code: $STATUS
-
-  # fail the whole test suite if bower install failed
-  [ $STATUS = 0 ] || exit 1
-  [ -d vendor/assets/bower_components ] || exit 1
-fi
+rm -rf vendor/assets/bower_components
 
 # make sure yarn is installed, in the right version
 bundle exec rake webpacker:check_yarn || npm install -g yarn
