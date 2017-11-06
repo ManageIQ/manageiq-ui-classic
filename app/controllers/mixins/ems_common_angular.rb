6c543ec52775bf0df175302b036e5832bed3e919
Tomas Coufal <tcoufal@redhat.com>
2017-11-06 20:46:43 +0100

    Add verification credential param for google

--- app/controllers/mixins/ems_common_angular.rb
+++ app/controllers/mixins/ems_common_angular.rb
@@ -125,7 +125,7 @@ module Mixins
       when 'ManageIQ::Providers::Vmware::CloudManager'
         [params[:default_hostname], params[:default_api_port], user, password, true]
       when 'ManageIQ::Providers::Google::CloudManager'
-        [params[:project], params[:service_account], {:service => "compute"}]
+        [params[:project], params[:service_account], {:service => "compute"}, true]
       when 'ManageIQ::Providers::Microsoft::InfraManager'
         [{:endpoint => params[:default_hostname], :user => user, :password => password}, true]
       when 'ManageIQ::Providers::Openstack::InfraManager'
