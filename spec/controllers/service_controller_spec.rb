4c7b7bd598a7d63072d864052cfeb77d504218b5
lgalis <lgalis@redhat.com>
2017-11-07 09:19:21 -0500

    Add specs for custom buttons on nested lists

--- spec/controllers/service_controller_spec.rb
+++ spec/controllers/service_controller_spec.rb
@@ -98,6 +98,26 @@ describe ServiceController do
       expect(assigns(:breadcrumbs)).to eq([{:name => "Abc (All Generic Objects)", :url => "/service/show/#{service.id}?display=generic_objects"}])
     end
 
+    it 'displays one generic object from the nested list' do
+      EvmSpecHelper.create_guid_miq_server_zone
+      login_as FactoryGirl.create(:user)
+      controller.instance_variable_set(:@breadcrumbs, [])
+      service = FactoryGirl.create(:service, :name => "Abc")
+      definition = FactoryGirl.create(:generic_object_definition,
+                                      :properties => {:associations => {"vms" => "Vm", "services" => "Service"}})
+      go = FactoryGirl.create(
+        :generic_object,
+        :generic_object_definition => definition,
+        :name                      => 'go_assoc',
+        :services                  => [service]
+      )
+      service.add_resource(go)
+
+      get :show, :params => { :id => service.id, :display => 'generic_objects', :show => go.id}
+      expect(response.status).to eq(200)
+      expect(assigns(:breadcrumbs)).to eq([{:name => "Abc (All Generic Objects)", :url => "/service/show/#{service.id}?display=generic_objects"}])
+    end
+
     context "#button" do
       render_views
 
