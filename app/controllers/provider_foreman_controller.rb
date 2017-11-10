a0ca148a9c33de4f9a608512b1a05166d675df2e
Harpreet Kataria <hkataria@redhat.com>
2017-11-10 13:13:40 -0500

    Merge pull request #2694 from martinpovolny/foreman_fixes
    Fix various issues in Foreman UI causes by GTL.
    (cherry picked from commit 4a76f01782ea4193be678ed8be84d1e0089a78e4)

--- app/controllers/provider_foreman_controller.rb
+++ app/controllers/provider_foreman_controller.rb
@@ -298,7 +298,7 @@ class ProviderForemanController < ApplicationController
     else
       options = {:model => "ConfiguredSystem", :match_via_descendants => 'ConfiguredSystem'}
       if empty_configuration_profile_record?(@configuration_profile_record)
-        options[:named_scope] = [[:with_manager, id], [:with_configuration_profile_id, nil]]
+        options[:named_scope] = [[:with_manager, id], [:without_configuration_profile_id]]
       else
         options[:named_scope] = [[:with_configuration_profile_id, @configuration_profile_record.id]]
       end
@@ -451,7 +451,7 @@ class ProviderForemanController < ApplicationController
        'manager_id'                     => provider_id
       }
 
-    if @view
+    unless @view.table.kind_of?(Hash)
       add_unassigned_configuration_profile_record_to_view(unassigned_profile_row, unassigned_configuration_profile)
     end
     {
