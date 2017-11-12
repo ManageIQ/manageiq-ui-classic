620973b7d0b1f7a91867b6464ce52be65788b570
yaacov <kobi.zamir@gmail.com>
2017-11-12 10:41:23 +0200

    Add a spec for the ad hoc page render

--- spec/controllers/ems_container_controller_spec.rb
+++ spec/controllers/ems_container_controller_spec.rb
@@ -32,6 +32,12 @@ describe EmsContainerController do
         expect(response.body).to_not be_empty
         expect(response).to render_template('container_topology/show')
       end
+
+      it "renders ad hoc view" do
+        get :show, :params => { :id => @container.id, :display => 'ad_hoc_metrics' }
+        expect(response.status).to eq(200)
+        expect(response.body).to_not be_empty
+      end
     end
 
     context "render dashboard" do
