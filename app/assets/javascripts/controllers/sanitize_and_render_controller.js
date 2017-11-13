1b03eb5f9187d7d32ee44db3cca0c5e2e8d43f6c
Martin Hradil <mhradil@redhat.com>
2017-11-13 14:15:27 +0000

    sanitizeAndRenderController - remove, part of angular-raw-stdout now
    (and for the variant where you want to remove dangerous html, there's miq-sanitize)

--- app/assets/javascripts/controllers/sanitize_and_render_controller.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('sanitizeRender', ['ngSanitize'])
-  .controller('SanitizeAndRenderController', ['$sce', function SanitizeAndRenderCtrl($sce) {
-    var vm = this;
-    vm.sanitizeRenderHtml = function(str) {
-      return $sce.trustAsHtml(str);
-    };
-  }]);
