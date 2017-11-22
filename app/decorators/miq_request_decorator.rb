d7257f3801c47d77e2a09e2f091c01b586a1e1ed
Milan Zázrivec <mzazrivec@redhat.com>
2017-11-22 16:24:06 +0100

    Merge pull request #2802 from skateman/miq-request-fileicon
    Drop fileicon decorator from MiqRequestDecorator
    (cherry picked from commit fa1e86ec8d47e6b44c0e69534cc2cadc8ed4485c)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1517941

--- app/decorators/miq_request_decorator.rb
+++ app/decorators/miq_request_decorator.rb
@@ -7,15 +7,4 @@ class MiqRequestDecorator < MiqDecorator
       "pficon pficon-error-circle-o"
     end
   end
-
-  def fileicon
-    case request_status.to_s.downcase
-    when "ok"
-      "100/checkmark.png"
-    when "error"
-      "100/x.png"
-    else
-      "100/miq_request.png"
-    end
-  end
 end
