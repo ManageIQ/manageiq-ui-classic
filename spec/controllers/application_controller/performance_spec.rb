72cb0df9893784be36f32b5a8c9a7a3e2e5b2479
Martin Hradil <himdel@seznam.cz>
2017-11-27 13:38:51 +0000

    Merge pull request #2819 from martinpovolny/clasification_find_by_name
    Performance: revert refactoring find_by_name vs find_by(:name ...
    (cherry picked from commit a211eded25e9a6e05b43a1892433e520367e2bea)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1517949

--- spec/controllers/application_controller/performance_spec.rb
+++ spec/controllers/application_controller/performance_spec.rb
@@ -1,7 +1,7 @@
 describe ApplicationController do
   context "#perf_planning_gen_data" do
     it "should not get nil error when submitting up Manual Input data" do
-      enterprise = FactoryGirl.create(:miq_enterprise)
+      _enterprise = FactoryGirl.create(:miq_enterprise)
       allow(MiqServer).to receive(:my_zone).and_return("default")
       sb = {
         :options => {
@@ -34,6 +34,63 @@ describe ApplicationController do
 
     it 'should handle Sundays' do
       expect(subject.call((1..6).to_a)).to eq([7])
+    end
+  end
+
+  context 'peformance charts generation' do
+    before(:each) do
+      EvmSpecHelper.create_guid_miq_server_zone
+    end
+
+    describe 'perf_gen_tag_data_before_wait' do
+      %w(Hourly Daily).each do |type|
+        it "calls initiate_wait_for_task for '#{type}'" do
+          controller.instance_variable_set(:@perf_record, FactoryGirl.create(:host))
+          controller.instance_variable_set(
+            :@perf_options,
+            {
+              :typ          => type,
+              :daily_date   => '12/12/2017',
+              :hourly_date  => '12/12/2017',
+              :tz           => TimeProfile.rollup_daily_metrics.all_timezones.first,
+              :cat          => 'foobar',
+              :time_profile => 'bar'
+            }
+          )
+          expect(controller).to receive(:initiate_wait_for_task)
+          controller.send(:perf_gen_tag_data_before_wait)
+        end
+      end
+    end
+
+    describe 'perf_gen_tag_data_after_wait' do
+      it 'calls Classification.find_by_name and other specific methods' do
+        task = double(:task)
+        allow(task).to receive_message_chain(:miq_report_result, :report_results)
+        expect(task).to receive(:destroy)
+
+        expect(MiqTask).to receive(:find).and_return(task)
+
+        cat = double(:cat)
+        allow(cat).to receive(:description)
+        expect(Classification).to receive(:find_by_name).with('cat').and_return(cat)
+
+        controller.instance_variable_set(:@sb, {})
+        controller.instance_variable_set(:@perf_record, FactoryGirl.create(:host))
+        controller.instance_variable_set(
+          :@perf_options,
+          {
+            :typ   => 'Hourly',
+            :cat   => 'cat',
+            :model => 'Host',
+          }
+        )
+        expect(controller).to receive(:prepare_perf_tag_chart).at_least(:once)
+        expect(controller).to receive(:gen_perf_chart).at_least(:once)
+        allow(controller).to receive(:perf_zoom_url)
+        expect(controller).to receive(:perf_report_to_html)
+        controller.send(:perf_gen_tag_data_after_wait)
+      end
     end
   end
 end
