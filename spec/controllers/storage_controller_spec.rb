8250ba255ea8908309e68967da96cf453d5c9294
Milan Zazrivec <mzazrivec@redhat.com>
2017-12-04 10:10:56 +0100

    The id returned by report_data() is now a string
    The behavior changed in 716037a305eb462f5ee6a6ae4caadd593f7ee015

--- spec/controllers/storage_controller_spec.rb
+++ spec/controllers/storage_controller_spec.rb
@@ -297,7 +297,7 @@ describe StorageController do
         )
         results = assert_report_data_response
         expect(results['data']['rows'].length).to eq(1)
-        expect(results['data']['rows'][0]['long_id']).to eq(storage.id)
+        expect(results['data']['rows'][0]['long_id']).to eq(storage.id.to_s)
       end
     end
   end
