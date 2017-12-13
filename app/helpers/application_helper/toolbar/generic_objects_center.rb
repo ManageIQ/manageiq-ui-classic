40be2565b5d477ce33571bac652ddeedcf0d55a0
Aparna Karve <akarve@redhat.com>
2017-12-12 16:41:49 -0800

    Set :send_checked => true in order to have miq_grid_checks defined

--- app/helpers/application_helper/toolbar/generic_objects_center.rb
+++ app/helpers/application_helper/toolbar/generic_objects_center.rb
@@ -13,9 +13,11 @@ class ApplicationHelper::Toolbar::GenericObjectsCenter < ApplicationHelper::Tool
           'pficon pficon-edit fa-lg',
           N_('Edit Tags for the selected Generic Object Instances'),
           N_('Edit Tags'),
-          :url_parms => "main_div",
-          :enabled   => false,
-          :onwhen    => "1+"),
+          :url_parms    => "main_div",
+          :send_checked => true,
+          :enabled      => false,
+          :onwhen       => "1+"
+        ),
       ]
     ),
   ])
