ffa6ad568ed5980d3f5b1c258ff14c295b313027
Evert Mulder <evert.mulder@gmail.com>
2017-12-20 15:54:25 +0100

    Add resize support to reconfigure screen

--- app/controllers/application_controller/ci_processing.rb
+++ app/controllers/application_controller/ci_processing.rb
@@ -13,7 +13,8 @@ module ApplicationController::CiProcessing
     include Mixins::Actions::VmActions::Resize
     include Mixins::Actions::VmActions::RightSize
     include Mixins::Actions::VmActions::Reconfigure
-    helper_method :supports_reconfigure_disks?
+    helper_method(:supports_reconfigure_disks?)
+    helper_method(:supports_reconfigure_disksize?)
     include Mixins::Actions::VmActions::PolicySimulation
     include Mixins::Actions::VmActions::Transform
 
