b76475b6701fea483c5d1271ed83658b952fe888
Dan Clarizio <dclarizi@redhat.com>
2017-12-21 13:22:27 -0800

    Merge pull request #3135 from AparnaKarve/bz1525113_tb_disable_provision_for_noneligible_cases
    Disable Provision Physical Server button for specific cases
    (cherry picked from commit a568f7ee6dde1b98eef4995a2efd6ddb544fd084)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1530787

---
+++ spec/helpers/application_helper/buttons/physical_server_provision_spec.rb
@@ -0,0 +1,50 @@
+describe ApplicationHelper::Button::PhysicalServerProvision do
+  describe '#disabled?' do
+    context "button should not be disabled" do
+      before do
+        ems = FactoryGirl.create(:ems_physical_infra)
+        @record = FactoryGirl.create(:physical_server, :ems_id => ems.id)
+      end
+
+      it "does not disable the Provision button" do
+        allow_any_instance_of(EmsPhysicalInfra).to receive(:supports_provisioning?).and_return(true)
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {}, {})
+        expect(button.disabled?).to be_falsey
+      end
+    end
+
+    context "button should be disabled when there are no Physical infrastructure providers" do
+      it "disable the Provision button" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {}, {})
+        expect(button.disabled?).to be_truthy
+      end
+    end
+
+    context "button should be disabled when there are no Physical Servers" do
+      before do
+        FactoryGirl.create(:ems_physical_infra)
+      end
+
+      it "disable the Provision button" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {}, {})
+        expect(button.disabled?).to be_truthy
+      end
+    end
+
+    context "button should be disabled when there are no Physical infrastructure providers that support VM Provisioning" do
+      before do
+        ems = FactoryGirl.create(:ems_physical_infra)
+        @record = FactoryGirl.create(:physical_server, :ems_id => ems.id)
+      end
+
+      it "disable the Provision button" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {}, {})
+        expect(button.disabled?).to be_truthy
+      end
+    end
+  end
+end
