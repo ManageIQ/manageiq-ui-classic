5b524311854dea9ed7ada6c96d83cf1b00981889
Edgar Hernández <ehernand@redhat.com>
2018-01-03 13:35:45 -0600

    Revert "Merge pull request #2430 from aljesusg/issue_1740"
    This reverts commit 867c0f484527b085f45008b16145d40c7b85647f, reversing
    changes made to 1506f611ade7819598149a14381a1e9bcdef499f.

--- app/helpers/application_helper/button/miq_report_copy.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationHelper::Button::MiqReportCopy < ApplicationHelper::Button::MiqReportAction
-  def disabled?
-    %w(MiddlewareServerPerformance MiddlewareDatasourcePerformance).include?(@record.db)
-  end
-end
