12a4b9ac659eb9a2d4e6839ed3251168a4f41438
lpichler <lpichler@redhat.com>
2018-01-04 19:56:42 +0100

    require allowed network providers

--- app/helpers/application_helper/button/cloud_network_new.rb
+++ app/helpers/application_helper/button/cloud_network_new.rb
@@ -3,6 +3,10 @@ class ApplicationHelper::Button::CloudNetworkNew < ApplicationHelper::Button::Bu
     ::EmsNetwork.all.any? { |ems| CloudNetwork.class_by_ems(ems).supports_create? }
   end
 
+  def role_allows_feature?
+    role_allows?(:feature => 'ems_network_show_list')
+  end
+
   def disabled?
     @error_message = _("No cloud providers support creating cloud networks.") unless supports_button_action?
     super || @error_message.present?
