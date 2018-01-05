d25e180a80fe5c4c6e89f5d7b6c13fc4e3818888
lpichler <lpichler@redhat.com>
2018-01-05 09:29:11 +0100

    Require allowed network providers for toolbar button
    in new network router form
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1526803

--- app/helpers/application_helper/button/network_router_new.rb
+++ app/helpers/application_helper/button/network_router_new.rb
@@ -6,6 +6,10 @@ class ApplicationHelper::Button::NetworkRouterNew < ApplicationHelper::Button::B
     end
   end
 
+  def role_allows_feature?
+    role_allows?(:feature => 'ems_network_show_list')
+  end
+
   # disable button if no active providers support create action
   def disabled?
     ::EmsNetwork.all.none? { |ems| NetworkRouter.class_by_ems(ems).supports_create? }
