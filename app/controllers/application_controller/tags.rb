cb8145ee8780100626217f903e157fee78255515
Milan Zázrivec <mzazrivec@redhat.com>
2018-01-10 12:20:10 +0100

    Merge pull request #3201 from ZitaNemeckova/fix_cat_dropdown
    Fix dropdown in tagging
    (cherry picked from commit c1a7b38b7ae06cbbb4afee8433079334675eb78c)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1533233

--- app/controllers/application_controller/tags.rb
+++ app/controllers/application_controller/tags.rb
@@ -222,7 +222,7 @@ module ApplicationController::Tags
     end
 
     # Set to first category, if not already set
-    @edit[:cat] ||= cats.min_by(&:description)
+    @edit[:cat] ||= cats.min_by(&:name)
 
     unless @object_ids.blank?
       @tagitems = @tagging.constantize.where(:id => @object_ids).sort_by { |t| t.name.try(:downcase).to_s }
