5486ed75d0d7292cc3b8e9e89920791840a15cfc
Jason Frey <fryguy9@gmail.com>
2018-01-15 16:19:55 -0500

    Merge pull request #3166 from agrare/bz_1529725_storage_smartstate_via_ems
    Scan Storage via EMS not Host
    (cherry picked from commit 805fae2b55f99da96b8f7dcf9ebbfb8aed1d5bf2)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1534753

--- spec/helpers/application_helper/buttons/storage_scan_spec.rb
+++ spec/helpers/application_helper/buttons/storage_scan_spec.rb
@@ -1,14 +1,15 @@
 describe ApplicationHelper::Button::StorageScan do
   let(:view_context) { setup_view_context_with_sandbox({}) }
-  let(:host) { FactoryGirl.create(:host, :state => 'on') }
-  let(:hosts) { [host] }
-  let(:authenticated_hosts) { hosts }
-  let(:record) { FactoryGirl.create(:storage, :hosts => hosts) }
+  let(:record) { FactoryGirl.create(:storage) }
   let(:feature) { :smartstate_analysis }
   let(:props) { {:options => {:feature => feature}} }
   let(:button) { described_class.new(view_context, {}, {'record' => record}, props) }
+  let(:ems)                  { FactoryGirl.create(:ems_vmware) }
+  let(:emss)                 { [ems] }
+  let(:emss_with_valid_auth) { [ems] }
 
-  before { allow(record).to receive(:active_hosts_with_authentication_status_ok).and_return(authenticated_hosts) }
+  before { allow(record).to receive(:ext_management_systems).and_return(emss) }
+  before { allow(record).to receive(:ext_management_systems_with_authentication_status_ok).and_return(emss_with_valid_auth) }
 
   it_behaves_like 'a generic feature button after initialization'
 
@@ -19,22 +20,19 @@ describe ApplicationHelper::Button::StorageScan do
   context 'when feature is supported' do
     describe '#calculate_properties' do
       before do
-        allow(record).to receive(:supports?).and_return(true)
         button.calculate_properties
       end
 
-      context 'and there are active hosts' do
-        context 'but none has valid credentials for the Datastore' do
-          let(:authenticated_hosts) { [] }
-          it_behaves_like 'a disabled button', 'There are no running Hosts with valid credentials for this Datastore'
-        end
-        context 'with valid credentials for this Datastore' do
-          it_behaves_like 'an enabled button'
-        end
+      context 'but no EMSs are present' do
+        let(:emss) { [] }
+        it_behaves_like 'a disabled button', 'Smartstate Analysis cannot be performed on selected Datastore'
       end
-      context 'and there are no active hosts' do
-        let(:host) { FactoryGirl.create(:host, :state => 'off') }
-        it_behaves_like 'a disabled button', 'SmartState Analysis cannot be performed when there is no active Host'
+      context 'but no EMS has valid credentials for the Datastore' do
+        let(:emss_with_valid_auth) { [] }
+        it_behaves_like 'a disabled button', 'There are no EMSs with valid credentials for this Datastore'
+      end
+      context 'with valid credentials for this Datastore' do
+        it_behaves_like 'an enabled button'
       end
     end
   end
