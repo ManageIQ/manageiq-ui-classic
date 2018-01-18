1eab7cb2c0588a1f46a8716675ca73885124ca03
Nick LaMuro <nicklamuro@gmail.com>
2018-01-18 12:57:40 -0600

    Add comment about leak in engine.rb

--- lib/manageiq/ui/classic/engine.rb
+++ lib/manageiq/ui/classic/engine.rb
@@ -31,8 +31,18 @@ module ManageIQ
   module UI
     module Classic
       class Engine < ::Rails::Engine
+
+        # NOTE:  If you are going to make changes to autoload_paths, please make
+        # sure they are all strings.  Rails will push these paths into the
+        # $LOAD_PATH.
+        #
+        # More info can be found in the ruby-lang bug:
+        #
+        #   https://bugs.ruby-lang.org/issues/14372
+        #
         config.autoload_paths << root.join('app', 'controllers', 'mixins').to_s
         config.autoload_paths << root.join('lib').to_s
+
         if Rails.env.production? || Rails.env.test?
           require 'uglifier'
           config.assets.js_compressor = Uglifier.new(
