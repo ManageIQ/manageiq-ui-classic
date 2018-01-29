698293a806e618bbcb66bf7f9ddb4fc8a9adba02
Ohad Levy <ohadlevy@gmail.com>
2018-01-29 15:48:13 +0200

    removed failing test.

--- app/javascript/packs/hello_angular.test.js
+++
@@ -1,7 +0,0 @@
-/** global: jest */
-import './application-common';
-
-describe('it should log to the console hello angular', () => {
-  window.console.log = jest.fn();
-  expect(window.console.log).toBeCalled();
-});
