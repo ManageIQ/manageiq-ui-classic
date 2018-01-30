bcd40074b0447e1f627d6e4b3602af6fc91c0ddf
Martin Hradil <mhradil@redhat.com>
2018-01-30 18:55:24 +0000

    javascript_include_tag & stylesheet_link_tag - prefix bower deps with bower_components/

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,11 @@ Rails.application.config.assets.precompile << proc do |filename, path|
 end
 
 Rails.application.config.assets.precompile += %w(
-  codemirror/modes/*.js
-  codemirror/themes/*.css
-  jquery
-  jquery-ui
+  bower_components/codemirror/modes/*.js
+  bower_components/codemirror/themes/*.css
+  bower_components/jquery/dist/jquery.js
+  bower_components/jquery-ui/jquery-ui.js
+
   jquery_overrides.js
   remote_consoles/*.js
   remote_console.js
