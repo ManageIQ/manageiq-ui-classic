c525ab7933f6f427073c393e6278e69307aaf50f
sasoc <saso.cvitkovic@gmail.com>
2018-01-30 11:03:10 +0100

    Fix Nuage provider update
    There were two problems when editing Nuage network provider. If
    network that you edited had no event, there would still be AMQP
    selected under the Events tab. That made Save button disabled
    since hostname must not be empty and everything has to be verified.
    Now None is selected in case of no event.
    The second problem was that when a network was updated error occurred
    while redirecting, since you would only get one argument and
    `ems_path` required two. Now `ems_path` works same as
    in the cloud provider controller.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1534457

---
+++ spec/controllers/mixins/ems_common_angular_spec.rb
@@ -0,0 +1,57 @@
+describe Mixins::EmsCommonAngular do
+  context '.retrieve_event_stream_selection' do
+    let(:network_controller) { EmsNetworkController.new }
+    let(:ems_nuage) { FactoryGirl.create(:ems_nuage_network_with_authentication) }
+    let(:ems_openstack) { FactoryGirl.create(:ems_openstack_with_authentication) }
+
+    it 'when amqp' do
+      ems_nuage.endpoints << Endpoint.create(:role => 'amqp', :hostname => 'hostname')
+      network_controller.instance_variable_set(:@ems, ems_nuage)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('amqp')
+    end
+
+    it 'when ceilometer' do
+      ems_openstack.endpoints << Endpoint.create(:role => 'ceilometer', :hostname => 'hostname')
+      network_controller.instance_variable_set(:@ems, ems_openstack)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('ceilometer')
+    end
+
+    it 'when ceilometer and amqp has empty hostname' do
+      ems_nuage.endpoints << Endpoint.create(:role => 'ceilometer', :hostname => 'hostname')
+      ems_nuage.endpoints << Endpoint.create(:role => 'amqp')
+      network_controller.instance_variable_set(:@ems, ems_nuage)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('ceilometer')
+    end
+
+    it 'when amqp and ceilometer has empty hostname' do
+      ems_nuage.endpoints << Endpoint.create(:role => 'ceilometer')
+      ems_nuage.endpoints << Endpoint.create(:role => 'amqp', :hostname => 'hostname')
+      network_controller.instance_variable_set(:@ems, ems_nuage)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('amqp')
+    end
+
+    it 'none when amqp and ceilometer have empty hostnames' do
+      ems_nuage.endpoints << Endpoint.create(:role => 'ceilometer')
+      ems_nuage.endpoints << Endpoint.create(:role => 'amqp')
+      network_controller.instance_variable_set(:@ems, ems_nuage)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('none')
+    end
+
+    it 'ceilometer when openstack provider when amqp and ceilometer have nil endpoints' do
+      network_controller.instance_variable_set(:@ems, ems_openstack)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('ceilometer')
+    end
+
+    it 'when none' do
+      network_controller.instance_variable_set(:@ems, ems_nuage)
+
+      expect(network_controller.send(:retrieve_event_stream_selection)).to eq('none')
+    end
+  end
+end
