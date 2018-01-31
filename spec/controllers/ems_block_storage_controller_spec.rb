9890ab09ce07da64ce539f4e7c93456bff7fb43e
lpichler <lpichler@redhat.com>
2018-01-31 11:49:59 +0100

    Fix that on report data request the check on role feauture was skipped
    role feature has been inherited from controller and action name and
    it leads to check role
    for example ems_block_storage_report_data and it should be
    ems_block_storage_show_list.

--- spec/controllers/ems_block_storage_controller_spec.rb
+++ spec/controllers/ems_block_storage_controller_spec.rb
@@ -1,3 +1,33 @@
 describe EmsBlockStorageController do
   include_examples :shared_examples_for_ems_block_storage_controller, %w(openstack)
+
+  describe "#check_generic_rbac" do
+    let(:feature) { MiqProductFeature.find_all_by_identifier(%w(cloud_subnet_new)) }
+    let(:role)    { FactoryGirl.create(:miq_user_role, :miq_product_features => feature) }
+    let(:group)   { FactoryGirl.create(:miq_group, :miq_user_role => role) }
+    let(:user)    { FactoryGirl.create(:user, :miq_groups => [group]) }
+
+    before do
+      EvmSpecHelper.create_guid_miq_server_zone
+      EvmSpecHelper.seed_specific_product_features(%w(cloud_subnet_new ems_block_storage_show_list))
+
+      allow(User).to receive(:current_user).and_return(user)
+      allow(Rbac).to receive(:role_allows?).and_call_original
+      login_as user
+    end
+
+    it "denies access because user don't have needed role feature" do
+      controller.action_name = 'report_data'
+      expect(controller.send(:check_generic_rbac)).to be_falsey
+    end
+
+    context 'user has needed role feature' do
+      let(:feature) { MiqProductFeature.find_all_by_identifier(%w(ems_block_storage_show_list)) }
+
+      it "allows access" do
+        controller.action_name = 'report_data'
+        expect(controller.send(:check_generic_rbac)).to be_truthy
+      end
+    end
+  end
 end
