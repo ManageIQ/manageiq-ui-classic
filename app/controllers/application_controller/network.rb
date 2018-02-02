58e9f54a3a8e8b8ed3e0fda25e0c69161834b265
Alexander Demichev <ademicev@redhat.com>
2018-02-02 15:54:18 +0100

    remove undercloud from filtered result

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,7 +2,8 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    openstack_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Openstack::NetworkManager).select(:id, :name, :parent_ems_id)
+    ems_clouds = ManageIQ::Providers::Openstack::NetworkManager.joins(:parent_manager).where(:parent_managers_ext_management_systems=>{:type=>'ManageIQ::Providers::Openstack::CloudManager'})
+    openstack_network_manager = Rbac::Filterer.filtered(ems_clouds).select(:id, :name, :parent_ems_id)
     redhat_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Redhat::NetworkManager).select(:id, :name, :parent_ems_id)
     openstack_network_manager + redhat_network_manager
   end
