bfce3bf4c8c1be6b00c29a675583ba9fa0ccf854
Martin Hradil <mhradil@redhat.com>
2018-02-09 14:14:21 +0000

    remote_consoles - fix bower dependency paths
    should have been part of #3343

--- app/assets/javascripts/remote_consoles/spice.js
+++ app/assets/javascripts/remote_consoles/spice.js
@@ -1,5 +1,5 @@
-//= require jquery
-//= require spice-html5-bower
+//= require bower_components/jquery/dist/jquery
+//= require bower_components/spice-html5-bower/spice-html5-rails
 //= require_tree ../locale
 //= require gettext/all
 
