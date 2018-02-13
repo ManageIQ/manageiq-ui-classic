6ffc72a14a80ff40dd160638109c66d480a4b354
Harpreet Kataria <hkataria@redhat.com>
2018-02-13 15:48:44 -0500

    Merge pull request #3403 from mansam/fix-bz-1544732
     Instances without tenants should not cause navigation to fail on InstanceAttach checks
    (cherry picked from commit 527ebafa6a2897f03bd9d34a5c31dc35bac759f1)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1544960

--- app/helpers/application_helper/button/instance_attach.rb
+++ app/helpers/application_helper/button/instance_attach.rb
@@ -1,6 +1,6 @@
 class ApplicationHelper::Button::InstanceAttach < ApplicationHelper::Button::Basic
   def disabled?
-    if @record.cloud_tenant.cloud_volumes.where(:status => 'available').count.zero?
+    if @record.cloud_tenant.nil? || @record.cloud_tenant.cloud_volumes.where(:status => 'available').count.zero?
       @error_message = _("There are no Cloud Volumes available to attach to this Instance.")
     end
     @error_message.present?
