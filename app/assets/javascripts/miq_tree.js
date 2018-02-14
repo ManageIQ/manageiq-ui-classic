f5ad53d802d5a3f25c0a19633142c2cbeba4f8b5
Milan Zázrivec <mzazrivec@redhat.com>
2018-02-14 12:41:12 +0100

    Merge pull request #3400 from skateman/copy-nodes
    Copy nodes object to fix iteration when adding new tree items
    (cherry picked from commit e8f8017c866e38483c91240b9b39dd071342c3d0)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1565389

--- app/assets/javascripts/miq_tree.js
+++ app/assets/javascripts/miq_tree.js
@@ -42,7 +42,7 @@ function miqTreeResetState(treename) {
 function miqRemoveNodeChildren(treename, key) {
   var node = miqTreeFindNodeByKey(treename, key);
   if (node.nodes) {
-    node.nodes.forEach(function (child) {
+    node.nodes.slice().forEach(function (child) {
       miqTreeObject(treename).removeNode(child);
     });
   }
