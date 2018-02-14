c1d8f212ec734e1dc4f9ede5358d12b7128f3274
Martin Hradil <himdel@seznam.cz>
2018-02-14 13:35:18 +0000

    Merge pull request #3128 from evertmulder/ResizeDisk_ReconfigureScreen
    Resize disk reconfigure screen
    (cherry picked from commit 8835899188969665caca9894400db51c48dbd645)

--- app/assets/javascripts/controllers/reconfigure/reconfigure_form_controller.js
+++ app/assets/javascripts/controllers/reconfigure/reconfigure_form_controller.js
@@ -2,25 +2,27 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
   var vm = this;
   var init = function() {
     vm.reconfigureModel = {
-      memory:                  '0',
-      memory_type:             '',
-      socket_count:            '1',
-      cores_per_socket_count:  '1',
-      total_cpus:              '1',
-      vmdisks:                 [],
-      hdType:                  'thin',
-      hdMode:                  'persistent',
-      hdSize:                  '',
-      hdUnit:                  'MB',
-      new_controller_type:     'VirtualLsiLogicController',
-      cb_dependent:            true,
-      addEnabled:              false,
-      cb_bootable:             false,
-      vmAddDisks:              [],
-      vmRemoveDisks:           []
+      memory: '0',
+      memory_type: '',
+      socket_count: '1',
+      cores_per_socket_count: '1',
+      total_cpus: '1',
+      vmdisks: [],
+      hdType: 'thin',
+      hdMode: 'persistent',
+      hdSize: '',
+      hdUnit: 'MB',
+      new_controller_type: 'VirtualLsiLogicController',
+      cb_dependent: true,
+      addEnabled: false,
+      enableAddDiskButton: true,
+      cb_bootable: false,
+      vmAddDisks: [],
+      vmRemoveDisks: [],
+      vmResizeDisks: [],
     };
     vm.cb_disks = false;
-    vm.hdpattern = "^[1-9][0-9]*$";
+    vm.hdpattern = '^[1-9][0-9]*$';
     vm.reconfigureFormId = reconfigureFormId;
     vm.afterGet = false;
     vm.objectIds = objectIds;
@@ -33,10 +35,11 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
 
     ManageIQ.angular.scope = vm;
 
-    if (reconfigureFormId == 'new')
+    if (reconfigureFormId === 'new') {
       vm.newRecord = true;
-    else
+    } else {
       vm.newRecord = false;
+    }
 
     miqService.sparkleOn();
     $http.get('reconfigure_form_fields/' + reconfigureFormId + ',' + vm.objectIds)
@@ -44,23 +47,37 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
       .catch(miqService.handleFailure);
   };
 
-  vm.canValidateBasicInfo = function () {
-    if (vm.isBasicInfoValid())
+  vm.setEnableAddDiskButton = function() {
+    var nrDisksAfterReconfigure = 0;
+    angular.forEach(vm.reconfigureModel.vmdisks, function(disk) {
+      switch (disk.add_remove) {
+        case 'remove':
+          break;
+        default:
+          nrDisksAfterReconfigure++;
+          break;
+      }
+    });
+    vm.reconfigureModel.enableAddDiskButton = (nrDisksAfterReconfigure < 4);
+  };
+
+  vm.canValidateBasicInfo = function() {
+    if (vm.isBasicInfoValid()) {
       return true;
-    else
-      return false;
+    }
+    return false;
   };
 
   vm.isBasicInfoValid = function() {
-    if(( $scope.angularForm.memory && !$scope.angularForm.memory.$valid) ||
-      ($scope.angularForm.socket_count && !$scope.angularForm.socket_count.$valid) ||
-      ($scope.angularForm.mem_type && !$scope.angularForm.mem_type.$valid) ||
-      ($scope.angularForm.cores_per_socket_count && !$scope.angularForm.cores_per_socket_count.$valid) ||
-      ($scope.angularForm.total_cpus && !$scope.angularForm.total_cpus.$valid)
-      ($scope.angularForm.hdSize && !$scope.angularForm.hdSize.$valid))
+    if (($scope.angularForm.memory && ! $scope.angularForm.memory.$valid) ||
+      ($scope.angularForm.socket_count && ! $scope.angularForm.socket_count.$valid) ||
+      ($scope.angularForm.mem_type && ! $scope.angularForm.mem_type.$valid) ||
+      ($scope.angularForm.cores_per_socket_count && ! $scope.angularForm.cores_per_socket_count.$valid) ||
+      ($scope.angularForm.total_cpus && ! $scope.angularForm.total_cpus.$valid) ||
+      ($scope.angularForm.hdSize && ! $scope.angularForm.hdSize.$valid)) {
       return false;
-    else
-      return true;
+    }
+    return true;
   };
 
   vm.cbChange = function() {
@@ -68,80 +85,106 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
     var cpuUnchanged = false;
     miqService.miqFlashClear();
 
-    if (!vm.newRecord)
+    if (! vm.newRecord) {
       return;
-    $scope.angularForm.$setValidity("unchanged", true);
+    }
+    $scope.angularForm.$setValidity('unchanged', true);
 
     if (vm.cb_memory) {
       var memorynow = vm.reconfigureModel.memory;
       var memoryprev = vm.modelCopy.memory;
-      if (vm.reconfigureModel.memory_type == 'GB')
+      if (vm.reconfigureModel.memory_type === 'GB') {
         memorynow *= 1024;
-      if(vm.modelCopy.memory_type == 'GB')
+      }
+      if (vm.modelCopy.memory_type === 'GB') {
         memoryprev *= 1024;
-      if (memorynow == memoryprev)
+      }
+      if (memorynow === memoryprev) {
         memUnchanged = true;
+      }
     }
 
-    if (vm.cb_cpu && ((vm.reconfigureModel.socket_count == vm.modelCopy.socket_count)) &&
-      (vm.reconfigureModel.cores_per_socket_count == vm.modelCopy.cores_per_socket_count))
+    if (vm.cb_cpu && ((vm.reconfigureModel.socket_count === vm.modelCopy.socket_count)) &&
+      (vm.reconfigureModel.cores_per_socket_count === vm.modelCopy.cores_per_socket_count)) {
       cpuUnchanged = true;
+    }
 
     if (vm.cb_memory && vm.cb_cpu && memUnchanged && cpuUnchanged) {
-      miqService.miqFlash("warn", __("Change Memory and Processor value to submit reconfigure request"));
-      $scope.angularForm.$setValidity("unchanged", false);
+      miqService.miqFlash('warn', __('Change Memory and Processor value to submit reconfigure request'));
+      $scope.angularForm.$setValidity('unchanged', false);
     } else {
       if (vm.cb_memory && memUnchanged) {
-        miqService.miqFlash("warn", __("Change Memory value to submit reconfigure request"));
-        $scope.angularForm.$setValidity("unchanged", false);
+        miqService.miqFlash('warn', __('Change Memory value to submit reconfigure request'));
+        $scope.angularForm.$setValidity('unchanged', false);
       }
       if (vm.cb_cpu && cpuUnchanged) {
-        miqService.miqFlash("warn", __("Change Processor Sockets or Cores Per Socket value to submit reconfigure request"));
-        $scope.angularForm.$setValidity("unchanged", false);
+        miqService.miqFlash('warn', __('Change Processor Sockets or Cores Per Socket value to submit reconfigure request'));
+        $scope.angularForm.$setValidity('unchanged', false);
       }
     }
   };
 
   vm.processorValueChanged = function() {
-    if (vm.reconfigureModel.socket_count != '' && vm.reconfigureModel.cores_per_socket_count != '') {
-      var vtotal_cpus = parseInt(vm.reconfigureModel.socket_count, 10) * parseInt(vm.reconfigureModel.cores_per_socket_count, 10);
-      vm.reconfigureModel.total_cpus = vtotal_cpus.toString();
+    if (vm.reconfigureModel.socket_count !== '' && vm.reconfigureModel.cores_per_socket_count !== '') {
+      var vtotalCpus = parseInt(vm.reconfigureModel.socket_count, 10) * parseInt(vm.reconfigureModel.cores_per_socket_count, 10);
+      vm.reconfigureModel.total_cpus = vtotalCpus.toString();
     }
     vm.cbChange();
   };
 
   vm.memtypeChanged = function() {
-    if (vm.reconfigureModel.memory_type == "GB" && vm.mem_type_prev == "MB")
+    if (vm.reconfigureModel.memory_type === 'GB' && vm.mem_type_prev === 'MB') {
       vm.reconfigureModel.memory = ~~(parseInt(vm.reconfigureModel.memory, 10) / 1024);
-    else if (vm.reconfigureModel.memory_type == "MB" && vm.mem_type_prev == "GB")
+    } else if (vm.reconfigureModel.memory_type === 'MB' && vm.mem_type_prev === 'GB') {
       vm.reconfigureModel.memory =  parseInt(vm.reconfigureModel.memory, 10) * 1024;
+    }
     vm.mem_type_prev = vm.reconfigureModel.memory_type;
-    $scope.angularForm['memory'].$validate();
+    $scope.angularForm.memory.$validate();
     vm.cbChange();
   };
 
-
   vm.updateDisksAddRemove = function() {
-    vm.reconfigureModel.vmAddDisks.length = 0;
-    vm.reconfigureModel.vmRemoveDisks.length = 0;
-    for (var disk in vm.reconfigureModel.vmdisks) {
-      if (vm.reconfigureModel.vmdisks[disk]['add_remove'] === 'remove') {
-        vm.reconfigureModel.vmRemoveDisks.push({disk_name: vm.reconfigureModel.vmdisks[disk].hdFilename,
-                                   delete_backing: vm.reconfigureModel.vmdisks[disk].delete_backing});
-      } else if (vm.reconfigureModel.vmdisks[disk]['add_remove'] === 'add') {
-        var dsize = parseInt(vm.reconfigureModel.vmdisks[disk].hdSize, 10);
-        if (vm.reconfigureModel.vmdisks[disk].hdUnit == 'GB')
-          dsize *= 1024;
-        var dmode = (vm.reconfigureModel.vmdisks[disk].hdMode == 'persistent');
-        var dtype = (vm.reconfigureModel.vmdisks[disk].hdType == 'thin');
-        vm.reconfigureModel.vmAddDisks.push({disk_size_in_mb: dsize,
+    vm.reconfigureModel.vmAddDisks    = [];
+    vm.reconfigureModel.vmRemoveDisks = [];
+    vm.reconfigureModel.vmResizeDisks = [];
+    angular.forEach(vm.reconfigureModel.vmdisks, function(disk) {
+      if (disk.add_remove === 'remove') {
+        vm.reconfigureModel.vmRemoveDisks.push({
+          disk_name: disk.hdFilename,
+          delete_backing: disk.delete_backing});
+      } else if (disk.add_remove === 'resize') {
+        var diskSizeResize = parseInt(disk.hdSize, 10);
+        if (disk.hdUnit === 'GB') {
+          diskSizeResize *= 1024;
+        }
+        vm.reconfigureModel.vmResizeDisks.push({
+          disk_name: disk.hdFilename,
+          disk_size_in_mb: diskSizeResize});
+      } else if (disk.add_remove === 'add') {
+        var diskSizeAdd = parseInt(disk.hdSize, 10);
+        if (disk.hdUnit === 'GB') {
+          diskSizeAdd *= 1024;
+        }
+        var dmode = (disk.hdMode === 'persistent');
+        var dtype = (disk.hdType === 'thin');
+        vm.reconfigureModel.vmAddDisks.push({disk_size_in_mb: diskSizeAdd,
           persistent: dmode,
           thin_provisioned: dtype,
-          new_controller_type: vm.reconfigureModel.vmdisks[disk].new_controller_type,
-          dependent: vm.reconfigureModel.vmdisks[disk].cb_dependent,
-          bootable: vm.reconfigureModel.vmdisks[disk].cb_bootable});
+          new_controller_type: disk.new_controller_type,
+          dependent: disk.cb_dependent,
+          bootable: disk.cb_bootable});
+      } else if (disk.add_remove === '') {
+        disk.delete_backing = false;
       }
+    });
+    if (vm.reconfigureModel.vmAddDisks.length > 0 ||
+        vm.reconfigureModel.vmRemoveDisks.length > 0 ||
+        vm.reconfigureModel.vmResizeDisks.length > 0) {
+      vm.cb_disks = true;
+    } else {
+      vm.cb_disks = false;
     }
+    vm.setEnableAddDiskButton();
   };
 
   vm.resetAddValues = function() {
@@ -156,7 +199,8 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
   };
 
   vm.addDisk = function() {
-    vm.reconfigureModel.vmdisks.push({hdFilename:'',
+    vm.reconfigureModel.vmdisks.push({
+      hdFilename: '',
       hdType: vm.reconfigureModel.hdType,
       hdMode: vm.reconfigureModel.hdMode,
       hdSize: vm.reconfigureModel.hdSize,
@@ -166,13 +210,7 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
       cb_bootable: vm.reconfigureModel.cb_bootable,
       add_remove: 'add'});
     vm.resetAddValues();
-
     vm.updateDisksAddRemove();
-
-    if( vm.reconfigureModel.vmAddDisks.length > 0  || vm.reconfigureModel.vmRemoveDisks.length > 0)
-      vm.cb_disks = true;
-    else
-      vm.cb_disks = false;
   };
 
   vm.enableDiskAdd = function() {
@@ -184,57 +222,74 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
     vm.resetAddValues();
   };
 
-  vm.deleteDisk = function(name) {
-    for (var disk in vm.reconfigureModel.vmdisks) {
-      if (vm.reconfigureModel.vmdisks[disk].hdFilename === name)
-        vm.reconfigureModel.vmdisks[disk]['add_remove'] = 'remove';
-    }
-    vm.updateDisksAddRemove();
-
-    if( vm.reconfigureModel.vmAddDisks.length > 0  || vm.reconfigureModel.vmRemoveDisks.length > 0)
-      vm.cb_disks = true;
-    else
-      vm.cb_disks = false;
+  vm.enableResizeDisk = function(disk) {
+    disk.add_remove = 'resizing';
   };
 
-  vm.cancelAddRemoveDisk = function(vmDisk) {
+  vm.resizeDisk = function(disk) {
+    disk.add_remove = 'resize';
+    vm.updateDisksAddRemove();
+  };
+
+  vm.resizeDiskUnitChanged = function(disk) {
+    if (disk.hdUnit === 'MB') {
+      disk.hdSize = disk.hdSize * 1024;
+    } else if ((disk.hdSize % 1024) === 0) {
+      disk.hdSize = disk.hdSize / 1024;
+    } else {
+      disk.hdSize = '';
+    }
+  };
+
+  vm.deleteDisk = function(name) {
     for (var disk in vm.reconfigureModel.vmdisks) {
-      if (vm.reconfigureModel.vmdisks[disk].hdFilename === vmDisk['hdFilename']) {
-        if (vm.reconfigureModel.vmdisks[disk]['add_remove'] === 'remove') {
-          vm.reconfigureModel.vmdisks[disk]['add_remove'] = '';
-          vm.reconfigureModel.vmdisks[disk]['cb_deletebacking'] = false;
-          vm.reconfigureModel.vmdisks[disk]['cb_bootable'] = false;
-        } else if (vm.reconfigureModel.vmdisks[disk]['add_remove'] === 'add') {
-          var index = vm.reconfigureModel.vmdisks.indexOf(vmDisk);
-          vm.reconfigureModel.vmdisks.splice(index, 1);
-          break;
-        }
+      if (vm.reconfigureModel.vmdisks[disk].hdFilename === name) {
+        vm.reconfigureModel.vmdisks[disk].add_remove = 'remove';
       }
     }
     vm.updateDisksAddRemove();
+  };
 
-    if (!angular.equals(vm.reconfigureModel.vmAddDisks, vm.modelCopy.vmAddDisks) || !angular.equals(vm.reconfigureModel.vmRemoveDisks, vm.modelCopy.vmRemoveDisks))
-      vm.cb_disks = true;
-    else
-      vm.cb_disks = false;
+  vm.cancelAddRemoveDisk = function(vmDisk) {
+    switch (vmDisk.add_remove) {
+      case 'remove':
+        vmDisk.add_remove = '';
+        vmDisk.cb_deletebacking = false;
+        vmDisk.cb_bootable = false;
+        break;
+      case 'resize':
+      case 'resizing':
+        vmDisk.add_remove = '';
+        vmDisk.hdSize = vmDisk.orgHdSize;
+        vmDisk.hdUnit = vmDisk.orgHdUnit;
+        break;
+      case 'add':
+        var index = vm.reconfigureModel.vmdisks.indexOf(vmDisk);
+        vm.reconfigureModel.vmdisks.splice(index, 1);
+        break;
+      default:
+        break;
+    }
+    vm.updateDisksAddRemove();
   };
 
   var reconfigureEditButtonClicked = function(buttonName, serializeFields) {
     miqService.sparkleOn();
     var url = 'reconfigure_update/' + reconfigureFormId + '?button=' + buttonName;
-    if (serializeFields === undefined) {
+    if (typeof serializeFields === 'undefined') {
       miqService.miqAjaxButton(url);
     } else {
       miqService.miqAjaxButton(url, {objectIds: vm.objectIds,
-                                     cb_memory: vm.cb_memory,
-                                     cb_cpu: vm.cb_cpu,
-                                     memory: vm.reconfigureModel.memory,
-                                     memory_type: vm.reconfigureModel.memory_type,
-                                     socket_count: vm.reconfigureModel.socket_count,
-                                     cores_per_socket_count: vm.reconfigureModel.cores_per_socket_count,
-                                     vmAddDisks: vm.reconfigureModel.vmAddDisks,
-                                     vmRemoveDisks: vm.reconfigureModel.vmRemoveDisks
-                                    });
+        cb_memory: vm.cb_memory,
+        cb_cpu: vm.cb_cpu,
+        memory: vm.reconfigureModel.memory,
+        memory_type: vm.reconfigureModel.memory_type,
+        socket_count: vm.reconfigureModel.socket_count,
+        cores_per_socket_count: vm.reconfigureModel.cores_per_socket_count,
+        vmAddDisks: vm.reconfigureModel.vmAddDisks,
+        vmRemoveDisks: vm.reconfigureModel.vmRemoveDisks,
+        vmResizeDisks: vm.reconfigureModel.vmResizeDisks,
+      });
     }
   };
 
@@ -244,18 +299,20 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
   };
 
   vm.resetClicked = function() {
-    $scope.$broadcast ('resetClicked');
+    $scope.$broadcast('resetClicked');
     vm.reconfigureModel = angular.copy(vm.modelCopy);
     vm.cb_memory = vm.cb_memoryCopy;
     vm.cb_cpu = vm.cb_cpuCopy;
     vm.mem_type_prev = vm.reconfigureModel.memory_type;
+
+    angular.forEach(vm.reconfigureModel.vmdisks, function(disk) {
+      vm.cancelAddRemoveDisk(disk);
+    });
+
     $scope.angularForm.$setPristine(true);
     vm.updateDisksAddRemove();
-    if (!angular.equals(vm.reconfigureModel.vmAddDisks, vm.modelCopy.vmAddDisks) || !angular.equals(vm.reconfigureModel.vmRemoveDisks, vm.modelCopy.vmRemoveDisks))
-      vm.cb_disks = true;
-    else
-      vm.cb_disks = false;
-    miqService.miqFlash("warn", __("All changes have been reset"));
+
+    miqService.miqFlash('warn', __('All changes have been reset'));
   };
 
   vm.submitClicked = function() {
@@ -270,22 +327,22 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
 
   function getReconfigureFormData(response) {
     var data = response.data;
-
     vm.reconfigureModel.memory                 = data.memory;
     vm.reconfigureModel.memory_type            = data.memory_type;
     vm.reconfigureModel.socket_count           = data.socket_count;
     vm.reconfigureModel.cores_per_socket_count = data.cores_per_socket_count;
-    vm.mem_type_prev = vm.reconfigureModel.memory_type;
-    vm.cb_memory = data.cb_memory;
-    vm.cb_cpu = data.cb_cpu;
-    vm.reconfigureModel.vmdisks = angular.copy(data.disks);
-
+    vm.mem_type_prev                           = vm.reconfigureModel.memory_type;
+    vm.cb_memory                               = data.cb_memory;
+    vm.cb_cpu                                  = data.cb_cpu;
+    vm.reconfigureModel.vmdisks                = angular.copy(data.disks);
     vm.updateDisksAddRemove();
-
     angular.forEach(vm.reconfigureModel.vmdisks, function(disk) {
-      if (disk !== undefined
-        && ( disk.add_remove !== 'add' && disk.add_remove !== 'remove' )) {
-        disk.delete_backing = false;
+      if (typeof disk !== 'undefined') {
+        disk.orgHdSize = disk.hdSize;
+        disk.orgHdUnit = disk.hdUnit;
+        if ( disk.add_remove !== 'add' && disk.add_remove !== 'remove' ) {
+          disk.delete_backing = false;
+        }
       }
     });
     if (data.socket_count && data.cores_per_socket_count) {
@@ -295,9 +352,7 @@ ManageIQ.angular.app.controller('reconfigureFormController', ['$http', '$scope',
     vm.modelCopy = angular.copy( vm.reconfigureModel );
     vm.cb_memoryCopy = vm.cb_memory;
     vm.cb_cpuCopy = vm.cb_cpu;
-
     miqService.sparkleOff();
   }
-
   init();
 }]);
