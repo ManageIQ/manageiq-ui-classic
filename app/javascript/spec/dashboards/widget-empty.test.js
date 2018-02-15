1c0af0e374860a1b7012e95ec789314d50407702
Martin Hradil <mhradil@redhat.com>
2018-02-15 12:45:36 +0000

    minor fixes

--- app/javascript/spec/dashboards/widget-empty.test.js
+++ app/javascript/spec/dashboards/widget-empty.test.js
@@ -40,16 +40,17 @@ require('../../../assets/javascripts/components/widget-wrapper');
 
 describe('widget-empty', function() {
   var $scope, element, $compile;
+
   beforeEach(module('ManageIQ'));
   beforeEach(inject(function(_$compile_, $rootScope, $templateCache) {
     // FIXME: templateRequest is using $http to get the template, but angular-mocks prevents it
     $templateCache.put('/static/dropdown-menu.html.haml', '<div></div>');
     $scope = $rootScope;
-    $scope.miqButtonClicked = function(){};
+    $scope.miqButtonClicked = function() {};
     $scope.validForm = true;
     $compile = _$compile_;
-
   }));
+
   it('is rendered in widget-wrapper if widget-blank is set to true', function(done) {
     element = angular.element(
       '<form name="angularForm">' +
@@ -57,10 +58,13 @@ describe('widget-empty', function() {
       '</form>'
     );
     element = $compile(element)($scope);
+
     $scope.$digest();
-    setTimeout(function(){
+
+    setTimeout(function() {
       var widget = element.find("widget-empty");
-      expect(widget.length).not.toBe(0);
+      expect(widget.length).toBe(1);
+
       done();
     });
   });
