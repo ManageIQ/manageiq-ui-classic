fe2ef47e10be183326c6e51be3f4fa46b5d962a0
Martin Hradil <mhradil@redhat.com>
2018-02-15 12:42:04 +0000

    Widget-* specs - make them work in jest :)
    just needed to include/mock all the dependencies
    and not use jquery selectors

--- app/javascript/spec/dashboards/widget-empty.test.js
+++ app/javascript/spec/dashboards/widget-empty.test.js
@@ -1,21 +1,54 @@
-import * as angular from 'angular';
-import 'angular-mocks';
+require('angular');
+require('angular-mocks');
+const module = window.module;
+const inject = window.inject;
+
+window.ManageIQ = {
+  angular: {
+    app: angular.module('ManageIQ', []),
+  },
+};
+window.__ = (x) => x;
+
+// FIXME: app/assets/javascripts/services/
+ManageIQ.angular.app.service('miqService', function() {
+  this.handleFailure = () => null;
+});
+
+// FIXME: miq_application.js
+window.miqDeferred = () => {
+  var deferred = {};
+
+  deferred.promise = new Promise(function(resolve, reject) {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+
+  return deferred;
+};
+
+require('../../../assets/javascripts/components/widget-chart');
+require('../../../assets/javascripts/components/widget-empty');
+require('../../../assets/javascripts/components/widget-error');
+require('../../../assets/javascripts/components/widget-footer');
+require('../../../assets/javascripts/components/widget-menu');
+require('../../../assets/javascripts/components/widget-report');
+require('../../../assets/javascripts/components/widget-rss');
+require('../../../assets/javascripts/components/widget-spinner');
+require('../../../assets/javascripts/components/widget-wrapper');
+
 
 describe('widget-empty', function() {
   var $scope, element, $compile;
-    console.log(angular.mock);
-
-    beforeEach(() => {
-      angular.mock.module('ManageIQ');
-    });
-
-  beforeEach(angular.mock.inject(function(_$compile_, $rootScope, $templateCache) {
+  beforeEach(module('ManageIQ'));
+  beforeEach(inject(function(_$compile_, $rootScope, $templateCache) {
     // FIXME: templateRequest is using $http to get the template, but angular-mocks prevents it
     $templateCache.put('/static/dropdown-menu.html.haml', '<div></div>');
     $scope = $rootScope;
     $scope.miqButtonClicked = function(){};
     $scope.validForm = true;
     $compile = _$compile_;
+
   }));
   it('is rendered in widget-wrapper if widget-blank is set to true', function(done) {
     element = angular.element(
--- app/javascript/spec/dashboards/widget-wrapper.test.js
+++ app/javascript/spec/dashboards/widget-wrapper.test.js
@@ -1,3 +1,43 @@
+require('angular');
+require('angular-mocks');
+const module = window.module;
+const inject = window.inject;
+
+window.ManageIQ = {
+  angular: {
+    app: angular.module('ManageIQ', []),
+  },
+};
+window.__ = (x) => x;
+
+// FIXME: app/assets/javascripts/services/
+ManageIQ.angular.app.service('miqService', function() {
+  this.handleFailure = () => null;
+});
+
+// FIXME: miq_application.js
+window.miqDeferred = () => {
+  var deferred = {};
+
+  deferred.promise = new Promise(function(resolve, reject) {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+
+  return deferred;
+};
+
+require('../../../assets/javascripts/components/widget-chart');
+require('../../../assets/javascripts/components/widget-empty');
+require('../../../assets/javascripts/components/widget-error');
+require('../../../assets/javascripts/components/widget-footer');
+require('../../../assets/javascripts/components/widget-menu');
+require('../../../assets/javascripts/components/widget-report');
+require('../../../assets/javascripts/components/widget-rss');
+require('../../../assets/javascripts/components/widget-spinner');
+require('../../../assets/javascripts/components/widget-wrapper');
+
+
 describe('widget-wrapper', function() {
   var $scope, element, $compile;
   var widgetTypes = ['chart', 'menu', 'report', 'rss'];
@@ -11,22 +51,29 @@ describe('widget-wrapper', function() {
     $scope = $rootScope;
 
     $compile = _$compile_;
-    spyOn($http, 'get').and.callFake(function(){
-      return Promise.resolve({
-        data: {
-          content: "<div></div>",
-          minimized: false,
-          shortcuts: [],
-        },
-        status: 200,
-        statusText: 'OK',
-      });
+    spyOn($http, 'get').and.callFake(function(url) {
+      if (url === '/static/dropdown-menu.html.haml') {
+        return Promise.resolve({
+          data: "<div></div>",
+          status: 200,
+          statusText: 'OK',
+        });
+      } else {
+        return Promise.resolve({
+          data: {
+            content: "<div></div>",
+            minimized: false,
+            shortcuts: [],
+          },
+          status: 200,
+          statusText: 'OK',
+        });
+      }
     });
   }));
 
   widgetTypes.forEach(function (widget) {
     it('renders widget-' + widget +  ' when widget-type is ' + widget, function(done) {
-      debugger;
       element = angular.element(
         '<form name="angularForm">' +
         '  <widget-wrapper widget-id="42" widget-blank=false widget-buttons="null" widget-type="' + widget + '"></widget-wrapper>' +
@@ -35,7 +82,7 @@ describe('widget-wrapper', function() {
       element = $compile(element)($scope);
       $scope.$digest();
 
-      var $ctrl = element.find('widget-wrapper > div').scope().vm;
+      var $ctrl = element.find('widget-wrapper').find('div').scope().vm;
       $ctrl.promise.catch(function () {}).then(function(){
         $scope.$digest();
 
