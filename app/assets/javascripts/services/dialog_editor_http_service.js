eee9f52c294ca49ff2d80f195a5d9c9bfe90d26a
Milan Zázrivec <mzazrivec@redhat.com>
2018-02-19 15:10:09 +0100

    Merge pull request #3414 from skateman/dialog-editor-http
    Extract all API/HTTP calls from dialogEditorController to a service
    (cherry picked from commit a39e0f55ac6d60d0d016c0897b5774e2f35c9ebe)

---
+++ app/assets/javascripts/services/dialog_editor_http_service.js
@@ -0,0 +1,26 @@
+ManageIQ.angular.app.service('DialogEditorHttp', ['$http', 'API', function($http, API) {
+  this.loadDialog = function(id) {
+    return API.get('/api/service_dialogs/' + id + '?attributes=content,buttons,label');
+  };
+
+  this.saveDialog = function(id, action, data) {
+    return API.post('/api/service_dialogs' + id, {
+      action: action,
+      resource: data,
+    }, {
+      skipErrors: [400],
+    });
+  };
+
+  this.treeSelectorLoadData = function() {
+    return $http.get('/tree/automate_entrypoint').then(function(response) {
+      return response.data;
+    });
+  };
+
+  this.treeSelectorLazyLoadData = function(node) {
+    return $http.get('/tree/automate_entrypoint?id=' + encodeURIComponent(node.key)).then(function(response) {
+      return response.data;
+    });
+  };
+}]);
