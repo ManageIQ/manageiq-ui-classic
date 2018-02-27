77e1acce42a748baab139387b9442e17f069082f
Martin Hradil <mhradil@redhat.com>
2018-02-27 09:07:45 +0000

    RepositoryFormComponent - convert ansible repository form to a component

---
+++ app/assets/javascripts/components/repository-form-component.js
@@ -0,0 +1,5 @@
+ManageIQ.angular.app.component('repositoryFormComponent', {
+  controller: 'repositoryFormController',
+  controllerAs: 'vm',
+  templateUrl: '/static/ansible-repository-form.html.haml',
+});
