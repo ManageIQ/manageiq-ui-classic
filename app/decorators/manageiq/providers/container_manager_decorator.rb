6cdf8f918089c5ec7b58d1d26f5582f6adfb07c5
Dávid Halász <dhalasz@redhat.com>
2018-03-06 10:36:06 +0100

    Do not inherit decorators from other than MiqDecorator

--- app/decorators/manageiq/providers/container_manager_decorator.rb
+++ app/decorators/manageiq/providers/container_manager_decorator.rb
@@ -1,18 +1,28 @@
-module ManageIQ::Providers
-  class ContainerManagerDecorator < MiqDecorator
-    def quadicon(_n = nil)
-      {
-        :top_left     => {:text => container_nodes.size},
-        :top_right    => {:state_icon => enabled? ? "on" : "paused"},
-        :bottom_left  => {
-          :fileicon => fileicon,
-          :tooltip  => type
-        },
-        :bottom_right => {
-          :fileicon => status_img(self),
-          :tooltip  => authentication_status
-        }
+class ManageIQ::Providers::ContainerManagerDecorator < MiqDecorator
+  def self.fonticon
+    'pficon pficon-server'
+  end
+
+  def fonticon
+    nil
+  end
+
+  def fileicon
+    "svg/vendor-#{image_name}.svg"
+  end
+
+  def quadicon(_n = nil)
+    {
+      :top_left     => {:text => container_nodes.size},
+      :top_right    => {:state_icon => enabled? ? "on" : "paused"},
+      :bottom_left  => {
+        :fileicon => fileicon,
+        :tooltip  => type
+      },
+      :bottom_right => {
+        :fileicon => status_img(self),
+        :tooltip  => authentication_status
       }
-    end
+    }
   end
 end
--- app/decorators/manageiq/providers/infra_manager_decorator.rb
+++ app/decorators/manageiq/providers/infra_manager_decorator.rb
@@ -1,18 +1,28 @@
-module ManageIQ::Providers
-  class InfraManagerDecorator < ExtManagementSystemDecorator
-    def quadicon(_n = nil)
-      {
-        :top_left     => {:text => hosts.size},
-        :top_right    => {:text => total_vms},
-        :bottom_left  => {
-          :fileicon => fileicon,
-          :tooltip  => type
-        },
-        :bottom_right => {
-          :fileicon => status_img(self),
-          :tooltip  => authentication_status
-        }
+class ManageIQ::Providers::InfraManagerDecorator < ExtManagementSystemDecorator
+  def self.fonticon
+    'pficon pficon-server'
+  end
+
+  def fonticon
+    nil
+  end
+
+  def fileicon
+    "svg/vendor-#{image_name}.svg"
+  end
+
+  def quadicon(_n = nil)
+    {
+      :top_left     => {:text => hosts.size},
+      :top_right    => {:text => total_vms},
+      :bottom_left  => {
+        :fileicon => fileicon,
+        :tooltip  => type
+      },
+      :bottom_right => {
+        :fileicon => status_img(self),
+        :tooltip  => authentication_status
       }
-    end
+    }
   end
 end
