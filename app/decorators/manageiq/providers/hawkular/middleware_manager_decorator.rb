7799ab247fb764be4861b56dadd4b9fd51c72ec7
Dávid Halász <dhalasz@redhat.com>
2018-03-07 09:53:05 +0100

    Drop middleware decorator quadicon definition

--- app/decorators/manageiq/providers/hawkular/middleware_manager_decorator.rb
+++ app/decorators/manageiq/providers/hawkular/middleware_manager_decorator.rb
@@ -6,23 +6,4 @@ class ManageIQ::Providers::Hawkular::MiddlewareManagerDecorator < MiqDecorator
   def self.fileicon
     "svg/vendor-hawkular.svg"
   end
-
-  def quadicon(_n = nil)
-    {
-      :top_left     => {
-        :text => middleware_domains.size
-      },
-      :top_right    => {
-        :text => middleware_servers.size
-      },
-      :bottom_left  => {
-        :fileicon => fileicon,
-        :tooltip  => type
-      },
-      :bottom_right => {
-        :fileicon => status_img(self),
-        :tooltip  => authentication_status
-      }
-    }
-  end
 end
