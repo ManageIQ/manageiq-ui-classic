6434a6b9f75e7433dc708dfec84b6e6116b63593
Dávid Halász <dhalasz@redhat.com>
2018-03-13 17:40:19 +0100

    Define single_quad methods with static icons for the related decorators

--- app/decorators/container_volume_decorator.rb
+++ app/decorators/container_volume_decorator.rb
@@ -2,4 +2,10 @@ class ContainerVolumeDecorator < MiqDecorator
   def self.fonticon
     'pficon pficon-volume'
   end
+
+  def single_quad
+    {
+      :fileicon => '100/container_volume.png'
+    }
+  end
 end
--- app/decorators/host_aggregate_decorator.rb
+++ app/decorators/host_aggregate_decorator.rb
@@ -2,4 +2,10 @@ class HostAggregateDecorator < MiqDecorator
   def self.fonticon
     'pficon pficon-screen'
   end
+
+  def single_quad
+    {
+      :fileicon => '100/host_aggregate.png'
+    }
+  end
 end
