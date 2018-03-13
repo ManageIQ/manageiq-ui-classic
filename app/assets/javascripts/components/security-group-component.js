d153e1151ea94023e84b04183a72dc7bd943750a
swi2 <swi2@rejpal33.mendelu.cz>
2018-03-13 09:11:32 +0100

    securityGroupComponent - convert securityGroupController to component

---
+++ app/assets/javascripts/components/security-group-component.js
@@ -0,0 +1,5 @@
++ManageIQ.angular.app.component('securityGroupComponent', {
+ +  controller: 'securityGroupController',
+ +  controllerAs: 'vm',
+ +  templateUrl: '/static/security-group-form.html.haml',
+ +});
