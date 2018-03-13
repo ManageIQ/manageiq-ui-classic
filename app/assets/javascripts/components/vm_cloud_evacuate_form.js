c2b2f3a93565473729c7424a4db85085656924fc
Lucie Kaplanova <lucie.kap@gmail.com>
2018-03-13 09:10:58 +0100

    vmCloudEvacuateForm - create component and static-view

---
+++ app/assets/javascripts/components/vm_cloud_evacuate_form.js
@@ -0,0 +1,9 @@
+
+
+ManageIQ.angular.app.component('vmCloudEvacuateForm', {
+	controller: 'vmCloudEvacuateFormController',
+  controllerAs: 'vm',
+
+  templateUrl: '/static/ansible-evacuate-form.html.haml',
+
+});
