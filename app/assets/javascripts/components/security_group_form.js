f845911a8d632f2b02cde60e0ba8d7061f8dfc86
Aleandra Cicmancova <sasa.cicmancova@gmail.com>
2018-03-13 09:16:52 +0100

    Changed controller to component form.

---
+++ app/assets/javascripts/components/security_group_form.js
@@ -0,0 +1,5 @@
+MangageIQ.angular.app.component('securityGroupForm', {
+controllerAs: 'vm',
+controller: 'securityGroupForm',
+templateUrl: 'app/views/static/security_group_form.new.html.haml'
+});
---
+++ app/assets/javascripts/components/security_group_form_edit.js
@@ -0,0 +1,6 @@
+ManageIQ.angular.app.component('securityGroupForm', {
+	controller: 'securityFormController',
+	controllerAs: 'vm',
+	templateUrl: '/static/security_group_form.new.html.haml',
+
+});
