f208eaa48ff65b0bbae0832c8927dcb4e59042f3
domcucha <kousaldominik@seznam.cz>
2018-03-13 09:11:51 +0100

    convert cloud_tenant to component: create static and component files

---
+++ app/assets/javascripts/components/cloudTenantForm.js
@@ -0,0 +1,9 @@
+ManageIQ.angular.app.component('cloudTenantForm', {
+	controller: 'cloudTenantFormController',
+	controllerAs: 'vm',
+	templateUrl: '/static/cloudTenantForm/cloud-tenant-form-edit.html.haml',
+	bindings: {
+		'cloudTenantFormId': '@',
+	},
+	
+});
