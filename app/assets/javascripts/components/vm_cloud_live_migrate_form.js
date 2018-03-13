136220d80a2d516fce8ad4e2bdf615465d347c59
Dušan Neckař <neckar.dusan@seznam.cz>
2018-03-13 09:12:04 +0100

    vmCloudLiveMigrateForm - Create new component and View for component

---
+++ app/assets/javascripts/components/vm_cloud_live_migrate_form.js
@@ -0,0 +1,5 @@
+ManageIQ.angular.app.component('vmCloudLiveMigrateForm', {
+  controller: 'vmCloudLiveMigrateFormController',
+  controllerAs: 'vm',
+  templateUrl: '/static/_live_migrate.html.haml',
+});
