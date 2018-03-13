ce47b0856d7d88b096f427c5281d68cdb7524b92
Roman Jakl <xjakl@mendelu.cz>
2018-03-13 09:12:36 +0100

    changed controller to component

---
+++ app/assets/javascripts/components/cloud_network_form.js
@@ -0,0 +1,5 @@
+ManageIQ.angular.app.component('cloudNetworkForm', {
+  controllerAs: 'vm',
+  controller: 'cloudNetworkFormController',
+  templateUrl: '/static/cloud_network/change.html.haml',
+});
