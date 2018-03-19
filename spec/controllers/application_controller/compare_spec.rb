41646df50631fe98ab7d95ad96a67c40eb9a6c62
Martin Slemr <mslemr@redhat.com>
2018-03-19 18:46:21 +0100

    Fixes drift's total counter
    Like compare's total counters, similar code
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1469111

--- spec/controllers/application_controller/compare_spec.rb
+++ spec/controllers/application_controller/compare_spec.rb
@@ -16,15 +16,20 @@ describe ApplicationController do
 end
 
 describe EmsClusterController do
+  let(:compare_ids) { [1, 2] }
+  let(:drift_ids) { [1.day.ago, 2.days.ago] }
+
   # view = MiqCompare originally
   # section = Hash[:name => :_model_]
   # fields = [{:name => :name, :header => "Name"},
   #           {:name => 'hosts.size', :header => 'Number of hosts'},
   #           {:name => 'vms.size', :header => 'Number of vms'}
   # ]
-  # view.ids = [1,2]
-  # view.results = {1 => {:_model_ => {:name => {:_value_ => "Cluster1"}, :"hosts.size" => {:_value_ => 1}, :"vms_size" => {:_value_ => 1} } },
-  #                 2 => {:_model_ => {:name => {:_value_ => "Cluster2"}, :"hosts.size" => {:_value_ => 1}, :"vms_size" => {:_value_ => 1} } }
+  # view.ids: [Time, Time] for drifts
+  # view.ids = [1,2] for compares
+  #
+  # view.results = {view.ids[0] => {:_model_ => {:name => {:_value_ => "Cluster1"}, :"hosts.size" => {:_value_ => 1}, :"vms_size" => {:_value_ => 1} } },
+  #                 view.ids[1] => {:_model_ => {:name => {:_value_ => "Cluster2"}, :"hosts.size" => {:_value_ => 1}, :"vms_size" => {:_value_ => 1} } }
   # }
   class self::TestView
     attr_accessor :ids, :results
@@ -38,36 +43,30 @@ describe EmsClusterController do
   let(:view) { self.class::TestView.new }
 
   let(:section) { {:name => :_model_} }
-  let(:fields) { {} }
 
-  let(:sample_values) do
-    # name of fields has to be same
-    {
-      1 => [{:name => :field1, :value => 1},
-            {:name => :field2, :value => 20},
-            {:name => :field3, :value => 30},
-            {:name => :field4, :value => 40},
-            {:name => :field5, :value => 'string 1'},
-            {:name => :field6, :value => 'string 2'}],
+  let(:values1) do
+    [{:name => :field1, :value => 1},
+     {:name => :field2, :value => 20},
+     {:name => :field3, :value => 30},
+     {:name => :field4, :value => 40},
+     {:name => :field5, :value => 'string 1'},
+     {:name => :field6, :value => 'string 2'}]
+  end
 
-      2 => [{:name => :field1, :value => 10},
-            {:name => :field2, :value => 20},
-            {:name => :field3, :value => 'xx'},
-            {:name => :field4, :value => nil},
-            {:name => :field5, :value => 'string 1'},
-            {:name => :field6, :value => 'other string'}]
-    }
+  let(:values2) do
+    [{:name => :field1, :value => 10},
+     {:name => :field2, :value => 20},
+     {:name => :field3, :value => 'xx'},
+     {:name => :field4, :value => nil},
+     {:name => :field5, :value => 'string 1'},
+     {:name => :field6, :value => 'other string'}]
   end
 
   before(:each) do
     controller.instance_variable_set(:@compressed, false)
     controller.instance_variable_set(:@exists_mode, false)
-
-    view.ids = [1, 2]
-    view.results = {}
-    @fields = []
-    init_fields_and_results(sample_values)
   end
+
   # simulation of fields param
   def fill_fields(rows)
     rows.each do |row|
@@ -86,31 +85,86 @@ describe EmsClusterController do
     end
   end
 
+  def init_sample_values(ids)
+    @sample_values = {
+      ids[0] => values1,
+      ids[1] => values2
+    }
+  end
+
   def init_fields_and_results(clusters)
-    fill_fields(clusters[1])
+    fill_fields(clusters[view.ids[0]])
 
     fill_view_results(clusters)
   end
 
-  it "sets total fields in section with 'All attributes' button to number of fields" do
-    controller.instance_variable_set(:@sb, :miq_temp_params => 'all')
+  context 'compares' do
+    before(:each) do
+      view.ids = compare_ids
 
-    all_fields_count = sample_values[1].size
+      @sample_values = {}
+      init_sample_values(view.ids)
 
-    expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(all_fields_count)
+      view.results = {}
+      @fields = []
+      init_fields_and_results(@sample_values)
+    end
+
+    it "sets total fields in section with 'All attributes' button to number of fields" do
+      controller.instance_variable_set(:@sb, :miq_temp_params => 'all')
+
+      all_fields_count = @sample_values[view.ids[0]].size
+
+      expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(all_fields_count)
+    end
+
+    it "sets total fields in section with 'Attributes with different values' button to number of fields where clusters differs" do
+      controller.instance_variable_set(:@sb, :miq_temp_params => 'different')
+
+      diff_fields_count = 4 # differences in sample_values
+      expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(diff_fields_count)
+    end
+
+    it "sets total fields in section with 'Attributes with same values' button to number of fields with same values among clusters" do
+      controller.instance_variable_set(:@sb, :miq_temp_params => 'same')
+
+      same_fields_count = 2 # same values in sample_values
+      expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(same_fields_count)
+    end
   end
 
-  it "sets total fields in section with 'Attributes with different values' button to number of fields where clusters differs" do
-    controller.instance_variable_set(:@sb, :miq_temp_params => 'different')
+  context 'drifts' do
+    before(:each) do
+      view.ids = drift_ids
 
-    diff_fields_count = 4 # differences in sample_values
-    expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(diff_fields_count)
-  end
+      @sample_values = {}
+      init_sample_values(view.ids)
 
-  it "sets total fields in section with 'Attributes with same values' button to number of fields with same values among clusters" do
-    controller.instance_variable_set(:@sb, :miq_temp_params => 'same')
+      view.results = {}
+      @fields = []
+      init_fields_and_results(@sample_values)
+    end
 
-    same_fields_count = 2 # same values in sample_values
-    expect(controller.send(:comp_section_fields_total, view, section, @fields)).to eq(same_fields_count)
+    it "sets total fields in section with 'All attributes' button to number of fields" do
+      controller.instance_variable_set(:@sb, :miq_drift_params => 'all')
+
+      all_fields_count = @sample_values[view.ids[0]].size
+
+      expect(controller.send(:drift_section_fields_total, view, section, @fields)).to eq(all_fields_count)
+    end
+
+    it "sets total fields in section with 'Attributes with different values' button to number of fields where clusters differs" do
+      controller.instance_variable_set(:@sb, :miq_drift_params => 'different')
+
+      diff_fields_count = 4 # differences in sample_values
+      expect(controller.send(:drift_section_fields_total, view, section, @fields)).to eq(diff_fields_count)
+    end
+
+    it "sets total fields in section with 'Attributes with same values' button to number of fields with same values among clusters" do
+      controller.instance_variable_set(:@sb, :miq_drift_params => 'same')
+
+      same_fields_count = 2 # same values in sample_values
+      expect(controller.send(:drift_section_fields_total, view, section, @fields)).to eq(same_fields_count)
+    end
   end
 end
