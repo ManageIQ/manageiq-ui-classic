c1e9d1a80762d6b9f8642d5dc7c73c4e42fd860c
swi2 <swi2@rejpal42.mendelu.cz>
2018-03-20 10:45:35 +0100

    New formular OK, loading of Networkproviders to be fixed

--- app/assets/javascripts/components/floating-ip-form-component.js
+++ app/assets/javascripts/components/floating-ip-form-component.js
@@ -2,4 +2,8 @@ ManageIQ.angular.app.component('floatingIpFormComponent', {
 	controller: 'floatingIpFormController',
 	controllerAs: 'vm',
 	templateUrl: '/static/floating-ip-form.html.haml',
+	bindings: {
+		'repositoryId': '@',
+	 },
 });
+
--- app/assets/javascripts/components/floatingIpFormComponent.js
+++
@@ -1,5 +0,0 @@
-ManageIQ.angular.app.component('FloatingIpFormComponent', {
-er: 'FloatingIpFormController',
-	controllerAs: 'vm',
-	templateUrl: '/static/floating-ip-form.html.haml',
-});
