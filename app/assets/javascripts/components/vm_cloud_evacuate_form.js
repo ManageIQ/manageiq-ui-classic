39cdbbd590dcd6f92dfea6e7784da23833f625d6
Lucie Kaplanova <lucie.kap@gmail.com>
2018-03-20 10:42:27 +0100

    vmCloudEvacuateForm - update params in controller and add bindings to component

--- app/assets/javascripts/components/vm_cloud_evacuate_form.js
+++ app/assets/javascripts/components/vm_cloud_evacuate_form.js
@@ -4,5 +4,9 @@ ManageIQ.angular.app.component('vmCloudEvacuateForm', {
 	controller: 'vmCloudEvacuateFormController',
   controllerAs: 'vm',
   templateUrl: '/static/ansible-evacuate-form.html.haml',
-
+  bindings: {
+    'vmCloudEvacuateFormId': '@',
+    'message': '@',
+    'evacuate_items': '@',
+  },
 });
