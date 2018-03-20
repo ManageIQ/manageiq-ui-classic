31e404b433ca958a5109f76dd29894798c0c4fa2
Dušan Neckař <neckar.dusan@seznam.cz>
2018-03-20 10:42:29 +0100

    vmCloudLiveMigrateForm - Replaced items from angular controller scope to model and set up

--- app/assets/javascripts/components/vm_cloud_live_migrate_form.js
+++ app/assets/javascripts/components/vm_cloud_live_migrate_form.js
@@ -4,4 +4,8 @@ ManageIQ.angular.app.component('vmCloudLiveMigrateForm', {
   controller: 'vmCloudLiveMigrateFormController',
   controllerAs: 'vm',
   templateUrl: '/static/live_migrate.html.haml',
+  bindings: {
+    'vmCloudLiveMigrateFormId': '@',
+    'message': '@',
+  },
 });
