5ded5a9ffb47b60772616c8b1ee74f23d5ad8260
Lucie Kaplanova <lucie.kap@gmail.com>
2018-03-20 09:50:31 +0100

    vmCloudEvacuateForm - displaced buttons and params with '@' from static-view to view

--- app/assets/javascripts/components/vm_cloud_evacuate_form.js
+++ app/assets/javascripts/components/vm_cloud_evacuate_form.js
@@ -3,7 +3,6 @@
 ManageIQ.angular.app.component('vmCloudEvacuateForm', {
 	controller: 'vmCloudEvacuateFormController',
   controllerAs: 'vm',
-
   templateUrl: '/static/ansible-evacuate-form.html.haml',
 
 });
