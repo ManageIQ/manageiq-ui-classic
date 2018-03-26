ea0a5fe38d7bf0578d29a8dc1b24feb298903320
Miha Pleško <miha.plesko@xlab.si>
2018-03-26 13:43:40 +0200

    Enable WebMKS console access for VMware vCloud's VMs
    With this commit we bring button "Access -> VM Console" to life. We're
    able to reuse many good stuff from Vmware:.InfraManager, but there are
    also some vCloud specifics. Namely, the JavaScript SDK needs to be
    initialized with two extra parameters:
    
    ```
    VCDProxyHandshakeVmxPath // vmx path of the vm (obtained as part of mks ticket)
    enableUint8Utf8          // only uint8utf8 protocol works so we need to enable it
    ```
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- config/routes.rb
+++ config/routes.rb
@@ -3064,6 +3064,7 @@ Rails.application.routes.draw do
         filesystem_download
         retirement_info
         reconfigure_form_fields
+        launch_vmware_console
         launch_html5_console
         perf_chart_chooser
         protect
@@ -3090,6 +3091,7 @@ Rails.application.routes.draw do
         advanced_settings
         accordion_select
         button
+        console
         edit_vm
         resize_vm
         resize_field_changed
