d26d30a2492db2ba6ea708b06df50f13f0300e78
Lucie Kaplanova <lucie.kap@gmail.com>
2018-03-27 09:39:58 +0200

    vmCloudEvacuateForm - vmCloudEvacuateFormController inline in component

--- app/assets/javascripts/components/vm_cloud_evacuate_form.js
+++ app/assets/javascripts/components/vm_cloud_evacuate_form.js
@@ -1,12 +1,61 @@
 
 
 ManageIQ.angular.app.component('vmCloudEvacuateForm', {
-	controller: 'vmCloudEvacuateFormController',
+	controller: vmCloudEvacuateFormController,
   controllerAs: 'vm',
-  templateUrl: '/static/ansible-evacuate-form.html.haml',
+  templateUrl: '/static/evacuate-form.html.haml',
   bindings: {
     'vmCloudEvacuateFormId': '@',
     'message': '@',
     'evacuate_items': '@',
   },
 });
+
+vmCloudEvacuateFormController.$inject = ['$http', '$scope', 'miqService'];
+function vmCloudEvacuateFormController ($http, $scope, miqService) {
+  var vm = this;
+
+  vm.vmCloudModel = {
+    auto_select_host:    true,
+    destination_host:    null,
+    on_shared_storage:   true,
+    admin_password:      null
+  };
+  vm.hosts = [];
+  vm.formId = vm.vmCloudEvacuateFormId;
+  vm.modelCopy = angular.copy(vm.vmCloudModel);
+
+  ManageIQ.angular.scope = vm;
+  $scope.saveable = miqService.saveable;
+
+  if (vm.vmCloudEvacuateFormId) {
+    $http.get('/vm_cloud/evacuate_form_fields/' + vm.vmCloudEvacuateFormId)
+      .then(getEvacuateFormData)
+      .catch(miqService.handleFailure);
+  }
+
+  $scope.cancelClicked = function() {
+    miqService.sparkleOn();
+    var url = '/vm_cloud/evacuate_vm?button=cancel';
+    if (vm.vmCloudEvacuateFormId) {
+      url = '/vm_cloud/evacuate_vm/' + vm.vmCloudEvacuateFormId + '?button=cancel';
+    }
+    miqService.miqAjaxButton(url);
+  };
+
+  $scope.submitClicked = function() {
+    miqService.sparkleOn();
+    var url = '/vm_cloud/evacuate_vm?button=submit';
+    if (vm.vmCloudEvacuateFormId) {
+      url = '/vm_cloud/evacuate_vm/' + vm.vmCloudEvacuateFormId + '?button=submit';
+    }
+    miqService.miqAjaxButton(url, vm.vmCloudModel);
+  };
+
+  function getEvacuateFormData(response) {
+    var data = response.data;
+    Object.assign(vm, data);
+    vm.modelCopy = angular.copy(vm.vmCloudModel);
+    miqService.sparkleOff();
+  }
+};
