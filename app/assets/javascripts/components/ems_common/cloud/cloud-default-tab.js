b3a887535a13c42169a88c7a2d1d1b9d5b09dd2f
Martin Maroši <marvusm.mmi@gmail.com>
2018-04-05 11:30:40 +0200

    Added default tab of form.

---
+++ app/assets/javascripts/components/ems_common/cloud/cloud-default-tab.js
@@ -0,0 +1,17 @@
+ManageIQ.angular.app.component('emsCloudDefaultTab', {
+  bindings: {
+    showSecurityProtocol: '<?',
+    openstackSecurityProtocols: '<',
+    formModel: '<',
+    newRecord: '<?',
+    modelCopy: '<',
+    prefix: '@',
+    validateClicked: '<',
+    authenticationRequired: '<',
+    postValidationModel: '<',
+    postValidationModelRegistry: '<'
+  },
+  templateUrl: '/static/ems-common/tabs/default-tab.html.haml',
+  controller: function() {
+  }
+})
