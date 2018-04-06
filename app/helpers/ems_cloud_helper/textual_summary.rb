9073596b3e859cfef9673a671778510d4f878dab
Zita Nemeckova <znemecko@redhat.com>
2018-04-06 11:08:28 +0200

    Remove specific name for group for Google provider
    Google provider have provider_region set to nil so it will never have provider_region row displayed so removing specific name of that row for Google provider

--- app/helpers/ems_cloud_helper/textual_summary.rb
+++ app/helpers/ems_cloud_helper/textual_summary.rb
@@ -40,8 +40,7 @@ module EmsCloudHelper::TextualSummary
   #
   def textual_provider_region
     return nil if @record.provider_region.nil?
-    label_val = @record.type.include?("Google") ? _("Preferred Region") : _("Region")
-    {:label => label_val, :value => @record.description}
+    {:label => _("Region"), :value => @record.description}
   end
 
   def textual_region
