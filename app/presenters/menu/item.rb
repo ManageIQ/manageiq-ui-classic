ce66b61a1dfd576e8b1bbcf07aaf2a2816b55726
Martin Povolny <mpovolny@redhat.com>
2018-04-06 11:18:14 +0200

    Merge pull request #3726 from skateman/help-menu-defaults
    Add back Menu::Item defaults attribute as it is required by help_menu
    (cherry picked from commit dd6320bc78f44676e261a9ce5eac7611585fd798)

--- app/presenters/menu/item.rb
+++ app/presenters/menu/item.rb
@@ -1,5 +1,5 @@
 module Menu
-  Item = Struct.new(:id, :name, :feature, :rbac_feature, :href, :type, :parent_id) do
+  Item = Struct.new(:id, :name, :feature, :rbac_feature, :href, :type, :parent_id, :defaults) do
     extend ActiveModel::Naming
 
     def self.base_class
@@ -10,7 +10,7 @@ module Menu
       model_name
     end
 
-    def initialize(an_id, a_name, features, rbac_feature, href, type = :default, parent_id = nil)
+    def initialize(an_id, a_name, features, rbac_feature, href, type = :default, parent_id = nil, defaults = nil)
       super
       @parent = nil
       @name = a_name.kind_of?(Proc) ? a_name : -> { a_name }
