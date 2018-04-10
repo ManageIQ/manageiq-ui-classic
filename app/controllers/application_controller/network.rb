163aa1a76d2d49c911caeb8022840a817d5f7839
Jakub Dobrik <kubodobrik@azet.sk>
2018-04-10 10:12:20 +0200

    Roll back of unnecesary change in network.rb

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,6 +2,9 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    ::ManageIQ::Providers::NetworkManager.select(:id, :name, :parent_ems_id)
+    ems_clouds = ManageIQ::Providers::Openstack::NetworkManager.joins(:parent_manager).where(:parent_managers_ext_management_systems=>{:type=>'ManageIQ::Providers::Openstack::CloudManager'})
+    openstack_network_manager = Rbac::Filterer.filtered(ems_clouds).select(:id, :name, :parent_ems_id)
+    redhat_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Redhat::NetworkManager).select(:id, :name, :parent_ems_id)
+    openstack_network_manager + redhat_network_manager
   end
 end
