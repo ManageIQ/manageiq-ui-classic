230894b63e03d809177986613c2317e1f94903b7
Milan Zazrivec <mzazrivec@redhat.com>
2018-04-12 13:44:12 +0200

    Fix list of GTL items in set ownership screens
    We need to render list of filtered items (i.e. items which do support
    setting ownership) as opposed to all selected items.

--- app/controllers/mixins/actions/vm_actions/ownership.rb
+++ app/controllers/mixins/actions/vm_actions/ownership.rb
@@ -97,7 +97,7 @@ module Mixins
 
           @groups = {} # Create new entries hash (2nd pulldown)
           Rbac.filtered(MiqGroup.non_tenant_groups).each { |g| @groups[g.description] = g.id.to_s }
-          @edit[:object_ids] = ownership_ids
+          @edit[:object_ids] = @ownershipitems
           @view = get_db_view(klass == VmOrTemplate ? Vm : klass) # Instantiate the MIQ Report view object
           @view.table = ReportFormatter::Converter.records2table(@ownershipitems, @view.cols + ['id'])
           session[:edit] = @edit
