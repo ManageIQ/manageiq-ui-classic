85de501dd7216acd7ea8ef1f318c035f3144e7df
CZJanRichter <cz.jan.richter@gmail.com>
2018-04-12 12:25:45 +0200

    reset find_checked_ids_with_rbac refactoring, not done yet

---
+++ spec/controllers/report_controller/schedules_spec.rb
@@ -0,0 +1,24 @@
+describe ReportController do
+  describe "#schedule_edit" do
+    let(:admin_user) { FactoryGirl.create(:user, :role => "super_administrator")}
+    before do
+      EvmSpecHelper.create_guid_miq_server_zone
+      login_as admin_user
+      allow(User).to receive(:current_user).and_return(admin_user)
+      
+      FactoryGirl.create(:miq_schedule, :name => "tester1")
+      FactoryGirl.create(:miq_schedule, :name => "tester2")
+      
+      allow(controller).to receive(:assert_privileges)
+      allow(controller).to receive(:checked_or_params).and_return(MiqSchedule.all.ids)
+      controller.instance_variable_set(:@_params, {:button => "reset"})
+      # this part doesn`t work yet, the code crashes at replace_right cell in schedules.rb:253
+      controller.instance_variable_set(:@options, {:menu_edit_action => "reset"})
+    end
+    it "testing" do
+      controller.send(:schedule_edit)
+      expected_id = controller.instance_variable_get(:@schedule).id
+      expect(expected_id).to eq((MiqSchedule.find_by! name: "tester1").id)
+    end
+  end
+end
