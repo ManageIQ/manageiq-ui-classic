ed90960d631a59b59255f69f41a05c38821eb25e
lpichler <lpichler@redhat.com>
2018-04-16 17:34:58 +0200

    Use let and adapt name in dashboard spec

--- spec/controllers/miq_report_controller/dashboards_spec.rb
+++ spec/controllers/miq_report_controller/dashboards_spec.rb
@@ -1,25 +1,23 @@
 describe ReportController do
   context "::Dashboards" do
+    let(:miq_widget_set) { FactoryGirl.create(:miq_widget_set, :owner => user.current_group, :set_data => {:col1 => [], :col2 => [], :col3 => []}) }
+    let(:user)           { FactoryGirl.create(:user, :features => "db_edit") }
+
     before do
       login_as user
-      @db = FactoryGirl.create(:miq_widget_set,
-                               :owner    => user.current_group,
-                               :set_data => {:col1 => [], :col2 => [], :col3 => []})
     end
 
     context "#db_edit" do
-      let(:user) { FactoryGirl.create(:user, :features => "db_edit") }
-
       it "dashboard owner remains unchanged" do
         allow(controller).to receive(:db_fields_validation)
         allow(controller).to receive(:replace_right_cell)
-        owner = @db.owner
+        owner = miq_widget_set.owner
         new_hash = {:name => "New Name", :description => "New Description", :col1 => [1], :col2 => [], :col3 => []}
         current = {:name => "New Name", :description => "New Description", :col1 => [], :col2 => [], :col3 => []}
-        controller.instance_variable_set(:@edit, :new => new_hash, :db_id => @db.id, :current => current)
-        controller.instance_variable_set(:@_params, :id => @db.id, :button => "save")
+        controller.instance_variable_set(:@edit, :new => new_hash, :db_id => miq_widget_set.id, :current => current)
+        controller.instance_variable_set(:@_params, :id => miq_widget_set.id, :button => "save")
         controller.db_edit
-        expect(@db.owner.id).to eq(owner.id)
+        expect(miq_widget_set.owner.id).to eq(owner.id)
         expect(assigns(:flash_array).first[:message]).to include("saved")
         expect(controller.send(:flash_errors?)).not_to be_truthy
       end
