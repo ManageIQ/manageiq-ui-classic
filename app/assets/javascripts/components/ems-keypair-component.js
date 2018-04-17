df2009f1509847afd2962e7e4f85cbee0356f068
swi2 <swi2@rejpal44.mendelu.cz>
2018-04-17 10:34:44 +0200

    Binding "prefix" and "nq_reqd_userid" variables.

--- app/assets/javascripts/components/ems-keypair-component.js
+++ app/assets/javascripts/components/ems-keypair-component.js
@@ -2,4 +2,8 @@ ManageIQ.angular.app.component('emsKeypairComponent', {
   	controller: 'emsKeypairController',
 	controllerAs: 'vm',
 	templateUrl: 'auth_keypair_angular_bootstrap.html.haml',
+  bindings: {
+    'prefix': '@',
+    'ng_reqd_userid': '<',
+  },
 });
