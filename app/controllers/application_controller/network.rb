475c49856e6cfe166b438f0da7271b0ad0a58b08
Jakub Dobrik <kubodobrik@azet.sk>
2018-04-17 09:25:42 +0200

    Bindings corrected, removed controller file, corrected component file

--- app/controllers/application_controller/network.rb
+++ app/controllers/application_controller/network.rb
@@ -2,9 +2,6 @@ module ApplicationController::Network
   extend ActiveSupport::Concern
 
   def network_managers
-    ems_clouds = ManageIQ::Providers::Openstack::NetworkManager.joins(:parent_manager).where(:parent_managers_ext_management_systems=>{:type=>'ManageIQ::Providers::Openstack::CloudManager'})
-    openstack_network_manager = Rbac::Filterer.filtered(ems_clouds).select(:id, :name, :parent_ems_id)
-    redhat_network_manager = Rbac::Filterer.filtered(ManageIQ::Providers::Redhat::NetworkManager).select(:id, :name, :parent_ems_id)
-    openstack_network_manager + redhat_network_manager
+    ::ManageIQ::Providers::NetworkManager.select(:id, :name, :parent_ems_id)
   end
 end
