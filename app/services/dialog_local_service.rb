23889eaafdfe160111987013172e2e23473df3f9
Harpreet Kataria <hkataria@redhat.com>
2018-04-18 13:50:13 -0400

    Adjusted target_type value and added spec test.
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1568372

--- app/services/dialog_local_service.rb
+++ app/services/dialog_local_service.rb
@@ -130,6 +130,8 @@ class DialogLocalService
       "miq_template"
     when /InfraManager/
       "ext_management_system"
+    when /ServiceAnsiblePlaybook/
+      "service"
     else
       obj.class.name.demodulize.underscore
     end
