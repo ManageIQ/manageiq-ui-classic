168900c23dc2d2676299c7fa87484fd969922d4e
Saulo S. Toledo <saulotoledo@gmail.com>
2018-04-22 14:20:53 -0300

    Adding the toolbars to the switches pages

--- app/helpers/application_helper/toolbar/physical_switch_center.rb
+++ app/helpers/application_helper/toolbar/physical_switch_center.rb
@@ -1,2 +1,26 @@
 class ApplicationHelper::Toolbar::PhysicalSwitchCenter < ApplicationHelper::Toolbar::Basic
+  button_group(
+    'physical_switch_vmdb',
+    [
+      select(
+        :physical_switch_vmdb_choice,
+        'fa fa-cog fa-lg',
+        t = N_('Configuration'),
+        t,
+        :items => [
+          button(
+            :physical_switch_refresh,
+            'fa fa-refresh fa-lg',
+            N_('Refresh relationships and power states for all items related to this Physical Switch'),
+            N_('Refresh Relationships and Power States'),
+            :image   => "refresh",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "refresh", "controller": "physicalSwitchToolbarController"}'},
+            :confirm => N_("Refresh relationships and power states for all items related to this Physical Switch?"),
+            :options => {:feature => :refresh}
+          ),
+        ]
+      ),
+    ]
+  )
 end
--- app/helpers/application_helper/toolbar/physical_switches_center.rb
+++ app/helpers/application_helper/toolbar/physical_switches_center.rb
@@ -1,2 +1,26 @@
 class ApplicationHelper::Toolbar::PhysicalSwitchesCenter < ApplicationHelper::Toolbar::Basic
+  button_group(
+    'physical_switch_vmdb',
+    [
+      select(
+        :physical_switch_vmdb_choice,
+        'fa fa-cog fa-lg',
+        t = N_('Configuration'),
+        t,
+        :items => [
+          button(
+            :physical_switch_refresh,
+            'fa fa-refresh fa-lg',
+            N_('Refresh relationships and power states for all items related to these Physical Switches'),
+            N_('Refresh Relationships and Power States'),
+            :image   => "refresh",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "refresh", "controller": "physicalSwitchToolbarController"}'},
+            :confirm => N_("Refresh relationships and power states for all items related to these Physical Switches?"),
+            :options => {:feature => :refresh}
+          ),
+        ]
+      ),
+    ]
+  )
 end
