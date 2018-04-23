c01297418ad6acced7ba3121973b8168ff9e6cda
Martin Hradil <mhradil@redhat.com>
2018-04-23 15:19:08 +0000

    optionally WEBPACK_EXCLUDE_NODE_MODULES
    by default we compile all the javascript, even from node_modules
    we have to do so in production because we do have es6 library dependencies
    
    but for devels using current browsers, there's much less point to compiling node_modules
    adding a way to stop
    
    this would break some people in development if default, so making this opt-in
    
    `bin/webpack` .. Done in 138.50s
    `WEBPACK_EXCLUDE_NODE_MODULES=1 bin/webpack` .. Done in 64.52s

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,4 +1,12 @@
-module.exports = {
+const { env } = require('process')
+
+let config = {
   test: /\.js$/,
-  loader: 'babel-loader'
+  loader: 'babel-loader',
 }
+
+if (env.WEBPACK_EXCLUDE_NODE_MODULES) {
+  config.exclude = /node_modules/;
+}
+
+module.exports = config;
