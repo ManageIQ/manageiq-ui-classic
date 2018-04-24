ad755eb8a5aac70e457b6303a3c31a75881789a1
Martin Hradil <mhradil@redhat.com>
2018-04-24 13:31:30 +0000

    webpack loaders - deal with optional node_modules even in react.js
    turns out we have 2 loaders matching the same set of files - which means all these files get compiled twice

--- config/webpack/loaders/react.js
+++ config/webpack/loaders/react.js
@@ -1,7 +1,15 @@
-module.exports = {
+const { env } = require('process')
+
+let config = {
   test: /\.(js|jsx)$/,
   loader: 'babel-loader',
   query: {
     presets: ['react'],
   },
-};
+}
+
+if (env.WEBPACK_EXCLUDE_NODE_MODULES) {
+  config.exclude = /node_modules/;
+}
+
+module.exports = config;
