9d3818f8c7e4a8467e171f19b73e446699af2979
Dávid Halász <dhalasz@redhat.com>
2018-04-27 11:34:50 +0200

    Call rubocop -a on all the decorators

--- app/decorators/filesystem_decorator.rb
+++ app/decorators/filesystem_decorator.rb
@@ -1,15 +1,15 @@
 class FilesystemDecorator < MiqDecorator
   def fonticon
     convert = {
-      "dll"      => "fa fa-cogs",
-      "doc"      => "fa fa-file-word-o",
-      "exe"      => "fa fa-file-o",
-      "ini"      => "fa fa-cog",
-      "log"      => "fa fa-file-text-o",
-      "pdf"      => "fa fa-file-pdf-o",
-      "txt"      => "fa fa-file-text-o",
-      "unknown"  => "fa fa-file-o",
-      "zip"      => "fa fa-file-archive-o"
+      "dll"     => "fa fa-cogs",
+      "doc"     => "fa fa-file-word-o",
+      "exe"     => "fa fa-file-o",
+      "ini"     => "fa fa-cog",
+      "log"     => "fa fa-file-text-o",
+      "pdf"     => "fa fa-file-pdf-o",
+      "txt"     => "fa fa-file-text-o",
+      "unknown" => "fa fa-file-o",
+      "zip"     => "fa fa-file-archive-o"
     }
     convert[name.downcase] || "fa fa-file-o"
   end
