5cdf627c151949f1ecf771f1c21fc6f4d739ea30
CZJanRichter <cz.jan.richter@gmail.com>
2018-05-10 11:12:33 +0200

    rubocop conditional assignment offense fix

--- app/controllers/report_controller/schedules.rb
+++ app/controllers/report_controller/schedules.rb
@@ -241,11 +241,11 @@ module ReportController::Schedules
       add_flash(_("All changes have been reset"), :warning) if params[:button] == "reset"
       if x_active_tree != :reports_tree
         # dont set these if new schedule is being added from a report show screen
-        if params[:id] == "new"
-          @schedule = MiqSchedule.new(:userid => session[:userid])
-        else
-          @schedule = find_record_with_rbac(MiqSchedule, checked_or_params)
-        end
+        @schedule = if params[:id] == "new"
+                      MiqSchedule.new(:userid => session[:userid])
+                    else
+                      find_record_with_rbac(MiqSchedule, checked_or_params)
+                    end
         session[:changed] = false
       end
       schedule_set_form_vars
