febdb4224a733e5ec08a49b35fcc7da725fa5b8d
Martin Maroši <marvusm.mmi@gmail.com>
2018-05-14 12:33:19 +0200

    Added React component factory.

--- app/javascript/miq-component/helpers.js
+++ app/javascript/miq-component/helpers.js
@@ -4,3 +4,13 @@ import reactBlueprint from '../miq-component/react-blueprint';
 export function addReact(name, component) {
   return registry.define(name, reactBlueprint(component));
 }
+
+export function componentFactory(blueprintName, selector, props) {
+  const { isDefined } = ManageIQ.component;
+  if (!isDefined(blueprintName)) {
+    throw new Error(`Attempt to create instance of ${blueprintName}. Blueprint ${blueprintName} is not defined!`);
+  }
+  const element = document.querySelector(selector);
+  const allProps = { ...element.dataset, ...props };
+  ManageIQ.component.newInstance(blueprintName, allProps, element);
+}
