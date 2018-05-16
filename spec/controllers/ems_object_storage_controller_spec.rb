587ed899734f8e7742762d9f633576975f90f655
Hilda Stastna <hstastna@redhat.com>
2018-05-16 17:43:34 +0200

    Add spec test to check rendered toolbar on Object Storage Managers page

--- spec/controllers/ems_object_storage_controller_spec.rb
+++ spec/controllers/ems_object_storage_controller_spec.rb
@@ -1,3 +1,12 @@
 describe EmsObjectStorageController do
   include_examples :shared_examples_for_ems_object_storage_controller, %w(openstack)
+
+  describe '#show_list' do
+    render_views
+
+    it 'renders the Object Storage Managers Toolbar' do
+      expect(ApplicationHelper::Toolbar::GtlView).to receive(:definition)
+      post :show_list
+    end
+  end
 end
