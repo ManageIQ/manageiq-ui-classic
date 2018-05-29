804a04c4235e182bb2dadcb172eedcd53d61a22f
Martin Povolny <mpovolny@redhat.com>
2018-05-29 11:52:52 +0200

    Quick-fix broken schedule test.

--- spec/controllers/report_controller/schedules_spec.rb
+++ spec/controllers/report_controller/schedules_spec.rb
@@ -1,21 +1,27 @@
 describe ReportController do
   describe "#schedule_edit" do
     let(:admin_user) { FactoryGirl.create(:user, :role => "super_administrator") }
+
+    let(:schedule) { FactoryGirl.create(:miq_schedule, :name => "tester1") }
+
     before do
       EvmSpecHelper.create_guid_miq_server_zone
       login_as admin_user
       allow(User).to receive(:current_user).and_return(admin_user)
-      FactoryGirl.create(:miq_schedule, :name => "tester1")
+
+      schedule
       FactoryGirl.create(:miq_schedule, :name => "tester2")
+
       allow(controller).to receive(:assert_privileges)
       allow(controller).to receive(:checked_or_params).and_return(MiqSchedule.all.ids)
       allow(controller).to receive(:replace_right_cell).and_return(true)
     end
+
     it "reset rbac testing" do
       controller.send(:schedule_edit)
       controller.instance_variable_set(:@_params, :button => "reset")
       expected_id = controller.instance_variable_get(:@schedule).id
-      expect(expected_id).to eq((MiqSchedule.find_by! :name => "tester1").id)
+      expect(expected_id).to eq(schedule.id)
     end
   end
 end
