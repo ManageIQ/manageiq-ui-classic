81eb6faca3baa13d998fa27fbf9b859a9a09f76a
Dávid Halász <dhalasz@redhat.com>
2018-06-02 15:30:22 +0200

    Remove the quadicon definition from the ExtManagementSystemDecorator

--- app/decorators/ext_management_system_decorator.rb
+++ app/decorators/ext_management_system_decorator.rb
@@ -11,23 +11,6 @@ class ExtManagementSystemDecorator < MiqDecorator
     "svg/vendor-#{image_name}.svg"
   end
 
-  def quadicon
-    icon = {
-      :top_left     => {:text => try(:hosts).try(:size).to_i},
-      :top_right    => {:text => ""},
-      :bottom_left  => {
-        :fileicon => fileicon,
-        :tooltip  => ui_lookup(:model => type)
-      },
-      :bottom_right => {
-        :fileicon => QuadiconHelper.status_img(authentication_status),
-        :tooltip  => authentication_status
-      }
-    }
-    icon[:middle] = { :fileicon => '100/shield.png' } if get_policies.present?
-    icon
-  end
-
   def single_quad
     {
       :fonticon => fonticon
