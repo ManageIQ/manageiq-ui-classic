208a0d4f556d79002d0b7da940d850b3fc4d838a
Martin Hradil <mhradil@redhat.com>
2018-06-05 11:53:27 +0000

    Remove angular exception in webpack config
    backport of #4058

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -48,14 +48,6 @@ module.exports = {
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
 
-    // Workaround for angular/angular#11580
-    new webpack.ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)@angular/,
-      resolve(__dirname, '../../'), // location of your src
-      {} // a map of your routes
-    ),
-
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true,
