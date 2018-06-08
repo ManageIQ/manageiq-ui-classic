8cb5792599e34e85c09629f36b915c35e45d15ad
Dávid Halász <dhalasz@redhat.com>
2018-06-08 10:53:20 +0200

    Update GuestDeviceDecorator with fonticons based on device_type

--- app/decorators/guest_device_decorator.rb
+++ app/decorators/guest_device_decorator.rb
@@ -1,5 +1,26 @@
 class GuestDeviceDecorator < MiqDecorator
-  def self.fonticon
-    'ff ff-network-card'
+  def fonticon
+    case device_type
+    when 'ethernet'
+      'ff ff-network-card'
+    when 'cdrom'
+      'ff ff-cdrom'
+    when 'scsi'
+      'ff ff-scsi'
+    when 'serial'
+      'ff ff-serial'
+    when 'usb'
+      'fa fa-usb'
+    when 'parallel'
+      'ff ff-parallel'
+    when 'sound'
+      'fa fa-volume-up'
+    when 'management'
+      'ff ff-network-interface'
+    when 'physical port'
+      'ff ff-port'
+    else
+      'pficon pficon-unknown'
+    end
   end
 end
