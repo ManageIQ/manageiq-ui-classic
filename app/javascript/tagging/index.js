2b831b8ee6a105646130d6eba91fd5ec2f5cdaf3
PanSpagetka <rknaur@redhat.com>
2018-06-13 15:42:48 +0200

    Change exports

--- app/javascript/tagging/index.js
+++ app/javascript/tagging/index.js
@@ -2,6 +2,5 @@ import taggingApp from './reducers';
 import { TaggingConnected, TaggingWithButtonsConnected } from './containers/tagging';
 
 export { taggingApp, TaggingConnected, TaggingWithButtonsConnected };
-export * from './components/tagging';
-export * from './components/tagView';
+export * from './components/Tagging/Tagging';
 export * from './actions';
