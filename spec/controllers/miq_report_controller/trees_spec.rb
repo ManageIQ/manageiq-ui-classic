68cf0da7c79f75f0e0e9f697a4f574c9e973465b
Nick LaMuro <nicklamuro@gmail.com>
2018-06-14 16:43:10 -0500

    [GAP] Prefer valid_report_column? and contains_records? from MiqReportResult
    https://bugzilla.redhat.com/show_bug.cgi?id=1594386
    
    Gaprindashvili Backport commit from:
    
        https://github.com/ManageIQ/manageiq-ui-classic/pull/4143
    
    Original SHA:
    
       ff7ee24f4d50b38e96d5c0eccd8ca7398d6d310e
    
    Rest of original message below...
    
    * * *
    
    These are new methods added in:
    
    https://github.com/ManageIQ/manageiq/pull/17590
    
    And are far more efficient for the use cases of ChargebackController and
    ReportController::SavedReports.

--- spec/controllers/miq_report_controller/trees_spec.rb
+++ spec/controllers/miq_report_controller/trees_spec.rb
@@ -23,17 +23,21 @@ describe ReportController do
       it 'renders show' do
         user = FactoryGirl.create(:user_with_group)
         login_as user
+        session[:settings] = {:perpage => {:reports => 20}}
         controller.instance_variable_set(:@html, "<h1>Test</h1>")
-        allow(controller).to receive(:report_first_page)
+
         report = FactoryGirl.create(:miq_report_with_results)
-        allow(report).to receive(:contains_records?).and_return(true)
         task = FactoryGirl.create(:miq_task)
         task.update_attributes(:state => "Finished")
         task.reload
-        report_result = FactoryGirl.create(:miq_report_result,
+
+        report_result = FactoryGirl.build(:miq_report_result,
                                            :miq_group_id => user.current_group.id,
-                                           :miq_task_id  => task.id)
-        allow_any_instance_of(MiqReportResult).to receive(:report_results).and_return(report)
+                                           :miq_task_id  => task.id,
+                                           :miq_report   => report)
+        report_result.report = report.to_hash.merge(:extras=> {:total_html_rows => 100})
+        report_result.save
+
         binary_blob = FactoryGirl.create(:binary_blob,
                                          :resource_type => "MiqReportResult",
                                          :resource_id   => report_result.id)
@@ -41,7 +45,7 @@ describe ReportController do
                            :data           => "--- Quota \xE2\x80\x93 Max CPUs\n...\n",
                            :binary_blob_id => binary_blob.id)
 
-        post :tree_select, :params => { :id => "rr-#{report_result.id}", :format => :js, :accord => 'savedreports' }
+        post :tree_select, :params => { :id => "rr-#{report_result.id}", :ppsetting => 20, :format => :js, :accord => 'savedreports' }
         expect(response).to render_template('shared/_report_chart_and_html')
       end
     end
