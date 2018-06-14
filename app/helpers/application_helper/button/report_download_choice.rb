cb118f0c34462eca296e460d7b01cee29b51066c
Nick LaMuro <nicklamuro@gmail.com>
2018-06-14 16:52:53 -0500

    Prefer `.exists?` over `.length?` in report button helpers
    https://bugzilla.redhat.com/show_bug.cgi?id=1590908
    
    Calls to `.length` will fetch the entire record set or rows for the
    MiqReportResult, and in the current case of these button helpers, throw
    them all away without using any of the data fetched.  In most cases, the
    `html_details` relation is probably what was is being used, so even
    caching these ahead of time is wasteful.
    
    By using `.exists?` as an alternative, we basically do a:
    
      SELECT 1 as one
      FROM miq_report_result_details
      WHERE "miq_report_result_details"."miq_report_result_id" = ?
      LIMIT 1
    
    Which only returns a single digit from the database if it has at least
    one record and nothing if it doesn't.  This will also be cached in a
    controller action by the ActiveRecord query cache, so it will also
    require zero queries the second time around.

--- app/helpers/application_helper/button/report_download_choice.rb
+++ app/helpers/application_helper/button/report_download_choice.rb
@@ -1,5 +1,5 @@
 class ApplicationHelper::Button::ReportDownloadChoice < ApplicationHelper::Button::Basic
   def disabled?
-    MiqReportResult.find(@report_result_id).try(:miq_report_result_details).try(:length).to_i == 0
+    !MiqReportResult.find(@report_result_id).try(:miq_report_result_details).try(:exists?)
   end
 end
