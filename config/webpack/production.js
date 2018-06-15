976cc9ecc3c677f5a0f0260658267eae612406db
Martin Hradil <mhradil@redhat.com>
2018-06-15 15:30:21 +0000

    Webpack - add default mode
    not added to test environment, so no additional plugins there
    
    https://webpack.js.org/concepts/mode/

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -8,6 +8,7 @@ const CompressionPlugin = require('compression-webpack-plugin')
 const sharedConfig = require('./shared.js')
 
 module.exports = merge(sharedConfig, {
+  mode: 'production',
   output: { filename: '[name]-[chunkhash].js' },
   devtool: 'source-map',
   stats: 'normal',
