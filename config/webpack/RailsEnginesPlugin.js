d03cae39c629205e84f2d1a61a4bd8d4c8b68888
Martin Hradil <mhradil@redhat.com>
2018-06-19 00:29:10 +0000

    RailsEnginesPlugin - expand to deal with recursive dependencies
    most of the recursion logic is heavily inspired by ModulesInHierachicDirectoriesPlugin
    (including the dependency on enhanced-resolve@4.0.0 helpers)
    
    for things not under node_modules:
      identify the rails engine
      look for the module under that engine's node_modules
    
    for things under node_modules:
      identify the rails engine
      generate all the paths from request.path until / (by removing 1 trailing component each time)
      trim those paths to end at the engine boundary
      find the required package name by taking the first component from request.request
      filter the paths by node_modules/$package existence
      look for the module under those remaining paths, starting from the longest

--- config/webpack/RailsEnginesPlugin.js
+++ config/webpack/RailsEnginesPlugin.js
@@ -1,3 +1,8 @@
+"use strict";
+
+const getPaths = require("enhanced-resolve/lib/getPaths");
+const forEachBail = require("enhanced-resolve/lib/forEachBail");
+
 module.exports = class RailsEnginesPlugin {
   constructor(source, target, engines) {
     this.source = source;
@@ -9,16 +14,50 @@ module.exports = class RailsEnginesPlugin {
     const target = resolver.ensureHook(this.target);
 
     resolver.getHook(this.source).tapAsync("RailsEnginesPlugin", (request, resolveContext, callback) => {
-      let obj = {
-        ...request,
-        railsEngine: Object.keys(this.engines).filter((engine) => request.path.startsWith(this.engines[engine]))[0],
-      };
+      const engine = Object.keys(this.engines).filter((engine) => request.path.startsWith(this.engines[engine]))[0];
+      const engineRoot = this.engines[engine];
+      const inNodeModules = request.path.includes('node_modules');
 
-      obj.path = `${this.engines[obj.railsEngine]}/node_modules`;
-      obj.request = `./${obj.request}`;
-      // console.log({ path: obj.path, request: obj.request });
+      if (! engine) {
+        throw `RailsEnginesPlugin: no engine found for ${request.path} ${request.request}`;
+      }
 
-      resolver.doResolve(target, obj, "whatever", resolveContext, callback);
+      if (! inNodeModules) {
+        const obj = {
+          ...request,
+          path: `${engineRoot}/node_modules`,
+          request: `./${request.request}`,
+        };
+
+        resolver.doResolve(target, obj, `engine ${engine}`, resolveContext, callback);
+        return;
+      }
+
+      // only look in paths under the plugin
+      const paths = getPaths(request.path).paths;
+      const engineBoundary = paths.indexOf(engineRoot);
+
+      const cutPaths = paths.slice(0, engineBoundary + 1);  // including the engine root
+      const joinedPaths = cutPaths.map((p) => resolver.join(p, 'node_modules'));
+
+      const packageName = request.request.split('/')[0];
+
+      const fs = resolver.fileSystem;
+      forEachBail(joinedPaths, (addr, callback) => {
+        fs.stat(resolver.join(addr, packageName), (err, stat) => {
+          if (err || !stat || !stat.isDirectory()) {
+            return callback();
+          }
+
+          const obj = {
+            ...request,
+            path: addr,
+            request: `./${request.request}`,
+          };
+
+          return resolver.doResolve(target, obj, `modules in ${addr}`, resolveContext, callback);
+        });
+      }, callback);
     });
   }
 };
