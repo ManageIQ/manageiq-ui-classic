f81b4c5e36011a61026a3233901cf5f3bc6a6403
Martin Povolny <mpovolny@redhat.com>
2018-06-26 20:54:16 +0200

    Toolbar: show {attach|detach} only for OpenStack instances.

--- app/helpers/application_helper/button/instance_detach.rb
+++ app/helpers/application_helper/button/instance_detach.rb
@@ -1,4 +1,9 @@
 class ApplicationHelper::Button::InstanceDetach < ApplicationHelper::Button::Basic
+  def visible?
+    # FIXME: feature for attach/detach is missing, testing class for now
+    ManageIQ::Providers::Openstack::CloudManager::Vm === @record
+  end
+
   def disabled?
     if @record.number_of(:cloud_volumes).zero?
       @error_message = _("This Instance has no attached Cloud Volumes.")
