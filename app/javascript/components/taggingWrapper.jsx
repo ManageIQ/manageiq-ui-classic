1dab3a1210226aad53d7fa953a7495f02ea6b426
PanSpagetka <rknaur@redhat.com>
2018-06-28 14:35:07 +0200

    Add spinner, fix lint issues

--- app/javascript/components/taggingWrapper.jsx
+++ app/javascript/components/taggingWrapper.jsx
@@ -1,56 +1,74 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Spinner } from 'patternfly-react';
 import { TaggingWithButtonsConnected, taggingApp } from '@manageiq/react-ui-components/dist/tagging';
 import '@manageiq/react-ui-components/dist/tagging.css';
 
-class TaggingWrapper extends React.Component{
-  componentDidMount()  {
-    this.loadState(this.props.tags);
-  }
-
+class TaggingWrapper extends React.Component {
   constructor(props) {
     super(props);
     ManageIQ.redux.addReducer(taggingApp);
   }
 
-  loadState = (state) => this.props.loadState(state);
-  reset = () =>  this.props.reset();
+  componentDidMount() {
+    this.loadState(this.props.tags);
+  }
+
+  loadState = state => this.props.loadState(state);
+  reset = () => this.props.reset();
   isLoaded = () => this.props.isLoaded();
 
   render() {
-    if (!this.props.isLoaded) return <div>bkjfbkjnkjfdkjfkj</div>
+    if (!this.props.isLoaded) return <Spinner />;
     const { urls } = this.props;
     return (<TaggingWithButtonsConnected
-          saveButton={
-            {
-              onClick: (assignedTags) => {
-                $.post(urls.save_url, { data: JSON.stringify(assignedTags)});
-              },
-              href: '',
-              type: 'button',
-              disabled: false,
-              description: 'Save'
-            }
-          }
-          cancelButton={{ onClick: () => { this.reset(); $.post(urls.cancel_url);  },
-            href: '', type: 'button', disabled: false, description: 'Cancel'}}
-          resetButton={{ onClick: () => this.reset(),
-            href: '', type: 'button', disabled: false, description: 'Reset'}}
-        />
-      )
-    }
+      saveButton={{
+          onClick: (assignedTags) => {
+            $.post(urls.save_url, { data: JSON.stringify(assignedTags) });
+          },
+          href: '',
+          type: 'button',
+          disabled: false,
+          description: 'Save',
+        }
+      }
+      cancelButton={{
+        onClick: () => { this.reset(); $.post(urls.cancel_url); },
+        href: '',
+        type: 'button',
+        disabled: false,
+        description: 'Cancel',
+        }
+      }
+      resetButton={{
+        onClick: () => this.reset(),
+        href: '',
+        type: 'button',
+        disabled: false,
+        description: 'Reset',
+        }
+      }
+    />);
+  }
 }
 
-const mapDispatchToProps = dispatch => ({
-  loadState: state => dispatch({ initialState: state, type: "UI-COMPONENTS_TAGGING_LOAD_STATE" }),
-  reset: () => dispatch({ type: "UI-COMPONENTS_TAGGING_RESET_STATE" })
-})
+TaggingWrapper.propTypes = {
+  reset: PropTypes.func.isRequired,
+  loadState: PropTypes.func.isRequired,
+  isLoaded: PropTypes.bool.isRequired,
+  ulrs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tags: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
-const mapStateToProps = ({ tagging }) => {
-  console.log(tagging);
-  return {
-  isLoaded: !!tagging,
-}}
+const mapDispatchToProps = dispatch => ({
+  loadState: state => dispatch({ initialState: state, type: 'UI-COMPONENTS_TAGGING_LOAD_STATE' }),
+  reset: () => dispatch({ type: 'UI-COMPONENTS_TAGGING_RESET_STATE' }),
+});
+
+const mapStateToProps = ({ tagging }) => ({
+  isLoaded: false,
+});
 
 const TaggingWrapperConnected = connect(
   mapStateToProps,
