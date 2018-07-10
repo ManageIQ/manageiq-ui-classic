faff33580ec4dc784c8ea1af3bb7ffe74bbe35d0
Martin Maroši <marvusm.mmi@gmail.com>
2018-07-10 16:03:27 +0200

    Added configuration to enable redux routing for react components.

--- app/javascript/miq-redux/middleware.ts
+++ app/javascript/miq-redux/middleware.ts
@@ -1,5 +1,10 @@
-import { Middleware } from 'redux';
+import thunk from 'redux-thunk';
+import { routerMiddleware } from 'connected-react-router';
 
-export const middlewares: Middleware[] = [];
-
-// add middleware for handling async operations and side effects
+/**
+ * @param history history for redux routing for react components * 
+ * @description
+ *  routerMiddleware adds middleware which is listening for location changes and updates router state
+ *  thunk is a middleware for async redux functions
+ */
+export const createMiddlewares = ({ history }) => ([routerMiddleware(history), thunk]);
