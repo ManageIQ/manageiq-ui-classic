543cdeeeaf546a97ec7b6ef7c33c24ac4063feff
Hilda Stastna <hstastna@redhat.com>
2018-07-12 14:42:43 +0200

    Remove Snapshot field from EC2 Instance summary screen
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1583754

--- app/helpers/vm_cloud_helper/textual_summary.rb
+++ app/helpers/vm_cloud_helper/textual_summary.rb
@@ -23,8 +23,10 @@ module VmCloudHelper::TextualSummary
       _("Properties"),
       %i(
         name region server description ipaddress mac_address custom_1 container preemptible tools_status
-        load_balancer_health_check_state osinfo architecture snapshots advanced_settings resources guid
-        virtualization_type root_device_type ems_ref
+        load_balancer_health_check_state osinfo architecture
+      ) + (@record.type == 'ManageIQ::Providers::Amazon::CloudManager::Vm' ? %i() : %i(snapshots)) +
+      %i(
+        advanced_settings resources guid virtualization_type root_device_type ems_ref
       )
     )
   end
