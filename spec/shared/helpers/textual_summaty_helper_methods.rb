c3e8fbfae21fcdab49d77175c6ad6256e76e0928
Richard Vsiansky <r.vsia@seznam.cz>
2018-07-16 16:04:31 +0200

    Add shared_examples test methods for textual_group in helpers

--- spec/shared/helpers/textual_summaty_helper_methods.rb
+++
@@ -1,9 +0,0 @@
-shared_examples_for 'method_exists' do |methods|
-  methods.each do |method|
-    describe "##{method}" do
-      it "method exists" do
-        expect(respond_to?(method)).to be_truthy
-      end
-    end
-  end
-end
