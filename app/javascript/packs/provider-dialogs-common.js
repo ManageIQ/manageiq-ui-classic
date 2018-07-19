ccd28898fe6516bde585c6a65c6e44087d5dfe65
Martin Povolny <mpovolny@redhat.com>
2018-07-19 09:45:00 +0200

    Toolbar: Invoke dialog player.

--- app/javascript/packs/provider-dialogs-common.js
+++ app/javascript/packs/provider-dialogs-common.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import renderModal from '../provider-dialogs/modal';
 
+ManageIQ.angular.app.component('providerDialogUser', {
+  bindings: {
+    dialog: '<',
+    entityName: '@',
+    actionName: '@',
+  },
+  template: '<dialog-user dialog="$ctrl.dialog" refresh-field="$ctrl.refreshField(field)" on-update="$ctrl.setDialogData(data)"></dialog-user>',
+  controller: [function() {
+    let formData = {};
+
+    /* this.refreshField = (field) => {
+      // dynamic fields are not supported in this context
+      console.log('TODO dialogFieldRefreshService.refreshField', field);
+      return Promise.resolve({values:[], options:[]});
+    }; */
+
+    this.setDialogData = (data) => {
+      formData = data.data;
+
+      ManageIQ.redux.store.dispatch({
+        type: 'FormButtons.saveable',
+        payload: data.validations.isValid,
+      });
+    };
+
+    this.$onInit = () => {
+      ManageIQ.redux.store.dispatch({
+        type: 'FormButtons.init',
+        payload: {
+          newRecord: true,
+          pristine: false,
+          addClicked: () => {
+            apiCall(
+              {
+                entity_name: this.entityName,
+                action_name: this.actionName,
+              },
+              this.formData
+            );
+          },
+        },
+      });
+    };
+  }],
+});
+
 function reactModal(buttonData) {
   const props = {
     recordId: ManageIQ.record.recordId,
@@ -12,11 +58,9 @@ function reactModal(buttonData) {
   renderModal(__(buttonData.modal_title), inner);
 }
 
-function apiCall(buttonData) {
-  const url = '/' + buttonData.entity_name + '/' + ManageIQ.record.recordId;
-
-  API.post(url, { action: buttonData.action_name }).then(function(response) {
-
+function apiCall(buttonData, dialogData) {
+  const url = `/api/${buttonData.entity_name}/${ManageIQ.record.recordId}`;
+  API.post(url, { action: buttonData.action_name, data: dialogData }).then((response) => {
     let errorMessage;
 
     if (response.error) {
@@ -26,9 +70,35 @@ function apiCall(buttonData) {
         errorMessage = __('An unknown error has occurred.');
       }
       add_flash(errorMessage, 'error');
+      return;
     }
     add_flash(buttonData.success_message, 'success');
+  }).catch((error) => {
+    add_flash(__('An unknown error has occurred calling the API.'));
   });
+}
+
+// careful: the dialogDefinigion is string, not JSON here
+function dialogModal(dialogDefinition, buttonData) {
+  const elem = $('<provider-dialog-user></provider-dialog-user>');
+  elem.attr('dialog', dialogDefinition);
+  elem.attr('action-name', buttonData.action_name);
+  elem.attr('entity-name', buttonData.entity_name);
+
+  const id = 'angular-provider-dialog';
+
+  const inner = class extends React.Component {
+    componentDidMount() {
+      elem.appendTo(`#${id}`);
+      miq_bootstrap(`#${id}`);
+    }
+
+    render() {
+      return <div id={id} />;
+    }
+  };
+
+  renderModal(buttonData.dialog_title, inner);
 }
 
 window.listenToRx(function (buttonData) {
@@ -36,11 +106,22 @@ window.listenToRx(function (buttonData) {
     return;
   }
 
-  if (buttonData.entity_name) {
-    apiCall(buttonData);
-  } else if (buttonData.component_name) {
-    // console.error('provider-dialogs: missing component_name', buttonData);
+  if (buttonData.component_name) {
     reactModal(buttonData);
+  } else if (buttonData.dialog_name) {
+    http.post(
+      '/dashboard/dialog_definition',
+      { class: buttonData.class, name: buttonData.dialog_name },
+      { headers: { 'Content-Type': 'application/json' } },
+    ).then((response) => {
+      if (response.error) {
+        add_flash(__('Failed to fetch dialog definition'), 'error');
+        return;
+      }
+      dialogModal(response.dialog, buttonData);
+    });
+  } else if (buttonData.entity_name) {
+    apiCall(buttonData);
   }
 });
 
