e32be495e50fbe2d2277709d0ba11052a38a00bf
Martin Povolny <mpovolny@redhat.com>
2018-07-19 09:44:10 +0200

    Button and dialog plugins: add simple API call.

--- app/javascript/packs/provider-dialogs-common.js
+++ app/javascript/packs/provider-dialogs-common.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import renderModal from '../provider-dialogs/modal'
-
-window.listenToRx(function(buttonData) {
-  if (!buttonData || buttonData.controller !== 'provider_dialogs') {
-    return;
-  }
-
-  if (! buttonData.component_name) {
-    console.error('provider-dialogs: missing component_name', buttonData);
-    return;
-  }
-
-  reactModal(buttonData);
-});
+import renderModal from '../provider-dialogs/modal';
 
 function reactModal(buttonData) {
   const props = {
     recordId: ManageIQ.record.recordId,
   };
 
-  let Component = ManageIQ.component.getReact(buttonData.component_name);
-  let inner = () => <Component {...props} />;
+  const Component = ManageIQ.component.getReact(buttonData.component_name);
+  const inner = () => <Component {...props} />;
 
   renderModal(__(buttonData.modal_title), inner);
 }
+
+function apiCall(buttonData) {
+  const url = '/' + buttonData.entity_name + '/' + ManageIQ.record.recordId;
+
+  API.post(url, { action: buttonData.action_name }).then(function(response) {
+
+    let errorMessage;
+
+    if (response.error) {
+      if (response.error.message) {
+        errorMessage = response.error.message;
+      } else {
+        errorMessage = __('An unknown error has occurred.');
+      }
+      add_flash(errorMessage, 'error');
+    }
+    add_flash(buttonData.success_message, 'success');
+  });
+}
+
+window.listenToRx(function (buttonData) {
+  if (!buttonData || buttonData.controller !== 'provider_dialogs') {
+    return;
+  }
+
+  if (buttonData.entity_name) {
+    apiCall(buttonData);
+  } else if (buttonData.component_name) {
+    // console.error('provider-dialogs: missing component_name', buttonData);
+    reactModal(buttonData);
+  }
+});
+
