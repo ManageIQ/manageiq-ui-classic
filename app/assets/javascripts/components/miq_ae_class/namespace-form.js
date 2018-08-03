824d3fff5ae3219206ff6c8a5613d5af8b4a5baf
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-03 16:26:23 +0200

    Avoid using string math in flash messages

--- app/assets/javascripts/components/miq_ae_class/namespace-form.js
+++ app/assets/javascripts/components/miq_ae_class/namespace-form.js
@@ -48,8 +48,13 @@ ManageIQ.angular.app.component('namespaceForm', {
 
     vm.cancelClicked = function() {
       miqService.sparkleOn();
-      var type = vm.aeNsDomain ? __("Domain") : __("Namespace");
-      var message = vm.newRecord ? sprintf(__('Add of %s \"%s\" cancelled by user.'), type, vm.namespaceModel.name) : sprintf(__('Edit of %s \"%s\" cancelled by user.'), type, vm.namespaceModel.name);
+      var message = '';
+      if (vm.newRecord) {
+        message = vm.aeNsDomain ? __('Add of Domain was cancelled by user.') : __('Add of Namespace was cancelled by user.');
+      } else {
+        message = vm.aeNsDomain ? __('Edit of Domain "%s" was cancelled by user.') : __('Edit of Namespace "%s" was cancelled by user.');
+        message = sprintf(message, vm.namespaceModel.name);
+      }
       var url = '/miq_ae_class/explorer';
       miqFlashLater({
         message: message,
