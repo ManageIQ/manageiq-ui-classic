3908ac2b610f850fa9a2047614554f24fc332bef
Zita Nemeckova <znemecko@redhat.com>
2018-08-08 10:57:53 +0200

    Fix specs - remove count of sparkleOns, remove volume_id and check volume instead, add missing spaces

--- spec/javascripts/components/cloud_volume_backup/cloud_volume_backup_form_spec.js
+++ spec/javascripts/components/cloud_volume_backup/cloud_volume_backup_form_spec.js
@@ -1,5 +1,5 @@
 describe('cloud-volume-backup-form', function() {
-  var $componentController, vm, miqService, API;
+  var $componentController, vm, miqService, API, $httpBackend, $scope;
 
   beforeEach(module('ManageIQ'));
   beforeEach(inject(function (_$componentController_, _$httpBackend_, _miqService_, $rootScope) {
@@ -13,9 +13,9 @@ describe('cloud-volume-backup-form', function() {
     spyOn(miqService, 'sparkleOn');
     spyOn(miqService, 'sparkleOff');
 
-    var bindings = {cloudVolumeBackupFormId: 1111};
-    vm = $componentController('cloudVolumeBackupFormComponent', null, bindings);
-    var volumeChoicesResponse = {volume_choices: ["pepa","pepik"]};
+    var bindings = {recordId: 1111};
+    vm = $componentController('cloudVolumeBackupForm', null, bindings);
+    var volumeChoicesResponse = {volume_choices: [{ name : "pepa" }, { name: "pepik" }]};
     $httpBackend.whenGET('/cloud_volume_backup/volume_form_choices').respond(volumeChoicesResponse);
     vm.$onInit();
     $httpBackend.flush();
@@ -29,7 +29,6 @@ describe('cloud-volume-backup-form', function() {
     it('sets the modelCopy', function(){
       var expectedModel = {
         volume: "pepa",
-        volume_id: '',
       };
       expect(vm.modelCopy).toEqual(expectedModel);
     })
@@ -39,16 +38,13 @@ describe('cloud-volume-backup-form', function() {
     beforeEach(function(){
       vm.cancelClicked();
     });
+
     it('turns the spinner on via the miqService', function(){
       expect(miqService.sparkleOn).toHaveBeenCalled();
     });
-    it('turns the spinner twice via the miqService', function(){
-      expect(miqService.sparkleOn.calls.count()).toBe(2);
-    });
+
     it('delegates miqService ajaxButton', function(){
-      var cancelUrl = '/cloud_volume_backup/backup_restore/';
-      var buttonUrl = '?button=cancel';
-      expect(miqService.miqAjaxButton).toHaveBeenCalledWith(cancelUrl + vm.cloudVolumeBackupFormId + buttonUrl);
+      expect(miqService.miqAjaxButton).toHaveBeenCalledWith('/cloud_volume_backup/backup_restore/1111?button=cancel');
     });
   });
 
@@ -56,36 +52,33 @@ describe('cloud-volume-backup-form', function() {
     beforeEach(function(){
       vm.saveClicked();
     });
+
     it('turns the spinner on via the miqService', function(){
       expect(miqService.sparkleOn).toHaveBeenCalled();
     });
-    it('turns the spinner twice via the miqService', function(){
-      expect(miqService.sparkleOn.calls.count()).toBe(2);
-    });
+
     it('delegates miqService ajaxButton', function(){
-      var restoreUrl = '/cloud_volume_backup/backup_restore/';
-      var buttonUrl = '?button=restore';
-      expect(miqService.miqAjaxButton).toHaveBeenCalledWith(restoreUrl + vm.cloudVolumeBackupFormId + buttonUrl, vm.cloudVolumeBackupModel, { complete: false });
+      expect(miqService.miqAjaxButton).toHaveBeenCalledWith('/cloud_volume_backup/backup_restore/1111?button=restore', vm.cloudVolumeBackupModel, { complete: false });
     });
   });
 
   describe('#resetClick', function(){
     beforeEach(function(){
-      vm.cloudVolumeBackupModel.volume_id = 42;
+      vm.cloudVolumeBackupModel.volume = "pepik";
       $scope.angularForm = {
-        $setPristine: function(value) {}
+        $setPristine: function(value) {},
       };
       vm.resetClicked($scope.angularForm);
     });
+
     it('turns the spinner on via the miqService', function(){
       expect(miqService.sparkleOn).toHaveBeenCalled();
     });
-    it('turns the spinner twice via the miqService', function(){
-      expect(miqService.sparkleOn.calls.count()).toBe(2);
-    });
+
     it('sets vm.cloudVolumeBackupModel to vm.modelCopy', function(){
-      expect(vm.modelCopy).toEqual(vm.cloudVolumeBackupModel);
+      expect(vm.cloudVolumeBackupModel.volume).toEqual("pepa");
     });
+
     it('sets flash message to be a warning with correct message', function(){
       expect(miqService.miqFlash).toHaveBeenCalledWith("warn", "All changes have been reset");
     });
