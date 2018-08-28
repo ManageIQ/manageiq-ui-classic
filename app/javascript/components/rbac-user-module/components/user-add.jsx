bdf69739f5a0f7cb5ccd72bdedebfe2fe90435d3
Martin Maroši <marvusm.mmi@gmail.com>
2018-08-28 10:41:45 +0200

    Enabled user editing in rbac user form.

--- app/javascript/components/rbac-user-module/components/user-add.jsx
+++ app/javascript/components/rbac-user-module/components/user-add.jsx
@@ -3,8 +3,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Spinner } from 'patternfly-react';
 import { goBack } from 'connected-react-router';
+import { Redirect } from 'react-router-dom';
 import { RbacUserForm } from '@manageiq/react-ui-components/dist/rbac-forms';
-import { loadGroups, saveUser } from '../redux/actions';
+import { loadGroups, saveUser, editUser } from '../redux/actions';
 
 const parseUserValues = ({
   name,
@@ -27,33 +28,39 @@ class UserAdd extends Component {
     }
   }
   render() {
-    const { groups, goBack, saveUser } = this.props;
+    const { groups, goBack, saveUser, isEditing, editUser, user } = this.props;
+    if (isEditing && !user) return <Redirect to="/add" />
     if (!groups) return <div><Spinner loading size="lg" /></div>;
     return (
       <div>
         <h1>User add</h1>
         <RbacUserForm
           groups={groups}
-          newRecord
+          newRecord={!isEditing}
+          initialValues={user ? {...user, groups: user.groups.map(({ groupId }) => groupId)} : undefined}
           onCancel={goBack}
-          onSave={values => {
-            console.log('values: ', values)
-            saveUser(parseUserValues(values));
-          }}
+          onSave={values => isEditing ? editUser(parseUserValues(values), user.id) : saveUser(parseUserValues(values))}
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ usersReducer: { groups } }) => ({
-  groups,
-});
+const mapStateToProps = ({ usersReducer: { groups, rows } }, { match : { url, params: { userId } } }) => {
+  const isEditing = url.match(/^\/edit\/[0-9]+$/) && userId;
+  const user = isEditing ? rows.find(({ id }) => id === userId) : undefined;
+  return {
+    groups,
+    isEditing,
+    user,
+  }
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   loadGroups,
   goBack,
   saveUser,
+  editUser,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserAdd);
