1729bfe188b2174a85cf3390b633faa15de22736
Milan Zazrivec <mzazrivec@redhat.com>
2018-09-13 16:28:35 +0200

    Remove unused class variable: @@logo_file

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -5,7 +5,6 @@ class ConfigurationController < ApplicationController
 
   logo_dir = File.expand_path(Rails.root.join('public', 'upload'))
   Dir.mkdir(logo_dir) unless File.exist?(logo_dir)
-  @@logo_file = File.join(logo_dir, "custom_logo.png")
 
   VIEW_RESOURCES = DEFAULT_SETTINGS[:views].keys.each_with_object({}) { |value, acc| acc[value.to_s] = value }.freeze
 
