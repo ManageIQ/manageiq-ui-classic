74bee4611f3f701727228a28df158d69e5a69cc4
Milan Zázrivec <mzazrivec@redhat.com>
2018-10-22 17:14:47 +0200

    Merge pull request #4760 from h-kataria/fixed_provider_links_on_alerts_list
    Fixed links to point to correct provider controller based upon type.
    
    (cherry picked from commit 5a0d67dfaa4c729ceb5defa30147767b18e6a9ea)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1637609

--- app/controllers/restful_redirect_controller.rb
+++ app/controllers/restful_redirect_controller.rb
@@ -3,22 +3,36 @@ class RestfulRedirectController < ApplicationController
 
   def index
     case params[:model]
+    when 'ExtManagementSystem'
+      record = ExtManagementSystem.find_by(:id => params[:id])
+      record ? redirect_to(polymorphic_path(record)) : handle_missing_record
     when 'ServiceTemplateTransformationPlanRequest'
       req = ServiceTemplateTransformationPlanRequest.select(:source_id).find(params[:id])
-      redirect_to(:controller => 'migration', :action => 'index', :anchor => "plan/#{req.source_id}")
+      req ? redirect_to(:controller => 'migration', :action => 'index', :anchor => "plan/#{req.source_id}") : handle_missing_record
     when 'MiqRequest'
       redirect_to(:controller => 'miq_request', :action => 'show', :id => params[:id])
     when 'VmOrTemplate'
-      klass = VmOrTemplate.select(:id, :type).find(params[:id]).try(:class)
-      controller = if klass && (VmCloud > klass || TemplateCloud > klass)
-                     'vm_cloud'
-                   else
-                     'vm_infra'
-                   end
-      redirect_to(:controller => controller, :action => 'show', :id => params[:id])
+      record = VmOrTemplate.select(:id, :type).find(params[:id])
+      record ? handle_vm_redirect(record) : handle_missing_record
     else
-      flash_to_session(_("Could not find the given \"%{model}\" record.") % {:model => ui_lookup(:model => params[:model])}, :error)
-      redirect_to(:controller => 'dashboard', :action => 'show')
+      handle_missing_record
     end
+  end
+
+  private
+
+  def handle_vm_redirect(record)
+    klass = record.class
+    controller = if klass && (VmCloud > klass || TemplateCloud > klass)
+                   'vm_cloud'
+                 else
+                   'vm_infra'
+                 end
+    redirect_to(:controller => controller, :action => 'show', :id => params[:id])
+  end
+
+  def handle_missing_record
+    flash_to_session(_("Could not find the given \"%{model}\" record.") % {:model => ui_lookup(:model => params[:model])}, :error)
+    redirect_to(:controller => 'dashboard', :action => 'show')
   end
 end
