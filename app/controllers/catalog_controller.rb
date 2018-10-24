e025500ea244353815bea877619f17e292d4878a
Hilda Stastna <hstastna@redhat.com>
2018-10-24 17:17:31 +0200

    Fix removing catalog items from Catalog Bundle resource list
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1639557

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -442,6 +442,7 @@ class CatalogController < ApplicationController
     rearrange_groups_array
     build_ae_tree(:catalog, :automate_tree) # Build Catalog Items tree
     changed = (@edit[:new] != @edit[:current])
+    @available_catalogs = available_catalogs.sort # Get available catalogs with tenants and ancestors
     render :update do |page|
       page << javascript_prologue
       page.replace("basic_info_div", :partial => "form_basic_info")
