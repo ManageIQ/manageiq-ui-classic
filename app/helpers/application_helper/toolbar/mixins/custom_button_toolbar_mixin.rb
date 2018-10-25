16237216496cfd32a3b03f38df3aa4ed1e9692aa
Roman Blanco <rblanco@redhat.com>
2018-10-25 14:42:32 +0200

    Alias PersistentVolume instead of adding it to the list of base models

--- app/helpers/application_helper/toolbar/mixins/custom_button_toolbar_mixin.rb
+++ app/helpers/application_helper/toolbar/mixins/custom_button_toolbar_mixin.rb
@@ -1,21 +1,26 @@
 module ApplicationHelper::Toolbar::Mixins::CustomButtonToolbarMixin
+  # FIXME: replace with CustomButton.button_classes
+  # (ServiceTemplate, VmOrTemplate, GenericObjectDefinition are exceptions)
   APPLIES_TO_CLASS_BASE_MODELS = %w(AvailabilityZone CloudNetwork CloudObjectStoreContainer CloudSubnet CloudTenant
                                     CloudVolume ContainerGroup ContainerImage ContainerNode ContainerProject
                                     ContainerTemplate ContainerVolume EmsCluster ExtManagementSystem
                                     GenericObject GenericObjectDefinition Host LoadBalancer
                                     MiqGroup MiqTemplate NetworkRouter OrchestrationStack
-                                    PersistentVolume SecurityGroup Service
-                                    ServiceTemplate Storage Switch Tenant User Vm VmOrTemplate).freeze
+                                    SecurityGroup Service ServiceTemplate Storage Switch Tenant
+                                    User Vm VmOrTemplate).freeze
 
   def custom_button_appliable_class?(model)
+    # FIXME: merge with model replacement in 'custom_button_class_model'
     model = "MiqTemplate" if model == "Image"
     model = "Vm" if model == "Instance"
+    model = "ContainerVolume" if model == "PersistentVolume"
     model = "ExtManagementSystem" if model == "StorageManager"
     APPLIES_TO_CLASS_BASE_MODELS.include?(model)
   end
 
   def custom_button_class_model(applies_to_class)
     # TODO: Give a better name for this concept, including ServiceTemplate using Service
+    #       'custom_button_appliable_class' -> 'display_to_model'
     # This should probably live in the model once this concept is defined.
     unless custom_button_appliable_class?(applies_to_class)
       raise ArgumentError, "Received: #{applies_to_class}, expected one of #{APPLIES_TO_CLASS_BASE_MODELS}"
@@ -24,6 +29,8 @@ module ApplicationHelper::Toolbar::Mixins::CustomButtonToolbarMixin
     case applies_to_class
     when "ServiceTemplate"
       Service
+    when "PersistentVolume"
+      ContainerVolume
     when "GenericObjectDefinition"
       GenericObject
     when "Instance"
