dc419aead8b970e849f02d15d1a7a8518f3a8fe6
Martin Maroši <marvusm.mmi@gmail.com>
2018-11-01 10:45:18 +0100

    Added missing elint configuration

--- app/javascript/.eslintrc.js
+++ app/javascript/.eslintrc.js
@@ -7,6 +7,13 @@ module.exports = {
   extends: ['airbnb', 'plugin:jest/recommended'],
   plugins: ['jest', 'react'],
   rules: {
-    'react/jsx-filename-extension': 'off',
+    "react/jsx-filename-extension": "off",
+    "max-len": ["error", {
+      code: 150,
+      ignoreComments: true
+    }],
+    "no-use-before-define": ["error", { "functions": false, "classes": false }],
+    "no-underscore-dangle": "off",
+    "import/prefer-default-export": "off"
   },
 };
