6b04cd19b3f0fbaa2d4dbad7c496a98899458459
Zita Nemeckova <znemecko@redhat.com>
2018-11-07 11:25:06 +0100

    miq_grid

--- app/javascript/spec/helpers/old_js_file_require_helper.js
+++ app/javascript/spec/helpers/old_js_file_require_helper.js
@@ -2,3 +2,4 @@ global.window = global;
 require('../../../assets/javascripts/miq_application.js');
 require('../../../assets/javascripts/miq_flash.js');
 require('../../../assets/javascripts/resizable_sidebar.js');
+require('../../../assets/javascripts/miq_global.js');
