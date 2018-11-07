4ec922966d7e814ae9fca1159c4dca6d847f97a4
Zita Nemeckova <znemecko@redhat.com>
2018-11-07 10:59:11 +0100

    resizable_sidebar

--- app/javascript/spec/helpers/old_js_file_require_helper.js
+++ app/javascript/spec/helpers/old_js_file_require_helper.js
@@ -1,4 +1,4 @@
 global.window = global;
 require('../../../assets/javascripts/miq_application.js');
 require('../../../assets/javascripts/miq_flash.js');
-
+require('../../../assets/javascripts/resizable_sidebar.js');
