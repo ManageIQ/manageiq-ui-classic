75e015707c8cc765b9d1a954f42010a214381f78
Martin Povolny <mpovolny@redhat.com>
2018-11-14 10:30:59 +0100

    CloudVolumes: use "any?", test rather than rescue.

--- app/helpers/application_helper/button/cloud_volume_backup_create.rb
+++ app/helpers/application_helper/button/cloud_volume_backup_create.rb
@@ -1,5 +1,8 @@
 class ApplicationHelper::Button::CloudVolumeBackupCreate < ApplicationHelper::Button::ButtonNewDiscover
   def disabled?
-    ManageIQ::Providers::CloudManager.all.none? { |ems| ems.class::CloudVolume.supports?(:backup_create) rescue false }
+    !ManageIQ::Providers::CloudManager.any? do |ems|
+      Module.const_defined?("#{ems.class}::CloudVolume") &&
+        ems.class::CloudVolume.supports?(:backup_create)
+    end
   end
 end
