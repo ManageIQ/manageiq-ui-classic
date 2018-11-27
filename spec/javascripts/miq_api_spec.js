e0b455eac95a91b6f5eed34c0cfc4d965345fec6
Martin Hradil <mhradil@redhat.com>
2018-11-27 16:00:28 +0000

    miq_api spec - ignore api call, fix old vanillaJsApi -> API

--- spec/javascripts/miq_api_spec.js
+++ spec/javascripts/miq_api_spec.js
@@ -2,15 +2,17 @@ describe('miq_api.js', function() {
   it("can base64 encode utf8 passwords", function(done) {
     var getArgs;
 
-    spyOn(vanillaJsAPI, 'get').and.callFake(function(_url, args) {
+    spyOn(API, 'get').and.callFake(function(_url, args) {
       getArgs = args;
       return Promise.resolve({});
     });
 
-    vanillaJsAPI.login('foo', 'páššwøřď 密码 密碼')
+    API.mock.ignore('/api/auth');
+
+    API.login('foo', 'páššwøřď 密码 密碼')
       .then(function() {
-        expect(vanillaJsAPI.get).toHaveBeenCalled();
-        expect(getArgs.headers.Authorization).toBe("Basic Zm9vOnDDocWhxaF3w7jFmcSPIOWvhueggSDlr4bnorw=")
+        expect(API.get).toHaveBeenCalled();
+        expect(getArgs.headers.Authorization).toBe("Basic Zm9vOnDDocWhxaF3w7jFmcSPIOWvhueggSDlr4bnorw=");
       })
       .then(done, done);
   });
