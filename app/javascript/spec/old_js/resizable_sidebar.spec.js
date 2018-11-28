68c2c24147e9a9c3576913f10ebdde073a0be720
Zita Nemeckova <znemecko@redhat.com>
2018-11-28 10:28:05 +0100

    get jQuery

--- app/javascript/spec/old_js/resizable_sidebar.spec.js
+++ app/javascript/spec/old_js/resizable_sidebar.spec.js
@@ -1,4 +1,5 @@
 require('../helpers/set_fixtures_helper.js');
+require('../helpers/old_js_file_require_helper.js');
 
 describe('resizable-sidebar.js', () => {
   beforeEach(() => {
@@ -35,44 +36,43 @@ describe('resizable-sidebar.js', () => {
 
   it('hide sidebar', () => {
     $('.resize-left').click();
+    expect($('#left').hasClass('col-md-2')).toBe(false);
+    expect($('#left').hasClass('col-md-pull-10')).toBe(false);
+    expect($('#left').hasClass('hidden-lg')).toBe(true);
+    expect($('#left').hasClass('hidden-md')).toBe(true);
 
-    expect($('#left')).not.toHaveClass('col-md-2');
-    expect($('#left')).not.toHaveClass('col-md-pull-10');
-    expect($('#left')).toHaveClass('hidden-lg');
-    expect($('#left')).toHaveClass('hidden-md');
-
-    expect($('#right')).not.toHaveClass('col-md-10');
-    expect($('#right')).not.toHaveClass('col-md-push-2');
-    expect($('#right')).toHaveClass('col-md-12');
-    expect($('#right')).toHaveClass('col-md-push-0');
+    expect($('#right').hasClass('col-md-10')).toBe(false);
+    expect($('#right').hasClass('col-md-push-2')).toBe(false);
+    expect($('#right').hasClass('col-md-12')).toBe(true);
+    expect($('#right').hasClass('col-md-push-0')).toBe(true);
   });
 
   it('show sidebar', () => {
     $('.resize-left').click();
     $('.resize-right').click();
 
-    expect($('#left')).not.toHaveClass('hidden-md');
-    expect($('#left')).not.toHaveClass('hidden-lg');
-    expect($('#left')).toHaveClass('col-md-2');
-    expect($('#left')).toHaveClass('col-md-pull-10');
+    expect($('#left').hasClass('hidden-md')).toBe(false);
+    expect($('#left').hasClass('hidden-lg')).toBe(false);
+    expect($('#left').hasClass('col-md-2')).toBe(true);
+    expect($('#left').hasClass('col-md-pull-10')).toBe(true);
 
-    expect($('#right')).not.toHaveClass('col-md-12');
-    expect($('#right')).not.toHaveClass('col-md-push-0');
-    expect($('#right')).toHaveClass('col-md-10');
-    expect($('#right')).toHaveClass('col-md-push-2');
+    expect($('#right').hasClass('col-md-12')).toBe(false);
+    expect($('#right').hasClass('col-md-push-0')).toBe(false);
+    expect($('#right').hasClass('col-md-10')).toBe(true);
+    expect($('#right').hasClass('col-md-push-2')).toBe(true);
   });
 
   it('broaden sidebar', () => {
     for (var i=2; i<=5; i++) {
-      expect($('#left')).not.toHaveClass('col-md-' + (i-1));
-      expect($('#left')).not.toHaveClass('col-md-pull-' + (11-i));
-      expect($('#left')).toHaveClass('col-md-' + i);
-      expect($('#left')).toHaveClass('col-md-pull-' + (12-i));
+      expect($('#left').hasClass('col-md-' + (i-1))).toBe(false);
+      expect($('#left').hasClass('col-md-pull-' + (11-i))).toBe(false);
+      expect($('#left').hasClass('col-md-' + i)).toBe(true);
+      expect($('#left').hasClass('col-md-pull-' + (12-i))).toBe(true);
 
-      expect($('#right')).not.toHaveClass('col-md-' + (13-i));
-      expect($('#right')).not.toHaveClass('col-md-push-' + (i+1));
-      expect($('#right')).toHaveClass('col-md-' + (12-i));
-      expect($('#right')).toHaveClass('col-md-push-' + i);
+      expect($('#right').hasClass('col-md-' + (13-i))).toBe(false);
+      expect($('#right').hasClass('col-md-push-' + (i+1))).toBe(false);
+      expect($('#right').hasClass('col-md-' + (12-i))).toBe(true);
+      expect($('#right').hasClass('col-md-push-' + i)).toBe(true);
 
       $('.resize-right').click();
     }
@@ -84,15 +84,15 @@ describe('resizable-sidebar.js', () => {
     }
 
     for (var i=5; i>=2; i--) {
-      expect($('#left')).not.toHaveClass('col-md-' + (i+1));
-      expect($('#left')).not.toHaveClass('col-md-pull-' + (13-i));
-      expect($('#left')).toHaveClass('col-md-' + i);
-      expect($('#left')).toHaveClass('col-md-pull-' + (12-i));
+      expect($('#left').hasClass('col-md-' + (i+1))).toBe(false);
+      expect($('#left').hasClass('col-md-pull-' + (13-i))).toBe(false);
+      expect($('#left').hasClass('col-md-' + i)).toBe(true);
+      expect($('#left').hasClass('col-md-pull-' + (12-i))).toBe(true);
 
-      expect($('#right')).not.toHaveClass('col-md-' + (11-i));
-      expect($('#right')).not.toHaveClass('col-md-push-' + (i-1));
-      expect($('#right')).toHaveClass('col-md-' + (12-i));
-      expect($('#right')).toHaveClass('col-md-push-' + i);
+      expect($('#right').hasClass('col-md-' + (11-i))).toBe(false);
+      expect($('#right').hasClass('col-md-push-' + (i-1))).toBe(false);
+      expect($('#right').hasClass('col-md-' + (12-i))).toBe(true);
+      expect($('#right').hasClass('col-md-push-' + i)).toBe(true);
 
       $('.resize-left').click();
     }
@@ -102,10 +102,9 @@ describe('resizable-sidebar.js', () => {
     for (var i=0; i<5; i++) {
       $('.resize-right').click();
     }
-    expect($('#left')).toHaveClass('col-md-5');
-    expect($('#left')).toHaveClass('col-md-pull-7');
-    expect($('#right')).toHaveClass('col-md-7');
-    expect($('#right')).toHaveClass('col-md-push-5');
+    expect($('#left').hasClass('col-md-5')).toBe(true);
+    expect($('#left').hasClass('col-md-pull-7')).toBe(true);
+    expect($('#right').hasClass('col-md-7')).toBe(true);
+    expect($('#right').hasClass('col-md-push-5')).toBe(true);
   });
-
 });
