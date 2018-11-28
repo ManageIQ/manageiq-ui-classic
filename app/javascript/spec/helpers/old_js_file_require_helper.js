dde56371027b399b3a1f8acd80aa329ba69fbd84
Zita Nemeckova <znemecko@redhat.com>
2018-11-28 14:38:03 +0100

    Resolve miq_tree_spec

--- app/javascript/spec/helpers/old_js_file_require_helper.js
+++ app/javascript/spec/helpers/old_js_file_require_helper.js
@@ -1,7 +1,9 @@
 global.window = global;
 window.$ = window.jQuery = require('jquery');
+require('patternfly-bootstrap-treeview');
 require('../../../assets/javascripts/miq_application.js');
 require('../../../assets/javascripts/miq_flash.js');
 require('../../../assets/javascripts/resizable_sidebar.js');
 require('../../../assets/javascripts/miq_global.js');
 require('../../../assets/javascripts/miq_grid.js');
+require('../../../assets/javascripts/miq_tree.js');
