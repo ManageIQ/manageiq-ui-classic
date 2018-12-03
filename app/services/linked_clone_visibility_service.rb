e2fd09263714831a2f549854ed339f8739d7bef2
Milan Zazrivec <mzazrivec@redhat.com>
2018-12-03 10:56:11 +0100

    Fix rubocop warnings in LinkedCloneVisibilityService

--- app/services/linked_clone_visibility_service.rb
+++ app/services/linked_clone_visibility_service.rb
@@ -5,7 +5,7 @@ class LinkedCloneVisibilityService
     field_names_to_hide = []
 
     if provision_type.to_s == 'vmware'
-      if snapshot_count > 0
+      if snapshot_count.positive?
         field_names_to_edit += [:linked_clone]
       else
         field_names_to_show += [:linked_clone]
@@ -17,7 +17,7 @@ class LinkedCloneVisibilityService
         field_names_to_hide += [:snapshot]
       end
     else
-      field_names_to_hide += [:linked_clone, :snapshot]
+      field_names_to_hide += %i(linked_clone snapshot)
     end
 
     {:hide => field_names_to_hide, :edit => field_names_to_edit, :show => field_names_to_show}
