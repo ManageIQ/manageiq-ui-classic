3f877375c7155a1b260c83b14d464832bd4c13f2
Milan Zazrivec <mzazrivec@redhat.com>
2018-12-11 10:34:24 +0100

    Fix rubocop warning in FlavorHelper::TextualSummary

--- app/helpers/flavor_helper/textual_summary.rb
+++ app/helpers/flavor_helper/textual_summary.rb
@@ -75,7 +75,7 @@ module FlavorHelper::TextualSummary
   def textual_instances
     num   = @record.number_of(:vms)
     h     = {:label => _('Instances'), :icon => "pficon pficon-virtual-machine", :value => num}
-    if num > 0 && role_allows?(:feature => "vm_show_list")
+    if num.positive? && role_allows?(:feature => "vm_show_list")
       h[:link]  = url_for_only_path(:action => 'show', :id => @record, :display => 'instances')
       h[:title] = _("Show all Instances")
     end
