f4951f1f08f4c5a53a9b1fe45790e26cb8b748b3
Milan Zazrivec <mzazrivec@redhat.com>
2018-12-12 12:23:10 +0100

    Fix rubocop warnings in InfraNetworkingHelper::TextualSummary

--- app/helpers/infra_networking_helper/textual_summary.rb
+++ app/helpers/infra_networking_helper/textual_summary.rb
@@ -21,7 +21,7 @@ module InfraNetworkingHelper::TextualSummary
   def textual_hosts
     num = @record.number_of(:hosts)
     h = {:label => title_for_hosts, :icon => "pficon pficon-container-node", :value => num}
-    if num > 0 && role_allows?(:feature => "host_show_list")
+    if num.positive? && role_allows?(:feature => "host_show_list")
       h = {:label => title_for_hosts, :icon => "pficon pficon-container-node", :value => num}
       h[:explorer] = true
       h[:link] = url_for_only_path(:action => 'hosts', :id => @record, :db => 'switch')
