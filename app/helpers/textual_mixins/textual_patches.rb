113e4a0c9c4eb38f82ced59e58e9e02b60bd3d94
Milan Zazrivec <mzazrivec@redhat.com>
2018-12-17 13:33:56 +0100

    Fix rubocop warning in TextualMixins::TextualPatches

--- app/helpers/textual_mixins/textual_patches.rb
+++ app/helpers/textual_mixins/textual_patches.rb
@@ -4,7 +4,7 @@ module TextualMixins::TextualPatches
     return nil if os == "unknown" || os =~ /linux/
     num = @record.number_of(:patches)
     h = {:label => _("Patches"), :icon => "fa fa-shield", :value => num}
-    if num > 0
+    if num.positive?
       h[:title] = n_("Show the Patch defined on this VM", "Show the Patches defined on this VM", num)
       h[:explorer] = true
       h[:link] = url_for_only_path(:action => 'patches', :id => @record, :db => controller.controller_name)
