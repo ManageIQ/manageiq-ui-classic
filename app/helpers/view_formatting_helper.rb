5d079a5884351b62bb3ebce2b9f83af4cf2b7427
Milan Zazrivec <mzazrivec@redhat.com>
2018-12-18 13:21:29 +0100

    Fix rubocop warnings in ViewFormattingHelper

--- app/helpers/view_formatting_helper.rb
+++ app/helpers/view_formatting_helper.rb
@@ -1,12 +1,12 @@
 module ViewFormattingHelper
   def format_form_group(label, value = nil)
     output = content_tag(:label, :class => "col-md-2 control-label") do
-               _(label)
+      _(label)
     end
     output << content_tag(:div, :class => "col-md-8") do
-                content_tag(:p, :class => "form-control-static") do
-                  value
-                end
+      content_tag(:p, :class => "form-control-static") do
+        value
+      end
     end
     output
   end
