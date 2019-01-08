6905a2586fcf9b943509756989bea3965dce936d
Milan Zázrivec <mzazrivec@redhat.com>
2019-01-08 08:21:14 +0100

    Merge pull request #5129 from lpichler/add_spec_for_manage_tenants_quota_button
    Add spec for button tenant manage quota in OpsController
    
    (cherry picked from commit 0c75474e9be9f0a87a87551b33b7af2e2ceaa5af)

---
+++ spec/helpers/application_helper/buttons/basic_spec.rb
@@ -0,0 +1,37 @@
+describe ApplicationHelper::Button::Basic do
+  context "Button Manage Tenant Quota in OpsController" do
+    let(:controller) { OpsController.new }
+
+    let(:button) { ApplicationHelper::Button::Basic.new(controller, nil, {}, {:id => 'rbac_tenant_manage_quotas'}) }
+    let(:tenant_alpha) { FactoryBot.create(:tenant, :parent => Tenant.root_tenant) }
+    let(:tenant_omega) { FactoryBot.create(:tenant, :parent => tenant_alpha) }
+
+    let(:feature) { MiqProductFeature.find_all_by_identifier(["rbac_tenant_manage_quotas_tenant_#{tenant_omega.id}"]) }
+    let(:role_with_access_to_omega_rbac_tenant_manage_quota_permission) { FactoryGirl.create(:miq_user_role, :miq_product_features => feature) }
+
+    let(:group_alpha) { FactoryBot.create(:miq_group, :tenant => tenant_alpha, :miq_user_role => role_with_access_to_omega_rbac_tenant_manage_quota_permission) }
+    let(:user_alpha)  { FactoryBot.create(:user, :miq_groups => [group_alpha]) }
+
+    before do
+      EvmSpecHelper.seed_specific_product_features(%w(rbac_tenant_manage_quotas))
+      Tenant.seed
+
+      @view_context = setup_view_context_with_sandbox({})
+      User.current_user = user_alpha
+      @button_on_tenant_alpha = described_class.new(@view_context, {}, {'record' => tenant_alpha}, {})
+      @button_on_tenant_omega = described_class.new(@view_context, {}, {'record' => tenant_omega}, {})
+    end
+
+    it "doesn't displays button Manage Tenant Quota for alpha tenant without tenant product permission for alpha tenant" do
+      controller.instance_variable_set(:@_params, :id => "tn_#{tenant_alpha.id}")
+
+      expect(button.role_allows_feature?).to be_falsey
+    end
+
+    it "displays button Manage Tenant Quota from a tenant omega with tenant product permission for omega tenant" do
+      controller.instance_variable_set(:@_params, :id => "tn_#{tenant_omega.id}")
+
+      expect(button.role_allows_feature?).to be_truthy
+    end
+  end
+end
