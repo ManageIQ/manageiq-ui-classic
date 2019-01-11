15744a8b37ace7cb7d61d223ce3ff2cdcab71eed
Martin Hradil <mhradil@redhat.com>
2019-01-11 16:42:39 +0000

    TODO rest of haml

--- app/javascript/components/analysis-profile-form/edit_table.jsx
+++ app/javascript/components/analysis-profile-form/edit_table.jsx
@@ -44,32 +44,8 @@ class EditTable extends Component {
           </tr>
         </thead>
         <tbody>
-/*
-          {canAdd && (
-            %tr{:title => _("Click to add a new entry"), :onclick => remote_function(:url => {:action => 'ap_ce_select', :add => 'new', :item => "file", :id => scan_id})}
-              %td= _("<New Entry>")
-              %td
-              %td.action-cell
-                %button.btn.btn-default.btn-block.btn-sm
-                  = _("Add")
-          )}
-          {adding && (
-            %tr
-              %td
-                = text_field("entry", "fname", :maxlength => ViewHelper::MAX_NAME_LEN, :style => "width: 100%;")
-                = hidden_field("item", "type1", :value => "file")
-              %td
-                = check_box_tag("entry_content", 1, false, :id => "entry_content")
-              %td.action-cell
-                %button.btn.btn-default.btn-block.btn-sm{:title => _("Add this entry"),
-                  "data-submit"         => 'ap_form_div',
-                  "data-miq_sparkle_on" => true,
-                  :remote               => true,
-                  "data-method"         => :post,
-                  'data-click_url'      => {:url => "#{url}?accept=accept"}.to_json}
-                  = _("Save")
-          )}
-*/
+          {canAdd && (<div>TODO</div>)}
+          {adding && (<div>TODO</div>)}
           {data.map((row, index) => (
             <tr className={(['row0', 'row1'])[index % 2]}>
               {(editing !== row) && (
@@ -86,8 +62,43 @@ class EditTable extends Component {
                   </td>
                 </React.Fragment>
               )}
+              {(editing === row) && (<div>TODO</div>)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default EditTable;
+
 /*
-              {(editing === row) && (
+canAdd:
+            %tr{:title => _("Click to add a new entry"), :onclick => remote_function(:url => {:action => 'ap_ce_select', :add => 'new', :item => "file", :id => scan_id})}
+              %td= _("<New Entry>")
+              %td
+              %td.action-cell
+                %button.btn.btn-default.btn-block.btn-sm
+                  = _("Add")
+
+adding:
+            %tr
+              %td
+                = text_field("entry", "fname", :maxlength => ViewHelper::MAX_NAME_LEN, :style => "width: 100%;")
+                = hidden_field("item", "type1", :value => "file")
+              %td
+                = check_box_tag("entry_content", 1, false, :id => "entry_content")
+              %td.action-cell
+                %button.btn.btn-default.btn-block.btn-sm{:title => _("Add this entry"),
+                  "data-submit"         => 'ap_form_div',
+                  "data-miq_sparkle_on" => true,
+                  :remote               => true,
+                  "data-method"         => :post,
+                  'data-click_url'      => {:url => "#{url}?accept=accept"}.to_json}
+                  = _("Save")
+
+editing:
                 %td
                   = text_field("entry", "fname", :maxlength => ViewHelper::MAX_NAME_LEN, "value" => session[:edit_filename], :style => "width: 100%;")
                   = hidden_field("item", "type1", :value => "file")
@@ -103,14 +114,4 @@ class EditTable extends Component {
                     "data-method"         => :post,
                     'data-click_url'      => {:url => "#{url}?accept=accept"}.to_json}
                     = _("Save")
-              )}
 */
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
-
-export default EditTable;
