8f5afb1833089849686b2a564c120f4c4fbe5794
Martin Hradil <mhradil@redhat.com>
2019-01-15 14:43:43 +0000

    DashboardDelete button - add needs :@dashboard
    switching to CI > Reports > accordion Dashboards would fail, because
    when the "All Dashboards" item is selected, `@dashboard` gets set to nil,
    and the toolbar button fails on `nil.read_only`.
    
    Adding `needs :@dashboard` will cause the button to get hidden without throwing.
    
    Fixes https://github.com/ManageIQ/manageiq-ui-classic/issues/5153

--- app/helpers/application_helper/button/dashboard_delete.rb
+++ app/helpers/application_helper/button/dashboard_delete.rb
@@ -1,4 +1,6 @@
 class ApplicationHelper::Button::DashboardDelete < ApplicationHelper::Button::Basic
+  needs :@dashboard
+
   def disabled?
     @error_message = _('Default Dashboard cannot be deleted') if @dashboard.read_only
     @error_message.present?
