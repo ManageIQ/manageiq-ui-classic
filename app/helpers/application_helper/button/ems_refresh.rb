34d26d9d0867c7581881dd20413faa64a93fa497
Dávid Halász <dhalasz@redhat.com>
2019-01-15 13:29:07 +0100

    Disable the provider refresh button for suspended providers

--- app/helpers/application_helper/button/ems_refresh.rb
+++ app/helpers/application_helper/button/ems_refresh.rb
@@ -3,6 +3,7 @@ class ApplicationHelper::Button::EmsRefresh < ApplicationHelper::Button::Basic
 
   def disabled?
     super
+    @error_message ||= _("Unable to refresh the provider while it is suspended") unless @record.enabled?
     @error_message ||= _("Credentials must be valid to refresh a provider") unless @record.authentication_status.downcase == "valid"
     @error_message.present?
   end
