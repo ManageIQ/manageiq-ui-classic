f197ed1c0294d6912d6b0b6b11944f1ce74cf320
Martin Hradil <mhradil@redhat.com>
2019-01-15 15:45:53 +0000

    DashboardDelete button spec - fix db vs dashboard, add missing spec for nil

--- spec/helpers/application_helper/buttons/dashboard_delete_spec.rb
+++ spec/helpers/application_helper/buttons/dashboard_delete_spec.rb
@@ -1,7 +1,7 @@
 describe ApplicationHelper::Button::DashboardDelete do
   let(:view_context) { setup_view_context_with_sandbox({}) }
   let(:dashboard) { FactoryBot.create(:miq_widget_set, :read_only => read_only) }
-  let(:button) { described_class.new(view_context, {}, {'db' => dashboard}, {}) }
+  let(:button) { described_class.new(view_context, {}, {:dashboard => dashboard}, {}) }
 
   describe '#calculate_properties' do
     before { button.calculate_properties }
@@ -10,9 +10,21 @@ describe ApplicationHelper::Button::DashboardDelete do
       let(:read_only) { true }
       it_behaves_like 'a disabled button', 'Default Dashboard cannot be deleted'
     end
+
     context 'when dashboard is writable' do
       let(:read_only) { false }
       it_behaves_like 'an enabled button'
+    end
+  end
+
+  describe '#skipped?' do
+    context 'when dashboard is nil' do
+      let(:dashboard) { nil }
+      subject { button }
+
+      it "button is hidden" do
+        expect(subject.skipped?).to be_truthy
+      end
     end
   end
 end
