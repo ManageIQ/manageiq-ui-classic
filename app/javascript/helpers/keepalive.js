3271950d5f5204401778b541b15b4f4e00d18282
Martin Hradil <mhradil@redhat.com>
2019-02-04 14:34:04 +0000

    miqKeepAlive - stop when the token expires
    just so the user doesn't come back to see a thousand flash messages

--- app/javascript/helpers/keepalive.js
+++ app/javascript/helpers/keepalive.js
@@ -3,6 +3,8 @@ import idleJs from 'idle-js';
 
 let last_ping = new Date() / 1000;
 let idle = null;
+let interval = null;
+let stopped = true;
 
 export default function miqKeepAlive() {
   if (! ManageIQ.login.timeout) {
@@ -10,7 +12,9 @@ export default function miqKeepAlive() {
     return;
   }
 
-  window.setInterval(maybePing, ManageIQ.login.timeout / 3 * 1000);
+  stopped = false;
+
+  interval = window.setInterval(maybePing, ManageIQ.login.timeout / 3 * 1000);
 
   idle = new idleJs({
     idle: 60000, // idle after a minute
@@ -23,6 +27,10 @@ export default function miqKeepAlive() {
 
 // ping, except when hidden, inactive, or pinged in the last timeout/3 seconds
 function maybePing() {
+  if (stopped) {
+    return;
+  }
+
   const now = new Date() / 1000;
 
   if (! idle.visible) {
@@ -43,16 +51,31 @@ function maybePing() {
 
 // actually renews the cookie
 function ping() {
-  http.post('/ui/keepalive')
+  http.post('/ui/keepalive', {}, {
+    skipErrors: [401, 422], // logged out, invalid csrf
+  })
     .then((response) => {
       ManageIQ.login.timeout = response.timeout;
       last_ping = new Date() / 1000;
     })
     .catch(() => {
-      window.add_flash(__("You have been logged out becuase of session timeout."), "warning");
+      window.add_flash(__('You have been logged out because of session timeout.'), 'warning');
 
       if (miqInAForm()) {
-        window.add_flash(__("Please make sure to log in in a different tab before submitting the form."), "info");
+        window.add_flash(__('Please make sure to log in in a different tab before submitting the form.'), 'info');
       }
+
+      stop();
     });
 }
+
+function stop() {
+  window.clearInterval(interval);
+  interval = null;
+
+  // idle.stop();
+  stopped = true;
+}
+
+miqKeepAlive.ping = ping;
+miqKeepAlive.stop = stop;
