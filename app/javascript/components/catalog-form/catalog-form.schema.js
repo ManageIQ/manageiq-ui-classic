60dd4eb8a8e7688719d9e0370eade29fcc8c2d5b
Richard Vsiansky <r.vsia@seznam.cz>
2019-02-27 10:32:40 +0100

    Adds async validator to catalog form

--- app/javascript/components/catalog-form/catalog-form.schema.js
+++ app/javascript/components/catalog-form/catalog-form.schema.js
@@ -1,4 +1,19 @@
-import { componentTypes, validatorTypes } from '@data-driven-forms/react-form-renderer';
+import { componentTypes } from '@data-driven-forms/react-form-renderer';
+import debouncePromise from '../../helpers/promise-debounce';
+import { API } from '../../http_api';
+
+export const asyncValidator = value => API.get(`/api/service_catalogs?expand=resources&filter[]=name=${value}`)
+  .then((json) => {
+    if (json.resources.length > 0) {
+      return __('Name has already been taken');
+    }
+    if (value === '' || value === undefined) {
+      return __("Name can't be blank");
+    }
+    return undefined;
+  });
+
+const asyncValidatorDebounced = debouncePromise(asyncValidator);
 
 function createSchema(options) {
   const fields = [{
@@ -7,10 +22,9 @@ function createSchema(options) {
     fields: [{
       component: componentTypes.TEXT_FIELD,
       name: 'name',
-      validate: [{
-        type: validatorTypes.REQUIRED,
-        message: __("Name can't be blank"),
-      }],
+      validate: [
+        asyncValidatorDebounced,
+      ],
       label: __('Name'),
       maxLength: 40,
       autoFocus: true,
