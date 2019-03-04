228b6dda933065e3ca82a185dbd7cbd7749e4ab0
Dávid Halász <dhalasz@redhat.com>
2019-03-04 13:39:04 +0100

    Catalog trees: move the unassigned node add into the ServiceCatalogs
    It's better to use super + 1 at the only place where it is being used.

--- app/presenters/tree_builder_catalogs_class.rb
+++ app/presenters/tree_builder_catalogs_class.rb
@@ -4,20 +4,7 @@ class TreeBuilderCatalogsClass < TreeBuilder
 
   private
 
-  def x_get_tree_roots(count_only, options)
-    objects = count_only_or_objects_filtered(count_only, ServiceTemplateCatalog.all, "name")
-    case options[:type]
-    when :stcat
-      objects
-    when :sandt
-      if count_only
-        objects + 1
-      else
-        objects.unshift(ServiceTemplateCatalog.new(
-                          :name        => 'Unassigned',
-                          :description => 'Unassigned Catalogs'
-        ))
-      end
-    end
+  def x_get_tree_roots(count_only, _options)
+    count_only_or_objects_filtered(count_only, ServiceTemplateCatalog.all, "name")
   end
 end
