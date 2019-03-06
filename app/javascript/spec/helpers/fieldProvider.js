dc30b80de8b68738d35cabafe04dbce3e95fc739
Martin Marosi <marvusm.mmi@gmail.com>
2019-03-06 10:38:48 +0100

    Update FieldProvider mock for testing.

--- app/javascript/spec/helpers/fieldProvider.js
+++ app/javascript/spec/helpers/fieldProvider.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { createElement } from 'react';
+/**
+ * Mocks FieldProvider for custom data driven form fields
+ */
+export const FieldProviderComponent = ({
+  input,
+  render,
+  meta,
+  component,
+  children,
+  ...rest
+}) => {
+  const fieldInput = {
+    onChange: jest.fn(),
+    ...input,
+  };
+  const fieldMeta = {
+    ...meta,
+  };
 
-export const FieldProviderComponent = ({ component, ...props }) => (
-  <div>{ component({ input: { name: 'Foo', onChange: jest.fn() }, meta: { error: false, touched: false }, ...props }) }</div>
-);
+  if (typeof children === 'function') {
+    return children({ ...rest, input: fieldInput, meta: fieldMeta });
+  }
+
+  if (typeof component === 'object') {
+    return createElement(component, {
+      ...rest, input: fieldInput, meta: fieldMeta, children,
+    });
+  }
+
+  if (typeof component === 'function') {
+    const Component = component;
+    return <Component {...rest} input={fieldInput} meta={fieldMeta}>{children}</Component>;
+  }
+
+  return render({
+    ...rest, input: fieldInput, meta: fieldMeta, children,
+  });
+};
