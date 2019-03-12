0fe24fc38a5abde447659e54c627f5b281ba4d1a
Dávid Halász <dhalasz@redhat.com>
2019-03-12 16:49:09 +0100

    Do not rely on the tree type in the TreeBuilderAeClass

--- app/presenters/tree_builder_ae_class.rb
+++ app/presenters/tree_builder_ae_class.rb
@@ -1,6 +1,6 @@
 class TreeBuilderAeClass < TreeBuilder
-  has_kids_for MiqAeClass, %i(x_get_tree_class_kids type)
-  has_kids_for MiqAeNamespace, %i(x_get_tree_ns_kids type)
+  has_kids_for MiqAeClass, [:x_get_tree_class_kids]
+  has_kids_for MiqAeNamespace, [:x_get_tree_ns_kids]
 
   private
 
@@ -30,29 +30,14 @@ class TreeBuilderAeClass < TreeBuilder
     count_only_or_objects(count_only, objects)
   end
 
-  def x_get_tree_class_kids(object, count_only, type)
+  def x_get_tree_class_kids(object, count_only)
     instances = count_only_or_objects(count_only, object.ae_instances, %i(display_name name))
     # show methods in automate explorer tree
-    if type == :ae # FIXME: is this ever false?
-      methods = count_only_or_objects(count_only, object.ae_methods, %i(display_name name))
-      instances + methods
-    else
-      instances
-    end
+    methods = count_only_or_objects(count_only, object.ae_methods, %i(display_name name))
+    instances + methods
   end
 
-  def x_get_tree_ns_kids(object, count_only, type)
-    if type == :automate
-      if object.respond_to?(:ae_namespaces) && filter_ae_objects(object.ae_namespaces).size == 1
-        open_node("aen-#{object.id}")
-        open_node("aen-#{object.ae_namespaces.first.id}")
-      end
-
-      if object.respond_to?(:ae_classes) && filter_ae_objects(object.ae_classes).size == 1
-        open_node("aen-#{object.id}")
-        open_node("aec-#{object.ae_classes.first.id}")
-      end
-    end
+  def x_get_tree_ns_kids(object, count_only)
     objects = filter_ae_objects(object.ae_namespaces)
     unless MiqAeClassController::MIQ_AE_COPY_ACTIONS.include?(@sb[:action])
       ns_classes = filter_ae_objects(object.ae_classes)
--- app/presenters/tree_builder_automate.rb
+++ app/presenters/tree_builder_automate.rb
@@ -33,4 +33,22 @@ class TreeBuilderAutomate < TreeBuilderAeClass
                   :base_id      => "root",
                   :highlighting => true)
   end
+
+  def x_get_tree_class_kids(object, count_only)
+    count_only_or_objects(count_only, object.ae_instances, %i(display_name name))
+  end
+
+  def x_get_tree_ns_kids(object, count_only)
+    if object.respond_to?(:ae_namespaces) && filter_ae_objects(object.ae_namespaces).size == 1
+      open_node("aen-#{object.id}")
+      open_node("aen-#{object.ae_namespaces.first.id}")
+    end
+
+    if object.respond_to?(:ae_classes) && filter_ae_objects(object.ae_classes).size == 1
+      open_node("aen-#{object.id}")
+      open_node("aec-#{object.ae_classes.first.id}")
+    end
+
+    super(object, count_only)
+  end
 end
