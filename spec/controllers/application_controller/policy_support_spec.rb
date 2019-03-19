48a8dd5a4ca3e26c3fc91a04a5702ffd118c22c1
Hilda Stastna <hstastna@redhat.com>
2019-03-19 15:26:14 +0100

    Add specs regarding canceling policy simulation

--- spec/controllers/application_controller/policy_support_spec.rb
+++ spec/controllers/application_controller/policy_support_spec.rb
@@ -17,4 +17,24 @@ describe ApplicationController do
       controller.send(:assign_policies, Host)
     end
   end
+
+  describe '#policy_sim_cancel' do
+    before do
+      allow(controller).to receive(:flash_to_session).and_call_original
+      allow(controller).to receive(:previous_breadcrumb_url)
+      allow(controller).to receive(:redirect_to)
+    end
+
+    it 'calls redirect_to method' do
+      expect(controller).to receive(:redirect_to)
+      controller.send(:policy_sim_cancel)
+    end
+
+    it 'adds the message to flash array' do
+      flash_msg = 'Edit policy simulation was cancelled by the user'
+      expect(controller).to receive(:flash_to_session).with(flash_msg)
+      controller.send(:policy_sim_cancel)
+      expect(controller.instance_variable_get(:@flash_array)).to eq([{:message => flash_msg, :level => :success}])
+    end
+  end
 end
