98f3b08dbe6689423f340979bf0fcd2712375aa6
Richard Vsiansky <r.vsia@seznam.cz>
2019-04-09 10:36:17 +0200

    Adds default options to debounce promise to allow submitting
    * unresolved promises prevented ending of validation, so all promises all resolved

--- app/javascript/helpers/promise-debounce.js
+++ app/javascript/helpers/promise-debounce.js
@@ -1,6 +1,6 @@
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
-export default (asyncFunction, debounceTime = 250, options = undefined) => AwesomeDebouncePromise(
+export default (asyncFunction, debounceTime = 250, options = { onlyResolvesLast: false }) => AwesomeDebouncePromise(
   asyncFunction,
   debounceTime,
   options,
