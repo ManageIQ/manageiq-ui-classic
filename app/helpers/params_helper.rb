d3c3878c7cde6f682c6e25928b7c90a4c01b664f
Hilda Stastna <hstastna@redhat.com>
2019-04-18 18:17:25 +0200

    Make Add button respond properly on changes while adding Catalog Item
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1728774
    
    Also add new copy_params_if_present method to params helper

--- app/helpers/params_helper.rb
+++ app/helpers/params_helper.rb
@@ -3,9 +3,20 @@ module ParamsHelper
     target[key] = source[key] if source[key]
   end
 
+  def copy_param_if_present(target, source, key)
+    target[key] = source[key].presence if source[key]
+  end
+
   def copy_params_if_set(target, source, list)
     list.each do |key|
       copy_param_if_set(target, source, key)
+    end
+    target
+  end
+
+  def copy_params_if_present(target, source, list)
+    list.each do |key|
+      copy_param_if_present(target, source, key)
     end
     target
   end
