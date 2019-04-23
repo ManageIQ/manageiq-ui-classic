7d6eea01b0a6b9db48806eb78e0ee990df997740
PanSpagetka <rknaur@redhat.com>
2019-04-23 14:01:02 +0200

    Export TagView

--- app/javascript/tagging/index.js
+++ app/javascript/tagging/index.js
@@ -1,7 +1,9 @@
 import taggingApp from './reducers';
 import { TaggingConnected, TaggingWithButtonsConnected } from './containers/tagging';
+import TagView from './components/InnerComponents/TagView';
 import './sass/style.scss';
 
-export { taggingApp, TaggingConnected, TaggingWithButtonsConnected };
+
+export { taggingApp, TaggingConnected, TaggingWithButtonsConnected, TagView };
 export * from './components/Tagging/Tagging';
 export * from './actions';
