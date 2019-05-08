ff35fe10291f061a59c7fff77269202d1bbb4293
Dávid Halász <dhalasz@redhat.com>
2019-05-08 07:08:28 +0200

    Open the homonymic tree nodes for dialog editor entrypoint selection
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1553846

--- app/controllers/tree_controller.rb
+++ app/controllers/tree_controller.rb
@@ -5,7 +5,30 @@ class TreeController < ApplicationController
   before_action :check_privileges
 
   def automate_entrypoint
-    json = fetch_tree(TreeBuilderAutomateEntrypoint, :automate_entrypoint_tree, params[:id])
+    json = fetch_tree(TreeBuilderAutomateEntrypoint, :automate_entrypoint_tree, params[:id]) do |tree|
+      if params[:fqname].present?
+        MiqAeInstance.get_homonymic_across_domains_noprefix(current_user, params[:fqname], true).each do |instance|
+          # Parse the namespace, class and instance from the fqname
+          namespace, klass, instance, _ = MiqAeEngine::MiqAePath.split(instance.fqname)
+
+          begin
+            # Collect all the db records based on the parsed fqname
+            open_nodes = namespace.split('/').each_with_object([]) do |ns, items|
+              items << MiqAeNamespace.find_by!(:name => ns, :parent => items.last)
+            end
+            open_nodes << MiqAeClass.find_by!(:name => klass, :namespace_id => open_nodes.last.id)
+            open_nodes << MiqAeInstance.find_by!(:name => instance, :class_id => open_nodes.last.id)
+          rescue ActiveRecord::RecordNotFound
+            # Skip the iteration steop if one of the records is not found
+            # FIXME: we probably need foreign keys instead of this magic
+            next
+          else
+            # Set the related tree nodes to open if all the records have been found
+            open_nodes.each { |node| tree.open_node(TreeNode.new(node, {}, {}).key) }
+          end
+        end
+      end
+    end
     render :body => json, :content_type => 'application/json'
   end
 
