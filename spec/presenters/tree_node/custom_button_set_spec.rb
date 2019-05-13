b21a5ba2c112b8222c444347d909267e82e9d254
Dávid Halász <dhalasz@redhat.com>
2019-05-13 14:32:58 +0200

    Test against tree name and not type in TreeNode::CustomButtonSet

--- spec/presenters/tree_node/custom_button_set_spec.rb
+++ spec/presenters/tree_node/custom_button_set_spec.rb
@@ -1,6 +1,7 @@
 describe TreeNode::CustomButtonSet do
-  subject { described_class.new(object, nil, {}, nil) }
+  subject { described_class.new(object, nil, options, nil) }
   let(:object) { FactoryBot.create(:custom_button_set, :description => "custom button set description") }
+  let(:options) { {} }
 
   include_examples 'TreeNode::Node#key prefix', 'cbg-'
   include_examples 'TreeNode::Node#icon', 'pficon pficon-folder-close'
@@ -8,6 +9,16 @@ describe TreeNode::CustomButtonSet do
   describe "#tooltip" do
     it "returns the prefixed title" do
       expect(subject.tooltip).to eq("Button Group: custom button set description")
+    end
+  end
+
+  describe '#text' do
+    context 'service catalogs tree' do
+      let(:options) { {:tree => :sandt_tree} }
+
+      it 'appends a (Group) suffix' do
+        expect(subject.text).to end_with('(Group)')
+      end
     end
   end
 end
