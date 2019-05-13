7d3ef62b52819f924a9fc74d40cc2ef14f5f9854
Dávid Halász <dhalasz@redhat.com>
2019-05-13 14:03:01 +0200

    Test against tree name and not type in TreeNode::EmsFolder

--- spec/presenters/tree_node/ems_folder_spec.rb
+++ spec/presenters/tree_node/ems_folder_spec.rb
@@ -17,7 +17,7 @@ describe TreeNode::EmsFolder do
       include_examples 'TreeNode::Node#tooltip prefix', 'Folder'
 
       context 'type is vat' do
-        let(:options) { {:type => :vat} }
+        let(:options) { {:tree => :vat_tree} }
 
         include_examples 'TreeNode::Node#icon', 'pficon pficon-folder-close-blue'
       end
