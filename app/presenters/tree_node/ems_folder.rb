6513138437d128cdc12ccf3071731ef3b39442ba
Dávid Halász <dhalasz@redhat.com>
2019-05-13 15:15:47 +0200

    Break up the TreeNode::EmsFolder set_attributes into two parts

--- app/presenters/tree_node/ems_folder.rb
+++ app/presenters/tree_node/ems_folder.rb
@@ -1,10 +1,9 @@
 module TreeNode
   class EmsFolder < Node
-    set_attributes(:icon, :tooltip) do
-      # TODO: move this logic into TreeBuilder#override for the specific trees
-      icon = %i[vandt_tree vat_tree].include?(@options[:tree]) ? 'pficon pficon-folder-close-blue' : @object.decorate.fonticon
-      tooltip = _("Folder: %{folder_name}") % {:folder_name => @object.name}
-      [icon, tooltip]
+    set_attribute(:icon) do
+      %i[vandt_tree vat_tree].include?(@options[:tree]) ? 'pficon pficon-folder-close-blue' : @object.decorate.fonticon
     end
+
+    set_attribute(:tooltip) { _("Folder: %{folder_name}") % {:folder_name => @object.name} }
   end
 end
