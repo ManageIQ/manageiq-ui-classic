851b3aa9ecec1149e8c308c1ff0439d165b7c555
Zita Nemeckova <znemecko@redhat.com>
2019-05-22 11:06:18 +0200

    gtl_formatter: Move logic from if/case to Hash

--- lib/gtl_formatter.rb
+++ lib/gtl_formatter.rb
@@ -6,129 +6,108 @@ class GtlFormatter
   }.freeze
 
   def self.format_cols(view, row)
-    rows = []
-    @row = row
-    @view = view
+    cols = []
+
+    state = {"state" => :state_format}
+    hardware = {"hardware.bitness" => :hardware_bitness_format}
+    special_cases = {
+      "AutomationRequest"                                              => state,
+      "Container"                                                      => state,
+      "MiqTask"                                                        => state,
+      "MiqProvision"                                                   => state,
+      "MiqRequest"                                                     => state.merge({
+        'approval_state' => :miq_request_format,
+      }),
+      "ManageIQ_Providers_CloudManager_Template-all_vms_and_templates" => hardware,
+      "ManageIQ_Providers_CloudManager_Vm-vms"                         => hardware,
+      "ManageIQ_Providers_CloudManager_Vm-all_vms_and_templates"       => hardware,
+      "ManageIQ_Providers_CloudManager_Vm"                             => hardware,
+      "ManageIQ_Providers_CloudManager_Template"                       => hardware.merge({
+        "image?" => :cloud_manager_template_format,
+      }),
+      "MiqSchedule" => :timezone, # all fields have same specific format
+      "OpenscapRuleResult" => {
+        "result"   => :result_format,
+        "severity" => :severity_format,
+      },
+      "ServiceTemplate" => {
+        "prov_type" => :service_template_format,
+      },
+    }
 
     view.col_order.each_with_index do |col, col_idx|
       next if view.column_is_hidden?(col)
 
-      @col = col
       span = nil
 
-      if view.extras[:filename] == "MiqRequest"
-        celltext = miq_request_format(view.col_order[col_idx], row[col])
-      elsif view.extras[:filename] == "ServiceTemplate"
-        celltext = service_template_format(view.col_order[col_idx], row[col])
-      elsif view.extras[:filename] == "OpenscapRuleResult"
-        celltext, span = openscap_role_result_format(view.col_order[col_idx], row[col])
-      elsif %w[AutomationRequest MiqRequest Container MiqTask MiqProvision].include?(view.extras[:filename])
-        celltext = state_format(view.col_order[col_idx], row[col])
-      elsif %w[ManageIQ_Providers_CloudManager_Template-all_vms_and_templates ManageIQ_Providers_CloudManager_Vm-vms
-               ManageIQ_Providers_CloudManager_Vm-all_vms_and_templates ManageIQ_Providers_CloudManager_Vm
-               ManageIQ_Providers_CloudManager_Template].include?(view.extras[:filename])
-        celltext = hardware_bitness_format(view.col_order[col_idx], row[col])
-      elsif view.extras[:filename] == "ManageIQ_Providers_CloudManager_Template"
-        celltext = cloud_manager_template_format(view.col_order[col_idx], row[col])
+      if special_cases[view.extras[:filename]].kind_of?(Symbol)
+        celltext = send(special_cases[view.extras[:filename]], view, row, col)
+      elsif special_cases[view.extras[:filename]].kind_of?(Hash) && special_cases[view.extras[:filename]][view.col_order[col_idx]].present?
+        celltext, span = send(special_cases[view.extras[:filename]][view.col_order[col_idx]], row[col])
       else
         celltext = format_col_for_display(view, row, col)
       end
+
       item = {:text => celltext}
       item[:span] = span if span.present?
-      rows.push(item)
+      cols.push(item)
     end
-    rows
+    cols
   end
 
-  def self.cloud_manager_template_format(key, value)
-    if key == 'image?'
-      value ? _("Image") : _("Snapshot")
-    else
-      hardware_bitness_format(key, value)
-    end
+  def self.cloud_manager_template_format(value)
+    [value ? _("Image") : _("Snapshot"), nil]
   end
 
-  def self.hardware_bitness_format(key, value)
-    if key == 'hardware.bitness'
-      value ? "#{value} bit" : ''
-    else
-      format_col_for_display(@view, @row, @col)
-    end
+  def self.hardware_bitness_format(value)
+    [value ? "#{value} bit" : '', nil]
   end
 
-  def self.state_format(key, value)
-    if key == 'state'
-      value.to_s.titleize
-    else
-      format_col_for_display(@view, @row, @col)
-    end
+  def self.state_format(value)
+    [value.to_s.titleize, nil]
   end
 
-  def self.miq_request_format(key, value)
-    if key == 'approval_state'
-      _(PROV_STATES[value])
-    else
-      state_format(key, value)
-    end
+  def self.miq_request_format(value)
+    [_(PROV_STATES[value]), nil]
   end
 
-  def self.openscap_role_result_format(key, value)
-    span = nil
-    if key == "result"
-      span = result_span_class(value)
-      celltext = value.titleize
-    elsif key == "severity"
-      span = severity_span_class(value)
-      celltext = value.titleize
-    else
-      celltext = format_col_for_display(@view, @row, @col)
-    end
-    [celltext, span]
+  def self.service_template_format(value)
+    [value ? _(ServiceTemplate::CATALOG_ITEM_TYPES[value]) : '', nil]
   end
 
-  def self.service_template_format(key, value)
-    if key == "prov_type"
-      value ? _(ServiceTemplate::CATALOG_ITEM_TYPES[value]) : ''
-    else
-      format_col_for_display(@view, @row, @col)
-    end
-  end
-
-  def self.timezone(view, row)
-    if view.extras[:filename] == "MiqSchedule"
-      timezone = MiqServer.my_server.server_timezone
-      # Use scheduled tz for formatting, if configured
-      timezone = row['run_at'][:tz] if row['run_at'] && row['run_at'][:tz]
-    else
-      timezone = Time.zone
-    end
-    timezone
+  def self.timezone(view, row, col)
+    timezone = MiqServer.my_server.server_timezone
+    # Use scheduled tz for formatting, if configured
+    timezone = row['run_at'][:tz] if row['run_at'] && row['run_at'][:tz]
+    format_col_for_display(view, row, col, timezone)
   end
 
   # Format a column in a report view for display on the screen
-  def self.format_col_for_display(view, row, col)
-    view.format(col, row[col], :tz => timezone(view, row)).gsub(/\\/, '\&') # Call format, then escape any backslashes
+  def self.format_col_for_display(view, row, col, tz = Time.zone)
+    view.format(col, row[col], :tz => tz).gsub(/\\/, '\&') # Call format, then escape any backslashes
   end
 
-  def self.result_span_class(value)
-    case value.downcase
-    when "pass"
-      "label label-success center-block"
-    when "fail"
-      "label label-danger center-block"
-    else
-      "label label-primary center-block"
-    end
+  def self.result_format(value)
+    span = case value.downcase
+           when "pass"
+             "label label-success center-block"
+           when "fail"
+             "label label-danger center-block"
+           else
+             "label label-primary center-block"
+           end
+    [value.titleize, span]
   end
 
-  def self.severity_span_class(value)
-    case value.downcase
-    when "high"
-      "label label-danger center-block"
-    when "medium"
-      "label label-warning center-block"
-    else
-      "label label-low-severity center-block"
-    end
+  def self.severity_format(value)
+    span = case value.downcase
+           when "high"
+             "label label-danger center-block"
+           when "medium"
+             "label label-warning center-block"
+           else
+             "label label-low-severity center-block"
+           end
+    [value.titleize, span]
   end
 end
