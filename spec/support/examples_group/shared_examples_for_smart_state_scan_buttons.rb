d6e6c723dd92839eb953bea3cd18a19807dab668
Brandon Dunne <brandondunne@hotmail.com>
2019-05-28 10:45:05 -0400

    Rewrite smart state scan specs

--- spec/support/examples_group/shared_examples_for_smart_state_scan_buttons.rb
+++ spec/support/examples_group/shared_examples_for_smart_state_scan_buttons.rb
@@ -1,36 +1,47 @@
 shared_examples_for 'a smart state scan button' do
-  let(:my_zone) { MiqServer.my_server.zone }
-  let(:server) { FactoryBot.create(:miq_server, :zone => my_zone, :active_roles => roles) }
   let(:smartproxy_role) { ServerRole.find_by(:name => 'smartproxy') }
-  let(:smartscan_role) { ServerRole.find_by(:name => 'smartstate') }
+  let(:smartstate_role) { ServerRole.find_by(:name => 'smartstate') }
 
-  describe '#calculate_properties' do
+  describe "in my zone" do
+    let(:server) { MiqServer.my_server }
+    context "both roles enabled" do
+      before do
+        FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartproxy_role, :active => true)
+        FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartstate_role, :active => true)
+        button.calculate_properties
+      end
+
+      it_behaves_like 'an enabled button'
+    end
+
+    context "only smartproxy role enabled" do
+      before do
+        FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartproxy_role, :active => true)
+        button.calculate_properties
+      end
+
+      it_behaves_like 'a disabled button', "There is no server with the smartstate role enabled"
+    end
+
+    context "only smartstate role enabled" do
+      before do
+        FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartstate_role, :active => true)
+        button.calculate_properties
+      end
+
+      it_behaves_like 'a disabled button', "There is no server with the smartproxy role enabled"
+    end
+  end
+
+  describe "only enabled in another zone" do
+    let(:server) { FactoryBot.create(:miq_server, :zone => FactoryBot.create(:zone)) }
+
     before do
-      EvmSpecHelper.local_guid_miq_server_zone
-      server
+      FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartproxy_role, :active => true)
+      FactoryBot.create(:assigned_server_role, :miq_server => server, :server_role => smartstate_role, :active => true)
       button.calculate_properties
     end
 
-    MiqServer::ServerSmartProxy::SMART_ROLES.each do |role|
-      context "when there is a server with enabled #{role} role" do
-        let(:roles) { [smartproxy_role, smartscan_role] }
-        let(:active_role) { true }
-        it_behaves_like 'an enabled button'
-      end
-
-      context "when there is no server with enabled #{role} role" do
-        let(:roles) { [role == 'smartproxy' ? smartscan_role : smartproxy_role] }
-        let(:active_role) { false }
-        it_behaves_like 'a disabled button', "There is no server with the #{role} role enabled"
-      end
-    end
-
-    context "when there is no server in the zone" do
-      let(:roles) { [smartproxy_role, smartscan_role] }
-      let(:active_role) { true }
-      let(:zone1) { FactoryBot.create(:zone) }
-      let(:server) { FactoryBot.create(:miq_server, :zone => zone1, :active_roles => roles) }
-      it_behaves_like 'a disabled button', "There is no server with the #{MiqServer::ServerSmartProxy::SMART_ROLES.last} role enabled"
-    end
+    it_behaves_like 'a disabled button', "There is no server with the #{MiqServer::ServerSmartProxy::SMART_ROLES.last} role enabled"
   end
 end
