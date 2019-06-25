ManageIQ/manageiq-ui-classic

commit c7536077b36b56ebbaa9fe9ec6bbcf22aab32248

Fix rubocop warnings in MiqPolicyController::AlertProfiles

app/controllers/miq_policy_controller/alert_profiles.rb
@@ -149,8 +149,8 @@ module MiqPolicyController::AlertProfiles
     return unless load_edit("alert_profile_edit__#{params[:id]}", "replace_cell__explorer")
     @alert_profile = @edit[:alert_profile_id] ? MiqAlertSet.find(@edit[:alert_profile_id]) : MiqAlertSet.new
 
-    @edit[:new][:description] = params[:description].blank? ? nil : params[:description] if params[:description]
-    @edit[:new][:notes] = params[:notes].blank? ? nil : params[:notes] if params[:notes]
+    @edit[:new][:description] = params[:description].presence
+    @edit[:new][:notes] = params[:notes].presence
 
     send_button_changes
   end
@@ -160,7 +160,7 @@ module MiqPolicyController::AlertProfiles
     @alert_profile = @assign[:alert_profile]
 
     if params.key?(:chosen_assign_to)
-      @assign[:new][:assign_to] = params[:chosen_assign_to].blank? ? nil : params[:chosen_assign_to]
+      @assign[:new][:assign_to] = params[:chosen_assign_to].presence
       @assign[:new][:cat] = nil # Clear chosen tag category
     end
 
@@ -318,7 +318,7 @@ module MiqPolicyController::AlertProfiles
   def alert_profile_get_all
     @alert_profiles = MiqAlertSet.all.sort_by { |as| as.description.downcase }
     set_search_text
-    @alert_profiles = apply_search_filter(@search_text, @alert_profiles) unless @search_text.blank?
+    @alert_profiles = apply_search_filter(@search_text, @alert_profiles) if @search_text.present?
     @right_cell_text = _("All Alert Profiles")
     @right_cell_div = "alert_profile_list"
   end

commit 1852d95845bda69bd0f4323e1e60b4aa00b04b12

Fixed issue introduced in c7536077b36b56ebbaa9fe9ec6bbcf22aab32248
When saving `Notes` field value, it was setting `Description` to blank, causing flash error on screen

Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1723815

app/controllers/miq_policy_controller/alert_profiles.rb
@@ -149,8 +149,8 @@ module MiqPolicyController::AlertProfiles
     return unless load_edit("alert_profile_edit__#{params[:id]}", "replace_cell__explorer")
     @alert_profile = @edit[:alert_profile_id] ? MiqAlertSet.find(@edit[:alert_profile_id]) : MiqAlertSet.new
 
-    @edit[:new][:description] = params[:description].presence
-    @edit[:new][:notes] = params[:notes].presence
+    @edit[:new][:description] = params[:description].presence if params[:description]
+    @edit[:new][:notes] = params[:notes].presence if params[:notes]
 
     send_button_changes
   end

spec/controllers/miq_policy_controller/alert_profiles_spec.rb
@@ -94,6 +94,26 @@ describe MiqPolicyController do
       end
     end
 
+    context '#alert_profile_field_changed' do
+      before do
+        @ap = FactoryBot.create(:miq_alert_set)
+        allow(controller).to receive(:send_button_changes)
+        edit = {
+          :key => "alert_profile_edit__#{@ap.id}",
+          :new => {:name => "foo", :description => "foo description"}
+        }
+        session[:edit] = edit
+      end
+
+      it 'Edit of notes field does not reset value of description field' do
+        controller.params = {:id => @ap.id, :notes => 'foo note'}
+        controller.alert_profile_field_changed
+        edit_new = assigns(:edit)[:new]
+        expect((edit_new)[:description]).to eq("foo description")
+        expect((edit_new)[:notes]).to eq("foo note")
+      end
+    end
+
     context "#alert_profile_assign_changed" do
       before do
         alert_profile_set = FactoryBot.create(:miq_alert_set, :set_type => "MiqAlertSet", :mode => "MiqServer")