5051fbd05ac8d0e1d4d121c972206b9f41ab45e0
d-m-u <drewuhlmann@gmail.com>
2019-07-02 06:56:36 -0400

    The factory handles this

--- spec/controllers/application_controller/compare_spec.rb
+++ spec/controllers/application_controller/compare_spec.rb
@@ -1,7 +1,7 @@
 describe ApplicationController do
   context "#drift_history" do
     it "resets @display to main" do
-      vm = FactoryBot.create(:vm_vmware, :name => "My VM")
+      vm = FactoryBot.create(:vm_vmware)
       controller.instance_variable_set(:@display, "vms")
       controller.instance_variable_set(:@sb, {})
       controller.instance_variable_set(:@drift_obj, vm)
