3f4607dc4f31973668c17ff39a6a15defd2885b4
Martin Marosi <marvusm.mmi@gmail.com>
2019-07-10 14:27:45 +0200

    Reverted changes to git import form.

--- app/helpers/miq_ae_tools_helper.rb
+++ app/helpers/miq_ae_tools_helper.rb
@@ -2,4 +2,15 @@ module MiqAeToolsHelper
   def git_import_button_enabled?
     GitBasedDomainImportService.available?
   end
+
+  def git_import_submit_help
+    unless git_import_button_enabled?
+      content_tag(
+        :i,
+        "",
+        :class => ["fa", "fa-lg", "fa-question-circle"],
+        :title => _("Please enable the git owner role in order to import git repositories")
+      )
+    end
+  end
 end
