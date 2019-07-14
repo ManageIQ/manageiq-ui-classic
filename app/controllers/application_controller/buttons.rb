e166c36130a6d265ca2a08254ae36af168b11a40
Martin Povolny <mpovolny@redhat.com>
2019-07-14 13:36:47 +0200

    Opening URLs through automate: remove debugging code, fix style.

--- app/controllers/application_controller/buttons.rb
+++ app/controllers/application_controller/buttons.rb
@@ -92,7 +92,7 @@ module ApplicationController::Buttons
 
   # Provider, Service, User, Group, Tenant, Cloud Tenant, Generic Object
   # TODO: test each
-  MODEL_WITH_OPEN_URL = %w(Provider Service User MiqGroup Tenant CloudTenant GenericObject Vm).freeze
+  MODEL_WITH_OPEN_URL = %w[Provider Service User MiqGroup Tenant CloudTenant GenericObject Vm].freeze
 
   def automate_button_field_changed
     unless params[:target_class]
@@ -251,13 +251,8 @@ module ApplicationController::Buttons
     external_url = ExternalUrl.find_by(
       :resource_id   => params[:targetId],
       :resource_type => params[:realTargetType],
-      :user           => User.current_user
+      :user          => User.current_user
     )
-    binding.pry if external_url.nil?
-
-    # FIXME: remove this fallback, once the form passes the targetClass
-    #external_url ||= ExternalUrl.find_by(:resource_id => params[:targetId])
-
     # FIXME: remove this fallback once the ':remote_console_url=' is removed from automate
     external_url ||= SystemConsole.find_by(:vm_id => params[:targetId])
 
@@ -274,13 +269,8 @@ module ApplicationController::Buttons
     external_url = ExternalUrl.find_by(
       :resource_id   => params[:id],
       :resource_type => params[:cls],
-      :user           => User.current_user
+      :user          => User.current_user
     )
-    binding.pry if external_url.nil?
-
-    # FIXME: remove this fallback once we have the class here
-    external_url ||= ExternalUrl.find_by(:resource_id => params[:id])
-
     # FIXME: remove this fallback once the ':remote_console_url=' is removed from automate
     external_url ||= SystemConsole.find_by(:vm_id => params[:id])
 
