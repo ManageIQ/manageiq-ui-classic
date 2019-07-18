c16482b824589aee4120de35b3ef57a631d578d6
Martin Hradil <mhradil@redhat.com>
2019-07-18 14:03:29 +0000

    optimization controller spec - test title, breadcrumbs, 200 responses

--- spec/controllers/optimization_controller_spec.rb
+++ spec/controllers/optimization_controller_spec.rb
@@ -16,21 +16,92 @@ describe OptimizationController do
     end
   end
 
-  describe "#show_list" do
-    subject { get(:show_list) }
+  describe "#show_hardcoded_reports" do
+    let(:report) { FactoryBot.create(:miq_report) }
+    before do
+      allow(OptimizationController).to receive(:hardcoded_reports).and_return([report])
+    end
 
-    it "renders a GTL page" do
-      is_expected.to have_http_status 200
-      is_expected.to render_template(:partial => "layouts/_gtl")
+    context "request" do
+      subject { get(:show_list, :params => {:id => nil}) }
+
+      it "renders a page" do
+        is_expected.to have_http_status 200
+      end
+    end
+
+    context "controller" do
+      before do
+        get(:show_list, :params => {:id => nil})
+      end
+
+      it "uses Optimization for title" do
+        expect(controller.title).to eq("Optimization")
+      end
+
+      it "sets breadcrumbs right" do
+        expect(controller.data_for_breadcrumbs.pluck(:title)).to eq(['Overview',
+                                                                     'Optimization'])
+      end
+    end
+  end
+
+  describe "#show_saved_reports" do
+    let(:report) { FactoryBot.create(:miq_report) }
+
+    context "request" do
+      subject { get(:show_list, :params => {:id => report.id}) }
+
+      it "renders a page" do
+        is_expected.to have_http_status 200
+      end
+    end
+
+    context "controller" do
+      before do
+        get(:show_list, :params => {:id => report.id})
+      end
+
+      it "uses report name for title" do
+        expect(controller.title).to eq(report.name)
+      end
+
+      it "sets breadcrumbs right" do
+        expect(controller.data_for_breadcrumbs.pluck(:title)).to eq(['Overview',
+                                                                     'Optimization',
+                                                                     report.name])
+      end
     end
   end
 
   describe "#show" do
-    subject { get(:show, :params => {:id => 123}) }
+    let(:report) { FactoryBot.create(:miq_report_with_results) }
+    let(:result) { report.miq_report_results.first }
+    let(:saved) { result.tap { |rr| rr.name = 'saved'; rr.save } }
 
-    it "renders a page" do
-      is_expected.to have_http_status 200
-      is_expected.to render_template(:partial => "optimization/show")
+    context "request" do
+      subject { get(:show, :params => {:id => saved.id}) }
+
+      it "renders a page" do
+        is_expected.to have_http_status 200
+      end
+    end
+
+    context "controller" do
+      before do
+        get(:show, :params => {:id => saved.id})
+      end
+
+      it "uses saved report name for title" do
+        expect(controller.title).to eq(saved.name)
+      end
+
+      it "sets breadcrumbs right" do
+        expect(controller.data_for_breadcrumbs.pluck(:title)).to eq(['Overview',
+                                                                     'Optimization',
+                                                                     report.name,
+                                                                     saved.name])
+      end
     end
   end
 end
