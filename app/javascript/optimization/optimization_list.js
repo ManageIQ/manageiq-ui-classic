ac90d2a6a4276cb7420db351826a7e3a3d85862b
Martin Hradil <mhradil@redhat.com>
2019-07-22 11:22:35 +0000

    OptimizationList - use Empty component instead of function

--- app/javascript/optimization/optimization_list.js
+++ app/javascript/optimization/optimization_list.js
@@ -5,14 +5,14 @@ import { DataDrivenTable } from '../components/simple-table/simple-table.jsx';
 import { miqOptimizationTransform } from '../optimization/transform.js';
 import { refresh } from './refresh.js';
 
-const empty = () => (
+const Empty = () => (
   <div className="alert alert-info">
     <span className="pficon pficon-info"></span>
     <strong>
       {__("No Records Found.")}
     </strong>
   </div>
-)
+);
 
 export default function OptimizationList({columns, rows, refresh_url, ...props}) {
   const [state, set] = useState({
@@ -24,7 +24,7 @@ export default function OptimizationList({columns, rows, refresh_url, ...props})
   refresh.set = set;
 
   if (! state.columns || ! state.rows || ! state.rows.length) {
-    return empty();
+    return <Empty />;
   }
 
   return <DataDrivenTable columns={state.columns} rows={state.rows} transform={miqOptimizationTransform} />
