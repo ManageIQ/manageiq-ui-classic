3389bddbbf92111c8084818ff5c1a2ac66d697c1
Harpreet Kataria <hkataria@redhat.com>
2019-07-25 14:01:20 -0400

    Fixed issues with 'Back' button on right size screen
    Multiple issues fixed with back button on non-explorer version of VM Right Size recommendations screen
    
    - fixed a typo it should be `@record.id`
    - fixed call to javascript_prologue, method does not expect argument
    - added code to redirect back to previous screen when back button is pressed
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1733207

--- spec/controllers/vm_controller_spec.rb
+++ spec/controllers/vm_controller_spec.rb
@@ -20,4 +20,26 @@ describe VmController do
       expect(response).to render_template('vm_common/_live_migrate')
     end
   end
+
+  describe "#right_size" do
+    let(:vm) { FactoryBot.create(:vm_vmware) }
+    before do
+      stub_user(:features => :all)
+    end
+
+    it "when back button is pressed, it redirects back to previous screen" do
+      breadcrumbs = [
+        {:name => "Foo", :url => "/ems_infra/show_list"},
+        {:name => "Bar", :url => "/ems_infra/1?display=vms"},
+        {:name => "Right Size", :url => "/vm/right_size"}
+      ]
+      controller.instance_variable_set(:@breadcrumbs, breadcrumbs)
+      page = double('page')
+      allow(page).to receive(:<<).with(any_args)
+      expect(page).to receive(:redirect_to).with("/ems_infra/1?display=vms")
+      expect(controller).to receive(:render).with(:update).and_yield(page)
+      controller.params = {:id => vm.id, :button => 'back'}
+      controller.send(:right_size)
+    end
+  end
 end
