0528088bb831bb5cf8e1a73ae4df22d136e53456
Milan Zazrivec <mzazrivec@redhat.com>
2019-07-26 17:58:00 +0200

    Don't run the test for single rvm version specified

--- spec/other/travis_spec.rb
+++ spec/other/travis_spec.rb
@@ -3,9 +3,11 @@ describe 'travis.yml' do
     travis = YAML.safe_load(File.open(ManageIQ::UI::Classic::Engine.root.join('.travis.yml')))
 
     versions = travis['rvm']
-    excludes = travis.dig('matrix', 'exclude').map { |ex| ex['rvm'] }.sort.uniq
 
-    expect(excludes.length).to eq(1)
-    expect(versions.first).to eq(excludes.first)
+    if versions.length > 1
+      excludes = travis.dig('matrix', 'exclude').map { |ex| ex['rvm'] }.sort.uniq
+      expect(excludes.length).to eq(versions.length - 1)
+      expect(versions[0..-2]).to eq(excludes)
+    end
   end
 end
