a5f9b9f7be9de9821bcffc2eb0c245f27d899dd3
Milan Zazrivec <mzazrivec@redhat.com>
2019-07-26 18:11:31 +0200

    Add test for travis variant with single ruby version

--- spec/other/travis_spec.rb
+++ spec/other/travis_spec.rb
@@ -1,13 +1,19 @@
 describe 'travis.yml' do
-  it "matches versions and excludes" do
-    travis = YAML.safe_load(File.open(ManageIQ::UI::Classic::Engine.root.join('.travis.yml')))
+  let(:travis) { YAML.safe_load(File.open(ManageIQ::UI::Classic::Engine.root.join('.travis.yml'))) }
+  let(:versions) { travis['rvm'] }
 
-    versions = travis['rvm']
-
+  it "matches versions and excludes for multiple ruby versions" do
     if versions.length > 1
       excludes = travis.dig('matrix', 'exclude').map { |ex| ex['rvm'] }.sort.uniq
       expect(excludes.length).to eq(versions.length - 1)
       expect(versions[0..-2]).to eq(excludes)
+    end
+  end
+
+  it "does not exclude any testsuite for single ruby version" do
+    if versions.length == 1
+      excludes = travis.dig('matrix', 'exclude')
+      expect(excludes).to be(nil)
     end
   end
 end
