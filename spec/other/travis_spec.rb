7ed3d0dee7668883c041a2e6b1246fac4e69899c
Martin Hradil <mhradil@redhat.com>
2019-07-29 13:26:53 +0200

    Merge pull request #5885 from mzazrivec/update_travis_spec_for_corner_cases
    Update travis spec for corner cases
    
    (cherry picked from commit 68ade2fc2613bc71ae09423e8943ec30f93ae2be)

--- spec/other/travis_spec.rb
+++ spec/other/travis_spec.rb
@@ -1,11 +1,19 @@
 describe 'travis.yml' do
-  it "matches versions and excludes" do
-    travis = YAML.safe_load(File.open(ManageIQ::UI::Classic::Engine.root.join('.travis.yml')))
+  let(:travis) { YAML.safe_load(File.open(ManageIQ::UI::Classic::Engine.root.join('.travis.yml'))) }
+  let(:versions) { travis['rvm'] }
 
-    versions = travis['rvm']
-    excludes = travis.dig('matrix', 'exclude').map { |ex| ex['rvm'] }.sort.uniq
+  it "matches versions and excludes for multiple ruby versions" do
+    if versions.length > 1
+      excludes = travis.dig('matrix', 'exclude').map { |ex| ex['rvm'] }.sort.uniq
+      expect(excludes.length).to eq(versions.length - 1)
+      expect(versions[0..-2]).to eq(excludes)
+    end
+  end
 
-    expect(excludes.length).to eq(1)
-    expect(versions.first).to eq(excludes.first)
+  it "does not exclude any testsuite for single ruby version" do
+    if versions.length == 1
+      excludes = travis.dig('matrix', 'exclude')
+      expect(excludes).to be(nil)
+    end
   end
 end
