c8c10d35ecace059127f90ab32d0b28339ea9005
d-m-u <drewuhlmann@gmail.com>
2019-08-06 10:19:10 -0400

    Remove report names from settings hash when they're deleted

--- spec/controllers/miq_report_controller/reports_spec.rb
+++ spec/controllers/miq_report_controller/reports_spec.rb
@@ -19,6 +19,21 @@ describe ReportController, "::Reports" do
       expect(MiqReport.find_by(:id => report.id)).to be_nil
     end
 
+    it "deletes the report name from the group settings hash" do
+      report = FactoryBot.create(:miq_report, :rpt_type => "Custom", :miq_group => user.current_group)
+      report.miq_group.update(:settings => {"report_menus" => [["foo", [["bar", [report.name]]]]]})
+      session['sandboxes'] = {
+        controller.controller_name => { :active_tree => 'report_1',
+                                        :trees       => {'report_1' => {:active_node => "xx-0_xx-0-0_rep-#{report.id}"}}}
+      }
+
+      get :x_button, :params => { :id => report.id, :pressed => 'miq_report_delete' }
+      report.miq_group.reload
+      expect(response.status).to eq(200)
+      expect(MiqReport.find_by(:id => report.id)).to be_nil
+      expect(report.miq_group.settings).to eq("report_menus" => [["foo", [["bar", []]]]])
+    end
+
     it "cant delete default reports" do
       FactoryBot.create(:miq_report)
       report = FactoryBot.create(:miq_report, :rpt_type => "Default")
