4695a4495acf25a030f9618171df96ff53d4f37b
Levente Berky <lberky@redhat.com>
2019-08-07 16:53:54 +0200

    Updated set ownership shema field type (SELECT_COMPONENT is deprecated)

--- app/javascript/components/set-ownership-form/ownership-form.schema.js
+++ app/javascript/components/set-ownership-form/ownership-form.schema.js
@@ -2,13 +2,13 @@ import { componentTypes } from '@data-driven-forms/react-form-renderer';
 
 const createSchema = (ownerOptions, groupOptions) => ({
   fields: [{
-    component: componentTypes.SELECT_COMPONENT,
+    component: componentTypes.SELECT,
     name: 'user',
     id: 'user_name',
     label: __('Select an Owner:'),
     options: ownerOptions,
   }, {
-    component: componentTypes.SELECT_COMPONENT,
+    component: componentTypes.SELECT,
     name: 'group',
     id: 'group_name',
     label: __('Select a Group:'),
