f09a72a8be22eb554889a49e111acbc35e3fec58
Milan Zazrivec <mzazrivec@redhat.com>
2019-08-12 10:07:41 +0200

    Add missing gettext to host controller

--- app/controllers/host_controller.rb
+++ app/controllers/host_controller.rb
@@ -279,7 +279,7 @@ class HostController < ApplicationController
         render :update do |page|
           page << javascript_prologue
           new_url = url_for_only_path(:action => "update", :button => "validate", :type => params[:type], :remember_host => "true", :escape => false)
-          page << "if (confirm('The Host SSH key has changed, do you want to accept the new key?')) miqAjax('#{new_url}');"
+          page << "if (confirm('#{_('The Host SSH key has changed, do you want to accept the new key?')}')) miqAjax('#{new_url}');"
         end
         return
       rescue => bang
