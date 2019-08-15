98335ca9bd195aff14191b0c4afa7d4d9703030d
Dávid Halász <dhalasz@redhat.com>
2019-08-15 15:00:58 +0200

    Adjust the 🍞crumb component to able to receive 🌳nodes through Rx

--- app/javascript/components/breadcrumbs/index.jsx
+++ app/javascript/components/breadcrumbs/index.jsx
@@ -6,47 +6,66 @@ import { unescape } from 'lodash';
 const parsedText = text => unescape(text).replace(/<[/]{0,1}strong>/g, '');
 
 class Breadcrumbs extends Component {
-  renderItems = () => {
-    const {
-      items, controllerName,
-    } = this.props;
-    return items.filter((item, index) => index !== (items.length - 1)).map((item, index) => {
-      const text = parsedText(item.title);
-      if ((item.url || item.key) && !item.action) {
-        if (item.key) {
-          return (
-            <Breadcrumb.Item
-              key={`${item.key}-${index}`} // eslint-disable-line react/no-array-index-key
-              onClick={() => sendDataWithRx({ breadcrumbSelect: { path: `/${controllerName}/tree_select`, key: item.key } })}
-            >
-              {text}
-            </Breadcrumb.Item>
-          );
-        }
-        return (
-          <Breadcrumb.Item
-            key={item.url || index}
-            href={item.url}
-          >
-            {text}
-          </Breadcrumb.Item>
-        );
+  constructor(props) {
+    super(props);
+    this.state = { nodes: [] };
+  }
+
+  componentDidMount() {
+    this.subscription = window.listenToRx((event) => {
+      if (event.treeNodeHierarchy) {
+        this.setState({ nodes: event.treeNodeHierarchy });
       }
-      return <li key={index}>{text}</li>; // eslint-disable-line react/no-array-index-key
     });
-  };
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
 
   render() {
     const {
-      items, title, controllerName, ...rest
+      items, controllerName, title, ...rest
     } = this.props;
 
+    const { nodes } = this.state;
+    // This head-tail is a little different: the head is an array of items while the tail is the last item.
+    const head = items.concat(nodes); // We are expecting incoming tree nodes from Rx through the state
+    const tail = head.pop();
+
+    // Map the head items one-by-one to Breadcrumb.Item components
+    const renderedItems = (head).map((item, index) => {
+      const text = parsedText(item.title);
+
+      if ((item.url || item.key) && !item.action) {
+        const clickFn = () => sendDataWithRx({
+          breadcrumbSelect: {
+            path: `/${controllerName}/tree_select`,
+            key: item.key,
+          },
+        });
+
+        return (
+          <Breadcrumb.Item key={`${item.key}-${index + 0}`} onClick={clickFn}>
+            { text }
+          </Breadcrumb.Item>
+        );
+      }
+
+      return (
+        <Breadcrumb.Item key={item.url || (index + 0)} href={item.url}>
+          { text }
+        </Breadcrumb.Item>
+      );
+    });
+
     return (
+      // Display the mapped (head) items first and then map the last (tail) item as active
       <Breadcrumb style={{ marginBottom: 0 }} {...rest}>
-        {items && this.renderItems()}
+        {items && renderedItems}
         <Breadcrumb.Item active>
           <strong>
-            {items && items.length > 0 ? parsedText(items[items.length - 1].title) : parsedText(title)}
+            {tail ? parsedText(tail.title) : parsedText(title)}
           </strong>
         </Breadcrumb.Item>
       </Breadcrumb>
