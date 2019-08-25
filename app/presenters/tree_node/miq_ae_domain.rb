8720d1731da70d395b2ace6ac7c99b8ac36ba90b
Dávid Halász <dhalasz@redhat.com>
2019-08-25 10:42:42 +0200

    Extract the icon/image from TreeNode::MiqAeDomain to the decorators

--- app/presenters/tree_node/miq_ae_domain.rb
+++ app/presenters/tree_node/miq_ae_domain.rb
@@ -2,22 +2,7 @@ module TreeNode
   class MiqAeDomain < MiqAeNode
     include MiqAeClassHelper
 
-    set_attributes(:icon, :image) do
-      # Having a "flat" case here makes the code more readable
-      # rubocop:disable LiteralAsCondition
-      case true
-      when @object.git_enabled?
-        image = 'svg/ae_git_domain.svg'
-      when @object.name == MiqAeDatastore::MANAGEIQ_DOMAIN
-        icon = 'ff ff-manageiq'
-      when !@object.top_level_namespace
-        icon = 'fa fa-globe'
-      else
-        image = "svg/vendor-#{@object.top_level_namespace.downcase}.svg"
-      end
-      # rubocop:enable LiteralAsCondition
-      [icon, image]
-    end
+    set_attribute(:image) { @object.try(:decorate).try(:fileicon) }
 
     def text
       title = super
