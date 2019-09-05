7dd5016c347d4588980e5c66e0f679d2a406d16d
Harpreet Kataria <hkataria@redhat.com>
2019-09-05 11:05:01 -0400

    Merge pull request #6103 from skateman/active-snapshot-select
    Select active snapshot instead of the last one in the snapshots 🌳
    
    (cherry picked from commit 7de5d523f8dbfbf71488345bd01493010479caa0)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1805803

--- app/presenters/tree_builder_snapshots.rb
+++ app/presenters/tree_builder_snapshots.rb
@@ -23,18 +23,24 @@ class TreeBuilderSnapshots < TreeBuilder
     }
   end
 
-  # The tree name is the same for any snapshot tree, so it doesn't make sense to
-  # load the selected node from the session. This method always selects the last
-  # node in the tree.
+  # The tree name is the same for any snapshot tree, so it doesn't make sense to load the selected
+  # node from the session. This method tries to find the active snapshot based on its suffix.
+  # FIXME: deciding based on the string is not ideal, it should be evaluated on the node object instead
   def active_node_set(tree_nodes)
-    # Find the last node
     stack = [tree_nodes.last]
     while stack.any?
       node = stack.pop
+      # If the node's text has an active suffix, return with it
+      if node.try(:[], :text).try(:ends_with?, " (#{_('Active')})")
+        active = node
+        break
+      end
+
       stack.push(node[:nodes].last) if node[:nodes].try(:any?)
     end
     # Set it as the active node in the tree state
-    @tree_state.x_node_set(node[:key], @name)
+    # If no active snapshot has been found, return with the last node
+    @tree_state.x_node_set(active.try(:[], :key) || node[:key], @name)
   end
 
   def x_get_tree_roots(count_only = false, _options = {})
