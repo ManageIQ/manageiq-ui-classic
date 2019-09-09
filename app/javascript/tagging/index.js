dbb6f13bb42355979df9572b0d1546a798a6d6a2
Martin Hradil <mhradil@redhat.com>
2019-09-09 17:06:26 +0000

    dist/tagging.css - revert CSS changes from dd13c14 (#132)
    Looks like the intent of https://github.com/ManageIQ/react-ui-components/pull/132/commits/dd13c144a2df4066268847423b7fab428f03fb8d was to remove css related to the old Select component.
    
    Instead, all pf3 tagging css was removed, causing ui-classic to fail on missing @manageiq/react-ui-components/dist/tagging.css
    
    Putting it back :)

--- app/javascript/tagging/index.js
+++ app/javascript/tagging/index.js
@@ -1,6 +1,7 @@
 import taggingApp from './reducers';
 import { TaggingConnected, TaggingWithButtonsConnected } from './containers/tagging';
 import TagView from './components/InnerComponents/TagView';
+import './sass/style.scss';
 
 export { taggingApp, TaggingConnected, TaggingWithButtonsConnected, TagView };
 export * from './components/Tagging/Tagging';
