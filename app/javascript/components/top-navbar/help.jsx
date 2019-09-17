9f25a2fe1496de67630537cc96823683e42e3734
Filip Hlavac <filip.hlavac01@gmail.com>
2019-09-17 15:05:12 +0200

    Created help component

---
+++ app/javascript/components/top-navbar/help.jsx
@@ -0,0 +1,60 @@
+/* global miqCheckForChanges */
+
+import React from 'react';
+import { Dropdown, Icon, MenuItem } from 'patternfly-react';
+
+const Help = ({
+  helpMenu,
+}) => (
+  helpMenu.map(section => (
+    section.visible && (
+    <Dropdown
+      key={section.id}
+      id="help-dd"
+      componentClass={
+            ({ children, ...props }) => (
+              <li
+                {...props}
+                className="dropdown"
+              >
+                {children}
+              </li>
+            )
+        }
+    >
+      <Dropdown.Toggle
+        noCaret
+        componentClass={
+                ({ children, ...props }) => (
+                  <a
+                    {...props}
+                    className="dropdown-toggle nav-item-iconic"
+                    data-toggle="dropdown"
+                    id="help-menu"
+                  >
+                    {children}
+                  </a>
+                )
+            }
+      >
+        <Icon type="pf" name="help" title={__('Help')} />
+      </Dropdown.Toggle>
+      <Dropdown.Menu aria-labelledby="help-menu">
+        {section.items.map(item => (
+          item.visible && (
+          <MenuItem
+            id={`help-menu-${item.name.toLowerCase()}`}
+            key={item.id}
+            href={item.link_params.href}
+            onClick={e => (item.type === 'modal' ? sendDataWithRx({ type: 'showAboutModal' }) : !miqCheckForChanges() && e.preventDefault())}
+          >
+            {item.name}
+          </MenuItem>
+          )
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+    )))
+);
+
+export default Help;
