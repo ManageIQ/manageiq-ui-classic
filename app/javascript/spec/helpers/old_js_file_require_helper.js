f765226f7bad39afe14ade52bb03521fef1f3f30
Zita Nemeckova <znemecko@redhat.com>
2019-09-17 11:17:21 +0200

    Remove deleted file

--- app/javascript/spec/helpers/old_js_file_require_helper.js
+++ app/javascript/spec/helpers/old_js_file_require_helper.js
@@ -5,7 +5,6 @@ window._ = require('lodash');
 require('patternfly-bootstrap-treeview');
 require('../../../assets/javascripts/miq_application.js');
 require('../../../assets/javascripts/miq_flash.js');
-require('../../../assets/javascripts/resizable_sidebar.js');
 require('../../../assets/javascripts/miq_global.js');
 require('../../../assets/javascripts/miq_grid.js');
 require('../../../assets/javascripts/miq_tree.js');
