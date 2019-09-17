13208e4a55867d153584565fa56f12acb5f7d40d
Filip Hlavac <filip.hlavac01@gmail.com>
2019-09-17 15:08:20 +0200

    Created superior right-section component

---
+++ app/javascript/components/top-navbar/right-section.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CustomLogo from './custom-logo';
+import Notifications from './notifications';
+import Help from './help';
+import Configuration from './configuration';
+import UserOptions from './user-options';
+import {
+  helpMenuProps, recursiveHelpMenuProps, userMenuProps, recursiveUserMenuProps,
+} from './recursive-props';
+
+const RightSection = ({
+  customLogo, currentUser, helpMenu, opsExplorerAllowed, applianceName, miqGroups, currentGroup, userMenu,
+}) => (
+  <React.Fragment>
+    <CustomLogo customLogo={customLogo} />
+    <Notifications />
+    <Help helpMenu={helpMenu} />
+    { currentUser && (
+      <React.Fragment>
+        <Configuration opsExplorerAllowed={opsExplorerAllowed} />
+        <UserOptions currentUser={currentUser} applianceName={applianceName} miqGroups={miqGroups} currentGroup={currentGroup} userMenu={userMenu} />
+      </React.Fragment>
+    )}
+  </React.Fragment>
+);
+
+const groupProps = {
+  description: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+RightSection.defaultProps = {
+  currentUser: null,
+};
+
+RightSection.propTypes = {
+  customLogo: PropTypes.bool.isRequired,
+  currentUser: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    userid: PropTypes.string.isRequired,
+  }),
+  opsExplorerAllowed: PropTypes.bool.isRequired,
+  applianceName: PropTypes.string.isRequired,
+  miqGroups: PropTypes.arrayOf(
+    PropTypes.shape({
+      ...groupProps,
+    }).isRequired,
+  ).isRequired,
+  currentGroup: PropTypes.shape({
+    ...groupProps,
+  }).isRequired,
+  helpMenu: PropTypes.arrayOf(PropTypes.shape({
+    ...helpMenuProps,
+    items: PropTypes.arrayOf(PropTypes.shape(recursiveHelpMenuProps)),
+  })).isRequired,
+  userMenu: PropTypes.arrayOf(PropTypes.shape({
+    ...userMenuProps,
+    items: PropTypes.arrayOf(PropTypes.shape(recursiveUserMenuProps)),
+  })).isRequired,
+};
+
+export default RightSection;
