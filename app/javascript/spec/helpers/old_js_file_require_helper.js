01af3e10ebd715ec580950630c0444bab2155615
Zita Nemeckova <znemecko@redhat.com>
2019-09-23 13:32:45 +0200

    Do not move miq_grid spec to Jest. Resolve its issue in followup PR

--- app/javascript/spec/helpers/old_js_file_require_helper.js
+++ app/javascript/spec/helpers/old_js_file_require_helper.js
@@ -7,7 +7,6 @@ require('patternfly-bootstrap-treeview');
 require('../../../assets/javascripts/miq_application.js');
 require('../../../assets/javascripts/miq_flash.js');
 require('../../../assets/javascripts/miq_global.js');
-require('../../../assets/javascripts/miq_grid.js');
 require('../../../assets/javascripts/miq_tree.js');
 require('../../../assets/javascripts/miq_toolbar.js');
 require('../../../assets/javascripts/miq_list_grid.js');
