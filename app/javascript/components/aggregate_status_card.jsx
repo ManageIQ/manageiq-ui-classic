6e8f7291f7822e111ce39c11bd4bad95ba776783
Martin Povolny <mpovolny@redhat.com>
2019-09-30 14:50:44 +0200

    Provider dashboard: style and other fixes.

--- app/javascript/components/aggregate_status_card.jsx
+++ app/javascript/components/aggregate_status_card.jsx
@@ -7,20 +7,19 @@ const AggregateStatusCard = ({ providerId, providerType }) => {
   const [data, setCardData] = useState({ loading: true });
 
   useEffect(() => {
-    const url = `/${providerType}_dashboard/aggregate_status_data/${providerId ? providerId : ''}`;
+    const url = `/${providerType}_dashboard/aggregate_status_data/${providerId || ''}`;
     http.get(url)
-    .then(response => {
-      console.log(response);
-      const aggStatusData = response.data.aggStatus;
-      setCardData({
-        loading: false,
-        status: aggStatusData.status,
-        aggStatus: aggStatusData.attrData,
-        showTopBorder: aggStatusData.showTopBorder,
-        aggregateLayout: aggStatusData.aggregateLayout,
-        aggregateClass: aggStatusData.aggregateClass,
-      })
-    });
+      .then((response) => {
+        const aggStatusData = response.data.aggStatus;
+        setCardData({
+          loading: false,
+          status: aggStatusData.status,
+          aggStatus: aggStatusData.attrData,
+          showTopBorder: aggStatusData.showTopBorder,
+          aggregateLayout: aggStatusData.aggregateLayout,
+          aggregateClass: aggStatusData.aggregateClass,
+        });
+      });
   }, []);
 
   if (data.loading) {
@@ -28,28 +27,27 @@ const AggregateStatusCard = ({ providerId, providerType }) => {
   }
 
   return (
-    <div className='aggregate_status'>
-      <div className='col-xs-12 col-sm-12 col-md-3 col-lg-2 here'>
+    <div className="aggregate_status">
+      <div className="col-xs-12 col-sm-12 col-md-3 col-lg-2 here">
         <PfAggregateStatusCard
-           layout={data.aggregateLayout}
-           className={data.aggregateClass}
-           data={data.status}
-           showTopBorder={data.showTopBorder}
+          layout={data.aggregateLayout}
+          className={data.aggregateClass}
+          data={data.status}
+          showTopBorder={data.showTopBorder}
         />
       </div>
-      <div className='col-xs-12 col-sm-12 col-md-9 col-lg-10'>
-        <div className='row'>
-          { data.aggStatus.map( st => (
-              <div key={st.id} className='col-xs-12 col-sm-6 col-md-4 col-lg-3'>
-                <PfAggregateStatusCard
-                  layout='mini'
-                  id={st.id}
-                  data={st}
-                  showTopBorder={true}
-                />
-              </div>
-            )
-          )}
+      <div className="col-xs-12 col-sm-12 col-md-9 col-lg-10">
+        <div className="row">
+          { data.aggStatus.map(st => (
+            <div key={st.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              <PfAggregateStatusCard
+                layout="mini"
+                id={st.id}
+                data={st}
+                showTopBorder
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
@@ -59,6 +57,10 @@ const AggregateStatusCard = ({ providerId, providerType }) => {
 AggregateStatusCard.propTypes = {
   providerId: PropTypes.string,
   providerType: PropTypes.string.isRequired,
+};
+
+AggregateStatusCard.defaultProps = {
+  providerId: null,
 };
 
 export default AggregateStatusCard;
--- app/javascript/components/pf_aggregate_status_card.jsx
+++ app/javascript/components/pf_aggregate_status_card.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PfAggregateStatusCard = ({ showTopBorder, altLayout, layout, data }) => {
+const PfAggregateStatusCard = ({
+  showTopBorder, altLayout, layout, data,
+}) => {
   const shouldShowTopBorder = () => (showTopBorder === true);
   const isAltLayout = () => (altLayout === true || layout === 'tall');
   const isMiniLayout = () => (layout === 'mini');
 
-  const normalLayoutClass = 
-    `card-pf card-pf-aggregate-status
+  const normalLayoutClass = `card-pf card-pf-aggregate-status
     ${isAltLayout() ? ' card-pf-aggregate-status-alt' : ''}
-    ${shouldShowTopBorder() ? ' card-pf-accented' : ''}`
+    ${shouldShowTopBorder() ? ' card-pf-accented' : ''}`;
 
   const renderNormalLayout = () => (
     <div className={normalLayoutClass}>
       <h2 className="card-pf-title">
         <a href={data.href}>
           { data.iconImage && (
-              data.largeIcon
+            data.largeIcon
               ? <img src={data.iconImage} alt="" width={72} height={72} />
               : <img src={data.iconImage} alt="" className="card-pf-icon-image" />
           )}
-          <span className={data.iconClass}></span>
+          <span className={data.iconClass} />
           <span className="card-pf-aggregate-status-count">{data.count}</span>
           <span className="card-pf-aggregate-status-title">{data.title}</span>
         </a>
       </h2>
       <div className="card-pf-body">
         <p className="card-pf-aggregate-status-notifications">
-          <span className="card-pf-aggregate-status-notification" ng-repeat="notification in data.notifications">
-            <a href={notification.href}>
-              { notification.iconImage &&
-                <React.Fragment>
-                  <img src={notification.iconImage} alt="" className="card-pf-icon-image" />
-                  <span className={notification.iconClass}></span>{ notification.count }
-                </React.Fragment>
-              }
-            </a>
-          </span>
+          { data.notifications.map(notification => (
+            <span key={notification} className="card-pf-aggregate-status-notification">
+              <a href={notification.href}>
+                { notification.iconImage && (
+                  <React.Fragment>
+                    <img src={notification.iconImage} alt="" className="card-pf-icon-image" />
+                    <span className={notification.iconClass} />
+                    { notification.count }
+                  </React.Fragment>
+                )}
+              </a>
+            </span>
+          ))}
         </p>
       </div>
     </div>
@@ -45,43 +49,40 @@ const PfAggregateStatusCard = ({ showTopBorder, altLayout, layout, data }) => {
   const displayNotification = () =>
     data.notification && (data.notification.iconImage || data.notification.iconClass || data.notification.count);
 
-  const miniLayoutClass =
-    `card-pf card-pf-aggregate-status card-pf-aggregate-status-mini${shouldShowTopBorder() ? ' card-pf-accented' : ''}`
+  const miniLayoutClass = `card-pf card-pf-aggregate-status card-pf-aggregate-status-mini${shouldShowTopBorder() ? ' card-pf-accented' : ''}`;
 
   const renderMiniLayout = () => (
     <div className={miniLayoutClass}>
       <h2 className="card-pf-title">
         <a href={data.href}>
-          { data.iconImage && 
-            <img src={data.iconImage} alt="" className="card-pf-icon-image" />
-          }
-          { data.iconClass &&
-            <span className={data.iconClass}></span>
-          }
+          { data.iconImage && <img src={data.iconImage} alt="" className="card-pf-icon-image" /> }
+          { data.iconClass && <span className={data.iconClass} /> }
           <span className="card-pf-aggregate-status-count">{data.count}</span>
           {data.title}
         </a>
       </h2>
       <div className="card-pf-body">
-        { displayNotification() &&
+        { displayNotification() && (
           <p className="card-pf-aggregate-status-notifications">
-            <span className="card-pf-aggregate-status-notification">
-              { data.notification.href &&
-                <a href={data.notification.href}>
-                  { data.notification.iconImage && 
-                    <img src={data.notification.iconImage} alt="" className="card-pf-icon-image" />
-                  }
-                  { data.notification.iconClass &&
-                    <React.Fragment>
-                      <span className={data.notification.iconClass}></span>
-                      { data.notification.count && <span>{data.notification.count}</span> }
-                    </React.Fragment>
-                  }
-                </a>
+            <span className="card-pf-aggregate-status-notification" title={data.notification.tooltip}>
+              { data.notification.href
+                ? (
+                  <a href={data.notification.href}>
+                    { data.notification.iconImage && <img src={data.notification.iconImage} alt="" className="card-pf-icon-image" /> }
+                    { data.notification.iconClass && <span className={data.notification.iconClass} /> }
+                    { data.notification.count && <span>{data.notification.count}</span> }
+                  </a>)
+                : (
+                  <span>
+                    { data.notification.iconImage && <img src={data.notification.iconImage} alt="" className="card-pf-icon-image" /> }
+                    { data.notification.iconClass && <span className={data.notification.iconClass} /> }
+                    { data.notification.count && <span>{data.notification.count}</span> }
+                  </span>
+                )
               }
             </span>
           </p>
-        }
+        )}
       </div>
     </div>
   );
@@ -92,7 +93,7 @@ const PfAggregateStatusCard = ({ showTopBorder, altLayout, layout, data }) => {
 PfAggregateStatusCard.propTypes = {
   layout: PropTypes.string,
   className: PropTypes.string,
-  data: PropTypes.any.isRequired,
+  data: PropTypes.arrayOf(PropTypes.any).isRequired,
   showTopBorder: PropTypes.bool.isRequired,
   altLayout: PropTypes.bool,
 };
@@ -102,6 +103,6 @@ PfAggregateStatusCard.defaultProps = {
   className: null, // bylo to tam?
   altLayout: false,
   showTopBorder: false, // urcite?
-}
+};
 
 export default PfAggregateStatusCard;
