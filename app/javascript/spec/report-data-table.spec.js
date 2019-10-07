73cf502aaadb1ca8565aab87109344885753438a
Martin Marosi <marvusm.mmi@gmail.com>
2019-10-07 16:11:27 +0200

    Use async act on report-data-table spec.

--- app/javascript/spec/report-data-table.spec.js
+++ app/javascript/spec/report-data-table.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import fetchMock from 'fetch-mock';
-import { act } from 'react-test-renderer';
+import { act } from 'react-dom/test-utils';
 import { EmptyState, Paginator, Table } from 'patternfly-react';
 
 import './helpers/mockAsyncRequest';
@@ -11,6 +11,7 @@ import ReportDataTable from '../components/report-data-table';
 
 describe('<ReportDataTable />', () => {
   let initialProps;
+  jest.setTimeout(30000);
 
   beforeEach(() => {
     initialProps = {
@@ -36,42 +37,35 @@ sort_by=${sortBy}&sort_order=${sortDirection}&\
 limit=${limit}&offset=${offset}`;
   };
 
-  it('should fetch and display report data when instantiated', (done) => {
+  it('should fetch and display report data when instantiated', async(done) => {
     fetchMock.getOnce(requestUrl(), { report: { col_order: [] }, count: 1, result_set: [{ foo: 'bar' }] });
-
     let wrapper;
-    act(() => {
+    await act(async() => {
       wrapper = mount(<ReportDataTable {...initialProps} />);
     });
-
-    setTimeout(() => {
-      wrapper.update();
-
-      expect(fetchMock.calls()).toHaveLength(1);
-      expect(wrapper.find('div.table-view-pf-toolbar')).toHaveLength(1);
-      expect(wrapper.find(Table.PfProvider)).toHaveLength(1);
-      expect(wrapper.find(Paginator)).toHaveLength(1);
-      done();
-    });
+    wrapper.update();
+    expect(fetchMock.calls()).toHaveLength(1);
+    expect(wrapper.find('div.table-view-pf-toolbar')).toHaveLength(1);
+    expect(wrapper.find(Table.PfProvider)).toHaveLength(1);
+    expect(wrapper.find(Paginator)).toHaveLength(1);
+    done();
   });
 
-  it('should display empty state when there is no data', (done) => {
+  it('should display empty state when there is no data', async(done) => {
     fetchMock.getOnce(requestUrl(), { report: { col_order: [] }, count: 0, result_set: [] });
 
     let wrapper;
-    act(() => {
+    await act(async() => {
       wrapper = mount(<ReportDataTable {...initialProps} />);
     });
 
-    setTimeout(() => {
-      wrapper.update();
+    wrapper.update();
 
-      expect(fetchMock.calls()).toHaveLength(1);
-      expect(wrapper.find('div.table-view-pf-toolbar')).toHaveLength(1);
-      expect(wrapper.find(Table.PfProvider)).toHaveLength(0);
-      expect(wrapper.find(Paginator)).toHaveLength(0);
-      expect(wrapper.find(EmptyState)).toHaveLength(1);
-      done();
-    });
+    expect(fetchMock.calls()).toHaveLength(1);
+    expect(wrapper.find('div.table-view-pf-toolbar')).toHaveLength(1);
+    expect(wrapper.find(Table.PfProvider)).toHaveLength(0);
+    expect(wrapper.find(Paginator)).toHaveLength(0);
+    expect(wrapper.find(EmptyState)).toHaveLength(1);
+    done();
   });
 });
