178a2835479ce7c5f855564f632ae8bbafbaa96d
Martin Hradil <mhradil@redhat.com>
2019-10-22 22:57:34 +0000

    dialogFieldRefreshService#refreshField - use outputConversion, try to unify with SUI
    also, we can get rid of the API angular promise to Promise conversion
    with the $scope.$apply() removal in dialogUser.ts in ui-components
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1744413

--- app/assets/javascripts/services/dialog_field_refresh_service.js
+++ app/assets/javascripts/services/dialog_field_refresh_service.js
@@ -1,25 +1,29 @@
-ManageIQ.angular.app.service('dialogFieldRefreshService', ['API', function(API) {
-  this.refreshField = function(dialogData, dialogField, url, idList) {
-    this.areFieldsBeingRefreshed = true;
-    var data = angular.toJson({
+ManageIQ.angular.app.service('dialogFieldRefreshService', ['API', 'DialogData', function(API, DialogData) {
+  var self = this;
+
+  self.refreshField = function(dialogData, dialogField, url, idList) {
+    self.areFieldsBeingRefreshed = true;
+
+    var data = {
       action: 'refresh_dialog_fields',
       resource: {
-        dialog_fields: dialogData,
+        dialog_fields: DialogData.outputConversion(dialogData),
         fields: dialogField,
         resource_action_id: idList.resourceActionId,
         target_id: idList.targetId,
         target_type: idList.targetType,
         real_target_type: idList.realTargetType,
       },
-    });
+    };
 
-    return new Promise(function(resolve) {
-      API.post(url + idList.dialogId, data).then(function(response) {
-        resolve(response.result[dialogField]);
+    return API.post(url + idList.dialogId, angular.toJson(data))
+      .then(function(response) {
+        // FIXME: API requests don't actually count towards $.active
         if ($.active < 1) {
-          this.areFieldsBeingRefreshed = false;
+          self.areFieldsBeingRefreshed = false;
         }
-      }.bind(this));
-    }.bind(this));
+
+        return response.result[dialogField];
+      });
   };
 }]);
