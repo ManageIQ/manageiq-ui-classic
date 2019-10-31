3fd2cb51789641c79b58fbcd2a5cba3dd765bb7a
Filip Hlavac <filip.hlavac01@gmail.com>
2019-10-31 16:23:25 +0100

    Updated automate-class-form schema and component

--- app/javascript/components/automate-class-form/automate-class-form.jsx
+++ app/javascript/components/automate-class-form/automate-class-form.jsx
@@ -1,95 +1,43 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import automateClassFormSchema from './automate-class-form.schema';
 import MiqFormRenderer from '../../forms/data-driven-form';
 // import { http } from '../../http_api';
 
-const submitForm = values =>
+const submitForm = () => true;
+/*
   http.post('/miq_ae_tools/retrieve_git_datastore', values)
-    .then(data => add_flash(data[0].message, data[0].level));
+    .then(data => add_flash(data[0].message, data[0].level)); */
 
-const AutomateClassForm = ({ disableSubmit }) => (
+const AutomateClassForm = ({
+  namespacePath, maxNameLen, initialValues,
+}) => (
   <div style={{ paddingBottom: 16 }}>
+    <h3>{__('Properties')}</h3>
+    <div className="form-horizontal">
+      <div className="form-group">
+        <label className="col-md-2 control-label">
+          {__('Fully Qualified Name')}
+        </label>
+        <div className="col-md-8">{namespacePath}</div>
+      </div>
+    </div>
     <MiqFormRenderer
-      initialValues={{ git_verify_ssl: true }}
-      schema={automateClassFormSchema()}
+      initialValues={initialValues}
+      schema={automateClassFormSchema(maxNameLen)} // TODO
       onSubmit={submitForm}
-      disableSubmit={disableSubmit ? ['pristine', 'dirty'] : ['pristine', 'invalid']}
     />
   </div>
 );
+
+AutomateClassForm.propTypes = {
+  namespacePath: PropTypes.string.isRequired,
+  maxNameLen: PropTypes.number.isRequired,
+  initialValues: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    display_name: PropTypes.string,
+    description: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default AutomateClassForm;
-
-/*
-- url = url_for_only_path(:action => 'form_field_changed', :id => (@ae_class.id || 'new'))
-- obs = {:interval => '.5', :url => url}.to_json
-%h3
-  = _('Properties')
-.form-horizontal
-  .form-group
-    %label.col-md-2.control-label
-      = _('Fully Qualified Name')
-    .col-md-8
-      = @sb[:namespace_path]
-  .form-group
-    %label.col-md-2.control-label
-      = _('Name')
-    .col-md-8
-      = text_field_tag("name", @edit[:new][:name],
-                        :maxlength         => ViewHelper::MAX_NAME_LEN,
-                        :class => "form-control",
-                        "data-miq_observe" => obs)
-      = javascript_tag(javascript_focus('name'))
-  .form-group
-    %label.col-md-2.control-label
-      = _('Display Name')
-    .col-md-8
-      = text_field_tag("display_name", @edit[:new][:display_name],
-                        :maxlength         => ViewHelper::MAX_NAME_LEN,
-                        :class => "form-control",
-                        "data-miq_observe" => obs)
-  .form-group
-    %label.col-md-2.control-label
-      = _('Description')
-    .col-md-8
-      = text_field_tag("description", @edit[:new][:description],
-                       :maxlength => ViewHelper::MAX_NAME_LEN,
-                       :class => "form-control",
-                       "data-miq_observe" => obs)
--------------
-import React from 'react';
-import PropTypes from 'prop-types';
-import datastoreSchema from './automate-class-form.schema';
-import MiqFormRenderer from '../../forms/data-driven-form';
-import { http } from '../../http_api';
-
-const submitForm = values =>
-  http.post('/miq_ae_tools/retrieve_git_datastore', values)
-    .then(data => add_flash(data[0].message, data[0].level));
-
-const ImportDatastoreViaGit = ({ disableSubmit }) => (
-  <div style={{ paddingBottom: 16 }}>
-    {disableSubmit
-    && (
-    <h3>{__('Please enable the git owner role in order to import git repositories')}</h3>
-    )}
-    <MiqFormRenderer
-      initialValues={{ git_verify_ssl: true }}
-      schema={datastoreSchema()}
-      onSubmit={submitForm}
-      disableSubmit={disableSubmit ? ['pristine', 'dirty'] : ['pristine', 'invalid']}
-    />
-  </div>
-);
-
-ImportDatastoreViaGit.propTypes = {
-  disableSubmit: PropTypes.bool,
-};
-
-ImportDatastoreViaGit.defaultProps = {
-  disableSubmit: false,
-};
-
-export default ImportDatastoreViaGit;
-*/
--- app/javascript/components/automate-class-form/automate-class-form.schema.js
+++ app/javascript/components/automate-class-form/automate-class-form.schema.js
@@ -1,10 +1,33 @@
 import { componentTypes, validatorTypes } from '@data-driven-forms/react-form-renderer';
 
-const automateClassFormSchema = () => ({
+const automateClassFormSchema = maxNameLen => ({
   fields: [{
-    name: 'git_url',
-    label: __('Git URL'),
+    name: 'name',
+    label: __('Name'),
     component: componentTypes.TEXT_FIELD,
+    autoFocus: true,
+    validate: [{
+      type: validatorTypes.MAX_LENGTH,
+      threshold: maxNameLen,
+    }],
+  },
+  {
+    name: 'display_name',
+    label: __('Display Name'),
+    component: componentTypes.TEXT_FIELD,
+    validate: [{
+      type: validatorTypes.MAX_LENGTH,
+      threshold: maxNameLen,
+    }],
+  },
+  {
+    name: 'description',
+    label: __('Description'),
+    component: componentTypes.TEXT_FIELD,
+    validate: [{
+      type: validatorTypes.MAX_LENGTH,
+      threshold: maxNameLen,
+    }],
   }],
 });
 
