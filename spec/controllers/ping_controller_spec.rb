fa85d3956b299872aa8b94676115849c4650a474
Jason Frey <jfrey@redhat.com>
2019-11-11 15:03:45 -0500

    Disable ping_controller_spec temporarily

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -1,10 +1,13 @@
-describe ::PingController do
-  before { EvmSpecHelper.create_guid_miq_server_zone }
-
-  it 'pongs' do
-    get :index
-
-    expect(response.status).to eq(200)
-    expect(response.body).to eq("pong")
-  end
-end
+# Temporarily commented until https://github.com/ManageIQ/manageiq-api/issues/708 as
+#   Rails is unable to resolve the PingController constant properly in specs.
+#
+# describe PingController do
+#   before { EvmSpecHelper.create_guid_miq_server_zone }
+#
+#   it 'pongs' do
+#     get :index
+#
+#     expect(response.status).to eq(200)
+#     expect(response.body).to eq("pong")
+#   end
+# end
