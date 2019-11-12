0c7e5c562423cabf023c8baa23caa900472b34c9
Martin Marosi <marvusm.mmi@gmail.com>
2019-11-12 13:12:06 +0100

    Added name field and async select zone field.

--- app/javascript/forms/provider-forms/cloud-provider-form/index.jsx
+++ app/javascript/forms/provider-forms/cloud-provider-form/index.jsx
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { componentTypes } from '@data-driven-forms/react-form-renderer';
+import { componentTypes, validatorTypes } from '@data-driven-forms/react-form-renderer';
 
 import { API } from '../../../http_api';
 import MiqFormRenderer from '../../data-driven-form';
 
+const loadProviderServerZones = () =>
+  API.get('/api/zones?expand=resources&attributes=id,name,visible&filter[]=visible!=false&sort_by=name')
+    .then(({ resources }) => resources.map(({ name, id }) => ({ value: id, label: name })));
+
 const initialSchema = emsTypes => ({
   fields: [{
+    component: componentTypes.TEXT_FIELD,
+    name: 'name',
+    label: __('Name'),
+    options: emsTypes,
+  }, {
     component: componentTypes.SELECT,
     name: 'type',
     label: __('Type'),
     placeholder: `<${__('Choose')}>`,
     options: emsTypes,
+    validate: [{
+      type: validatorTypes.REQUIRED,
+    }],
+  }, {
+    component: componentTypes.SELECT,
+    name: 'zone',
+    label: __('Zone'),
+    placeholder: `<${__('Choose')}>`,
+    loadOptions: loadProviderServerZones,
+    initialValue: '1',
   }],
 });
-
 
 const loadProviderTypes = () =>
   API.options('/api/providers')
