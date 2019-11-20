051150449c67dce18903ff9612c1b1878338e576
Filip Hlavac <filip.hlavac01@gmail.com>
2019-11-20 15:04:03 +0100

    Add NavbarHeader component design implementation

--- app/javascript/components/navbar-header/navbar-header.jsx
+++ app/javascript/components/navbar-header/navbar-header.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NavbarHeader = () => (
-  <h1>Hello world!</h1>
+const NavbarHeader = ({
+  customBrand,
+  imagePath,
+}) => (
+  <div className="navbar-header">
+    <button className="navbar-toggle" type="button">
+      <span className="sr-only">
+        {__('Toggle navigation')}
+      </span>
+      <span className="icon-bar" />
+      <span className="icon-bar" />
+      <span className="icon-bar" />
+    </button>
+    <a className="navbar-brand" href="/dashboard/start_url" title={__('Go to my start page')}>
+      <img
+        alt="ManageIQ"
+        className="navbar-brand-name"
+        src={customBrand ? '/upload/custom_brand.png' : imagePath}
+      />
+    </a>
+  </div>
 );
 
+NavbarHeader.propTypes = {
+  customBrand: PropTypes.bool.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
 export default NavbarHeader;
-
-
-/*
-  .navbar-header
-    %button{:type => "button", :class => "navbar-toggle"}
-      %span.sr-only
-        = _("Toggle navigation")
-      %span.icon-bar
-      %span.icon-bar
-      %span.icon-bar
-    %a.navbar-brand{:href => '/dashboard/start_url', :title => _("Go to my start page")}
-      %img.navbar-brand-name{:src => ::Settings.server.custom_brand ? '/upload/custom_brand.png' : image_path("layout/brand.svg"), :alt => "ManageIQ"}
-*/
