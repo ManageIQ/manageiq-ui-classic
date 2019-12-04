af9371f5227407c8ad22c8edc251757067414b9b
Martin Povolny <mpovolny@redhat.com>
2019-12-04 08:55:01 +0100

    Reconfigure: handle plural case toolbar button permissions.

--- app/helpers/application_helper/button/vm_reconfigure.rb
+++ app/helpers/application_helper/button/vm_reconfigure.rb
@@ -2,6 +2,6 @@ class ApplicationHelper::Button::VmReconfigure < ApplicationHelper::Button::Basi
   needs :@record
 
   def visible?
-    role_allows?(:identifier => 'vm_reconfigure_all', :any => true) && @record.reconfigurable?
+    role_allows?(:feature => 'vm_reconfigure_all', :any => true) && @record.reconfigurable?
   end
 end
