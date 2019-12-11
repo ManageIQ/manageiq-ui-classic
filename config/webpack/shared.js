30e12ba228723c7f54019e9ec3b2567cc556496d
Martin Hradil <mhradil@redhat.com>
2019-12-11 00:00:12 +0000

    webpack - merge resolve.alias and sharedPackages
    before:
      sharedPackages was used to make sure plugin code uses our version of packages,
      alias was used to make sure dependencies of dependencies use our version of packages
    
    moving all those packages to sharedPackages, leaving only renames and wipeouts in resolve.alias,
    and generating the rest of resolve.alias from sharedPackages

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -20,14 +20,18 @@ const entryPath = join(settings.source_path, settings.source_entry_path);
 const moduleDir = engines['manageiq-ui-classic'].node_modules;
 
 const sharedPackages = [
+  'angular',
+  'graphql', // TODO remove once this gets added in manageiq-graphql
   'jquery',
   'lodash',
+  'moment',
   'patternfly-react',
   'patternfly-sass',
+  'prop-types',
   'react',
   'react-dom',
-  'prop-types',
-  'graphql', // TODO remove once this gets added in manageiq-graphql
+  'react-redux',
+  'redux',
 ];
 
 let packPaths = {};
@@ -129,12 +133,8 @@ module.exports = {
 
   resolve: {
     alias: {
-      'react': resolveModule('react'), // only ever use one react version
-      'redux': resolveModule('redux'), // only ever use one redux version
-      'react-redux': resolveModule('react-redux'), // only ever use one react-redux version
+      ...sharedPackages.reduce((acc, pkg) => ({ ...acc, [pkg]: resolveModule(pkg) }), {}),
       'bootstrap-select': '@pf3/select', // never use vanilla bootstrap-select
-      'moment': resolveModule('moment'), // fix moment-strftime peerDependency issue
-      'angular': resolveModule('angular'), // fix for "Tried to load angular more than once"
       '@patternfly/patternfly': resolveModule('NONEXISTENT'),
       '@patternfly/patternfly-next': resolveModule('NONEXISTENT'),
     },
