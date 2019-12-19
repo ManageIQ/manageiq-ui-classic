c0fa1010475272680d857306ee6819f2cf4d07d7
Hilda Stastna <hstastna@redhat.com>
2019-12-19 17:32:05 +0100

    Remove useless toolbar button for Container Build scan (SSA)
    Issue: https://github.com/ManageIQ/manageiq-ui-classic/issues/6300

--- app/helpers/application_helper/toolbar/container_builds_center.rb
+++ app/helpers/application_helper/toolbar/container_builds_center.rb
@@ -1,24 +1,4 @@
 class ApplicationHelper::Toolbar::ContainerBuildsCenter < ApplicationHelper::Toolbar::Basic
-  button_group('container_build_vmdb', [
-    select(
-      :container_build_vmdb_choice,
-      nil,
-      t = N_('Configuration'),
-      t,
-      :items => [
-        button(
-          :container_build_scan,
-          'fa fa-search fa-lg',
-          N_('Perform SmartState Analysis on the selected items'),
-          N_('Perform SmartState Analysis'),
-          :url_parms    => "main_div",
-          :send_checked => true,
-          :confirm      => N_("Perform SmartState Analysis on the selected items?"),
-          :enabled      => false,
-          :onwhen       => "1+"),
-      ]
-    ),
-  ])
   button_group('container_build_policy', [
     select(
       :container_build_policy_choice,
