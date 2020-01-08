0536bd9350cac30c6a18f444f969a2a5eae779cd
Martin Povolny <mpovolny@redhat.com>
2020-01-08 15:37:10 +0100

    Reconfigure: use detailed RBAC features
    backport of https://github.com/ManageIQ/manageiq-ui-classic/pull/6469
    
    fixes https://bugzilla.redhat.com/show_bug.cgi?id=1770022

--- spec/helpers/application_helper/buttons/vm_reconfigure_spec.rb
+++ spec/helpers/application_helper/buttons/vm_reconfigure_spec.rb
@@ -1,5 +1,10 @@
 describe ApplicationHelper::Button::VmReconfigure do
   describe '#visible?' do
+    before do
+      @view_context = setup_view_context_with_sandbox({})
+      allow(@view_context).to receive(:role_allows?).and_return(true)
+    end
+
     context "record is vmware vm" do
       before do
         @record = FactoryGirl.create(:vm_vmware)
--- spec/shared/helpers/application_helper_buttons.rb
+++ spec/shared/helpers/application_helper_buttons.rb
@@ -1,6 +1,6 @@
 shared_examples_for 'will be skipped for this record' do |message|
   it message.to_s do
-    view_context = setup_view_context_with_sandbox({})
+    view_context = @view_context || setup_view_context_with_sandbox({})
     button = described_class.new(view_context, {}, {'record' => @record}, {})
     expect(button.visible?).to be_falsey
   end
@@ -8,7 +8,7 @@ end
 
 shared_examples_for 'will not be skipped for this record' do |message|
   it message.to_s do
-    view_context = setup_view_context_with_sandbox({})
+    view_context = @view_context || setup_view_context_with_sandbox({})
     button = described_class.new(view_context, {}, {'record' => @record}, {})
     expect(button.visible?).to be_truthy
   end
