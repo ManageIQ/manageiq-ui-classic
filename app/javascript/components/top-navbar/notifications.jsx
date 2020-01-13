dd97a769176769045a146fbc3721b910a69084b1
Filip Hlavac <filip.hlavac01@gmail.com>
2020-01-13 12:33:44 +0100

    Make notifications top navbar button use reducer

--- app/javascript/components/top-navbar/notifications.jsx
+++ app/javascript/components/top-navbar/notifications.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { Icon } from 'patternfly-react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { saveNotificationDrawerVisibility } from '../notification-drawer/helpers';
 
-const Notifications = ({
-  unreadCount,
-}) => (
-  <li className="dropdown">
-    <a
-      id="notifications-btn"
-      className="nav-item-iconic drawer-pf-trigger-icon"
-      title={`${unreadCount} ${__('unread notifications')}`}
-      onClick={(event) => {
-        sendDataWithRx({ type: 'toggleNotificationsList' });
-        event.preventDefault();
-      }}
-    >
-      <Icon type="fa" name="bell" />
-      <span className="badge badge-pf-bordered">{unreadCount > 0 ? ' ' : ''}</span>
-    </a>
-  </li>
-);
+const Notifications = () => {
+  const dispatch = useDispatch();
+  const isDrawerVisible = useSelector(({ notificationReducer: { isDrawerVisible } }) => isDrawerVisible);
+  const unreadCount = useSelector(({ notificationReducer: { unreadCount } }) => unreadCount);
 
-const mapStateToProps = ({ notificationReducer: { unreadCount } }) => ({ unreadCount });
+  useEffect(() => {
+    saveNotificationDrawerVisibility(isDrawerVisible);
+  }, [isDrawerVisible]);
 
-Notifications.propTypes = {
-  unreadCount: PropTypes.number.isRequired,
+  return (
+    <li className="dropdown">
+      <a
+        id="notifications-btn"
+        className="nav-item-iconic drawer-pf-trigger-icon"
+        title={`${unreadCount} ${__('unread notifications')}`}
+        onClick={() => {
+          dispatch({ type: '@@notifications/toggleDrawerVisibility', payload: isDrawerVisible });
+        }}
+      >
+        <Icon type="fa" name="bell" />
+        <span className="badge badge-pf-bordered">{unreadCount > 0 ? ' ' : ''}</span>
+      </a>
+    </li>
+  );
 };
 
-export default connect(mapStateToProps)(Notifications);
+export default (Notifications);
