815674c92a7c27a4434d3c326299579ef2b4108e
Filip Hlavac <filip.hlavac01@gmail.com>
2020-01-16 10:46:00 +0100

    Add getNotificationStatusIconName and viewDetail

--- app/javascript/components/notification-drawer/helpers.js
+++ app/javascript/components/notification-drawer/helpers.js
@@ -5,3 +5,17 @@ export const saveNotificationDrawerVisibility = (isDrawerVisible) => {
 export const unreadCountText = function(count) {
   return sprintf(__('%d New'), count);
 };
+
+export const getNotficationStatusIconName = notification => (
+  {
+    info: 'info',
+    error: 'error-circle-o',
+    danger: 'error-circle-o',
+    warning: 'warning-triangle-o',
+    success: 'ok',
+    ok: 'ok',
+  }[notification.type] || '');
+
+export const viewDetails = function(notification) {
+  window.location.href = `/restful_redirect?${$.param(notification.data.link)}`;
+};
