12d91af46eec64ab4f04088d37cebb653eaca32f
Zita Nemeckova <znemecko@redhat.com>
2020-01-16 16:04:46 +0100

    Fix error about empty? called on nil object

--- app/helpers/application_helper/button/utilization_download.rb
+++ app/helpers/application_helper/button/utilization_download.rb
@@ -1,7 +1,7 @@
 class ApplicationHelper::Button::UtilizationDownload < ApplicationHelper::Button::Basic
   def disabled?
     # to enable the button we are in the "Utilization" and have trend report
-    return false if @layout == 'miq_capacity_utilization' && @sb[:active_tab] == 'report' && !@sb.fetch_path(:trend_rpt).table.data.empty?
+    return false if @layout == 'miq_capacity_utilization' && @sb[:active_tab] == 'report' && @sb.fetch_path(:trend_rpt, :table, :data).present?
 
     # otherwise the button is off
     @error_message = _('No records found for this report')
