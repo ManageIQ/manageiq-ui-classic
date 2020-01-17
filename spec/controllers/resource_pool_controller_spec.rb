b419bd251a69d9b3ddf493d8fa783732e131e943
Hilda Stastna <hstastna@redhat.com>
2020-01-17 16:59:18 +0100

    Refactor resource_pool controller, make Edit VM thru Resource Pool work
    Issue: https://github.com/ManageIQ/manageiq-ui-classic/issues/6617

--- spec/controllers/resource_pool_controller_spec.rb
+++ spec/controllers/resource_pool_controller_spec.rb
@@ -18,13 +18,6 @@ describe ResourcePoolController do
       expect(controller.send(:flash_errors?)).not_to be_truthy
     end
 
-    it "when VM Retire is pressed" do
-      controller.params = {:pressed => "vm_retire"}
-      expect(controller).to receive(:retirevms).once
-      controller.button
-      expect(controller.send(:flash_errors?)).not_to be_truthy
-    end
-
     it "when VM Manage Policies is pressed" do
       controller.params = {:pressed => "vm_protect"}
       expect(controller).to receive(:assign_policies).with(VmOrTemplate)
@@ -98,16 +91,253 @@ describe ResourcePoolController do
           expect(controller.instance_variable_get(:@flash_array)).to eq([{:message => 'Check Compliance initiated for 1 VM and Instance from the ManageIQ Database', :level => :success}])
         end
       end
+
+      it 'calls check_compliance_vms' do
+        expect(controller).to receive(:check_compliance_vms)
+        controller.send(:button)
+      end
     end
 
     context 'reconfigure VMs' do
-      before do
-        controller.instance_variable_set(:@display, 'vms')
-        controller.params = {:pressed => 'vm_reconfigure'}
-      end
+      before { controller.params = {:pressed => 'vm_reconfigure'} }
 
       it 'calls vm_reconfigure' do
         expect(controller).to receive(:vm_reconfigure)
+        controller.send(:button)
+      end
+    end
+
+    context 'Extract Running Processes for selected VMs' do
+      let(:vm) { FactoryBot.create(:vm_vmware) }
+
+      before do
+        allow(controller).to receive(:assert_privileges)
+        allow(controller).to receive(:show)
+        allow(controller).to receive(:performed?).and_return(true)
+        allow(controller).to receive(:render)
+        controller.params = {:pressed => 'vm_collect_running_processes', :miq_grid_checks => vm.id.to_s}
+      end
+
+      it 'calls getprocessesvms' do
+        expect(controller).to receive(:getprocessesvms)
+        controller.send(:button)
+      end
+
+      it 'sets error flash message' do
+        controller.send(:button)
+        expect(controller.instance_variable_get(:@flash_array)).to eq([{:message => 'Collect Running Processes action does not apply to selected items', :level => :error}])
+      end
+    end
+
+    context 'Compare selected VMs' do
+      before { controller.params = {:pressed => 'vm_compare'} }
+
+      it 'calls comparemiq' do
+        expect(controller).to receive(:comparemiq)
+        controller.send(:button)
+      end
+    end
+
+    context 'Edit selected VM' do
+      before do
+        allow(controller).to receive(:render_or_redirect_partial)
+        controller.params = {:pressed => 'vm_edit'}
+      end
+
+      it 'calls edit_record' do
+        expect(controller).to receive(:edit_record)
+        controller.send(:button)
+      end
+    end
+
+    context 'Rename selected VM' do
+      before do
+        allow(controller).to receive(:performed?).and_return(true)
+        controller.params = {:pressed => 'vm_rename'}
+      end
+
+      it 'calls vm_rename' do
+        expect(controller).to receive(:vm_rename)
+        controller.send(:button)
+      end
+    end
+
+    context 'Set ownership for selected VMs' do
+      before { controller.params = {:pressed => 'vm_ownership'} }
+
+      it 'calls set_ownership' do
+        expect(controller).to receive(:set_ownership)
+        controller.send(:button)
+      end
+    end
+
+    context 'Policy Simulation for selected VMs' do
+      before { controller.params = {:pressed => 'vm_policy_sim'} }
+
+      it 'calls polsimvms' do
+        expect(controller).to receive(:polsimvms)
+        controller.send(:button)
+      end
+    end
+
+    context 'Provision VMs' do
+      before do
+        allow(controller).to receive(:render_or_redirect_partial)
+        controller.params = {:pressed => 'vm_miq_request_new'}
+      end
+
+      it 'calls prov_redirect' do
+        expect(controller).to receive(:prov_redirect).with(no_args)
+        controller.send(:button)
+      end
+    end
+
+    context 'Clone VMs' do
+      before do
+        allow(controller).to receive(:render_or_redirect_partial)
+        controller.params = {:pressed => 'vm_clone'}
+      end
+
+      it 'calls prov_redirect with appropriate argument' do
+        expect(controller).to receive(:prov_redirect).with('clone')
+        controller.send(:button)
+      end
+    end
+
+    context 'Publish VM to a Template' do
+      before do
+        allow(controller).to receive(:render_or_redirect_partial)
+        controller.params = {:pressed => 'vm_publish'}
+      end
+
+      it 'calls prov_redirect with appropriate argument' do
+        expect(controller).to receive(:prov_redirect).with('publish')
+        controller.send(:button)
+      end
+    end
+
+    context 'Retire VMs' do
+      before do
+        allow(controller).to receive(:performed?).and_return(true)
+        allow(controller).to receive(:show)
+        controller.params = {:pressed => 'vm_retire_now'}
+      end
+
+      it 'calls retirevms_now' do
+        expect(controller).to receive(:retirevms_now)
+        controller.send(:button)
+      end
+    end
+
+    context 'Shutdown Guest of a VM' do
+      before do
+        allow(controller).to receive(:performed?).and_return(true)
+        allow(controller).to receive(:show)
+        controller.params = {:pressed => 'vm_guest_shutdown'}
+      end
+
+      it 'calls guestshutdown' do
+        expect(controller).to receive(:guestshutdown)
+        controller.send(:button)
+      end
+    end
+
+    context 'Restart Guest of a VM' do
+      before do
+        allow(controller).to receive(:performed?).and_return(true)
+        allow(controller).to receive(:show)
+        controller.params = {:pressed => 'vm_guest_restart'}
+      end
+
+      it 'calls guestshutdown' do
+        expect(controller).to receive(:guestreboot)
+        controller.send(:button)
+      end
+    end
+
+    %w[delete refresh reset retire scan start stop suspend].each do |action|
+      context "#{action} for selected VMs displayed in a nested list" do
+        before { controller.params = {:pressed => "vm_#{action}"} }
+
+        it "calls #{action + 'vms'} method" do
+          allow(controller).to receive(:show)
+          allow(controller).to receive(:performed?).and_return(true)
+          expect(controller).to receive((action + 'vms').to_sym)
+          controller.send(:button)
+          expect(controller.send(:flash_errors?)).not_to be_truthy
+        end
+      end
+    end
+
+    context 'deleting Resource Pool' do
+      before do
+        allow(controller).to receive(:performed?).and_return(true)
+        controller.instance_variable_set(:@lastaction, 'show_list')
+        controller.instance_variable_set(:@display, nil)
+        controller.params = {:pressed => 'resource_pool_delete'}
+      end
+
+      it 'calls deleteresourcepools and replace_gtl_main_div' do
+        expect(controller).to receive(:deleteresourcepools)
+        expect(controller).to receive(:replace_gtl_main_div)
+        controller.send(:button)
+      end
+
+      context 'default div to refresh' do
+        before do
+          allow(controller).to receive(:deleteresourcepools)
+          allow(controller).to receive(:replace_gtl_main_div)
+        end
+
+        it 'sets @refresh_div to main div' do
+          controller.send(:button)
+          expect(controller.instance_variable_get(:@refresh_div)).to eq('main_div')
+        end
+      end
+
+      context 'nested list of Resource Pools' do
+        before do
+          allow(controller).to receive(:performed?).and_return(false)
+          controller.instance_variable_set(:@display, 'resource_pools')
+        end
+
+        it 'calls deleteresourcepools and render_flash' do
+          expect(controller).to receive(:deleteresourcepools)
+          expect(controller).to receive(:render_flash)
+          controller.send(:button)
+        end
+      end
+    end
+
+    context 'managing policies of Resource Pools' do
+      before do
+        controller.instance_variable_set(:@display, nil)
+        controller.params = {:pressed => 'resource_pool_protect'}
+      end
+
+      it 'calls assign_policies' do
+        expect(controller).to receive(:assign_policies).with(ResourcePool)
+        controller.send(:button)
+      end
+
+      context 'default div to refresh' do
+        before { allow(controller).to receive(:assign_policies) }
+
+        it 'sets @refresh_div to main div' do
+          controller.send(:button)
+          expect(controller.instance_variable_get(:@refresh_div)).to eq('main_div')
+        end
+      end
+    end
+
+    context 'tagging Resource Pool' do
+      before do
+        controller.instance_variable_set(:@display, nil)
+        controller.params = {:pressed => 'resource_pool_tag'}
+      end
+
+      it 'calls tag method' do
+        expect(controller).to receive(:tag).with(ResourcePool)
         controller.send(:button)
       end
     end
