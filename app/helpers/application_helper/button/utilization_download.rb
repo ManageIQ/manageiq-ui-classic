0b6d05b10c9bbdd001687b0776eab2b2307b4a7a
Zita Nemeckova <znemecko@redhat.com>
2020-01-20 11:29:51 +0100

    Move visibility of toolbar to the button. Prevents error when toolbar is rendered from js.
    Closes https://github.com/ManageIQ/manageiq-ui-classic/issues/6608

--- app/helpers/application_helper/button/utilization_download.rb
+++ app/helpers/application_helper/button/utilization_download.rb
@@ -1,4 +1,8 @@
 class ApplicationHelper::Button::UtilizationDownload < ApplicationHelper::Button::Basic
+  def visible?
+    @sb[:active_tab] == 'report'
+  end
+
   def disabled?
     # to enable the button we are in the "Utilization" and have trend report
     return false if @layout == 'miq_capacity_utilization' && @sb[:active_tab] == 'report' && @sb.fetch_path(:trend_rpt, :table, :data).present?
