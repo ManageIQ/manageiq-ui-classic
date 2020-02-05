cf9f28288edd8c257a16a428e898a477666883ce
Zita Nemeckova <znemecko@redhat.com>
2020-02-05 09:36:52 +0100

    Move it to it's own test suite

--- tools/ci/after_install.sh
+++ tools/ci/after_install.sh
@@ -12,20 +12,3 @@ if [ "$TEST_SUITE" = "spec:javascript" ]; then
   yarn list
   echo "travis_fold:end:YARN_LOCK"
 fi
-
-# Run only against PR that is based on master
-if [ "$TEST_SUITE" = "spec" -a "$TRAVIS_PULL_REQUEST" != "false" -a "$TRAVIS_BRANCH" = "master" ]; then
-  OLD=`mktemp`
-  NEW=`mktemp`
-
-  bundle exec debride --rails app/controllers/ | cut -d ":" -f1 > "$NEW"
-
-  git checkout  master -- app/
-
-  bundle exec debride --rails app/controllers/ | cut -d ":" -f1 > "$OLD"
-
-  echo "New possibly dead methods"
-
-  diff -Naur "$OLD" "$NEW" | grep '^+ '
-fi
-
