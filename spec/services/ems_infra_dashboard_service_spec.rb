e6aed35300b71c64b1d44c2ec0b9d091f60a8f60
Milan Zázrivec <mzazrivec@redhat.com>
2020-02-19 10:52:53 +0100

    Merge pull request #6544 from lpichler/remove_duplicates_in_infra_dasjboard
     Limit metric rollups to just default time profile
    
    (cherry picked from commit 56f20a0312713e82011d4cf26cf93d92a930377e)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1776684

--- spec/services/ems_infra_dashboard_service_spec.rb
+++ spec/services/ems_infra_dashboard_service_spec.rb
@@ -25,5 +25,33 @@ describe EmsInfraDashboardService do
       expect(subject.recent_hosts_data[:recentResources][:xData].count).to eq(2)
       expect(subject.recent_hosts_data[:recentResources][:yData].count).to eq(2)
     end
+
+    describe "#ems_utilization" do
+      let(:user_admin)         { FactoryBot.create(:user_admin) }
+      let(:rollup_timestamp)   { Time.zone.parse("2016-01-12T00:00:00.00000000") }
+      let(:time_profile)       { FactoryBot.create(:time_profile_utc) }
+      let(:other_region)       { FactoryBot.create(:miq_region) }
+      let(:other_time_profile) { FactoryBot.create(:time_profile_utc, :in_other_region, :other_region => other_region) }
+
+      before do
+        EvmSpecHelper.create_guid_miq_server_zone
+        User.current_user = user_admin
+        Timecop.travel(rollup_timestamp)
+        MiqRegion.seed
+        FactoryGirl.create(:metric_rollup, :resource => @ems1, :capture_interval_name => 'daily', :derived_memory_used => 2.kilobytes, :timestamp => rollup_timestamp, :time_profile => time_profile)
+        FactoryGirl.create(:metric_rollup, :resource => @ems1, :capture_interval_name => 'daily', :derived_memory_used => 2.kilobytes, :timestamp => rollup_timestamp, :time_profile => other_time_profile)
+      end
+
+      after do
+        Timecop.return
+      end
+
+      subject { EmsInfraDashboardService.new(@ems1.id, User, EmsInfra) }
+
+      it "skips duplicate metric rollups for calculation" do
+        utilization_data = subject.ems_utilization
+        expect(utilization_data[:memory][:used]).to eq(2)
+      end
+    end
   end
 end
