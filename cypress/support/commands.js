21a82058db1362a2b6f7f1bbb0b185cdb4bc71c8
Zita Nemeckova <znemecko@redhat.com>
2020-03-06 15:53:57 +0100

    Remove old specific GTL click commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -189,18 +189,6 @@ Cypress.Commands.add("gtl_no_record", () => {
   return cy.get('#miq-gtl-view > div.no-record').should('be.visible');
 });
 
-Cypress.Commands.add("gtl_grid_click", (name) => {
-  return cy.get('div[pf-card-view] > .card-view-pf > .card').contains(name).click();
-});
-
-Cypress.Commands.add("gtl_tile_click", (name) => {
-  return cy.get('div[pf-card-view] > .card-view-pf > .card').contains(name).click();
-});
-
-Cypress.Commands.add("gtl_list_click", (name) => {
-  return cy.get('#miq-gtl-view > miq-data-table > div > table').contains(name).click();
-});
-
 Cypress.Commands.add("gtl_click", (name) => {
   cy.get('#miq-gtl-view').then($gtlTile => {
     if ($gtlTile.find("miq-tile-view").length > 0) {
