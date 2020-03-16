fb5ba94ce67d08d5f6f147fb0bdf8047e23a6c75
Martin Hradil <mhradil@redhat.com>
2020-03-16 16:03:01 +0000

    workers-form: generate schema from a list of workers and allowed settings
    the schema should be identical, except that the sub-form names (unused but required) use the common field name prefix (`reporting_worker`), instead of a custom name like `reportingWorkers`.
    
    And that the free space created in #6767 gets filled by shifting the following workers.

--- app/javascript/components/workers-form/workers.schema.js
+++ app/javascript/components/workers-form/workers.schema.js
@@ -3,219 +3,142 @@ import {
   injectOption, generateBasicOptions, generateRange, generateRefreshOptions,
 } from './helpers';
 
-function addSchema(formValues) {
-  const basicOptions = generateBasicOptions();
-  const rangeNine = generateRange(10);
-  const rangeFour = generateRange(5);
-  const rangeFive = generateRange(6);
+// worker counts
+const range4 = generateRange(5); // 0..4
+const range5 = generateRange(6); // 0..5
+const range9 = generateRange(10); // 0..9
 
-  const fields = [{
-    component: 'dual-group',
-    name: 'group1',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'genericWorkers',
-        title: __('Generic Workers'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'generic_worker.count',
-            options: injectOption(rangeNine, formValues.generic_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'generic_worker.memory_threshold',
-            options: injectOption(basicOptions, formValues.generic_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      }, {
-        component: componentTypes.SUB_FORM,
-        name: 'priorityWorkers',
-        title: __('Priority Workers'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'priority_worker.count',
-            options: injectOption(rangeNine, formValues.priority_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'priority_worker.memory_threshold',
-            options: injectOption(basicOptions, formValues.priority_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      },
-    ],
-  }, {
-    component: 'dual-group',
-    name: 'group3',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'cuCollectors',
-        title: __('C & U Data Collectors'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'ems_metrics_collector_worker.defaults.count',
-            options: injectOption(rangeNine, formValues.ems_metrics_collector_worker.defaults.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'ems_metrics_collector_worker.defaults.memory_threshold',
-            options: injectOption(basicOptions, formValues.ems_metrics_collector_worker.defaults.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      }, {
-        component: componentTypes.SUB_FORM,
-        name: 'cuProccesors',
-        title: __('C & U Data Processors'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'ems_metrics_processor_worker.count',
-            options: injectOption(rangeFour, formValues.ems_metrics_processor_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'ems_metrics_processor_worker.memory_threshold',
-            options: injectOption(basicOptions, formValues.ems_metrics_processor_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      },
-    ],
-  }, {
-    component: 'dual-group',
-    name: 'group4',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'eventMonitor',
-        title: __('Event Monitor'),
-        fields: [{
-          component: componentTypes.SELECT,
-          name: 'event_catcher.memory_threshold',
-          options: injectOption(generateRefreshOptions(false, 100, 500), formValues.event_catcher.memory_threshold),
-          label: __('Memory threshold'),
-        },
-        ],
-      }, {
-        component: componentTypes.SUB_FORM,
-        name: 'refresh',
-        title: __('Refresh'),
-        fields: [{
-          component: componentTypes.SELECT,
-          name: 'ems_refresh_worker.defaults.memory_threshold',
-          options: injectOption(generateRefreshOptions(), formValues.ems_refresh_worker.defaults.memory_threshold),
-          label: __('Memory threshold'),
-        },
-        ],
-      },
-    ],
-  },
+// memory thresholds
+const mtBasic = generateBasicOptions(); // 200M, 250M ... 500M, 600M ... 1.5G
+const mtEventCatcher = generateRefreshOptions(false, 100, 500); // 500M, 600M ... 3G, 3.5G ... 10G
+const mtEmsRefresh = generateRefreshOptions(); // 200M, 250M ... 600M, 700M ... 3G, 3.5G ... 10G
+const mtSmartProxy = generateRefreshOptions(true); // 200M, 250M ... 600M, 700M ... 1.9G
+
+const workers = [
   {
-    component: 'dual-group',
-    name: 'group5',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'vmAnalysisCollectors',
-        title: __('VM Analysis Collectors'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'smart_proxy_worker.count',
-            options: injectOption(rangeFive, formValues.smart_proxy_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'smart_proxy_worker.memory_threshold',
-            options: injectOption(generateRefreshOptions(true), formValues.smart_proxy_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      },
-    ],
-  },
-  {
-    component: 'dual-group',
-    name: 'group6',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'uiWorker',
-        title: __('UI Worker'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'ui_worker.count',
-            options: injectOption(rangeNine, formValues.ui_worker.count, true),
-            label: __('Count'),
-            helperText: __('Changing the UI Workers Count will immediately restart the webserver'),
-          },
-        ],
-      }, {
-        component: componentTypes.SUB_FORM,
-        name: 'remoteWorkers',
-        title: __('Remote Console Workers'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'remote_console_worker.count',
-            options: injectOption(rangeNine, formValues.remote_console_worker.count, true),
-            label: __('Count'),
-          },
-        ],
-      },
-    ],
+    name: 'generic_worker',
+    title: __('Generic Workers'),
+    options: {
+      count: range9,
+      memory_threshold: mtBasic,
+    },
   }, {
-    component: 'dual-group',
-    name: 'group7',
-    fields: [
-      {
-        component: componentTypes.SUB_FORM,
-        name: 'reportingWorkers',
-        title: __('Reporting Workers'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'reporting_worker.count',
-            options: injectOption(rangeNine, formValues.reporting_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'reporting_worker.memory_threshold',
-            options: injectOption(basicOptions, formValues.reporting_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      }, {
-        component: componentTypes.SUB_FORM,
-        name: 'webServiceWorkers',
-        title: __('Web Service Workers'),
-        fields: [
-          {
-            component: componentTypes.SELECT,
-            name: 'web_service_worker.count',
-            options: injectOption(rangeNine, formValues.web_service_worker.count, true),
-            label: __('Count'),
-          }, {
-            component: componentTypes.SELECT,
-            name: 'web_service_worker.memory_threshold',
-            options: injectOption(basicOptions, formValues.web_service_worker.memory_threshold),
-            label: __('Memory threshold'),
-          },
-        ],
-      },
-    ],
-  },
-  ];
-  return { fields };
+    name: 'ems_metrics_collector_worker.defaults',  // .defaults?
+    title: __('C & U Data Collectors'),
+    options: {
+      count: range9,
+      memory_threshold: mtBasic,
+    },
+  }, {
+    name: 'event_catcher',
+    title: __('Event Monitor'),
+    options: {
+      memory_threshold: mtEventCatcher,
+    },
+  }, {
+    name: 'smart_proxy_worker',
+    title: __('VM Analysis Collectors'),
+    options: {
+      count: range5,
+      memory_threshold: mtSmartProxy,
+    },
+  }, {
+    name: 'ui_worker',
+    title: __('UI Worker'),
+    options: {
+      count: range9,
+      countHelperText: __('Changing the UI Workers Count will immediately restart the webserver'),
+    },
+  }, {
+    name: 'reporting_worker',
+    title: __('Reporting Workers'),
+    options: {
+      count: range9,
+      memory_threshold: mtBasic,
+    },
+  }, {
+    name: 'priority_worker',
+    title: __('Priority Workers'),
+    options: {
+      count: range9,
+      memory_threshold: mtBasic,
+    },
+  }, {
+    name: 'ems_metrics_processor_worker',
+    title: __('C & U Data Processors'),
+    options: {
+      count: range4,
+      memory_threshold: mtBasic,
+    },
+  }, {
+    name: 'ems_refresh_worker.defaults',  // .defaults?
+    title: __('Refresh'),
+    options: {
+      memory_threshold: mtEmsRefresh,
+    },
+  }, {
+    name: 'remote_console_worker',
+    title: __('Remote Console Workers'),
+    options: {
+      count: range9,
+    },
+  }, {
+    name: 'web_service_worker',
+    title: __('Web Service Workers'),
+    options: {
+      count: range9,
+      memory_threshold: mtBasic,
+    },
+  }
+];
+
+
+const countField = ({ name, options: { count: range, countHelperText: helperText } }, formValues) => ({
+  component: componentTypes.SELECT,
+  name: `${name}.count`,
+  options: injectOption(range, _.get(formValues, `${name}.count`), true),
+  label: __('Count'),
+  helperText,
+});
+
+const memoryThresholdField = ({ name, options: { memory_threshold: mtOptions } }, formValues) => ({
+  component: componentTypes.SELECT,
+  name: `${name}.memory_threshold`,
+  options: injectOption(mtOptions, _.get(formValues, `${name}.memory_threshold`)),
+  label: __('Memory threshold'),
+});
+
+
+const workerSubForm = (worker, formValues) => ({
+  component: componentTypes.SUB_FORM,
+  name: worker.name,
+  title: worker.title,
+  fields: _.compact([
+    worker.options.count && countField(worker, formValues),
+    worker.options.memory_threshold && memoryThresholdField(worker, formValues),
+  ]),
+});
+
+// [ a, b, c, d, e ] -> [ [ a, d ], [ b, e ], [ c, undefined ] ]
+const pairs = (array) => {
+  let [left, right] = _.chunk(array, Math.ceil(array.length / 2));
+  return _.zip(left, right);
+};
+
+const pairGroup = (fields, groupOptions) => ({
+  component: 'dual-group',
+  name: `group${groupOptions.index++}`,
+  fields,
+});
+
+function addSchema(formValues) {
+  const groupOptions = {
+    index: 1,
+  };
+  const workerForms = workers.map((worker) => workerSubForm(worker, formValues));
+  const dualGroups = pairs(workerForms).map((pair) => pairGroup(_.compact(pair), groupOptions));
+
+  return { fields: dualGroups };
 }
 
 export default addSchema;
