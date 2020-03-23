2bb69d70df847f83d3763c83d23b6a665e346cba
Hilda Stastna <hstastna@redhat.com>
2020-03-23 20:45:48 +0100

    Replace %div_for_paging for detaching Cloud Volume from an Instance
    Render angular 'custom_form_buttons_controller_as' partial for buttons instead,
    hide unnecessary form_buttons_div and paging_div.

--- app/assets/javascripts/controllers/vm_cloud/vm_cloud_detach_form_controller.js
+++ app/assets/javascripts/controllers/vm_cloud/vm_cloud_detach_form_controller.js
@@ -5,22 +5,23 @@ ManageIQ.angular.app.controller('vmCloudDetachFormController', ['$scope', 'vmClo
   vm.formId = vmCloudDetachFormId;
   vm.afterGet = false;
   vm.modelCopy = angular.copy(vm.vmCloudModel);
+  vm.saveable = miqService.saveable;
 
   ManageIQ.angular.scope = $scope;
 
-  $scope.submitClicked = function() {
+  vm.submitClicked = $scope.submitClicked = function() {
     miqService.sparkleOn();
     var url = '/vm_cloud/detach_volume/' + vmCloudDetachFormId + '?button=detach';
     miqService.miqAjaxButton(url, vm.vmCloudModel, {complete: false});
   };
 
-  $scope.cancelClicked = function() {
+  vm.cancelClicked = $scope.cancelClicked = function() {
     miqService.sparkleOn();
     var url = '/vm_cloud/detach_volume/' + vmCloudDetachFormId + '?button=cancel';
     miqService.miqAjaxButton(url);
   };
 
-  $scope.resetClicked = function() {
+  vm.resetClicked = $scope.resetClicked = function() {
     vm.vmCloudModel = angular.copy(vm.modelCopy);
     miqService.miqFlash('warn', __('All changes have been reset'));
   };
