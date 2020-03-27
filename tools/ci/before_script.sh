1b27484d15d37879e0d4a8e388dfa7a80a685c81
Martin Hradil <mhradil@redhat.com>
2020-03-27 14:16:55 +0000

    before_script: only run for JS tests and compile

--- tools/ci/before_script.sh
+++ tools/ci/before_script.sh
@@ -1,5 +1,8 @@
-# make sure yarn is installed, in the right version
-bundle exec rake webpacker:check_yarn || npm install -g yarn
+# not spec, and not cross repo
+if [ "$TEST_SUITE" = "spec:javascript" -o "$TEST_SUITE" = "spec:jest" -o "$TEST_SUITE" = "spec:compile" ]; then
+  # make sure yarn is installed, in the right version
+  bundle exec rake webpacker:check_yarn || npm install -g yarn
 
-# install & compile dependencies
-bundle exec rake update:ui
+  # install & compile dependencies
+  bundle exec rake update:ui
+fi
