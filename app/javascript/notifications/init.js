d633a9838488127aeb9d8c67a59c6de42e3c87ec
Dávid Halász <dhalasz@redhat.com>
2020-04-03 17:53:29 +0200

    Consume actioncable from NPM, move notifications code to modern JS

--- app/javascript/notifications/init.js
+++ app/javascript/notifications/init.js
@@ -1,12 +1,22 @@
+import ActionCable from 'actioncable';
+
+import { API } from '../http_api';
 import { addNotification, initNotifications } from '../miq-redux/actions/notifications-actions';
-import { listenToRx } from '../miq_observable.js';
 
 export function init() {
   ManageIQ.redux.store.dispatch(initNotifications(true));
 
-  listenToRx((data) => {
-    if (data.notification) {
-      ManageIQ.redux.store.dispatch(addNotification(data.notification));
-    }
+  const cable = ActionCable.createConsumer('/ws/notifications');
+
+  cable.subscriptions.create('NotificationChannel', {
+    disconnected: () => {
+      API.ws_init().then(null, () => {
+        console.warn('Unable to retrieve a valid ws_token!');
+        cable.connection.close({ allowReconnect: false });
+      });
+    },
+    received: (data) => {
+      ManageIQ.redux.store.dispatch(addNotification(data));
+    },
   });
 }
