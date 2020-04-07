0077a0b3fcfe5bcbb50acb7337b2586d0bdb608c
Martin Hradil <mhradil@redhat.com>
2020-04-07 17:53:34 +0000

    dialog-editor: finish renames, fix index imports
    and drop new from components

--- app/javascript/dialog-editor/index.js
+++ app/javascript/dialog-editor/index.js
@@ -1,9 +1,19 @@
+import { Box } from './box.component.js';
+import { DialogEditor } from './dialog-editor.component.js';
+import { DialogEditorService } from './dialog-editor.service.js';
+import { DialogValidationService } from './dialog-validation.service.js';
+import { Field } from './field.component.js';
+import { Modal } from './modal.component.js';
 import { ModalBox } from './modal-box.component.js';
 import { ModalField } from './modal-field.component.js';
+import { ModalFieldTemplate } from './modal-field-template.component.js';
 import { ModalTab } from './modal-tab.component.js';
+import { TabList } from './tab-list.component.js';
+import { Toolbox } from './toolbox.component.js';
+import { TreeSelector } from './tree-selector.component.js';
+import { Validation } from './validation.component.js';
 
 import './styles/index.scss';
-
 
 angular.module('miq.dialogEditor', [
   'frapontillo.bootstrap-switch',
@@ -11,51 +21,18 @@ angular.module('miq.dialogEditor', [
   'ngDragDrop',
   'ui.sortable',
 ])
-  .component('dialogEditor', new DialogEditor)
-  .component('dialogEditorBoxes', new Box)
-  .component('dialogEditorField', new Field)
-  .component('dialogEditorToolbox', Toolbox)
-  .component('dialogEditorModal', new Modal)
+  .component('dialogEditor', DialogEditor)
+  .component('dialogEditorBoxes', Box) // TODO rename
+  .component('dialogEditorField', Field)
+  .component('dialogEditorModal', Modal)
   .component('dialogEditorModalBox', ModalBox)
   .component('dialogEditorModalField', ModalField)
   .component('dialogEditorModalFieldTemplate', ModalFieldTemplate)
   .component('dialogEditorModalTab', ModalTab)
-  .component('dialogEditorTabs', new TabList)
+  .component('dialogEditorTabs', TabList) // TODO rename
+  .component('dialogEditorToolbox', Toolbox)
   .component('dialogEditorTreeSelector', TreeSelector)
   .component('dialogEditorValidation', Validation)
   .service('DialogEditor', DialogEditorService)
   .service('DialogValidation', DialogValidationService)
 ;
-
-//TODO fix imports and names, and files
-
-// components/box/index.ts
-import Box from './box.component.js';
-
-// components/dialog-editor/index.ts
-import DialogEditor from './dialogEditor.component.js';
-
-// components/field/index.ts
-import Field from './field.component.js';
-
-// components/modal/index.ts
-import Modal from './modal.component.js';
-
-// components/modal-field-template/index.ts
-import { ModalFieldTemplate } from './modalFieldTemplate.component.js';
-
-// components/tab-list/index.ts
-import TabList from './tabList.component.js';
-
-// components/toolbox/index.ts
-import { Toolbox } from './toolbox.component.js';
-
-// components/tree-selector/index.ts
-import TreeSelector from './treeSelector';
-
-// components/validation/index.ts
-import Validation from './validation';
-
-// services/index.ts
-import DialogEditorService from './dialogEditorService';
-import DialogValidationService from './dialogValidationService';
