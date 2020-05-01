381b955c73c8ca6dbca01d6f1cacc0bf744ae3e6
Martin Hradil <mhradil@redhat.com>
2020-05-01 18:02:55 +0000

    react helper - allow for custom id
    without this, ids get silently ignored for =react

--- app/helpers/reactjs_helper.rb
+++ app/helpers/reactjs_helper.rb
@@ -1,6 +1,6 @@
 module ReactjsHelper
   def react(name, data = {}, attributes = {}, element = 'div')
-    uid = unique_html_id('react')
+    uid = attributes[:id] || unique_html_id('react')
     capture do
       concat(content_tag(element, nil, attributes.merge(:id => uid)))
       concat(javascript_tag("ManageIQ.component.componentFactory('#{j(name)}', '##{j(uid)}', #{data.to_json})"))
