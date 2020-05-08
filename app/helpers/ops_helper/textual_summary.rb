70a266c93ea980ec4780b7432a018b4c5a689ede
Harpreet Kataria <hkataria@redhat.com>
2020-05-08 14:48:46 -0400

    Deleted more files and code referencing to Vmdb* models
    Fixes #6995

--- app/helpers/ops_helper/textual_summary.rb
+++ app/helpers/ops_helper/textual_summary.rb
@@ -5,18 +5,6 @@ module OpsHelper::TextualSummary
 
   TOP_TABLES_BY_COUNT = 5
 
-  def textual_group_vmdb_connection_properties
-    return if @record.nil?
-
-    TextualGroup.new(
-      _("Properties"),
-      %i[
-        vmdb_connection_name vmdb_connection_ipaddress vmdb_connection_vendor vmdb_connection_version
-        vmdb_connection_data_directory vmdb_connection_data_disk vmdb_connection_last_start_time
-      ]
-    )
-  end
-
   def textual_group_properties
     TextualTags.new(_('Properties'), %i[description parent groups subtenant project])
   end
@@ -47,112 +35,9 @@ module OpsHelper::TextualSummary
     TextualListview.new_from_hash(textual_vmdb_tables_most_wasted_space)
   end
 
-  def textual_group_vmdb_connection_capacity_data
-    return if @record.nil?
-
-    TextualGroup.new(
-      _("Capacity Data"),
-      %i[
-        vmdb_connection_timestamp vmdb_connection_total_space vmdb_connection_used_space vmdb_connection_free_space
-        vmdb_connection_total_index_nodes vmdb_connection_used_index_nodes vmdb_connection_free_index_nodes
-      ]
-    )
-  end
-
   #
   # Items
   #
-
-  def textual_vmdb_connection_name
-    {:label => _("Name"), :value => @record.name}
-  end
-
-  def textual_vmdb_connection_ipaddress
-    {:label => _("IP Address"), :value => @record.ipaddress}
-  end
-
-  def textual_vmdb_connection_vendor
-    {:label => _("Vendor"), :value => @record.vendor}
-  end
-
-  def textual_vmdb_connection_version
-    {:label => _("Version"), :value => @record.version}
-  end
-
-  def textual_vmdb_connection_data_directory
-    {:label => _("Data Directory"), :value => @record.data_directory}
-  end
-
-  def textual_vmdb_connection_data_disk
-    {:label => _("Data Disk"), :value => @record.data_disk}
-  end
-
-  def textual_vmdb_connection_last_start_time
-    {:label => _("Last Start Time"), :value => format_timezone(@record.last_start_time)}
-  end
-
-  def textual_vmdb_connection_timestamp
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Last Collection"), :value => metrics && format_timezone(metrics.timestamp)}
-  end
-
-  def textual_vmdb_connection_total_space
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Total Space on Volume"), :value => metrics && number_to_human_size(metrics.disk_total_bytes)}
-  end
-
-  def textual_vmdb_connection_free_space
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Free Space on Volume"), :value => metrics && number_to_human_size(metrics.disk_free_bytes)}
-  end
-
-  def textual_vmdb_connection_used_space
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Used Space on Volume"), :value => metrics && number_to_human_size(metrics.disk_used_bytes)}
-  end
-
-  def textual_vmdb_connection_total_index_nodes
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Total Index Nodes"), :value => metrics && number_with_delimiter(metrics.disk_total_inodes)}
-  end
-
-  def textual_vmdb_connection_used_index_nodes
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Used Index Nodes"), :value => metrics && number_with_delimiter(metrics.disk_used_inodes)}
-  end
-
-  def textual_vmdb_connection_free_index_nodes
-    metrics = VmdbDatabase.my_database.latest_hourly_metric
-    {:label => _("Free Index Nodes"), :value => metrics && number_with_delimiter(metrics.disk_free_inodes)}
-  end
-
-  def textual_vmdb_tables_most_rows
-    {
-      :title     => _("Tables with the Most Rows"),
-      :headers   => [_("Name"), _("Rows")],
-      :col_order => %w[name value],
-      :value     => vmdb_table_top_rows(:rows, TOP_TABLES_BY_COUNT)
-    }
-  end
-
-  def textual_vmdb_tables_largest_size
-    {
-      :title     => _("Largest Tables"),
-      :headers   => [_("Name"), _("Size")],
-      :col_order => %w[name value],
-      :value     => vmdb_table_top_rows(:size, TOP_TABLES_BY_COUNT)
-    }
-  end
-
-  def textual_vmdb_tables_most_wasted_space
-    {
-      :title     => _("Tables with Most Wasted Space"),
-      :headers   => [_("Name"), _("Wasted")],
-      :col_order => %w[name value],
-      :value     => vmdb_table_top_rows(:wasted_bytes, TOP_TABLES_BY_COUNT)
-    }
-  end
-
   def textual_description
     {:label => _('Description'), :value => @record.description}
   end
@@ -274,20 +159,6 @@ module OpsHelper::TextualSummary
       )
     end
     subprojects
-  end
-
-  def vmdb_table_top_rows(typ, limit)
-    rows = VmdbDatabase.my_database.top_tables_by(typ, limit)
-    rows.collect do |row|
-      {
-        :title    => row.name,
-        :name     => row.name,
-        :value    => typ == :rows ? number_with_delimiter(row.latest_hourly_metric.send(typ.to_s), :delimeter => ',') :
-                                 number_to_human_size(row.latest_hourly_metric.send(typ.to_s), :precision => 1),
-        :explorer => true,
-        :link     => "tb-#{row.id}"
-      }
-    end
   end
 
   def textual_tenant_quota_allocations
--- spec/helpers/ops_helper/textual_summary_spec.rb
+++ spec/helpers/ops_helper/textual_summary_spec.rb
@@ -3,26 +3,6 @@ describe OpsHelper::TextualSummary do
 
   before { instance_variable_set(:@record, record) }
 
-  include_examples "textual_group", "Properties", %i(
-    vmdb_connection_name
-    vmdb_connection_ipaddress
-    vmdb_connection_vendor
-    vmdb_connection_version
-    vmdb_connection_data_directory
-    vmdb_connection_data_disk
-    vmdb_connection_last_start_time
-  ), "vmdb_connection_properties"
-
-  include_examples "textual_group", "Capacity Data", %i(
-    vmdb_connection_timestamp
-    vmdb_connection_total_space
-    vmdb_connection_used_space
-    vmdb_connection_free_space
-    vmdb_connection_total_index_nodes
-    vmdb_connection_used_index_nodes
-    vmdb_connection_free_index_nodes
-  ), "vmdb_connection_capacity_data"
-
   context 'Tenant Textual Summary' do
     let(:record) { FactoryBot.create(:tenant) }
 
