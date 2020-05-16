751c4b3d658616c79f45f06e3159e08b1d4d1a49
Harpreet Kataria <hkataria@redhat.com>
2020-05-15 22:33:38 -0400

    Made tree building code generic

--- app/controllers/mixins/generic_list_mixin.rb
+++ app/controllers/mixins/generic_list_mixin.rb
@@ -8,6 +8,7 @@ module Mixins
       @showtype = nil
       @center_toolbar = self.class.toolbar_plural if self.class.toolbar_plural
       process_show_list
+      @tree = build_view_tree if @gtl_type == 'tree'
     end
   end
 end
