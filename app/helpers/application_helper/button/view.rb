9440ead421eab46e7871c8e836778de6b147fabb
Harpreet Kataria <hkataria@redhat.com>
2020-05-15 22:39:32 -0400

    An attempt to simplify setup and usage of `@gtl_buttons`
    - `@gtl_buttons` is only used in 2 places, first is in Catalog Explorer in Service Catalogs accordion, there is no Grid view button son that screen. Second is in Chargeback Rates list, there us no need for Tile/List view buttons as there is no need for Quadicons on that screen.
    - This PR also adds `show_tree_button?` method to determine whether a list view needs to display a tree view button.

--- app/helpers/application_helper/button/view.rb
+++ app/helpers/application_helper/button/view.rb
@@ -1,13 +1,7 @@
 class ApplicationHelper::Button::View < ApplicationHelper::Button::ButtonWithoutRbacCheck
-  # tree view GTL button is only visible on special screens
-  GTL_VIEW_TREE_BUTTON = [
-    'chargeback_rate'
-  ]
-
   def visible?
-    return false if self[:id] == "view_tree" && !GTL_VIEW_TREE_BUTTON.include?(@layout)
-    # only hide gtl button if they are not in @gtl_buttons
-    !@gtl_buttons || @gtl_buttons.include?(self[:id].to_s)
+    return false if self[:id] == "view_tree" && !@show_tree_button
+    @gtl_buttons.include?(self[:id].to_s)
   end
 
   def disabled?
