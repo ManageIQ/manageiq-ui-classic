b74c99cb9ec5f56427dbdd37a492118f4d788628
Adam Grare <agrare@redhat.com>
2020-05-19 14:34:55 -0400

    Fix cloud_volume_backup_restore

--- app/helpers/application_helper/button/cloud_volume_backup_restore.rb
+++ app/helpers/application_helper/button/cloud_volume_backup_restore.rb
@@ -1,8 +1,7 @@
 class ApplicationHelper::Button::CloudVolumeBackupRestore < ApplicationHelper::Button::ButtonNewDiscover
   def disabled?
     ManageIQ::Providers::CloudManager.none? do |ems|
-      Module.const_defined?("#{ems.class}::CloudVolume") &&
-        ems.class::CloudVolume.supports?(:backup_restore)
+      "#{ems.class}::CloudVolume".safe_constantize&.supports?(:backup_restore)
     end
   end
 end
