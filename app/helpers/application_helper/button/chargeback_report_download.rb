1d74bbafad684a29738396b1cb6f28f71b065022
Harpreet Kataria <hkataria@redhat.com>
2020-05-22 10:28:30 -0400

    Removed ChargebackReportDownload` toolbar class
    default RBAC checks for buttons are being done in the base class.

--- app/helpers/application_helper/button/chargeback_report_download.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationHelper::Button::ChargebackReportDownload < ApplicationHelper::Button::Basic
-    def role_allows_feature?
-      role_allows?(:feature => 'chargeback_reports')
-    end
-end
--- app/helpers/application_helper/toolbar/chargeback_report_center.rb
+++ app/helpers/application_helper/toolbar/chargeback_report_center.rb
@@ -12,21 +12,18 @@ class ApplicationHelper::Toolbar::ChargebackReportCenter < ApplicationHelper::To
           'fa fa-file-text-o fa-lg',
           N_('Download this report in text format'),
           N_('Download as Text'),
-          :klass => ApplicationHelper::Button::ChargebackReportDownload,
           :url   => "/render_txt"),
         button(
           :chargeback_reports_download_csv,
           'fa fa-file-text-o fa-lg',
           N_('Download this report in CSV format'),
           N_('Download as CSV'),
-          :klass => ApplicationHelper::Button::ChargebackReportDownload,
           :url   => "/render_csv"),
         button(
           :chargeback_reports_download_pdf,
           'pficon pficon-print fa-lg',
           N_('Print or export this report in PDF format'),
           N_('Print or export as PDF'),
-          :klass => ApplicationHelper::Button::Basic,
           :popup => true,
           :url   => "/render_pdf"),
       ]
