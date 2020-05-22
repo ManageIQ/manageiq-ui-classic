8f2deade1b4b0eeb026716f9606552a55993b348
Halász Dávid <skateman@users.noreply.github.com>
2020-05-22 16:08:57 +0200

    Merge pull request #7071 from himdel/menu-helper
    Menu link helper
    
    (cherry picked from commit 5a82d5e02a72b18aecde536ac7eb9f154b3f93b3)

---
+++ app/javascript/menu/item-type.js
@@ -0,0 +1,35 @@
+// there are 4 menu item types (used both in navbar and menu)
+// * default (href) - opens href
+// * big_iframe (id) - no menu, only navbar and ..a big iframe (external with our header)
+// * modal () - open the About Modal (extend for any modals)
+// * new_window (href) - opens href in new window (for external links)
+
+export const linkProps = ({type, href, id}) => ({
+  href: {
+    big_iframe: `/dashboard/iframe?id=${id}`,
+    default: href,
+    modal: undefined,
+    new_window: href,
+  }[type || 'default'],
+
+  target: (type === 'new_window' ? '_blank' : '_self'),
+
+  onClick: (event) => {
+    if (type === 'modal') {
+      sendDataWithRx({ type: 'showAboutModal' });
+      event.preventDefault();
+      return;
+    }
+
+    if (['default', 'big_iframe'].includes(type) && miqCheckForChanges() === false) {
+      event.preventDefault();
+      return;
+    }
+
+    if (href === '/dashboard/logout') {
+      ManageIQ.logoutInProgress = true;
+    }
+  },
+});
+
+export const itemId = (id, section) => (section ? `menu_section_${id}` : `menu_item_${id}`);
