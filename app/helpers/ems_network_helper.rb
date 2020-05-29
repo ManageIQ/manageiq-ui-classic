eb52249fc9ca2e0d8118a8778798009eee8d0544
Dávid Halász <dhalasz@redhat.com>
2020-05-29 14:47:12 +0200

    Replace the old network provider forms with the DDF-based ProviderForm

--- app/helpers/ems_network_helper.rb
+++ app/helpers/ems_network_helper.rb
@@ -1,3 +1,7 @@
 module EmsNetworkHelper
   include_concern 'TextualSummary'
+
+  def edit_redirect_path(lastaction, ems)
+    lastaction == 'show_list' ? ems_networks_path : ems_network_path(ems)
+  end
 end
