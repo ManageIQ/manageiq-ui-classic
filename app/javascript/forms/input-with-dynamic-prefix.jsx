a3d703defd9906f53f2c82bd2da61a9bff5c7631
Halász Dávid <skateman@users.noreply.github.com>
2020-06-05 11:52:08 +0200

    Merge pull request #7077 from ZitaNemeckova/fix_pxe_form
    Fix broken select in PXE server form
    
    (cherry picked from commit e0caf95e45e08e118e66de7f68d18cb8adf6f46f)

--- app/javascript/forms/input-with-dynamic-prefix.jsx
+++ app/javascript/forms/input-with-dynamic-prefix.jsx
@@ -62,6 +62,7 @@ const DataDrivenInputWithPrefix = ({
           </div>
           <div className="dynamic-prefix-input">
             <rawComponents.Select
+              classNamePrefix="ddorg__pf3-component-mapper__select"
               invalid={isRequired && !prefix}
               id={`dynamic-prefix-select-${rest.name}`}
               input={{
