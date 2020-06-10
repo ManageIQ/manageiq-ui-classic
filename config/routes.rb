ecbd5817887d0d0303d924b2386a8a02686a0de2
Halász Dávid <skateman@users.noreply.github.com>
2020-06-10 12:11:32 +0200

    Merge pull request #7114 from lpichler/remove_unused_routes_for_auth_key_pair_cloud_controller
    Remove unused routes from auth_key_pair cloud_controller
    
    (cherry picked from commit 4202fa9dd5f48b724305a95cd7969e79ac6cabb1)

--- config/routes.rb
+++ config/routes.rb
@@ -134,7 +134,6 @@ Rails.application.routes.draw do
         show
         show_list
         tagging_edit
-        tag_edit_form_field_changed
         ems_form_choices
         download_private_key
         ownership
@@ -144,7 +143,6 @@ Rails.application.routes.draw do
         button
         create
         dynamic_checkbox_refresh
-        form_field_changed
         listnav_search_selected
         ownership_update
         protect
