30556e136fedca4c6427b769a2bdabc5278ff2e8
Martin Hradil <mhradil@redhat.com>
2020-06-11 14:29:54 +0000

    Menu - update collapsed user icon
    replace initials with user avatar icon,
    and fix group icon color when collapsed

--- app/javascript/menu/username.jsx
+++ app/javascript/menu/username.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { SideNavItem } from 'carbon-components-react/es/components/UIShell';
+import { UserAvatar20 } from '@carbon/icons-react';
 
 export const Username = ({ applianceName, currentUser, expanded }) => {
   const title = `${currentUser.name} | ${currentUser.userid} | ${applianceName}`;
-  const initials = Array.from(currentUser.name).filter((x) => x.match(/\p{Upper}/u)).join('').substr(0, 3) || currentUser.name[0];
 
   return (
-    <SideNavItem className="padded menu-user vertical-center">
-      <span
-        className={expanded ? 'expanded' : 'collapsed'}
-        data-userid={currentUser.userid}
-        id="username_display"
-        title={title}
-      >
-        { expanded ? currentUser.name : initials }
-      </span>
-    </SideNavItem>
+    <div
+      className="menu-user"
+      data-userid={currentUser.userid}
+      title={title}
+    >
+      { expanded && (
+        <SideNavItem className="padded vertical-center">
+          <span>
+            {currentUser.name}
+          </span>
+        </SideNavItem>
+      )}
+      { expanded || (
+        <SideNavItem className="padded vertical-center">
+          <UserAvatar20 />
+        </SideNavItem>
+      )}
+    </div>
   );
 };
