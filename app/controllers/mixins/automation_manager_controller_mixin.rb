24f4544b7d11163193256a8f8b0279fbcf4f7492
Lucy Fu <lufu@redhat.com>
2020-06-15 12:51:49 -0400

    Enforce RBAC check on AutomationManagerController

--- app/controllers/mixins/automation_manager_controller_mixin.rb
+++ app/controllers/mixins/automation_manager_controller_mixin.rb
@@ -12,11 +12,13 @@ module Mixins
     end
 
     def show_list
+      assert_privileges('automation_manager_providers_view')
       flash_to_session
       redirect_to(:action => 'explorer')
     end
 
     def explorer
+      assert_privileges('automation_manager_providers_view')
       @explorer = true
       @lastaction = "explorer"
 
@@ -56,6 +58,7 @@ module Mixins
     end
 
     def change_tab
+      assert_privileges('automation_manager_providers_view')
       @sb[:active_tab] = params[:tab_id]
       replace_right_cell
     end
@@ -66,6 +69,7 @@ module Mixins
     end
 
     def cs_form_field_changed
+      assert_privileges('automation_manager_edit_provider')
       id = params[:id]
       return unless load_edit("cs_edit__#{id}", "replace_cell__explorer")
 
