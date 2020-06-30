3b8a1617d3fababfb8255d641d5f85e1d60e4139
Jason Frey <jfrey@redhat.com>
2020-06-30 13:46:36 -0400

    Merge branch 'MiqAeCustomizationController_privilege' into '5.12.z'
    Enforce RBAC check on MiqAeCustomizationController
    
    See merge request cloudforms/cfme-ui-classic!43
    
    (cherry picked from commit a7bef1619c5f32670b528c457f27fa1616f71cc8)

--- app/controllers/miq_ae_customization_controller/dialogs.rb
+++ app/controllers/miq_ae_customization_controller/dialogs.rb
@@ -7,6 +7,7 @@ module MiqAeCustomizationController::Dialogs
   end
 
   def dialog_list
+    assert_privileges('dialog_accord') # feature like miq_ae_customization_dialog_list is missing
     @lastaction = "dialog_list"
     @explorer = true
 
@@ -56,6 +57,7 @@ module MiqAeCustomizationController::Dialogs
   end
 
   def change_tab
+    assert_privileges('ab_button')
     get_node_info
     replace_right_cell(:nodetype => x_node)
   end
