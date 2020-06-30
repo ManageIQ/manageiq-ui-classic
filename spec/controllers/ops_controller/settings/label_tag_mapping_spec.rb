c2d3f1cf46c764057678fd2d24aa7457b9c21516
Libor Pichler <lpichler@redhat.com>
2020-06-30 11:42:35 +0200

    Add specs for mapping with all-entities

--- spec/controllers/ops_controller/settings/label_tag_mapping_spec.rb
+++ spec/controllers/ops_controller/settings/label_tag_mapping_spec.rb
@@ -23,6 +23,14 @@ describe OpsController do
       post :label_tag_mapping_edit, :params => { :button => 'add' }
     end
 
+    def use_form_to_create_all_entities_mapping(label_name, category)
+      post :label_tag_mapping_edit
+      post :label_tag_mapping_field_changed, :params => { :id => 'new', :entity => '_all_entities_' }
+      post :label_tag_mapping_field_changed, :params => { :id => 'new', :label_name => label_name }
+      post :label_tag_mapping_field_changed, :params => { :id => 'new', :category => category  }
+      post :label_tag_mapping_edit, :params => { :button => 'add' }
+    end
+
     it "creates new mapping on save" do
       use_form_to_create_mapping
       mapping = ContainerLabelTagMapping.last
@@ -41,6 +49,107 @@ describe OpsController do
       expect(mapping.label_value).to be nil
       expect(mapping.tag.classification.category?).to be true
       expect(mapping.tag.classification.description).to eq('Amazon Vms')
+    end
+
+    let!(:classification_department) { FactoryBot.create(:classification_department_with_tags) }
+    let(:label_name) { 'some-amazon-label' }
+    let(:all_entities) { '_all_entities_' }
+
+    it "doesn't create mapping for all entities when label is already mapped" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      errors = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }
+      expect(errors).to be_empty
+
+      controller.instance_variable_set(:@flash_array, nil)
+
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      error_message = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }.first[:message]
+      expect(error_message).to eq("Mapping for \"All Entities\", Label \"#{label_name}\" and Tag Category \"#{classification_department.name}\" already exists")
+    end
+
+    it "doesn't create mapping for all entities when category name doesn't exist" do
+      use_form_to_create_all_entities_mapping(label_name, "XXX")
+
+      error_message = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }.first[:message]
+      expect(error_message).to eq("Mapping for \"All Entities\", Label \"#{label_name}\": Tag Category \"XXX\" must exist")
+    end
+
+    let!(:classification_department) { FactoryBot.create(:classification_department_with_tags) }
+
+    it "doesn't create mapping for all entities when tag from label is already created" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      errors = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }
+      expect(errors).to be_empty
+
+      controller.instance_variable_set(:@flash_array, nil)
+
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      error_message = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }.first[:message]
+      expect(error_message).to eq("Mapping for \"All Entities\", Label \"#{label_name}\" and Tag Category \"#{classification_department.name}\" already exists")
+    end
+
+    it "creates new mapping for all entities on save" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+
+      mapping = ContainerLabelTagMapping.last
+      errors = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }
+      expect(errors).to be_empty
+      expect(mapping.labeled_resource_type).to eq(all_entities)
+      expect(mapping.label_name).to eq(label_name)
+      expect(mapping.label_value).to be nil
+      expect(mapping.tag.classification.category?).to be true
+      expect(mapping.tag.classification.description).to eq(classification_department.description)
+    end
+
+    let(:classification_cost_center) { FactoryBot.create(:classification_cost_center) }
+
+    it "can edit an existing mapping for all entities" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      mapping = ContainerLabelTagMapping.last
+
+      controller.instance_variable_set(:@flash_array, nil)
+
+      post :label_tag_mapping_edit, :params => { :id => mapping.id.to_s }
+      post :label_tag_mapping_field_changed, :params => { :id => mapping.id.to_s, :category => classification_cost_center.name }
+      post :label_tag_mapping_edit, :params => { :id => mapping.id.to_s, :button => 'save' }
+
+      errors = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }
+      expect(errors).to be_empty
+
+      mapping.reload
+
+      expect(mapping.tag.id).to eq(classification_cost_center.tag.id)
+    end
+
+    it "deletes mapping but it doesn't delete classifications and taggins for all entities" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      controller.instance_variable_set(:@flash_array, nil)
+
+      mapping = ContainerLabelTagMapping.last
+
+      expect(ContainerLabelTagMapping.where(:label_name => label_name, :tag => classification_department.tag).exists?).to be_truthy
+
+      post :label_tag_mapping_delete, :params => { :id => mapping.id.to_s }
+
+      errors = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }
+      expect(errors).to be_empty
+
+      expect(ContainerLabelTagMapping.where(:label_name => label_name, :tag => classification_department.tag).exists?).to be_falsey
+      expect(Classification.lookup_by_name(classification_department.name).name).to eq(classification_department.name)
+    end
+
+    it "cannot set category name of tag which does not exist on an existing mapping for all entities" do
+      use_form_to_create_all_entities_mapping(label_name, classification_department.name)
+      mapping = ContainerLabelTagMapping.last
+
+      controller.instance_variable_set(:@flash_array, nil)
+
+      post :label_tag_mapping_edit, :params => { :id => mapping.id.to_s }
+      post :label_tag_mapping_field_changed, :params => { :id => mapping.id.to_s, :category => 'XXX' }
+      post :label_tag_mapping_edit, :params => { :id => mapping.id.to_s, :button => 'save' }
+
+      error_message = controller.instance_variable_get(:@flash_array).select{|x| x[:level] == :error }.first[:message]
+      expect(error_message).to eq("Mapping for \"All Entities\", Label \"#{label_name}\": Tag Category \"XXX\" must exist")
     end
 
     it "can edit an existing mapping" do
