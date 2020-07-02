2163ba16e14fdec98c68b44d48a21cb3e017d03d
Martin Hradil <mhradil@redhat.com>
2020-07-02 14:05:17 +0200

    Merge pull request #7171 from h-kataria/ot_edit_added_note_component_fixed_mode_selection
    Added a "Note" field mapping to React forms.
    
    (cherry picked from commit 9573c49fca57ddc5d44ad0714e1ea5c1191e3e93)

--- app/javascript/forms/mappers/formsFieldsMapper.jsx
+++ app/javascript/forms/mappers/formsFieldsMapper.jsx
@@ -19,6 +19,7 @@ const fieldsMapper = {
   'dual-list-select': DualListSelect,
   'input-with-dynamic-prefix': InputWithDynamicPrefix,
   hr: () => <hr />,
+  note: props => <div className={props.className} role="alert">{props.label}</div>,
   'password-field': PasswordField,
   'validate-credentials': AsyncCredentials,
   'validate-provider-credentials': AsyncProviderCredentials,
