e26717951ba393d41c732cec2636c87868c3080a
Jason Frey <jfrey@redhat.com>
2020-07-06 16:49:51 -0400

    Merge branch 'EmsPhysicalInfraDashboardController_privilege' into '5.12.z'
    Enforce RBAC check on EmsPhysicalInfraDashboardController
    
    See merge request cloudforms/cfme-ui-classic!85
    
    (cherry picked from commit 5a6e2b94ecae19b528445caae5484a61821c75b7)

--- app/controllers/ems_physical_infra_dashboard_controller.rb
+++ app/controllers/ems_physical_infra_dashboard_controller.rb
@@ -9,20 +9,24 @@ class EmsPhysicalInfraDashboardController < ApplicationController
   after_action :set_session_data
 
   def show
+    assert_privileges('ems_physical_infra_show')
     if params[:id].nil?
       @breadcrumbs.clear
     end
   end
 
   def aggregate_status_data
+    assert_privileges('ems_physical_infra_show')
     render :json => {:data => aggregate_status(params[:id])}
   end
 
   def recent_servers_data
+    assert_privileges('ems_physical_infra_show')
     render :json => {:data => recent_servers(params[:id])}
   end
 
   def servers_group_data
+    assert_privileges('ems_physical_infra_show')
     render :json => {:data => servers_group(params[:id])}
   end
 
