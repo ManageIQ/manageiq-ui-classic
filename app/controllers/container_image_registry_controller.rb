bd049db7e0192e88ae7802e7c9c9800e24036037
Jason Frey <jfrey@redhat.com>
2020-07-06 16:51:13 -0400

    Merge branch 'ContainerImageRegistryController_privilege' into '5.12.z'
    Enforce RBAC check on ContainerImageRegistryController
    
    See merge request cloudforms/cfme-ui-classic!92
    
    (cherry picked from commit bfda809b802ca3d2afd7ad574b3821613dd0f80a)

--- app/controllers/container_image_registry_controller.rb
+++ app/controllers/container_image_registry_controller.rb
@@ -11,6 +11,16 @@ class ContainerImageRegistryController < ApplicationController
 
   feature_for_actions "#{controller_name}_show_list", *ADV_SEARCH_ACTIONS
 
+  def download_data
+    assert_privileges('container_image_registry_show_list')
+    super
+  end
+
+  def download_summary_pdf
+    assert_privileges('container_image_registry_show')
+    super
+  end
+
   private
 
   def textual_group_list
