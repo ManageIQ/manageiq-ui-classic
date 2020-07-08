02908a5921f918abce529e6faf0549e575eec4ed
Jason Frey <fryguy9@gmail.com>
2020-07-08 14:57:01 -0400

    Add webpack-stats-plugin for use in auditing modules usage

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -1,4 +1,4 @@
 # not spec, and not cross repo
 if [ "$TEST_SUITE" = "spec:javascript" -o "$TEST_SUITE" = "spec:jest" -o "$TEST_SUITE" = "spec:compile" ]; then
-  nvm install 10
+  nvm install 12
 fi
