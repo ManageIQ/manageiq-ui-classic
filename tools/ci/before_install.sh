f990a16bfdd75381a8095c063de883bf200a80a7
Nick LaMuro <nicklamuro@gmail.com>
2020-07-14 19:12:13 -0500

    [tools/ci] Support spec:cypress
    - Install node for spec:cypress
    - Do an app:cypress:ui:seed instead of update:ui
    
    The app:cypress:ui:seed handles compiling assets, so that would be
    duplicate work to run it twice.

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -1,4 +1,4 @@
 # not spec, and not cross repo
-if [ "$TEST_SUITE" = "spec:javascript" -o "$TEST_SUITE" = "spec:jest" -o "$TEST_SUITE" = "spec:compile" ]; then
+if [ "$TEST_SUITE" = "spec:javascript" -o "$TEST_SUITE" = "spec:jest" -o "$TEST_SUITE" = "spec:compile" -o "$TEST_SUITE" = "spec:cypress" ]; then
   nvm install 12
 fi
