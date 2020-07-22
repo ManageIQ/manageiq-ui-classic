7f299bbc8659a197e1b4a63608efe89a6630e4b2
Jason Frey <jfrey@redhat.com>
2020-07-22 15:54:38 -0400

    Merge branch 'timeline-perf-rbac' into '5.12.z'
    Enforce RBAC on the tl_chooser and perf_top_chart routes
    
    See merge request cloudforms/cfme-ui-classic!116
    
    (cherry picked from commit ed4229690a4e8a7b08de1b93759e9cdc49bb083a)

--- app/controllers/host_aggregate_controller.rb
+++ app/controllers/host_aggregate_controller.rb
@@ -411,4 +411,6 @@ class HostAggregateController < ApplicationController
 
   menu_section :clo
   feature_for_actions "#{controller_name}_show_list", *ADV_SEARCH_ACTIONS
+  feature_for_actions "#{controller_name}_timeline", :tl_chooser
+  feature_for_actions "#{controller_name}_perf", :perf_top_chart
 end
