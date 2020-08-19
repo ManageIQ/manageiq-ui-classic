fe8cbba06f96154b3cf8e4eddb3d0dd22fae2ecf
Dávid Halász <dhalasz@redhat.com>
2020-08-19 13:26:31 +0200

    Replace URI's input-with-dynamic-prefix in PXE form with a textfield

--- app/javascript/forms/mappers/formFieldsMapper.jsx
+++ app/javascript/forms/mappers/formFieldsMapper.jsx
@@ -6,7 +6,6 @@ import AsyncCredentials from '../../components/async-credentials/async-credentia
 import DualGroup from '../../components/dual-group';
 import DualListSelect from '../../components/dual-list-select';
 import EditPasswordField from '../../components/async-credentials/edit-password-field';
-import InputWithDynamicPrefix from '../input-with-dynamic-prefix';
 import PasswordField from '../../components/async-credentials/password-field';
 import { DataDrivenFormCodeEditor } from '../../components/code-editor';
 import FieldArray from '../../components/field-array';
@@ -18,7 +17,6 @@ const fieldsMapper = {
   'field-array': FieldArray,
   'dual-group': DualGroup,
   'dual-list-select': DualListSelect,
-  'input-with-dynamic-prefix': InputWithDynamicPrefix,
   hr: () => <hr />,
   note: props => <div className={props.className} role="alert">{props.label}</div>,
   'password-field': PasswordField,
