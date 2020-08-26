ae35ba1bfa0c7cfdae479f472f1e23b6c0ed3687
Dávid Halász <dhalasz@redhat.com>
2020-08-26 12:52:05 +0200

    DDF2: Clean up the hacks from ProtocolSelector by using useFieldApi

--- app/javascript/components/provider-form/protocol-selector.jsx
+++ app/javascript/components/provider-form/protocol-selector.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useContext } from 'react';
+import { get } from 'lodash';
 
-import { components } from '@data-driven-forms/pf3-component-mapper';
-import { useFormApi, useFieldApi } from '@@ddf';
+import componentMapper from '../../forms/mappers/componentMapper';
+import { useFormApi, useFieldApi, componentTypes } from '@@ddf';
 import { EditingContext } from './index';
+
+const Select = componentMapper[componentTypes.SELECT];
 
 const filter = (items, toDelete) => Object.keys(items).filter(key => !toDelete.includes(key)).reduce((obj, key) => ({
   ...obj,
@@ -13,70 +15,52 @@ const filter = (items, toDelete) => Object.keys(items).filter(key => !toDelete.i
 // This is a special <Select> component that allows altering underlying endpoints/authentications which is intended to
 // be used when there's a variety of protocols for a given implemented service. For example event stream collection in
 // some providers allows the user to choose from multiple protocols.
-const ProtocolSelector = ({ initialValue, ...props }) => {
-  const [loaded, setLoaded] = useState(false);
-  const { providerId } = useContext(EditingContext);
+const ProtocolSelector = ({ initialValue: defaultValue, options, ...props }) => {
   const formOptions = useFormApi();
-  const { input: { name, onChange, ...input }, options, ...rest } = useFieldApi(props);
+  const { providerId } = useContext(EditingContext);
 
-  const fieldState = formOptions.getFieldState(name);
+  // If editing an existing provider, we need to determine which endpoint protocol is being used.
+  // This is done by checking against the pivot field for each option. If a related the pivot is
+  // set, it means that the endpoint is used. If there is no pivot selected, we fall back to the
+  // passed initialValue.
+  const preSelected = !!providerId && options.find(({ pivot }) => get(formOptions.getState().values, pivot));
+  const initialValue = preSelected ? preSelected.value : defaultValue;
 
-  // Run this on the first render with a usable state
-  if (!loaded && fieldState) {
-    // If editing an existing provider, we need to determine which endpoint protocol is being used.
-    // This is done by checking against the pivot field for each option. If a related the pivot is
-    // set, it means that the endpoint is used. If there is no pivot selected, we fall back to the
-    // defined initialValue.
-    if (!!providerId) {
-      const selected = options.find(({ pivot }) => _.get(formOptions.getState().values, pivot));
-      const value = selected ? selected.value : initialValue;
-
-      // Reinitializing the form with the correct value for the select
+  const { input: { value } } = useFieldApi({ options, initialValue, ...props });
+  useEffect(() => {
+    if (value) {
       setTimeout(() => {
-        formOptions.initialize({
-          ...formOptions.getState().values,
-          [name]: value,
+        // Load the initial and current values for the endpoints/authentications after the field value has been changed
+        const {
+          initialValues: {
+            endpoints: initialEndpoints = {},
+            authentications: initialAuthentications = {},
+          },
+          values: {
+            endpoints: currentEndpoints = {},
+            authentications: currentAuthentications = {},
+          },
+        } = formOptions.getState();
+
+        // Map the values of all possible options into an array
+        const optionValues = options.map(({ value }) => value);
+        // Determine which endpoint/authentication pair has to be removed from the form state
+        const toDelete = optionValues.filter(option => option !== value);
+
+        // Adjust the endpoints/authentications and pass them to the form state
+        formOptions.change('endpoints', {
+          ...filter(initialEndpoints, toDelete),
+          ...filter(currentEndpoints, optionValues),
+        });
+        formOptions.change('authentications', {
+          ...filter(initialAuthentications, toDelete),
+          ...filter(currentAuthentications, optionValues),
         });
       });
     }
+  }, [value]);
 
-    setLoaded(true);
-  }
-
-  const enhancedChange = onChange => (value) => {
-    setTimeout(() => {
-      // Load the initial and current values for the endpoints/authentications after the field value has been changed
-      const {
-        initialValues: {
-          endpoints: initialEndpoints = {},
-          authentications: initialAuthentications = {},
-        },
-        values: {
-          endpoints: currentEndpoints = {},
-          authentications: currentAuthentications = {},
-        },
-      } = formOptions.getState();
-
-      // Map the values of all possible options into an array
-      const optionValues = options.map(({ value }) => value);
-      // Determine which endpoint/authentication pair has to be removed from the form state
-      const toDelete = optionValues.filter(option => option !== value);
-
-      // Adjust the endpoints/authentications and pass them to the form state
-      formOptions.change('endpoints', {
-        ...filter(initialEndpoints, toDelete),
-        ...filter(currentEndpoints, optionValues),
-      });
-      formOptions.change('authentications', {
-        ...filter(initialAuthentications, toDelete),
-        ...filter(currentAuthentications, optionValues),
-      });
-    });
-
-    return onChange(value);
-  };
-
-  return <components.Select input={{ name, ...input, onChange: enhancedChange(onChange) }} options={options} {...rest} />;
+  return <Select options={options} {...props} />;
 };
 
 export default ProtocolSelector;
