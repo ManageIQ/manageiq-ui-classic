9fc8068b355dd2b422de4d15d016fd94796e1f49
Martin Hradil <mhradil@redhat.com>
2020-09-09 13:22:33 +0000

    Merge pull request #7291 from Fryguy/better_webpack_stats
    Cross reference the webpack manifest with the actual node_modules
    
    (cherry picked from commit ef518cc88ff057e1e7fc6c51eec8322482ee4b86)

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -4,8 +4,11 @@
 
 const merge = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
-const { StatsWriterPlugin } = require('webpack-stats-plugin');
 const sharedConfig = require('./shared.js');
+
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
+const fs = require('fs');
+const sortBy = require('lodash/sortBy');
 
 module.exports = merge(sharedConfig, {
   mode: 'production',
@@ -24,12 +27,44 @@ module.exports = merge(sharedConfig, {
 
       transform(data) {
         // recursively flatten nested modules
-        const transformModule  = m  => m.modules ? transformModules(m.modules) : m.name;
+        const transformModule  = m  => m.modules ? transformModules(m.modules) : m.name.split("!").pop();
         const transformModules = ms => ms.flatMap(m => transformModule(m));
 
-        var modules = transformModules(data.modules).sort();
+        var modules = transformModules(data.modules)
         modules = [...new Set(modules)]; // uniq
-        return JSON.stringify(modules, null, 2);
+
+        // find the package.json file for each module
+        modules = modules.filter(m => m.includes("node_modules"));
+        var packagePaths = modules.map(m => {
+          var match = m.match(/(.*node_modules\/)([^\/]+)(\/[^\/]+)?/);
+          var path = [
+            `${match[1]}${match[2]}/package.json`,
+            `${match[1]}${match[2]}${match[3]}/package.json`,
+          ].find(p => fs.existsSync(p));
+
+          if (path == null) {
+            console.warn(`[webpack-manifest] WARN: Unable to find a package.json for ${m}`);
+          }
+
+          return path;
+        })
+        packagePaths = packagePaths.filter(p => p != null);
+        packagePaths = [...new Set(packagePaths)]; // uniq
+
+        // extract relevant package data from the package.json
+        var packages = packagePaths.map(p => {
+          var content = fs.readFileSync(p);
+          var package = JSON.parse(content);
+          return {
+            name: package.name,
+            license: package.license,
+            version: package.version,
+            location: p
+          }
+        });
+        packages = sortBy(packages, ['name', 'version']);
+
+        return JSON.stringify(packages, null, 2);
       }
     })
   ],
