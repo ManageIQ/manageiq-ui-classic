b5eff40545a1d636af7dda362d6109040333ffca
Dávid Halász <dhalasz@redhat.com>
2020-10-19 17:13:53 +0200

    Making the edit service form API-driven and function component

---
+++ app/javascript/components/edit-service-form/index.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Grid } from 'patternfly-react';
+import createSchema from './edit-service-form.schema';
+import MiqFormRenderer from '../../forms/data-driven-form';
+
+const EditServiceForm = ({ maxNameLen, maxDescLen, recordId }) => {
+  const [{ isLoading, initialValues }, setState] = useState({ isLoading: true });
+
+  useEffect(() => {
+    API.get(`/api/services/${recordId}`).then((initialValues) => {
+      setState({ isLoading: false, initialValues });
+    });
+  }, [recordId]);
+
+  const onSubmit = ({ name, description }) => {
+    miqSparkleOn();
+    API.patch(`/api/services/${recordId}`, { name, description }).then(() => {
+      miqAjaxButton(`/service/service_edit/${recordId}?button=save`);
+    }).catch(miqSparkleOff);
+  };
+
+  return !isLoading && (
+    <Grid fluid style={{ paddingTop: 20 }}>
+      <MiqFormRenderer
+        initialValues={initialValues}
+        schema={createSchema(maxNameLen, maxDescLen)}
+        onSubmit={onSubmit}
+        onCancel={() => miqAjaxButton(`/service/service_edit/${recordId}?button=cancel`)}
+        onReset={() => add_flash(__('All changes have been reset'), 'warn')}
+        canReset
+        buttonsLabels={{
+          submitLabel: __('Save'),
+        }}
+      />
+    </Grid>
+  );
+};
+
+EditServiceForm.propTypes = {
+  maxNameLen: PropTypes.number.isRequired,
+  maxDescLen: PropTypes.number.isRequired,
+  recordId: PropTypes.number.isRequired,
+};
+
+export default EditServiceForm;
