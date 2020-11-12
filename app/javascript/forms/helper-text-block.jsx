f5f2531feef99efdba054636cae4040e07e1ef63
Dávid Halász <dhalasz@redhat.com>
2020-11-12 20:08:27 +0100

    PF3->Carbon: Introduce the HelperTextBlock component

---
+++ app/javascript/forms/helper-text-block.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HelperTextBlock = ({ helperText, errorText, warnText }) => {
+  if (errorText) {
+    return <div className="bx--form-requirement ddorg__carbon-error-helper-text">{errorText}</div>;
+  }
+
+  if (warnText) {
+    return <div className="bx--form-requirement ddorg__carbon-warning-helper-text">{warnText}</div>;
+  }
+
+  if (helperText) {
+    return <div className="bx--form__helper-text">{helperText}</div>;
+  }
+
+  return null;
+};
+
+HelperTextBlock.propTypes = {
+  helperText: PropTypes.node,
+  errorText: PropTypes.node,
+  warnText: PropTypes.node,
+};
+
+HelperTextBlock.defaultProps = {
+  helperText: undefined,
+  errorText: undefined,
+  warnText: undefined,
+};
+
+export default HelperTextBlock;
