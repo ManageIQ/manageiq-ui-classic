54076a5cffae8890784879c0b74b8154a10b4c0f
Harpreet Kataria <hkataria@redhat.com>
2021-01-01 21:09:23 -0500

    Fixed routing spec test
    - Deleted tree_node spec test that's no longer needed
    - Added new missing views from previous commit

--- spec/presenters/tree_node/miq_alert_spec.rb
+++
@@ -1,8 +0,0 @@
-describe TreeNode::MiqAlert do
-  subject { described_class.new(object, nil, nil) }
-  let(:object) { FactoryBot.create(:miq_alert) }
-
-  include_examples 'TreeNode::Node#key prefix', 'al-'
-  include_examples 'TreeNode::Node#text description'
-  include_examples 'TreeNode::Node#icon', 'pficon pficon-warning-triangle-o'
-end
--- spec/routing/miq_alert_routing_spec.rb
+++ spec/routing/miq_alert_routing_spec.rb
@@ -3,33 +3,34 @@ require 'routing/shared_examples'
 describe 'routes for MiqAlertController' do
   let(:controller_name) { 'miq_alert' }
 
-  describe '#miq_alert_edit' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/miq_alert_edit")).to route_to("#{controller_name}#miq_alert_edit")
+  get_routes = %w[
+    copy
+    edit
+    new
+    show
+    show_list
+  ]
+
+  post_routes = %w[
+    alert_field_changed
+    edit
+    show
+    show_list
+  ]
+
+  describe 'GET routes' do
+    get_routes.each do |route|
+      it "##{route}" do
+        expect(get("/#{controller_name}/#{route}")).to route_to("#{controller_name}##{route}")
+      end
     end
   end
 
-  describe '#alert_field_changed' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/alert_field_changed")).to route_to("#{controller_name}#alert_field_changed")
-    end
-  end
-
-  describe '#x_button' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/x_button")).to route_to("#{controller_name}#x_button")
-    end
-  end
-
-  describe '#x_history' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/x_history")).to route_to("#{controller_name}#x_history")
-    end
-  end
-
-  describe '#x_show' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/x_show")).to route_to("#{controller_name}#x_show")
+  describe 'POST routes' do
+    post_routes.each do |route|
+      it "##{route}" do
+        expect(post("/#{controller_name}/#{route}")).to route_to("#{controller_name}##{route}")
+      end
     end
   end
 end
