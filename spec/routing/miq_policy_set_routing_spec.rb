02874cfe273242e3240a26534358e9d1f6c1628f
Harpreet Kataria <hkataria@redhat.com>
2021-01-01 21:51:54 -0500

    Fixed routing spec test

--- spec/routing/miq_policy_set_routing_spec.rb
+++ spec/routing/miq_policy_set_routing_spec.rb
@@ -3,33 +3,35 @@ require 'routing/shared_examples'
 describe 'routes for MiqPolicySetController' do
   let(:controller_name) { 'miq_policy_set' }
 
-  describe '#button' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/button")).to route_to("#{controller_name}#button")
+  get_routes = %w[
+    edit
+    new
+    show
+    show_list
+  ]
+
+  post_routes = %w[
+    button
+    edit
+    form_field_changed
+    reload
+    show
+    show_list
+  ]
+
+  describe 'GET routes' do
+    get_routes.each do |route|
+      it "##{route}" do
+        expect(get("/#{controller_name}/#{route}")).to route_to("#{controller_name}##{route}")
+      end
     end
   end
 
-  describe '#miq_policy_set_edit' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/edit")).to route_to("#{controller_name}#edit")
-    end
-  end
-
-  describe '#form_field_changed' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/form_field_changed")).to route_to("#{controller_name}#form_field_changed")
-    end
-  end
-
-  describe '#reload' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/reload")).to route_to("#{controller_name}#reload")
-    end
-  end
-
-  describe '#show' do
-    it 'routes with POST' do
-      expect(post("/#{controller_name}/show")).to route_to("#{controller_name}#show")
+  describe 'POST routes' do
+    post_routes.each do |route|
+      it "##{route}" do
+        expect(post("/#{controller_name}/#{route}")).to route_to("#{controller_name}##{route}")
+      end
     end
   end
 end
