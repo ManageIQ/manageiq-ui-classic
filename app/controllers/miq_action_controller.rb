4afe479a6658eb75d753de166c916939188090e5
Harpreet Kataria <hkataria@redhat.com>
2021-01-03 15:34:28 -0500

    split action details and form into their own views

--- app/controllers/miq_action_controller.rb
+++ app/controllers/miq_action_controller.rb
@@ -48,7 +48,7 @@ class MiqActionController < ApplicationController
       render :update do |page|
         page << javascript_prologue
         page.replace("flash_msg_div", :partial => "layouts/flash_msg")
-        page.replace_html("action_details_div", :partial => "action_details") unless @flash_errors
+        page.replace_html("form_div", :partial => "form") unless @flash_errors
       end
     end
   end
