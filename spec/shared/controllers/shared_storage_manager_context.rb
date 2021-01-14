291f2327a21af580e8426b1d01df5540af1e2b59
Adam Grare <agrare@redhat.com>
2021-01-14 12:01:56 -0500

    Fix swift manager subclass type check

--- spec/shared/controllers/shared_storage_manager_context.rb
+++ spec/shared/controllers/shared_storage_manager_context.rb
@@ -6,7 +6,7 @@ shared_context :shared_storage_manager_context do |t|
       @swift_manager    = @cinder_manager = nil
       @storage_managers = @ems_cloud.storage_managers
       @storage_managers.each do |sm|
-        if sm.type == "ManageIQ::Providers::StorageManager::SwiftManager"
+        if sm.type == "ManageIQ::Providers::Openstack::StorageManager::SwiftManager"
           @swift_manager = sm
         else
           @cinder_manager = sm
