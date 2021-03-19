22e67105b507fe235a4b2d3b8fd6955facfdd7e9
Oleg Barenboim <chessbyte@gmail.com>
2021-03-19 16:03:29 -0400

    split line causes message catalog to contain:
    "Server %{server_name} [%{server_id}] can only be deleted if it is stopped or \\\nhas not responded for a while"

--- app/helpers/application_helper/button/delete_server.rb
+++ app/helpers/application_helper/button/delete_server.rb
@@ -3,8 +3,7 @@ class ApplicationHelper::Button::DeleteServer < ApplicationHelper::Button::ZoneD
 
   def disabled?
     @error_message = unless @record.is_deleteable?
-                       _("Server %{server_name} [%{server_id}] can only be deleted if it is stopped or \
-has not responded for a while") % {:server_name => @record.name, :server_id => @record.id}
+                       _("Server %{server_name} [%{server_id}] can only be deleted if it is stopped or has not responded for a while") % {:server_name => @record.name, :server_id => @record.id}
                      end
     @error_message.present?
   end
