f3e7daff898a90bfba13100e31d265556ea74837
Jason Frey <fryguy9@gmail.com>
2021-04-14 15:28:33 -0400

    Merge pull request #7701 from kavyanekkalapu/change_notification_carbon
    change notification drawer to carbon and fix A11y
    
    (cherry picked from commit 4a829385a79a65c3b434d82e9188072e9632ede6)

--- app/javascript/components/notification-drawer/notification-drawer.jsx
+++ app/javascript/components/notification-drawer/notification-drawer.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { useDispatch, useSelector } from 'react-redux';
+import { ChevronLeft16, Close16 } from '@carbon/icons-react';
 import {
-  Button, NotificationDrawer as Drawer, Notification, MenuItem, NotificationContent, NotificationInfo, NotificationMessage, Icon,
-} from 'patternfly-react';
-import classnames from 'classnames';
+  Button, Accordion, AccordionItem, InlineNotification, OverflowMenu, OverflowMenuItem,
+} from 'carbon-components-react';
 import { getNotficationStatusIconName, newCountText, viewDetails } from './helpers';
 import { maxNotifications as maxNotificationsConstant } from '../../notifications/backend';
 import {
@@ -17,170 +17,174 @@ const NotificationDrawer = () => {
   const dispatch = useDispatch();
   const drawerTitle = __('Notifications');
   const [isDrawerExpanded, setDrawerExpanded] = useState(false);
-  const [isPanelExpanded, setPanelExpanded] = useState(true);
   const {
     isDrawerVisible, unreadCount, notifications, totalNotificationsCount, maxNotifications,
   } = useSelector(({ notificationReducer }) => notificationReducer);
-
-  useEffect(() => { initNotifications(); }, []);
-
+  useEffect(() => {
+    initNotifications();
+  }, []);
   return (
     isDrawerVisible ? (
       <div id="miq-notifications-drawer-container">
-        <Drawer expanded={isDrawerExpanded}>
-          <Drawer.Title
-            title={drawerTitle}
-            onCloseClick={() => dispatch(toggleDrawerVisibility())}
-            onExpandClick={() => setDrawerExpanded(!isDrawerExpanded)}
-          />
-          {notifications && (
-            <Drawer.Accordion>
-              <Drawer.Panel expanded={isPanelExpanded}>
-                <Drawer.PanelHeading>
-                  <Drawer.PanelTitle>
-                    <a
-                      id="eventsExpander"
-                      className={classnames({ collapsed: !isPanelExpanded })}
-                      onClick={() => setPanelExpanded(!isPanelExpanded)}
-                    >
-                      {__('Events')}
-                    </a>
-                  </Drawer.PanelTitle>
-                  <Drawer.PanelCounter text={newCountText(unreadCount)} />
-                </Drawer.PanelHeading>
-                { isPanelExpanded && (
-                  <Drawer.PanelCollapse>
-                    <Drawer.PanelBody>
-                      {notifications.slice(0, maxNotifications).map(notification => (
-                        <Notification
+        <div className={`notification-drawer ${isDrawerExpanded ? 'notification-drawer-expanded' : ''}`}>
+          <div className="panel-header">
+            <Button
+              hasIconOnly
+              renderIcon={ChevronLeft16}
+              className={!isDrawerExpanded ? 'collapsed heading-icon' : 'expanded  heading-icon'}
+              iconDescription={!isDrawerExpanded ? 'collapsed' : 'expanded'}
+              onClick={() => setDrawerExpanded(!isDrawerExpanded)}
+              onKeyPress={() => setDrawerExpanded(!isDrawerExpanded)}
+            />
+            <Button
+              hasIconOnly
+              renderIcon={Close16}
+              className="heading-icon closeIcon"
+              description={__('Close')}
+              iconDescription={__('Close')}
+              onClick={() => dispatch(toggleDrawerVisibility())}
+              onKeyPress={() => dispatch(toggleDrawerVisibility())}
+            />
+            <h3 className="notification-title">{drawerTitle}</h3>
+          </div>
+          <div className="notification-content">
+            <div>
+              {' '}
+              <Accordion align="start" className="Notification-accordion">
+                <AccordionItem
+                  title={(
+                    <div className="events-count">
+                      <span>{__('Events') }</span>
+                      <span>{newCountText(unreadCount)}</span>
+                    </div>
+                  )}
+                >
+                  <div className="notification-accordion-content">
+                    <div className="notification-content-messages">
+                      {notifications.slice(0, maxNotifications).map((notification) => (
+                        <InlineNotification
+                          kind={getNotficationStatusIconName(notification)}
+                          role="alert"
                           key={notification.id}
-                          seen={!notification.unread}
-                          type="notification"
-                        >
-                          <Drawer.Dropdown
-                            id={`dropdownKebabRight ${notification.id}`}
-                            pullRight
-                          >
-                            <MenuItem
-                              bsClass="dropdown"
-                              disabled={!notification.data.link}
-                              eventKey="1"
-                              header={false}
-                              onClick={() => viewDetails(notification)}
-                            >
-                              {__('View details')}
-                            </MenuItem>
-                            <MenuItem divider />
-                            <MenuItem
-                              id={`dropdownMarkRead-${notification.id}`}
-                              bsClass={classnames('dropdown', {
-                                disabled: !notification.unread,
-                              })}
-                              disabled={!notification.unread}
-                              eventKey="2"
-                              header={false}
-                              onClick={
-                                () => notification.unread && dispatch(markNotificationRead(notification))
-                              }
-                            >
-                              {__('Mark as read')}
-                            </MenuItem>
-                            <MenuItem
-                              id={`dropdownRemove-${notification.id}`}
-                              bsClass="dropdown"
-                              eventKey="3"
-                              header={false}
-                              onClick={
-                                () => dispatch(clearNotification(notification, !!maxNotifications))
-                              }
-                            >
-                              {__('Remove')}
-                            </MenuItem>
-                          </Drawer.Dropdown>
-                          <Icon
-                            className="pull-left"
-                            name={getNotficationStatusIconName(notification)}
-                            type="pf"
-                            onClick={
-                              () => notification.unread && dispatch(markNotificationRead(notification))
-                            }
-                          />
-                          <NotificationContent>
-                            <NotificationMessage
-                              title={notification.message}
-                              onClick={
-                                () => notification.unread && dispatch(markNotificationRead(notification))
-                              }
-                            >
-                              {notification.message}
-                            </NotificationMessage>
-                            <NotificationInfo
-                              leftText={moment(notification.timeStamp).format('MM/DD/YYYY')}
-                              rightText={moment(notification.timeStamp).format('h:mm:ss A')}
-                            />
-                          </NotificationContent>
-                        </Notification>
+                          className={!notification.unread ? 'read' : 'unread'}
+                          title={notification.message}
+                          statusIconDescription={getNotficationStatusIconName(notification)}
+                          lowContrast
+                          hideCloseButton
+                          subtitle={(
+                            <div className="subtitle-div">
+                              <div className="subtitle-div-notification-menu">
+                                <OverflowMenu
+                                  ariaLabel={__('open and close options')}
+                                  id={notification.id}
+                                  floatingmenu="true"
+                                  flipped
+                                  title={__('open and close options')}
+                                >
+
+                                  <OverflowMenuItem
+                                    className={`overflow-options details-${notification.id}`}
+                                    aria-label={__('View details')}
+                                    itemText={__('View details')}
+                                    disabled={!notification.data.link}
+                                    onKeyDown={() => viewDetails(notification)}
+                                    requireTitle
+                                    onClick={() => viewDetails(notification)}
+                                  />
+                                  <OverflowMenuItem
+                                    className={`overflow-options markasread-${notification.id}`}
+                                    aria-label={__('Mark as read')}
+                                    itemText={__('Mark as read')}
+                                    disabled={!notification.unread}
+                                    onKeyDown={
+                                      () => notification.unread && dispatch(markNotificationRead(notification))
+                                    }
+                                    requireTitle
+                                    onClick={
+                                      () => notification.unread && dispatch(markNotificationRead(notification))
+                                    }
+                                    hasDivider
+                                  />
+                                  <OverflowMenuItem
+                                    className={`overflow-options remove-${notification.id}`}
+                                    aria-label={__('Remove')}
+                                    itemText={__('Remove')}
+                                    onKeyDown={
+                                      () => dispatch(clearNotification(notification, !!maxNotifications))
+                                    }
+                                    requireTitle
+                                    onClick={
+                                      () => dispatch(clearNotification(notification, !!maxNotifications))
+                                    }
+                                    isDelete
+                                  />
+                                </OverflowMenu>
+                              </div>
+                              <div className="subtitle-div-notification-time">
+                                <span className="notification-time">
+                                  {moment(notification.timeStamp).format('MM/DD/YYYY')}
+                                  {' '}
+                                  {'|'}
+                                  {' '}
+                                  {moment(notification.timeStamp).format('h:mm:ss A')}
+                                </span>
+                              </div>
+
+                            </div>
+                          )}
+
+                        />
+
                       ))}
                       { maxNotificationsConstant < totalNotificationsCount && (
-                        <Notification id="notificationLimitBar" key="notificationLimit" className="text-center" seen>
-                          <NotificationContent>
-                            <NotificationMessage
-                              title="title"
-                            >
-                              { maxNotifications ? sprintf(__('Showing %s items out of %s total.'), maxNotifications, totalNotificationsCount) : sprintf(__('Showing all %s items.'), notifications.length) }
-                            </NotificationMessage>
-                            <a
-                              id="toggleMaxNotifications"
-                              className="btn btn-link"
-                              onClick={() =>
-                                dispatch(toggleMaxNotifications())
-                              }
-                            >
-                              { maxNotifications ? __('Show all (may take a while)') : sprintf(__('Show only the first %s'), maxNotificationsConstant)}
-                            </a>
-                          </NotificationContent>
-                        </Notification>)
-                      }
-                    </Drawer.PanelBody>
-                    <Drawer.PanelAction>
-                      <Drawer.PanelActionLink
-                        className="footer-button-left"
-                        data-toggle="mark-all-read"
-                      >
-                        <Button
-                          id="markAllReadBtn"
-                          bsStyle="link"
-                          disabled={unreadCount === 0}
-                          onClick={
-                            () => {
-                              dispatch(markAllRead(notifications));
-                            }
+                        <div className="maxnotifications-text">
+                          <span>
+                            { maxNotifications ? sprintf(__('Showing %s items out of %s total.'), maxNotifications, totalNotificationsCount)
+                              : sprintf(__('Showing all %s items.'), notifications.length) }
+                          </span>
+                          <Button
+                            id="toggleMaxNotifications"
+                            kind="ghost"
+                            onClick={() =>
+                              dispatch(toggleMaxNotifications())}
+                          >
+                            { maxNotifications ? __('Show all (may take a while)') : sprintf(__('Show only the first %s'), maxNotificationsConstant)}
+                          </Button>
+                        </div>
+                      )}
+                    </div>
+                    <div className="notification-footer">
+                      <Button
+                        id="markAllReadBtn"
+                        disabled={unreadCount === 0}
+                        kind="ghost"
+                        onClick={
+                          () => {
+                            dispatch(markAllRead(notifications));
                           }
-                        >
-                          {__('Mark All Read')}
-                        </Button>
-                      </Drawer.PanelActionLink>
-                      <Drawer.PanelActionLink
-                        className="footer-button-right"
-                        data-toggle="clear-all"
+                        }
                       >
-                        <Button
-                          id="clearAllBtn"
-                          bsStyle="link"
-                          disabled={notifications.length === 0}
-                          onClick={
-                            () => dispatch(clearAll(notifications, !!maxNotifications))
-                          }
-                        >
-                          {__('Clear All')}
-                        </Button>
-                      </Drawer.PanelActionLink>
-                    </Drawer.PanelAction>
-                  </Drawer.PanelCollapse>)}
-              </Drawer.Panel>
-            </Drawer.Accordion>
-          )}
-        </Drawer>
+                        {__('Mark All Read')}
+                      </Button>
+                      <Button
+                        id="clearAllBtn"
+                        kind="ghost"
+                        disabled={notifications.length === 0}
+                        onClick={
+                          () => dispatch(clearAll(notifications, !!maxNotifications))
+                        }
+                      >
+                        {__('Clear All')}
+                      </Button>
+                    </div>
+                  </div>
+
+                </AccordionItem>
+              </Accordion>
+            </div>
+
+          </div>
+        </div>
       </div>
     )
       : null
