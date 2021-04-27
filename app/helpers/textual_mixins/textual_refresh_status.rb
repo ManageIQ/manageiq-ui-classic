4bf63f3e7595440d724d2bf3b83785b865b7d56a
Kavya Nekkalapu <knekkal@us.ibm.com>
2021-04-27 16:06:29 -0400

    remove time zone in last refersh status

--- app/helpers/textual_mixins/textual_refresh_status.rb
+++ app/helpers/textual_mixins/textual_refresh_status.rb
@@ -10,7 +10,7 @@ module TextualMixins::TextualRefreshStatus
 
   def refresh_status(record)
     last_refresh_status = record.last_refresh_status&.titleize
-    refresh_time = record.last_refresh_date&.in_time_zone(Time.zone)
+    refresh_time = record.last_refresh_date
     if refresh_time
       last_refresh_date = refresh_time ? time_ago_in_words(refresh_time).titleize : _(Never)
       last_refresh_status << _(" - %{last_refresh_date} Ago") % {:last_refresh_date => last_refresh_date}
@@ -21,12 +21,12 @@ module TextualMixins::TextualRefreshStatus
                   {:value => record.last_refresh_error.try(:truncate, 120)}],
       :title  => record.last_refresh_error,
       :status => record.last_refresh_status&.titleize,
-      :stale  => refresh_time&.before?(2.days.ago) # checking if date is within last two days range.
+      :stale  => refresh_time ? refresh_time.before?(2.days.ago) : true # checking if date is within last two days range.
     }
   end
 
   def refresh_date(record)
-    last_refresh_date = record.last_refresh_date&.in_time_zone(Time.zone)
+    last_refresh_date = record.last_refresh_date
     {:label => _("Last Refresh Date"), :value => last_refresh_date}
   end
 end
