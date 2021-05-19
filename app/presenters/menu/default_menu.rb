ca4fec8b68635095b3749cd48ab69d2ce9317028
Joe Rafaniello <jrafanie@gmail.com>
2021-05-19 11:41:00 -0400

    Fix reference to the wrong feature

--- app/presenters/menu/default_menu.rb
+++ app/presenters/menu/default_menu.rb
@@ -255,7 +255,7 @@ module Menu
 
       def automation_manager_menu_section
         Menu::Section.new(:at, N_("Automation"), nil, [
-          Menu::Item.new('ems_automation', N_('Providers'), 'ems_automation', {:feature => 'ems_automation_show_list', :any => true}, '/ems_automation/show_list'),
+          Menu::Item.new('ems_automation', N_('Providers'), 'ems_automation', {:feature => 'ems_automation_manager_show_list', :any => true}, '/ems_automation/show_list'),
           Menu::Item.new('automation_manager_configured_system', N_('Configured Systems'), 'automation_manager_configured_system', {:feature => 'automation_manager_configured_system_show_list'}, '/automation_manager_configured_system/show_list'),
           Menu::Item.new('configuration_script', N_('Templates'), 'configuration_script', {:feature => 'configuration_script_show_list'}, '/configuration_script/show_list'),
           Menu::Item.new('configuration_job',  N_('Jobs'),     'configuration_job',  {:feature => 'configuration_job_show_list'},      '/configuration_job/show_list')
