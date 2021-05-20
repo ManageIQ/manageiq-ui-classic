b9748852f5915de8f768ba7db4d0192e756864a3
mengxiwuu <mengxiwuu@email.com>
2021-05-20 16:18:53 -0400

    convert add/edit zone to react

--- app/javascript/components/zone-form/index.jsx
+++ app/javascript/components/zone-form/index.jsx
@@ -1,77 +1,76 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import MiqFormRenderer from '@@ddf';
-import createSchema from './zone-form.schema';
 import PropTypes from 'prop-types';
+import createSchema from './zone-form.schema';
 import { API } from '../../http_api';
 import miqRedirectBack from '../../helpers/miq-redirect-back';
 
-const ZoneForm = ({recordId}) => {
-    const [{ initialValues, isLoading }, setState] = useState({
-        isLoading: !!recordId,
-    });
+const ZoneForm = ({ recordId }) => {
+  const [{ initialValues, isLoading }, setState] = useState({
+    isLoading: !!recordId,
+  });
 
-    const cancelUrl = recordId ? `/ops/zone_edit/${recordId}?button=cancel` : '/ops/zone_edit/?button=cancel';
-    const submitLabel = !!recordId ? __('Save') : __('Add');
+  const submitLabel = !!recordId ? __('Save') : __('Add');
 
-    const onSubmit = (values) => {
-        miqSparkleOn();
+  const onSubmit = (values) => {
+    miqSparkleOn();
 
-        const request = recordId ? API.patch(`/api/zones/${recordId}`, values) : API.post('/api/zones', values);;
-        request.then(() => {
-          const message = sprintf(
-            recordId
-              ? __('New zone "%s" has been successfully queued.')
-              : __('Add of Zone "%s" has been successfully queued.'),
-            values.name,
-          );
-          miqRedirectBack(message, undefined, '/ops');
-        }).catch(miqSparkleOff);
-      };
-
-      const onCancel = () => {
-        const message = sprintf(
-          recordId
-            ? __('Edit of Zone "%s" was canceled by the user.')
-            : __('Creation of new Zone was canceled by the user.'),
-          initialValues && initialValues.name,
-        );
-        miqRedirectBack(message, 'warning', '/ops');
-      };
-
-      useEffect(() => {
-        if (recordId) {
-          API.get(`/api/zones/${recordId}?attributes=authentications`).then(
-            ({ authentications, ...res }) => {
-              setState({
-                initialValues: {
-                  ...res,
-                  authentications: authentications[0],
-                },
-                isLoading: false,
-              });
-            },
-          );
-        }
-      }, []);
-
-      return !isLoading && (
-        <MiqFormRenderer
-          schema={createSchema(!!recordId)}
-          initialValues={initialValues}
-          canReset={!!recordId}
-          onSubmit={onSubmit}
-          onCancel={onCancel}
-          buttonsLabels={{ submitLabel }}
-        />
+    const request = recordId ? API.patch(`/api/zones/${recordId}`, values) : API.post('/api/zones', values);
+    request.then(() => {
+      const message = sprintf(
+        recordId
+          ? __('New zone "%s" has been successfully queued.')
+          : __('Add of Zone "%s" has been successfully queued.'),
+        values.name,
       );
-}
-
-  ZoneForm.propTypes = {
-    recordId: PropTypes.string,
+      miqRedirectBack(message, 'success', '/ops');
+    }).catch(miqSparkleOff);
   };
 
-  ZoneForm.defaultProps = {
-    recordId: undefined,
+  const onCancel = () => {
+    const message = sprintf(
+      recordId
+        ? __('Edit of Zone "%s" was canceled by the user.')
+        : __('Creation of new Zone was canceled by the user.'),
+      initialValues && initialValues.name,
+    );
+    miqRedirectBack(message, 'warning', '/ops');
   };
 
-  export default ZoneForm; 
+  useEffect(() => {
+    if (recordId) {
+      API.get(`/api/zones/${recordId}?attributes=authentications`).then(
+        ({ authentications, ...res }) => {
+          setState({
+            initialValues: {
+              ...res,
+              authentications: authentications[0],
+            },
+            isLoading: false,
+          });
+        },
+      );
+    }
+  }, []);
+
+  return !isLoading && (
+    <MiqFormRenderer
+      schema={createSchema(!!recordId)}
+      initialValues={initialValues}
+      canReset={!!recordId}
+      onSubmit={onSubmit}
+      onCancel={onCancel}
+      buttonsLabels={{ submitLabel }}
+    />
+  );
+};
+
+ZoneForm.propTypes = {
+  recordId: PropTypes.string,
+};
+
+ZoneForm.defaultProps = {
+  recordId: undefined,
+};
+
+export default ZoneForm;
