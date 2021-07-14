c64525091282e39000a959648428c84a18979da0
Kavya Nekkalapu <knekkal@us.ibm.com>
2021-07-14 16:02:33 -0400

    Fix lint errors and testcases

--- lib/gtl_formatter.rb
+++ lib/gtl_formatter.rb
@@ -13,16 +13,16 @@ class GtlFormatter
   }.freeze
 
   COLUMN_WITH_BACKGROUND_ICON = {
-    'normalized_state'       => 'normalized_state_image'
+    'normalized_state' => 'normalized_state_image'
   }.freeze
 
   COLUMN_WITH_OS_ICON = {
-    'os_image_name'       => 'os_info_details'
+    'os_image_name' => 'os_info_details'
   }.freeze
 
-  COLUMN_WITH_OS_TEXT = {
-    'product_name'       => 'product_name_image'
-  }.freeze
+  COLUMN_WITH_OS_TEXT = %w[
+    product_name
+  ].freeze
 
   COLUMN_WITH_IMAGE = {
     'ext_management_system.name' => 'fonticon_or_fileicon'
@@ -57,7 +57,6 @@ class GtlFormatter
   ].freeze
 
   def self.format_cols(view, row, controller, options)
-
     cols = []
 
     state = {"state" => :state_format}
@@ -100,7 +99,7 @@ class GtlFormatter
         celltext = send(special_cases[view.extras[:filename]], view, row, col)
       elsif special_cases[view.extras[:filename]].kind_of?(Hash) && special_cases[view.extras[:filename]][view.col_order[col_idx]].present?
         celltext, span = send(special_cases[view.extras[:filename]][view.col_order[col_idx]], row[col])
-      elsif COLUMN_WITH_IMAGE.keys.include?(col)
+      elsif COLUMN_WITH_IMAGE.key?(col)
         # Generate html for the list icon
         icon, icon2, image = send(COLUMN_WITH_IMAGE[col], record)
         text = format_col_for_display(view, row, col)
@@ -109,30 +108,30 @@ class GtlFormatter
                 :icon  => icon,
                 :icon2 => icon2,
                 :text  => text}.compact
-      elsif COLUMN_WITH_BACKGROUND_ICON.keys.include?(col)
-                # Generate html for the list icon with background
+      elsif COLUMN_WITH_BACKGROUND_ICON.key?(col)
+        # Generate html for the list icon with background
         icon = send(COLUMN_WITH_BACKGROUND_ICON[col], record)
         text = format_col_for_display(view, row, col)
-        item = {:title => text,
-                :icon  => icon,
+        item = {:title      => text,
+                :icon       => icon,
                 :background => textual_power_state_whitelisted(record[col]),
-                :text => text}.compact
-      elsif COLUMN_WITH_ICON.keys.include?(col)
+                :text       => text}.compact
+      elsif COLUMN_WITH_ICON.key?(col)
         # Generate html for the list icon
         icon = send(COLUMN_WITH_ICON[col], record)
         text = format_col_for_display(view, row, col)
         item = {:title => text,
                 :icon  => icon,
-                :text => text}.compact
+                :text  => text}.compact
       elsif COLUMN_WITH_TIME.include?(col)
         celltext = format_time_for_display(row, col)
-      elsif COLUMN_WITH_OS_ICON.keys.include?(col)
+      elsif COLUMN_WITH_OS_ICON.key?(col)
         @osicon = send(COLUMN_WITH_OS_ICON[col], record)
-        @ostext = format_col_for_display(view, row, col).blank? ? _(Unknown) : format_col_for_display(view, row, col)
+        @ostext = format_col_for_display(view, row, col).presence || _("Unknown")
       elsif COLUMN_WITH_OS_TEXT.include?(col)
         name = format_col_for_display(view, row, col)
-        text = name.blank? ?  @ostext : name 
-        image= @osicon  ? @osicon : ''
+        text = name.presence || @ostext
+        image = @osicon || ''
         item = {:title => text,
                 :image => ActionController::Base.helpers.image_path(image.to_s),
                 :text  => text}.compact
@@ -170,15 +169,8 @@ class GtlFormatter
   end
 
   def self.os_info_details(item)
-    return nil unless item
     "svg/os-#{item.os_image_name}.svg"
   end
-
-  def self.product_name_image(item)
-    return nil unless item
-    {:title => item.product_name}.compact
-  end
-
 
   def self.fonticon_or_fileicon(item)
     return nil unless item
