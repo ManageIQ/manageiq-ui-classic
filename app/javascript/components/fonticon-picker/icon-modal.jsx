573c5d0ef14b1cd9d9abf46040a5af8b7589cec1
jeffibm <jeffrey.bonson@ibm.com>
2021-08-03 19:39:40 +0530

    font-icon-picker pattern-fly items converted to carbon

--- app/javascript/components/fonticon-picker/icon-modal.jsx
+++ app/javascript/components/fonticon-picker/icon-modal.jsx
@@ -1,47 +1,35 @@
-import React, { useMemo } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  Grid, Row, Col,
-  Button,
-  Modal,
-  ButtonGroup,
-  Icon,
   Tabs,
   Tab,
-} from 'patternfly-react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-
+  Modal, ModalBody,
+} from 'carbon-components-react';
 import IconList from './icon-list';
 
 const IconModal = ({
-  showModal, hide, activeTab, selectedIcon, activeIcon, iconTypes, onModalApply, setState,
+  showModal, hide, activeTab, activeIcon, iconTypes, onModalApply, setState,
 }) =>
 
-
   (
-    <Modal show={showModal} onHide={hide} bsSize="large">
-      <Modal.Header>
-        <button
-          id="close-icon-picker-modal"
-          type="button"
-          className="close"
-          onClick={hide}
-          aria-label={__('Close')}
-        >
-          <Icon type="pf" name="close" />
-        </button>
-        <Modal.Title>{ __('Select an icon') }</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
+    <Modal
+      open={showModal}
+      modalHeading={__('Select an icon')}
+      primaryButtonText={__('Apply')}
+      secondaryButtonText={__('Cancel')}
+      onRequestSubmit={onModalApply}
+      onRequestClose={hide}
+      className="font-icon-list-modal"
+    >
+      <ModalBody className="font-icon-list-modal-body">
         <div className="fonticon-picker-modal">
           <Tabs
             id="font-icon-tabs"
-            activeKey={activeTab}
-            animation={false}
-            onSelect={(activeTab) => setState((state) => ({ ...state, activeTab }))}
+            onClick={(activeTab) => setState((state) => ({ ...state, activeTab }))}
           >
             { Object.keys(iconTypes).map((type) => (
-              <Tab eventKey={type} key={type} title={iconTypes[type]}>
+              <Tab key={type} label={iconTypes[type]}>
                 <IconList
                   {...{
                     type,
@@ -54,20 +42,7 @@ const IconModal = ({
             )) }
           </Tabs>
         </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          id="apply-icon-picker-icon"
-          bsStyle="primary"
-          onClick={onModalApply}
-          disabled={selectedIcon === activeIcon || activeIcon === undefined}
-        >
-          { __('Apply') }
-        </Button>
-        <Button id="cancel-icon-picker-modal" bsStyle="default" className="btn-cancel" onClick={hide}>
-          { __('Cancel') }
-        </Button>
-      </Modal.Footer>
+      </ModalBody>
     </Modal>
   );
 IconModal.propTypes = {
