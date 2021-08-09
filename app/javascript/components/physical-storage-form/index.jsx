51bb89ad5b75b24e16fb4187ba261f4650bcdfe9
Liran Livne <liran.livne@ibm.com>
2021-08-09 09:12:00 +0300

    implementation for editing physical storage

--- app/javascript/components/physical-storage-form/index.jsx
+++ app/javascript/components/physical-storage-form/index.jsx
@@ -1,40 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import MiqFormRenderer from '@@ddf';
+import { Loading } from 'carbon-components-react';
 import createSchema from './physical-storage-form.schema';
 import miqRedirectBack from '../../helpers/miq-redirect-back';
 
-const PhysicalStorageForm = ({ redirect }) => {
-  const state = useState(undefined);
+const PhysicalStorageForm = ({ recordId, storageManagerId }) => {
+  const [state, setState] = useState({});
+  const {isLoading, initialValues} = state;
+  const submitLabel = !!recordId ? __('Save') : __('Add');
 
-  const onSubmit = async(values) => {
+  const loadSchema = (appendState = {}) => () => {
+    setState((state) => ({
+      ...state,
+      ...appendState,
+    }));
+  };
+
+  useEffect(() => {
+    if (recordId) {
+      API.get(`/api/physical_storages/${recordId}`).then((initialValues) => {
+        API.options(`/api/physical_storages?ems_id=${initialValues.ems_id}`)
+          .then(loadSchema({ initialValues: { ...initialValues, edit: 'yes' }, isLoading: false }));
+      });
+    }
+    if (storageManagerId) {
+      API.options(`/api/physical_storages?ems_id=${storageManagerId}`)
+        .then(loadSchema({ initialValues: { ems_id: storageManagerId }, isLoading: false }));
+    }
+  }, [recordId, storageManagerId]);
+
+  const onSubmit = ({ edit: _edit, ...values }) => {
     miqSparkleOn();
-    const message = __('Physical storage added');
-    API.post('/api/physical_storages', { action: 'create', resource: values })
-      .then(() => miqRedirectBack(message, 'success', redirect)).catch(miqSparkleOff);
+
+    const request = recordId ? API.patch(`/api/physical_storages/${recordId}`, values) : API.post('/api/physical_storages', values);
+    request.then(() => {
+      const message = sprintf(
+        recordId
+          ? __('Modification of Physical Storage "%s" has been successfully queued.')
+          : __('Add of Physical Storage "%s" has been successfully queued.'),
+        values.name,
+      );
+      miqRedirectBack(message, undefined, '/physical_storage/show_list');
+    }).catch(miqSparkleOff);
   };
 
   const onCancel = () => {
-    const message = __('Adding of physical storage was cancelled by the user');
-    miqRedirectBack(message, 'success', redirect);
+    const message = sprintf(
+      recordId
+        ? __('Edit of Physical Storage "%s" was canceled by the user.')
+        : __('Add of new Physical Storage was cancelled by the user.'),
+      initialValues && initialValues.name,
+    );
+    miqRedirectBack(message, 'warning', '/physical_storage/show_list');
   };
 
-  return (
+  if (isLoading) return <Loading className="export-spinner" withOverlay={false} small />;
+
+  return !isLoading && (
     <MiqFormRenderer
-      schema={createSchema(...state)}
+      schema={createSchema(!!recordId, !!storageManagerId, initialValues, state, setState)}
+      initialValues={initialValues}
+      canReset={!!recordId}
       onSubmit={onSubmit}
+      onReset={() => add_flash(__('All changes have been reset'), 'warn')}
       onCancel={onCancel}
-      buttonsLabels={{
-        submitLabel: __('Add'),
-        cancelLabel: __('Cancel'),
-      }}
+      buttonsLabels={{ submitLabel }}
     />
   );
 };
 
 PhysicalStorageForm.propTypes = {
-  redirect: PropTypes.string.isRequired,
+  recordId: PropTypes.string,
+  storageManagerId: PropTypes.string,
+};
+PhysicalStorageForm.defaultProps = {
+  recordId: undefined,
+  storageManagerId: undefined,
 };
 
 export default PhysicalStorageForm;
---
+++ app/javascript/spec/physical-storage-form/physical-storage-form.spec.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import fetchMock from 'fetch-mock';
+import { shallow } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import PhysicalStorageForm from '../../components/physical-storage-form';
+import { mount } from '../helpers/mountForm';
+import miqRedirectBack from "../../helpers/miq-redirect-back";
+
+require('../helpers/miqSparkle.js');
+require('../helpers/miqAjaxButton.js');
+
+describe('Physical storage form component', () => {
+  const emsList = {
+    resources: [
+      { name: 'name1', id: 1 },
+      { name: 'name2', id: 2 },
+    ],
+  };
+
+  const physicalStorageMock = {
+    href:"https://9.151.190.197/api/physical_storages/1",
+    id:"1",
+    ems_ref:"d5ed1342-10a6-4604-9de5-f3cbaae5d467",
+    uid_ems:null,
+    name:"svc-178",
+    type:"ManageIQ::Providers::Autosde::StorageManager::PhysicalStorage",
+    access_state:null,
+    health_state:null,
+    overall_health_state:null,
+    ems_id:"2",
+    physical_rack_id:null,
+    drive_bays:null,
+    enclosures:null,
+    canister_slots:null,
+    created_at:"2021-08-19T08:54:41Z",
+    updated_at:"2021-08-19T08:56:22Z",
+    physical_chassis_id:null,
+    total_space:null,
+    physical_storage_family_id:"1",
+    actions:[
+      {"name":"edit","method":"patch","href":"https://9.151.190.197/api/physical_storages/1"},
+      {"name":"edit","method":"put","href":"https://9.151.190.197/api/physical_storages/1"},
+      {"name":"refresh","method":"post","href":"https://9.151.190.197/api/physical_storages/1"},
+      {"name":"delete","method":"post","href":"https://9.151.190.197/api/physical_storages/1"}
+    ]
+  };
+
+  const physicalStorageFamilyMock = {
+    href:"https://9.151.190.130/api/providers/2",
+    type:"ManageIQ::Providers::Autosde::StorageManager",
+    id:"2",
+    physical_storage_families:[
+      {"id":"1","name":"svc","version":"1.1","ems_id":"2","ems_ref":"4689c707-3064-4b1c-b001-7688bd9b5655","created_at":"2021-08-29T10:40:21Z","updated_at":"2021-08-29T10:40:21Z"},
+      {"id":"2","name":"xiv","version":"1.1","ems_id":"2","ems_ref":"b91e94ab-8056-4c61-bec6-00430e9c1e4c","created_at":"2021-08-29T10:40:21Z","updated_at":"2021-08-29T10:40:21Z"}
+    ]};
+
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it('should render adding form variant', (done) => {
+    const wrapper = shallow(<PhysicalStorageForm />);
+
+    setImmediate(() => {
+      wrapper.update();
+      expect(toJson(wrapper)).toMatchSnapshot();
+      done();
+    });
+  });
+
+  it('should render editing form variant', async(done) => {
+    fetchMock.getOnce('/api/physical_storages/1', physicalStorageMock);
+    fetchMock.mock('/api/physical_storages?ems_id=2', { data: { form_schema: { fields: [] } } }, { method: 'OPTIONS' });
+    fetchMock.mock('/api/providers?expand=resources&attributes=id,name,supports_block_storage&filter[]=supports_block_storage=true&filter[]=supports_add_storage=true', emsList);
+    fetchMock.mock('/api/providers?expand=resources&attributes=id,name,supports_block_storage&filter[]=supports_block_storage=true', emsList);
+    fetchMock.mock('/api/providers/2?attributes=type,physical_storage_families', physicalStorageFamilyMock);
+
+    let wrapper;
+    await act(async() => {
+      wrapper = mount(<PhysicalStorageForm recordId="1" />);
+    });
+    expect(fetchMock.called('/api/physical_storages/1')).toBe(true);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    done();
+  });
+
+  it('should call miqRedirectBack when canceling create form', async(done) => {
+    let wrapper;
+    await act(async() => {
+      wrapper = mount(<PhysicalStorageForm />);
+    });
+    wrapper.find('button').last().simulate('click');
+    expect(miqRedirectBack).toHaveBeenCalledWith('Add of new Physical Storage was cancelled by the user.', 'warning', '/physical_storage/show_list');
+    done();
+  });
+});
