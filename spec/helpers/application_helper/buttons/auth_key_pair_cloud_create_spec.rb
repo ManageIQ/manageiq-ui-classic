4ac1b639b17467bed70e0ce305e60fe5cdc068bb
Oleg Barenboim <chessbyte@gmail.com>
2021-09-28 14:05:36 -0400

    rspec refactoring

--- spec/helpers/application_helper/buttons/auth_key_pair_cloud_create_spec.rb
+++ spec/helpers/application_helper/buttons/auth_key_pair_cloud_create_spec.rb
@@ -1,41 +1,33 @@
 describe ApplicationHelper::Button::AuthKeyPairCloudCreate do
+  let(:button) { described_class.new(setup_view_context_with_sandbox({}), {}, {}, {}) }
+
+  def setup_ems(supports)
+    ems = object_double(ManageIQ::Providers::CloudManager.new)
+    allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(supports)
+    allow(Rbac).to receive(:filtered).and_return([ems])
+  end
+
   describe '#disabled?' do
-    it "when the create action is available then the button is not disabled" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new)
-      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(true)
-      allow(Rbac).to receive(:filtered).and_return([ems])
+    it "when the create action is supported, then the button is not disabled" do
+      setup_ems(true)
       expect(button.disabled?).to be false
     end
 
-    it "when the create action is unavailable then the button is disabled" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new)
-      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(false)
-      allow(Rbac).to receive(:filtered).and_return([ems])
+    it "when the create action is not supported, then the button is disabled" do
+      setup_ems(false)
       expect(button.disabled?).to be true
     end
   end
 
   describe '#calculate_properties' do
-    it "when the create action is unavailable the button has the error in the title" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new)
-      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(false)
-      allow(Rbac).to receive(:filtered).and_return([ems])
+    it "when the create action is not supported, then the button has the error in the title" do
+      setup_ems(false)
       button.calculate_properties
       expect(button[:title]).to eq("No cloud providers support key pair import or creation.")
     end
 
-    it "when the create action is available, the button has no error in the title" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new)
-      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(true)
-      allow(Rbac).to receive(:filtered).and_return([ems])
+    it "when the create action is supported, then the button has no error in the title" do
+      setup_ems(true)
       button.calculate_properties
       expect(button[:title]).to be nil
     end
