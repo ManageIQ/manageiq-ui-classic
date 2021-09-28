aebd2dc99ce8cadce023e091a95c71a908396ea4
Oleg Barenboim <chessbyte@gmail.com>
2021-09-28 11:50:36 -0400

    Fixed specs

--- spec/helpers/application_helper/buttons/auth_key_pair_cloud_create_spec.rb
+++ spec/helpers/application_helper/buttons/auth_key_pair_cloud_create_spec.rb
@@ -3,8 +3,8 @@ describe ApplicationHelper::Button::AuthKeyPairCloudCreate do
     it "when the create action is available then the button is not disabled" do
       view_context = setup_view_context_with_sandbox({})
       button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new, :class => ManageIQ::Providers::Openstack::CloudManager)
-      allow(ManageIQ::Providers::Openstack::CloudManager::AuthKeyPair).to receive(:is_available?).and_return(true)
+      ems = object_double(ManageIQ::Providers::CloudManager.new)
+      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(true)
       allow(Rbac).to receive(:filtered).and_return([ems])
       expect(button.disabled?).to be false
     end
@@ -12,8 +12,8 @@ describe ApplicationHelper::Button::AuthKeyPairCloudCreate do
     it "when the create action is unavailable then the button is disabled" do
       view_context = setup_view_context_with_sandbox({})
       button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new, :class => ManageIQ::Providers::Openstack::CloudManager)
-      allow(ManageIQ::Providers::Openstack::CloudManager::AuthKeyPair).to receive(:is_available?).and_return(false)
+      ems = object_double(ManageIQ::Providers::CloudManager.new)
+      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(false)
       allow(Rbac).to receive(:filtered).and_return([ems])
       expect(button.disabled?).to be true
     end
@@ -23,8 +23,8 @@ describe ApplicationHelper::Button::AuthKeyPairCloudCreate do
     it "when the create action is unavailable the button has the error in the title" do
       view_context = setup_view_context_with_sandbox({})
       button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new, :class => ManageIQ::Providers::Openstack::CloudManager)
-      allow(ManageIQ::Providers::Openstack::CloudManager::AuthKeyPair).to receive(:is_available?).and_return(false)
+      ems = object_double(ManageIQ::Providers::CloudManager.new)
+      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(false)
       allow(Rbac).to receive(:filtered).and_return([ems])
       button.calculate_properties
       expect(button[:title]).to eq("No cloud providers support key pair import or creation.")
@@ -33,8 +33,8 @@ describe ApplicationHelper::Button::AuthKeyPairCloudCreate do
     it "when the create action is available, the button has no error in the title" do
       view_context = setup_view_context_with_sandbox({})
       button = described_class.new(view_context, {}, {}, {})
-      ems = object_double(ManageIQ::Providers::CloudManager.new, :class => ManageIQ::Providers::Openstack::CloudManager)
-      allow(ManageIQ::Providers::Openstack::CloudManager::AuthKeyPair).to receive(:is_available?).and_return(true)
+      ems = object_double(ManageIQ::Providers::CloudManager.new)
+      allow(ems).to receive(:supports?).with(:auth_key_pair_create).and_return(true)
       allow(Rbac).to receive(:filtered).and_return([ems])
       button.calculate_properties
       expect(button[:title]).to be nil
