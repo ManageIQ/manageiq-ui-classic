31d041fca8bf5eaf8cc8023ea80e2a179daee7d6
Jeffrey Bonson <jeffrey.bonson@ibm.com>
2021-11-22 16:38:49 +0530

    removed patternfly references

--- app/javascript/spec/async-credentials/edit-password-field.spec.js
+++ app/javascript/spec/async-credentials/edit-password-field.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import { HelpBlock } from 'patternfly-react';
 import EditPasswordField from '../../components/async-credentials/edit-password-field';
+import HelperTextBlock from '../../forms/helper-text-block';
 
 jest.mock('@@ddf', () => ({
   useFieldApi: props => ({ meta: {}, input: {}, ...props }),
@@ -28,7 +28,7 @@ describe('Edit secret field form component', () => {
   it('should render correctly in error state', () => {
     initialProps.meta = { error: 'error message' };
     const wrapper = mount(<EditPasswordField {...initialProps} />);
-    expect(wrapper.find(HelpBlock)).toBeTruthy();
+    expect(wrapper.find(HelperTextBlock)).toBeTruthy();
   });
 
   it('should call setEditMode on input button click', () => {
