ab6e2794475961ad47d490fd84451ef567d49230
mengxiwuu <mengxiwuu@email.com>
2022-01-25 15:19:32 -0500

    chart layout

--- app/helpers/charting_helper.rb
+++ app/helpers/charting_helper.rb
@@ -24,10 +24,4 @@ module ChartingHelper
     when :c3 then c3chart_local(data, options.slice(:id))
     end
   end
-
-  def zoom_icon(zoom_url)
-    # could be just url or something like
-    # "javascript:miqAsyncAjax('/host/perf_chart_chooser/10000000000017?chart_idx=0')"
-    zoom_url =~ /clear('\))?$/ ? 'fa fa-search-minus fa-lg' : 'fa fa-search-plus fa-lg'
-  end
 end
