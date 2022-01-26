9c1a13a76307a4a861c875568d840a0eda7fc6a2
Jason Frey <fryguy9@gmail.com>
2022-01-26 16:22:10 -0500

    Merge pull request #8070 from kavyanekkalapu/fix_empty_chart_dashboard
    Fix dashboard empty chart
    
    (cherry picked from commit 2122193d76acf30c6b5f39fc7d0e1b7a4d5935a3)

--- app/javascript/components/dashboard-charts/index.jsx
+++ app/javascript/components/dashboard-charts/index.jsx
@@ -5,35 +5,38 @@ import {
   StackAreaChart, StackBarChartGraph, StackHorizontalChart,
 } from '../carbon-charts';
 import { getConvertedData } from '../carbon-charts/helpers';
+import EmptyChart from './emptyChart';
 
 const DashboardWidget = ({ data, id }) => {
   const convertedData = getConvertedData(data);
-
-  if (data.miqChart === 'Area') {
-    return (<AreaChartGraph data={convertedData} />);
-  }
-  if (data.miqChart === 'Donut') {
-    return (<DonutChartGraph data={convertedData} />);
-  }
-  if (data.miqChart === 'Line') {
-    return (<LineChartGraph data={convertedData} />);
-  }
-  if (data.miqChart === 'Pie') {
-    return (<PieChartGraph data={convertedData} />);
-  }
-  if (data.miqChart === 'StackedArea') {
-    return (<StackAreaChart data={convertedData} />);
-  }
-  if (data.miqChart === 'StackedBar' && data.data.groups) {
-    return (<StackHorizontalChart data={convertedData} />);
-  }
-  if ((data.miqChart === 'StackedColumn' || data.miqChart === 'Column') && data.data.groups) {
+  if (convertedData.length > 0) {
+    if (data.miqChart === 'Area') {
+      return (<AreaChartGraph data={convertedData} />);
+    }
+    if (data.miqChart === 'Donut') {
+      return (<DonutChartGraph data={convertedData} />);
+    }
+    if (data.miqChart === 'Line') {
+      return (<LineChartGraph data={convertedData} />);
+    }
+    if (data.miqChart === 'Pie') {
+      return (<PieChartGraph data={convertedData} />);
+    }
+    if (data.miqChart === 'StackedArea') {
+      return (<StackAreaChart data={convertedData} />);
+    }
+    if (data.miqChart === 'StackedBar' && data.data.groups) {
+      return (<StackHorizontalChart data={convertedData} />);
+    }
+    if ((data.miqChart === 'StackedColumn' || data.miqChart === 'Column') && data.data.groups) {
+      return (<StackBarChartGraph data={convertedData} />);
+    }
+    if ((data.miqChart === 'StackedColumn' || data.miqChart === 'Column') && !data.data.groups) {
+      return (<GroupBarChart data={convertedData} />);
+    }
     return (<StackBarChartGraph data={convertedData} />);
   }
-  if ((data.miqChart === 'StackedColumn' || data.miqChart === 'Column') && !data.data.groups) {
-    return (<GroupBarChart data={convertedData} />);
-  }
-  return (<StackBarChartGraph data={convertedData} />);
+  return <EmptyChart />;
 };
 
 DashboardWidget.propTypes = {
