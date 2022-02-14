16cfdc34cf93db7d3ead81d36ee549fb008c2529
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2022-02-14 16:23:08 -0500

    Fixed performance charts titles

--- app/javascript/components/performance-charts/index.jsx
+++ app/javascript/components/performance-charts/index.jsx
@@ -4,26 +4,30 @@ import LineChartGraph from './lineChart';
 import AreaChartGraph from './areaChart';
 import { getLineConvertedData } from '../carbon-charts/helpers';
 
-// eslint-disable-next-line no-unused-vars
-const PerformanceChartWidget = ({ data, id, size }) => {
+const PerformanceChartWidget = ({
+  // eslint-disable-next-line no-unused-vars
+  data, id, size, title,
+}) => {
   const convertedData = getLineConvertedData(data);
 
   if (data.miqChart === 'Area') {
-    return (<AreaChartGraph data={convertedData} format={data.miq.format} size={size} />);
+    return (<AreaChartGraph data={convertedData} format={data.miq.format} size={size} title={title} />);
   }
-  return (<LineChartGraph data={convertedData} format={data.miq.format} size={size} />);
+  return (<LineChartGraph data={convertedData} format={data.miq.format} size={size} title={title} />);
 };
 
 PerformanceChartWidget.propTypes = {
   data: PropTypes.instanceOf(Object),
   id: PropTypes.string,
   size: PropTypes.string,
+  title: PropTypes.string,
 };
 
 PerformanceChartWidget.defaultProps = {
   data: null,
   id: null,
   size: '400px',
+  title: '',
 };
 
 export default PerformanceChartWidget;
--- app/javascript/components/performance-charts/lineChart.js
+++ app/javascript/components/performance-charts/lineChart.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { LineChart } from '@carbon/charts-react';
 
-const LineChartGraph = ({ data, format, size }) => {
+const LineChartGraph = ({
+  data, format, size, title,
+}) => {
   const getYAxisValue = (format, value) => {
     // eslint-disable-next-line no-useless-escape
     const tmp = /^([0-9\,\.]+)(.*)/.exec(ManageIQ.charts.formatters[format.function].c3(format.options)(value));
@@ -10,6 +12,7 @@ const LineChartGraph = ({ data, format, size }) => {
   };
 
   const options = {
+    title,
     axes: {
       bottom: {
         mapsTo: 'key',
@@ -40,12 +43,14 @@ LineChartGraph.propTypes = {
   data: PropTypes.instanceOf(Array),
   format: PropTypes.instanceOf(Object),
   size: PropTypes.string,
+  title: PropTypes.string,
 };
 
 LineChartGraph.defaultProps = {
   data: null,
   format: null,
   size: '400px',
+  title: '',
 };
 
 export default LineChartGraph;
