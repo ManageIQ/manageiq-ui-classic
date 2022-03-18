90170ec01b9571a1078a7e58a51cf5f1424a8fc1
Kavya Nekkalapu <knekkal@us.ibm.com>
2022-03-18 12:30:41 -0400

    Add native console button for  ems cloud provider dashboard

--- app/helpers/application_helper/button/configured_system_console.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationHelper::Button::ConfiguredSystemConsole < ApplicationHelper::Button::Basic
-  needs :@record
-
-  def visible?
-    @record.supports_console?
-  end
-end
--- app/helpers/application_helper/button/physical_infra_console.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationHelper::Button::PhysicalInfraConsole < ApplicationHelper::Button::Basic
-  needs(:@record)
-
-  def visible?
-    @record.supports_console?
-  end
-end
