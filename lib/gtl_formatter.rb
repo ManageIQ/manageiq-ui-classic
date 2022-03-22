ManageIQ/manageiq-ui-classic

commit b1a5e86c013e44067de058bac6bfc6ef4b7e1e8b

Fix vms table os image

lib/gtl_formatter.rb
@@ -125,13 +125,15 @@ class GtlFormatter
                 :text  => text}.compact
       elsif COLUMN_WITH_TIME.include?(col)
         celltext = format_time_for_display(row, col)
-      elsif COLUMN_WITH_OS_ICON.key?(col)
-        @osicon = send(COLUMN_WITH_OS_ICON[col], record)
-        @ostext = format_col_for_display(view, row, col).presence || _("Unknown")
       elsif COLUMN_WITH_OS_TEXT.include?(col)
+        osicon_col = view.col_order.detect { |c| COLUMN_WITH_OS_ICON.key?(c) }
+        if osicon_col
+          osicon = send(COLUMN_WITH_OS_ICON[osicon_col], record)
+          ostext = format_col_for_display(view, row, osicon_col).presence || _("Unknown")
+        end
         name = format_col_for_display(view, row, col)
-        text = name.presence || @ostext
-        image = @osicon || ''
+        text = name.presence || ostext
+        image = osicon || ''
         item = {:title => text,
                 :image => ActionController::Base.helpers.image_path(image.to_s),
                 :text  => text}.compact

commit b83407b80a6c0a88dfe133370a24366d528f730f

fix sort columns
If a report is sorting by a ruby field
and the values are cached

Then `record` will not have all the attributes.

We noticed this with power_state, as it was blowing
up on the second page or when sort is toggled

fix is to access the `row` data instead of the `record` data

fixes bug introduced in b1a5e86c013e44

lib/gtl_formatter.rb
@@ -114,7 +114,7 @@ class GtlFormatter
         text = format_col_for_display(view, row, col)
         item = {:title      => text,
                 :icon       => icon,
-                :background => textual_power_state_whitelisted(record[col]),
+                :background => textual_power_state_whitelisted(row[col]),
                 :text       => text}.compact
       elsif COLUMN_WITH_ICON.key?(col)
         # Generate html for the list icon