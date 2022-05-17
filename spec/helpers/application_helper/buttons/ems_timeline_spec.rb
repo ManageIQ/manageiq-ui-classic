7762e154e87c077e64cb5e6a66b2eb34aebd69f8
Keenan Brock <keenan@thebrocks.net>
2022-05-17 14:49:57 -0400

    convert stubs to use classes (timeline)

--- spec/helpers/application_helper/buttons/ems_timeline_spec.rb
+++ spec/helpers/application_helper/buttons/ems_timeline_spec.rb
@@ -1,39 +1,52 @@
 describe ApplicationHelper::Button::EmsTimeline do
+  include Spec::Support::SupportsHelper
+
+  # there are no events. reference event to create it
+  let(:event)        { FactoryBot.create(:ems_event, :vm_or_template => record) }
+  let(:record)       { FactoryBot.build(:vm_cloud) }
+  let(:view_context) { setup_view_context_with_sandbox({}) }
+  let(:button)       { described_class.new(view_context, {}, {"record" => record}, {}) }
+
   describe '#disabled?' do
-    it "when the timeline action is available then the button is not disabled" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :supports_timeline? => true, :has_events? => true)}, {}
-      )
-      expect(button.disabled?).to be false
+    context "when the timeline action is available" do
+      before { temp_stub_supports(record, :timeline) }
+      it "is not disabled" do
+        event
+        expect(button.disabled?).to be false
+      end
     end
 
-    it "when the timeline action is unavailable then the button is disabled" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :supports_timeline? => false, :has_events? => false)}, {}
-      )
-      expect(button.disabled?).to be true
+    context "when the timeline action is unavailable" do
+      before { temp_stub_supports(record, :timeline, supports: false) }
+      it "is disabled" do
+        expect(button.disabled?).to be true
+      end
     end
   end
 
   describe '#calculate_properties' do
-    it "when the timeline action is unavailable the button has the error in the title" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :supports_timeline? => false, :has_events? => false)}, {}
-      )
-      button.calculate_properties
-      expect(button[:title]).to eq("No Timeline data has been collected for this Provider")
+    context "when the timeline action is unavailable" do
+      before { temp_stub_supports(record, :timeline, supports: false) }
+      it "has the error in the title" do
+        button.calculate_properties
+        expect(button[:title]).to eq("No Timeline data has been collected for this Provider")
+      end
     end
 
-    it "when the timeline is available, the button has no error in the title" do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :supports_timeline? => true, :has_events? => true)}, {}
-      )
-      button.calculate_properties
-      expect(button[:title]).to be nil
+    context "when the timeline is available" do
+      before { temp_stub_supports(record, :timeline) }
+      it "has no error in the title" do
+        event
+        button.calculate_properties
+        expect(button[:title]).to be nil
+      end
     end
+  end
+
+  private
+
+  def temp_stub_supports(record, feature, supports: true)
+    allow(record).to receive("supports_#{feature}?").and_return(supports)
+    allow(record).to receive(:unsupported_reason).and_return("Feature not available/supported") unless supports
   end
 end
