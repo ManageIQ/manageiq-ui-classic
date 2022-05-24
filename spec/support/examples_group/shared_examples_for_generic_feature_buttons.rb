ac59a40f8e1586eae8a591d7ce37d5cb261618e8
Keenan Brock <keenan@thebrocks.net>
2022-05-24 15:36:16 -0400

    Generic feature button examples and supports
    - using stub_supports for feature button (and w/ disabled)
    - generic button with disabled test now ensuring visible is always true
      so we can now use it for storage scan spec

--- spec/support/examples_group/shared_examples_for_generic_feature_buttons.rb
+++ spec/support/examples_group/shared_examples_for_generic_feature_buttons.rb
@@ -1,21 +1,14 @@
-shared_examples_for 'a generic feature button' do
+shared_examples_for 'a feature button' do
   include_examples 'a generic feature button after initialization'
 
   describe '#visible?' do
     subject { button.visible? }
-    before do
-      allow(record).to receive("respond_to?").with("supports_#{feature}?").and_return(true)
-      # HACK: to mock Array.wrap method for Double
-      allow(record).to receive("respond_to?").with(:to_ary).and_return(false)
-      allow(record).to receive("supports?").with(feature.to_sym).and_return(supports_feature)
-    end
-
     context "when record supports #{feature}" do
-      let(:supports_feature) { true }
+      before { stub_supports(record, feature) }
       it { expect(subject).to be_truthy }
     end
     context "when record does not support #{feature}" do
-      let(:supports_feature) { false }
+      before { stub_supports_not(record, feature, "Feature not available/supported") }
       it { expect(subject).to be_falsey }
     end
   end
@@ -28,24 +21,29 @@ shared_examples_for 'a generic feature button after initialization' do
   end
 end
 
-shared_examples_for 'a generic feature button with disabled' do
-  include_examples 'a generic feature button'
-  include_examples 'GenericFeatureButtonWithDisabled#calculate_properties'
-end
+shared_examples_for 'a feature button with disabled' do
+  include_examples 'a generic feature button after initialization'
 
-shared_examples_for 'GenericFeatureButtonWithDisabled#calculate_properties' do
-  describe '#disabled?' do
-    before do
-      allow(record).to receive("supports?").with(feature.to_sym).and_return(support)
-      allow(record).to receive(:unsupported_reason).with(feature).and_return("Feature not available/supported") if !support
+  # always visible  (not sure if we need to test)
+  describe '#visible?' do
+    subject { button.visible? }
+    context "when record supports #{feature}" do
+      before { stub_supports(record, feature) }
+      it { expect(subject).to be_truthy }
     end
+    context "when record does not support #{feature}" do
+      before { stub_supports_not(record, feature, "Feature not available/supported") }
+      it { expect(subject).to be_truthy }
+    end
+  end
 
-    context 'when feature is supported' do
-      let(:support) { true }
+  describe '#disabled?' do
+    context "when records support #{feature}" do
+      before { stub_supports(record, feature) }
       it_behaves_like 'an enabled button'
     end
-    context 'when feature is not supported' do
-      let(:support) { false }
+    context "when records do not support #{feature}" do
+      before { stub_supports_not(record, feature, "Feature not available/supported") }
       it_behaves_like 'a disabled button', 'Feature not available/supported'
     end
   end
