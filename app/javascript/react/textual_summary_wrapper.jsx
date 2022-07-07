c98fb3d7091db45a707b332ab7b7949119c2e38a
Jason Frey <fryguy9@gmail.com>
2022-07-07 14:59:58 -0400

    Merge pull request #8343 from jeffibm/fix-textual-summary
    Fix for not able to provision via images
    
    (cherry picked from commit 4daae98b9e6a1faa9ad9b442a3339a6a52b5eee8)

--- app/javascript/react/textual_summary_wrapper.jsx
+++ app/javascript/react/textual_summary_wrapper.jsx
@@ -6,8 +6,10 @@ import textualSummaryGenericClick from './textual_summary_click';
 export default (props) => {
   const onClick = props.onClick || textualSummaryGenericClick;
   const component = <TextualSummary onClick={onClick} {...props} />;
-  document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(component, document.body.appendChild(document.createElement('div')));
-  });
+  if (props.options) {
+    document.addEventListener('DOMContentLoaded', () => {
+      ReactDOM.render(component, document.body.appendChild(document.createElement('div')));
+    });
+  }
   return component;
 };
