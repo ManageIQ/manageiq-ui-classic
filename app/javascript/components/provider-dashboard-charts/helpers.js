80e0e251c51fc2093a25e1f1286996f079642ddf
Or Gur <Or.Gur@ocean.ibm.com>
2022-07-25 09:41:12 +0300

    `helpers.getHeatMapData`:  - now checks both for `data` existence and validity, and returns an empty array if false. - improved function structure.
    `HeatMapChartGraph.js`: `heatData1/2` are no longer necessary due to the above, `getHeatMapData` is passed `data[datapoint1/2]`.

--- app/javascript/components/provider-dashboard-charts/helpers.js
+++ app/javascript/components/provider-dashboard-charts/helpers.js
@@ -27,8 +27,8 @@ export const getLatestValue = (data) => {
 };
 
 export const getHeatMapData = (data) => {
+  const arr = [];
   if (data && data.node !== undefined) {
-    const arr = [];
     data.forEach((item) => {
       const obj = {};
       obj.percent = item.percent;
@@ -39,9 +39,8 @@ export const getHeatMapData = (data) => {
       obj.unit = item.unit;
       arr.push(obj);
     });
-    return arr;
   }
-  return [];
+  return arr;
 };
 
 export const getPodsData = (data, createdlabel, deletedLabel) => {
