0454551cce563c6bf1f0218355f03116e6c11e13
David Resende <david.resende@ibm.com>
2022-08-31 14:24:56 -0400

    Fixed Various UI Issues Related to the Provider Dashboard Charts

--- app/javascript/components/provider-dashboard-charts/provider-dashboard-utilization-chart/utilizationMemoryDonutChart.js
+++ app/javascript/components/provider-dashboard-charts/provider-dashboard-utilization-chart/utilizationMemoryDonutChart.js
@@ -76,7 +76,7 @@ const UtilizationMemoryDonutChart = ({ data, config }) => {
   const areaChartData = getConvertedData(memoryData, config.units);
 
   return (
-    <div>
+    <div className="utilization-memory-chart-pf">
       <div className="utilization-trend-chart-pf">
         <h3>{config.title}</h3>
         <div className="current-values">
