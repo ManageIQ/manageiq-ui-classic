a11be2ecfd7bc71e40dec7a7c2a8b6639b2c8b20
Jay Carman <jwcarman@us.ibm.com>
2022-10-07 14:19:26 -0500

    Update Placement Group "textual summary" specs

--- spec/helpers/placement_group_helper/textual_summary_spec.rb
+++ spec/helpers/placement_group_helper/textual_summary_spec.rb
@@ -1,4 +1,4 @@
 describe PlacementGroupHelper::TextualSummary do
-  include_examples "textual_group", "Relationships", %i(ems_cloud)
-  include_examples "textual_group", "Properties", %i(name type status ems_ref)
+  include_examples "textual_group", "Relationships", %i(ems_cloud instances)
+  include_examples "textual_group", "Properties", %i(name policy)
 end
