4ffad4ada28339d5489880a9caef96cf00a6bb10
David Resende <david.resende@ibm.com>
2022-11-24 09:06:24 -0500

    Add Banner to Server By Roles Page

--- app/presenters/tree_builder_servers_by_role.rb
+++ app/presenters/tree_builder_servers_by_role.rb
@@ -16,6 +16,8 @@ class TreeBuilderServersByRole < TreeBuilderDiagnostics
   def x_get_tree_server_roles
     ServerRole.all.sort_by(&:description).each_with_object([]) do |r, objects|
       next if @root.kind_of?(MiqRegion) && !r.regional_role? # Only regional roles under Region
+      next unless (@root.kind_of?(Zone) && r.miq_servers.any? { |s| s.my_zone == @root.name }) ||
+                  (@root.kind_of?(MiqRegion) && !r.miq_servers.empty?) # Skip if no assigned servers in this zone
       next if r.name == "database_owner"
       unless @sb[:diag_selected_id] # Set default selected record vars
         @sb[:diag_selected_model] = r.class.to_s
