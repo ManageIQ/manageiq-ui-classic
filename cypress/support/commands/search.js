67c390cf9d7bdcddfbe1c17aeb608ae9048a94af
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2023-06-09 10:33:44 -0400

    Fix searchbox cypress tests

--- cypress/support/commands/search.js
+++ cypress/support/commands/search.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
+
 // Searchbox related helpers
 Cypress.Commands.add('search_box', () => {
   return cy.get('#search_text').should('be.visible');
 });
 
 Cypress.Commands.add('no_search_box', () => {
-  return cy.get('#search_text').should('not.be.visible');
+  return cy.get('#search_text').should('not.exist');
 });
