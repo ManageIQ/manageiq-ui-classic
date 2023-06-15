9a3e9038adb0b9a626f74e0b63b44af8470d6da3
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2023-06-15 15:23:20 -0400

    Add logout cypress test

--- cypress/e2e/ui/login.cy.js
+++ cypress/e2e/ui/login.cy.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-undef */
 
-describe('Login', () => {
+describe('Login / Logout', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   // do not use cy.login() here
-  it('login page', () => {
+  it('Login page', () => {
     cy.get('#user_name')
       .type('admin');
     cy.get('#user_password')
@@ -13,5 +14,22 @@ describe('Login', () => {
     cy.get('#login').click();
 
     cy.get('.navbar-brand-name');
+  });
+
+  it('Logout button', () => {
+    cy.login();
+    cy.intercept('GET', '/api/auth?requester_type=ws').as('get');
+    cy.get('#menu_item_logout').click();
+
+    cy.get('@get').then((getCall) => {
+      expect(getCall.state).to.equal('Complete');
+      expect(getCall.response).to.include({
+        statusCode: 401,
+      });
+    });
+
+    cy.url().then((url) => {
+      expect(url).to.equal('http://localhost:3000/#/');
+    });
   });
 });
