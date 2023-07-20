5bd41db5035838aa1e6c9871c129a32036bc46e9
Jason Frey <fryguy9@gmail.com>
2023-07-20 14:09:56 -0400

    Delete the cypress videos for passing tests on CI
    This is for CI purposes, where these extra videos take up space, and
    aren't needed when trying to debug failures. Additionally, this commit
    removes the video compression since it takes time, and we are going to
    delete 90%+ of the videos anyway.

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,24 @@
 const { defineConfig } = require('cypress');
+const fs = require('fs')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     numTestsKeptInMemory: 0,
+    videoCompression: false,
+    setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        // Delete the video on CI if the spec passed and no tests retried
+        if (process.env.CI && results && results.video && fs.existsSync(results.video)) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+    },
   },
 });
