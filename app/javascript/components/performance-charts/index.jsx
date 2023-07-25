5e732d44a6c696a3c090ba59de9bb9e505b49633
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2023-07-25 12:10:36 -0400

    Fix utilization chart

--- app/javascript/components/performance-charts/index.jsx
+++ app/javascript/components/performance-charts/index.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LineChartGraph from './lineChart';
 import AreaChartGraph from './areaChart';
-import { getLineConvertedData } from '../carbon-charts/helpers';
+import GroupBarChart from './groupChart';
+import { getConvertedData, getLineConvertedData } from '../carbon-charts/helpers';
+import EmptyChart from '../dashboard-widgets/dashboard-charts/emptyChart';
 
 const PerformanceChartWidget = ({
   // eslint-disable-next-line no-unused-vars
   data, id, size, title,
 }) => {
-  const convertedData = getLineConvertedData(data);
-
+  let convertedData = getLineConvertedData(data);
+  if (data.miq && data.miq.empty) {
+    return (<EmptyChart />);
+  }
   if (data.miqChart === 'Area') {
     return (<AreaChartGraph data={convertedData} format={data.miq.format} size={size} title={title} />);
+  }
+  if ((data.miqChart === 'StackedColumn' || data.miqChart === 'Column') && !data.data.groups) {
+    convertedData = getConvertedData(data);
+    return (<GroupBarChart data={convertedData} format={data.miq.format} size={size} title={title} />);
   }
   return (<LineChartGraph data={convertedData} format={data.miq.format} size={size} title={title} />);
 };
