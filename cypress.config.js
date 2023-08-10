844db2ef00856bd261f64352ce6aa87fa209431a
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2023-08-10 16:24:39 -0400

    Fix cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-undef */
 const { defineConfig } = require('cypress');
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    viewportHeight: 800,
+    viewportWidth: 1800,
     numTestsKeptInMemory: 0,
     videoCompression: false,
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       on('after:spec', (spec, results) => {
         // Delete the video on CI if the spec passed and no tests retried
@@ -13,12 +17,12 @@ module.exports = defineConfig({
           // Do we have failures for any retry attempts?
           const failures = results.tests.some((test) =>
             test.attempts.some((attempt) => attempt.state === 'failed')
-          )
+          );
           if (!failures) {
-            fs.unlinkSync(results.video)
+            fs.unlinkSync(results.video);
           }
         }
-      })
+      });
     },
   },
 });
