0b35d5063c0540c044d936f8e3373c1e9204a93a
Jason Frey <fryguy9@gmail.com>
2023-09-12 10:03:38 -0400

    Revert "Merge pull request #8852 from akhilkr128/filter_effect_fix"
    This reverts commit 7e845813ca3588t5b57cae5f67a5a00d7a1000a18, reversing
    changes made to a8ece64a10d0cb1c8019c14b0a2f1dce16b86346.

--- app/controllers/vm_common.rb
+++ app/controllers/vm_common.rb
@@ -787,7 +787,6 @@ module VmCommon
   end
 
   def list_child_vms(model, node_id, title, show_list)
-    @edit = session[:edit]
     options = {
       :model       => model,
       :named_scope => scopes_for_role,
