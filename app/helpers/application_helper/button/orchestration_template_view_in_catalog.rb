7acae078641d72a8fa0372ae244d5a231b15c384
Keenan Brock <keenan@thebrocks.net>
2024-02-07 18:42:13 -0500

    convert ServiceTemplate.orderable? to supports?(:order)
    Trying to move this over to supports mixin
    (dropping orderable?)

--- app/helpers/application_helper/button/orchestration_template_view_in_catalog.rb
+++ app/helpers/application_helper/button/orchestration_template_view_in_catalog.rb
@@ -1,6 +1,6 @@
 class ApplicationHelper::Button::OrchestrationTemplateViewInCatalog < ApplicationHelper::Button::Basic
   def disabled?
-    @error_message = _('This Template is not orderable') unless @record.orchestration_template.orderable?
+    @error_message = _('This Template is not orderable') unless @record.orchestration_template.supports?(:order)
     @error_message.present?
   end
 end
