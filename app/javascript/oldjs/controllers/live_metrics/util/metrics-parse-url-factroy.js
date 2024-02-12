876c7e5740d733df14ac175b33cc156258d8aacd
Jason Frey <fryguy9@gmail.com>
2024-02-12 14:19:29 -0500

    Remove unnecessary regex

--- app/javascript/oldjs/controllers/live_metrics/util/metrics-parse-url-factroy.js
+++ app/javascript/oldjs/controllers/live_metrics/util/metrics-parse-url-factroy.js
@@ -1,36 +1,19 @@
 angular.module('miq.util').factory('metricsParseUrlFactory', function() {
-  var re = /([^&=]+)=?([^&]*)/g;
-  var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
-  var decode = function(str) {
-    return decodeURIComponent( str.replace(decodeRE, ' ') );
-  };
-
-  var parseParams = function(query) {
-    var params = {};
-    var e;
-
-    e = re.exec(query);
-    while (e) {
-      var k = decode( e[1] );
-      var v = decode( e[2] );
-
-      if (k.substring(k.length - 2) === '[]') {
-        k = k.substring(0, k.length - 2);
-        (params[k] || (params[k] = [])).push(v);
-      } else {
-        params[k] = v;
-      }
-
-      e = re.exec(query);
+  var paramsToObject = (entries) => {
+    const result = {}
+    for(const [key, value] of entries) {
+      result[key] = value;
     }
-    return params;
-  };
+    return result;
+  }
 
   return function(dash, $window) {
     // get the pathname and remove trailing / if exist
     var pathname = $window.location.pathname.replace(/\/$/, '');
 
-    dash.params = parseParams($window.location.search);
+    var params = new URLSearchParams($window.location.search);
+    dash.params = paramsToObject(params);
+
     dash.providerId = '/' + (/^\/[^\/]+\/([r\d]+)$/.exec(pathname)[1]);
 
     dash.tenantList = [];
