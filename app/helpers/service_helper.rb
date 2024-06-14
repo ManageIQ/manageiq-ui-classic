f1eb0821bba4e7b8ac1ba70b6cf9cb2dccf5aecb
jeffbonson <jeffbonson@gmail.com>
2024-06-14 18:49:12 +0530

    Fix service child links

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -1,3 +1,17 @@
 module ServiceHelper
   include_concern 'TextualSummary'
+
+  def child_service_summary(child_services)
+    rows = []
+    data = {:title => _("Child Services"), :mode => "miq_child_services"}
+    child_services.sort_by { |o| o.name.downcase }.each do |service|
+      rows.push(
+        {
+          :cells => [{:icon => 'pficon pficon-service', :value => service.name, :link => "/service/show/#{service.id}"}],
+        }
+      )
+    end
+    data[:rows] = rows
+    miq_structured_list(data)
+  end
 end
