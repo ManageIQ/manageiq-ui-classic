ed39b8d93df084900fa54229b18a24726a497045
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2024-10-15 15:03:51 -0400

    Remove unused namespace angular code

--- app/javascript/oldjs/components/index.js
+++ app/javascript/oldjs/components/index.js
@@ -1,7 +1,6 @@
 require('./ae_resolve_options/ae-resolve-options.js');
 require('./index.js');
 require('./miq-button.js');
-require('./miq_ae_class/namespace-form.js');
 require('./provider-option-field-input.js');
 require('./provier-option-section.js');
 require('./sanitize.js');
