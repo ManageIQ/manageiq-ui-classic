bd4adf4c02f9b29b38f8caaa5a5d6caca9b4a59b
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2024-10-17 13:02:22 -0400

    Remove unused angular controllers

--- app/javascript/oldjs/components/index.js
+++ app/javascript/oldjs/components/index.js
@@ -1,7 +1,5 @@
 require('./ae_resolve_options/ae-resolve-options.js');
 require('./index.js');
 require('./miq-button.js');
-require('./provider-option-field-input.js');
-require('./provier-option-section.js');
 require('./sanitize.js');
 require('./verify-button.js');
