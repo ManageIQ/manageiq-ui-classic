699919cd666431abecc81e1c22121dcf21fcb20b
Joe Rafaniello <jrafanie@gmail.com>
2025-04-28 16:11:59 -0400

    miqHideSearchClearButton is unused as of 450960c8e2e23ddd5fb8ce46e3259a765005ba60

--- app/javascript/oldjs/miq_application.js
+++ app/javascript/oldjs/miq_application.js
@@ -1309,29 +1309,6 @@ window.miqInitMainContent = function() {
   $('#main-content').css('height', `calc(100% - ${height}px)`);
 };
 
-window.miqHideSearchClearButton = function(explorer) {
-  // Hide the clear button if the search input is empty
-  $('.search-pf .has-clear .clear').each(function() {
-    if (!$(this).prev('.form-control').val()) {
-      $(this).hide();
-    }
-  });
-  // Show the clear button upon entering text in the search input
-  $('.search-pf .has-clear .form-control').keyup(function() {
-    const t = $(this);
-    t.nextAll('button.clear').toggle(Boolean(t.val()));
-  });
-  // Upon clicking the clear button, empty the entered text and hide the clear button
-  $('.search-pf .has-clear .clear').click(function() {
-    $(this).prev('.form-control').val('').focus();
-    $(this).hide();
-    // Clear Search text values as well
-    const url = `/${ManageIQ.controller}/search_clear` + `?in_explorer=${explorer}`;
-    ManageIQ.gridChecks = [];
-    miqJqueryRequest(url);
-  });
-};
-
 window.toggle_expansion = function(link) {
   link = $(link);
   link.find('i').toggleClass('fa-angle-right fa-angle-down');
