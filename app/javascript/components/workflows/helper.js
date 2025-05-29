08eb0dd2e9cc18c91ed0c08b772adf76fe153cba
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2025-05-29 12:03:26 -0400

    Enhance the workflow entry point modal

--- app/javascript/components/workflows/helper.js
+++ app/javascript/components/workflows/helper.js
@@ -2,11 +2,13 @@ import { rowData } from '../miq-data-table/helper';
 
 /** Function to return the header information for the service catalog item's entry points. */
 const entryPointsHeaderInfo = () => [
-  { header: __('Name'), key: 'name' },
+  { header: __('Repository'), key: 'configuration_script_source.name' },
+  { header: __('Workflow name'), key: 'name' },
 ];
 
 /** Function to return the cell data for a row item. */
-const celInfo = (workflow) => [
+const cellInfo = (workflow) => [
+  { text: workflow.configuration_script_source ? workflow.configuration_script_source.name : '' },
   { text: workflow.name },
 ];
 
@@ -14,7 +16,7 @@ const celInfo = (workflow) => [
 const rowInfo = (headers, response) => {
   const headerKeys = headers.map((item) => item.key);
   const rows = response.resources.filter((item) => item.payload).map((workflow) => ({
-    id: workflow.id.toString(), cells: celInfo(workflow), clickable: true,
+    id: workflow.id.toString(), cells: cellInfo(workflow), clickable: true,
   }));
   const miqRows = rowData(headerKeys, rows, false);
   return miqRows.rowItems;
