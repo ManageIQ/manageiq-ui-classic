1d5b2a81ce1bf11edc9e39a02de43b3c7b47a6e9
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2025-07-09 16:57:38 +0530

    Enhanced accordionItem comand to allow partial match and expand nested options

--- cypress/support/commands/explorer.js
+++ cypress/support/commands/explorer.js
@@ -18,9 +18,35 @@ Cypress.Commands.add('accordion', (title) => {
   return ret.parents('.panel');
 });
 
-// name: String of the record in the accordion panel to click.
-Cypress.Commands.add('accordionItem', (name) => {
+/**
+ * name: String of the record in the accordion panel to click.
+ * partialMatch: Boolean to indicate if the name should be matched partially.
+ * parentAccordId: Optional ID of the parent accordion to scope the search.
+ *                 If provided, the search will be limited to items within that accordion.
+ */
+Cypress.Commands.add('accordionItem', (name, partialMatch = false, parentAccordId) => {
   cy.get('#main-content'); // ensure screen loads first
 
-  cy.get('.list-group-item').contains(name).click();
+  const selector = parentAccordId
+    ? `.sidebar-pf-left #${parentAccordId} .list-group-item`
+    : '.sidebar-pf-left .list-group-item';
+  cy.get(selector).each(($el) => {
+    const text = $el.text().trim();
+    const isMatch = partialMatch
+      ? text.toLowerCase().includes(name.toLowerCase())
+      : text === name;
+
+    if (isMatch) {
+      const span = $el.find('span.fa-angle-right');
+      if (span.length) {
+        cy.wrap(span).click({ force: true });
+      } else {
+        cy.wrap($el).click({ force: true });
+      }
+
+      // Stop looping after finding and clicking the matching item
+      return false;
+    }
+    return true; // continue looping
+  });
 });
