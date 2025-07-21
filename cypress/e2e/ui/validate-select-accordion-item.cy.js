a0a77797be396379bf77a77b9073e66d8ea90b71
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2025-07-21 16:51:22 +0530

    Added samplecypress test to validate selectAccordianItem cypress command

---
+++ cypress/e2e/ui/validate-select-accordion-item.cy.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+
+describe('Validate clickItem', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.menu('Settings', 'Application Settings');
+  });
+
+  it('should fail when an invalid node label is passed', (done) => {
+    cy.accordion('Access Control');
+
+    cy.on('fail', (err) => {
+      expect(err.message).to.include('not found');
+      done();
+    });
+
+    cy.selectAccordionItem([
+      'ManageIQ Region: Region 0 [0]',
+      'Tenants',
+      'No Company', // This label does not exist
+    ]);
+  });
+
+  it('should support path arrays containing both exact strings and regex patterns', () => {
+    cy.accordion('Settings');
+    cy.selectAccordionItem([
+      /^ManageIQ Region/,
+      'Zones',
+      /^Zone: Default/,
+      'Server: EVM [1] (current)',
+    ]);
+  });
+
+  it('should not collapse already expanded node', () => {
+    cy.accordion('Settings');
+    cy.selectAccordionItem([
+      /^ManageIQ Region/,
+      'Zones',
+      /^Zone: Default/,
+      'Server: EVM [1] (current)',
+    ]);
+    // Above path is already expanded, so this should not collapse it
+    cy.selectAccordionItem([
+      /^ManageIQ Region/,
+      'Zones',
+      /^Zone: Default/,
+      'Server: EVM [1] (current)',
+    ]);
+  });
+});
