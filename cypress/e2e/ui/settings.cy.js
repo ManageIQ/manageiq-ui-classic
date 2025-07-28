d7a410d5f57f19ab65cb5ce9c7d01dcae9b66c72
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2025-07-28 12:34:09 -0400

    Added cypress tests for startpage

--- cypress/e2e/ui/settings.cy.js
+++ cypress/e2e/ui/settings.cy.js
@@ -1,6 +1,34 @@
 /* eslint-disable no-undef */
-describe('GTL', () => {
+describe('Settings > My Settings', () => {
   beforeEach(() => {
     cy.login();
+    cy.menu('Settings', 'My Settings');
+  });
+
+  it('Saves the start page setting', () => {
+    cy.get('#display\\.startpage').then((value) => {
+      expect(value[0].value).to.be.oneOf(['', 'Overview / Dashboard']);
+    });
+    cy.changeSelect('display\\.startpage', 'Overview / Utilization');
+    cy.intercept('PATCH', '/api/users/1').as('settingsUpdate');
+    cy.contains('button', 'Save').click();
+    cy.wait('@settingsUpdate').its('response.statusCode').should('eq', 200);
+
+    cy.get('#menu_item_logout').click();
+    cy.login();
+    cy.url().should('include', '/utilization');
+
+    cy.menu('Settings', 'My Settings');
+    cy.get('#display\\.startpage').then((value) => {
+      expect(value[0].value).to.equal('Overview / Utilization');
+    });
+    cy.changeSelect('display\\.startpage', 'Overview / Dashboard');
+    cy.intercept('PATCH', '/api/users/1').as('settingsUpdate');
+    cy.contains('button', 'Save').click();
+    cy.wait('@settingsUpdate').its('response.statusCode').should('eq', 200);
+
+    cy.get('#menu_item_logout').click();
+    cy.login();
+    cy.url().should('include', '/dashboard');
   });
 });
