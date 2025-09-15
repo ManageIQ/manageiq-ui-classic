81a8294d80209541a96b20c19162d43449c08d1e
Jason Frey <fryguy9@gmail.com>
2025-09-15 16:04:31 -0400

    Fix false negative in PingController after Rails 7.2
    Before Rails 7.2, ActiveRecord::Connection.connection.active? would fail
    if it couldn't connect to the database and succeed if it could. This was
    the basis for the check in the ping controller. In Rails 7.2 they now lazy
    connect, so until something actual uses the connection, that will return
    false.
    
    This creates a false negative in the PingController the very first time
    a connection is taken from the connection pool. Instead we can use our
    connectable? override method, which actually tries to make a connection
    before checking the connectivity.
    
    Additionally, when there was an error in the PingController, it would go
    through the standard error handler in the ApplicationController, which
    would, in turn, return a jquery-rjs payload with the error back to the
    client. However, this is not useful in the PingController as it's meant
    to be used externally. This commit introduces custom error handling to
    override the default handler.

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -7,4 +7,22 @@ describe PingController do
     expect(response.status).to eq(200)
     expect(response.body).to eq("pong")
   end
+
+  it 'fails gracefully with database errors' do
+    expect(ActiveRecord::Base).to receive(:connectable?).and_return(false)
+
+    get :index
+
+    expect(response.status).to eq(500)
+    expect(response.body).to eq("ERROR: Unable to connect to the database (PG::Error)")
+  end
+
+  it 'fails gracefully with non-database errors' do
+    expect(ActiveRecord::Base).to receive(:connectable?).and_raise(RuntimeError)
+
+    get :index
+
+    expect(response.status).to eq(500)
+    expect(response.body).to eq("ERROR: Unknown (RuntimeError)")
+  end
 end
