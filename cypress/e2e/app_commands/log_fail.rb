ad326039eb2c10c4ca20503568d2587f1c9d3911
Joe Rafaniello <jrafanie@gmail.com>
2025-09-26 15:36:54 -0400

    Change default to write yaml since json can't write some objects
    I was seeing Infinity trying to be written to file...

--- cypress/e2e/app_commands/log_fail.rb
+++ cypress/e2e/app_commands/log_fail.rb
@@ -6,12 +6,12 @@ system "tail -n 10000 -r log/#{Rails.env}.log | sed \"/APPCLEANED/ q\" | sed 'x;
 # Alternative command if the above does not work
 # system "tail -n 10000 log/#{Rails.env}.log | tac | sed \"/APPCLEANED/ q\" | sed 'x;1!H;$!d;x' > 'log/#{filename}.log'"
 
-# create a json debug file for server debugging
-json_result = {}
-json_result['error'] = command_options.fetch('error_message', 'no error message')
+# create a yaml debug file for server debugging
+result = {}
+result['error'] = command_options.fetch('error_message', 'no error message')
 
 if defined?(ActiveRecord::Base)
-  json_result['records'] =
+  result['records'] =
     ActiveRecord::Base.descendants.each_with_object({}) do |record_class, records|
       begin
         records[record_class.to_s] = record_class.limit(100).map(&:attributes)
@@ -21,6 +21,8 @@ if defined?(ActiveRecord::Base)
 end
 
 filename = command_options.fetch('runnable_full_title', 'no title').gsub(/[^[:print:]]/, '')
-File.open("#{Rails.root}/log/#{filename}.json", "w+") do |file|
-  file << JSON.pretty_generate(json_result)
+yml_filename = Rails.root.join("log", "#{filename}.yml"
+File.open(yml_filename, "w+") do |file|
+  file.write(Psych.dump(result))
 end
+$sterr.puts ("!!! Wrote #{yml_filename}")
