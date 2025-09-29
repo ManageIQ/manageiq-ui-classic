672f387761c4ec0372dff02be56d2ce74a2b383a
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2025-09-29 13:09:58 +0530

    Added command to validate form user-input fields

--- cypress/support/commands/command_constants.js
+++ cypress/support/commands/command_constants.js
@@ -9,4 +9,20 @@ export const LABEL_CONFIG_KEYS = {
   EXPECTED_TEXT: 'expectedText',
 };
 
+// Form user input field configuration keys
+export const FIELD_CONFIG_KEYS = {
+  ID: 'id',
+  FIELD_TYPE: 'fieldType',
+  INPUT_FIELD_TYPE: 'inputFieldType',
+  SHOULD_BE_DISABLED: 'shouldBeDisabled',
+  EXPECTED_VALUE: 'expectedValue',
+};
+
+// Form field types
+export const FIELD_TYPES = {
+  INPUT: 'input',
+  SELECT: 'select',
+  TEXTAREA: 'textarea',
+};
+
 /* ========================================================== */
