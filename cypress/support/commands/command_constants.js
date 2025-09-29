95ff00948529584c1c0766e09777fc7082b1cc35
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2025-09-29 13:08:09 +0530

    Added command to validate form labels

---
+++ cypress/support/commands/command_constants.js
@@ -0,0 +1,12 @@
+/*
+ * Form elements validation constants
+ * ==========================================================
+ */
+
+// Form label configuration keys
+export const LABEL_CONFIG_KEYS = {
+  FOR_VALUE: 'forValue',
+  EXPECTED_TEXT: 'expectedText',
+};
+
+/* ========================================================== */
