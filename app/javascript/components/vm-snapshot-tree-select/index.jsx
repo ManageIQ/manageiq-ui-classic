ab2265f81e79db852656c28866d351b7d5fdfcc6
Gilbert Cherrie <Gilbert.Cherrie@ibm.com>
2025-09-30 11:50:44 -0400

    Convert vm snapshot page to react

---
+++ app/javascript/components/vm-snapshot-tree-select/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+import SnapshotTree from './snapshot-tree';
+
+const VMSnapshotTreeSelect = ({
+  tree, selected, size, time,
+}) => {
+  const [snapshot, setSnapshot] = useState({ ...selected, size, time });
+
+  // eslint-disable-next-line react/prop-types
+  return (
+    <div>
+      <div className="snapshot-details-div">
+        <div className="snapshot-details">
+          <div className="snapshot-detail-title">
+            <p>
+              <b>
+                Description
+              </b>
+            </p>
+          </div>
+          <div className="snapshot-detail-value">
+            {snapshot.data ? snapshot.data.description : snapshot.description || ''}
+          </div>
+        </div>
+        <div className="snapshot-details">
+          <div className="snapshot-detail-title" id="size-title">
+            <p>
+              <b>
+                Size
+              </b>
+            </p>
+          </div>
+          <div className="snapshot-detail-value">
+            {snapshot.size || ''}
+          </div>
+        </div>
+        <div className="snapshot-details">
+          <div className="snapshot-detail-title" id="created-title">
+            <p>
+              <b>
+                Created
+              </b>
+            </p>
+          </div>
+          <div className="snapshot-detail-value">
+            {snapshot.time || ''}
+          </div>
+        </div>
+      </div>
+      <SnapshotTree nodes={tree.tree_nodes} setSnapshot={setSnapshot} />
+    </div>
+  );
+};
+
+VMSnapshotTreeSelect.propTypes = {
+  tree: PropTypes.objectOf(PropTypes.any).isRequired,
+  selected: PropTypes.objectOf(PropTypes.any),
+  size: PropTypes.string,
+  time: PropTypes.string,
+};
+
+VMSnapshotTreeSelect.defaultProps = {
+  selected: {},
+  size: '',
+  time: '',
+};
+
+export default VMSnapshotTreeSelect;
---
+++ app/javascript/components/vm-snapshot-tree-select/snapshot-tree.jsx
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Camera16, ChevronRight16, ChevronDown16, VirtualMachine16,
+} from '@carbon/icons-react';
+import TreeView, { flattenTree } from 'react-accessible-treeview';
+import './styles.css';
+import PropTypes from 'prop-types';
+
+const allNodeData = [];
+
+const convertData = (node) => {
+  allNodeData.push(
+    {
+      name: node.text,
+      id: node.key,
+      selectable: node.selectable,
+      tooltip: node.tooltip,
+      icon: node.icon,
+    }
+  );
+  const treeData = {
+    name: node.text, // Use the `text` property as the `name`
+    id: node.key, // Use the `key` property as the `id`
+    children: node.nodes ? node.nodes.map(convertData) : [], // Recursively process children
+  };
+
+  return treeData;
+};
+
+const SnapshotTree = ({ nodes, setSnapshot }) => {
+  const [selectedNode, setSelectedNode] = useState('');
+
+  const data = {
+    name: '',
+    children: nodes.map(convertData),
+  };
+  const treeData = flattenTree(data);
+  const expandedIds = [];
+  treeData.forEach((node) => {
+    expandedIds.push(node.id);
+
+    allNodeData.forEach((nodeData) => {
+      if (nodeData.id === node.id) {
+        const metadata = {
+          selectable: nodeData.selectable || false,
+          tooltip: nodeData.tooltip || nodeData.name,
+          icon: nodeData.icon || 'fa fa-camera',
+        };
+        node.metadata = metadata;
+      }
+    });
+  });
+
+  const nodeClick = (e, node) => {
+    if (node.metadata.selectable === false) {
+      // If the clicked node is already selected or root is selected, do nothing
+      return;
+    }
+
+    const ids = node.id.split('-');
+    const shortId = ids[ids.length - 1];
+    miqSparkleOn();
+    http.post(`/${ManageIQ.controller}/snap_pressed/${encodeURIComponent(shortId)}`).then((response) => {
+      if (response.data) {
+        const tempData = response.data;
+        tempData.size = response.data.size;
+        tempData.time = response.data.time;
+        setSnapshot(tempData);
+      }
+      miqSparkleOff();
+    });
+
+    e.stopPropagation();
+    setSelectedNode(e.target.id);
+  };
+
+  const addSelectedClassName = () => {
+    // Remove 'selected' class from all elements
+    const selectedElements = document.querySelectorAll('.selected-snapshot');
+    selectedElements.forEach((el) => {
+      el.classList.remove('selected-snapshot');
+    });
+
+    const selectedElement = document.getElementById(selectedNode);
+    if (selectedElement) {
+      selectedElement.parentNode.classList.add('selected-snapshot');
+    }
+  };
+
+  useEffect(() => {
+    addSelectedClassName();
+  }, [selectedNode]);
+
+  useEffect(() => {
+    treeData.forEach((node) => {
+      if (node.name.includes(__('(Active)'))) {
+        setSelectedNode(node.id);
+      }
+    });
+  }, []);
+
+  const ArrowIcon = (isOpen) => {
+    let icon = <ChevronRight16 />;
+    if (isOpen && isOpen.isOpen) {
+      icon = <ChevronDown16 />;
+    }
+    return <div className="arrow-div">{icon}</div>;
+  };
+
+  const NodeIcon = (icon) => {
+    if (icon === 'pficon pficon-virtual-machine') {
+      return <VirtualMachine16 />;
+    }
+    return <Camera16 />;
+  };
+
+  // First pull in node data and go through flattened tree to add metadata like icons and selectable
+  // Then add icons, tooltip and special handling
+
+  return (
+    <div>
+      <div className="checkbox">
+        <TreeView
+          data={treeData}
+          aria-label="Single select"
+          multiSelect={false}
+          onExpand={addSelectedClassName}
+          defaultExpandedIds={expandedIds}
+          propagateSelectUpwards
+          togglableSelect
+          nodeAction="check"
+          nodeRenderer={({
+            element,
+            isBranch,
+            isExpanded,
+            getNodeProps,
+            level,
+            handleExpand,
+          }) => (
+            <div
+              {...getNodeProps({ onClick: handleExpand })}
+              style={{ paddingLeft: 40 * (level - 1) }}
+            >
+              {isBranch && <ArrowIcon isOpen={isExpanded} />}
+              {element.metadata && element.metadata.icon && (
+                <div className="node-icon-div">
+                  <NodeIcon icon={element.metadata.icon} />
+                </div>
+              )}
+              <span
+                key={element.id}
+                id={element.id}
+                onClick={(e) => nodeClick(e, element)}
+                onKeyDown={(e) => e.key === 'Enter' && nodeClick(e)}
+                role="button"
+                tabIndex={0}
+                className="name"
+              >
+                {element.name}
+              </span>
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+SnapshotTree.propTypes = {
+  nodes: PropTypes.arrayOf(PropTypes.any).isRequired,
+  setSnapshot: PropTypes.func.isRequired,
+};
+
+SnapshotTree.defaultProps = {
+};
+
+export default SnapshotTree;
