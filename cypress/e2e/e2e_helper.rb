e4d2344417bc8449dcec17da9d6f3458cc15aceb
Joe Rafaniello <jrafanie@gmail.com>
2025-10-07 15:53:36 -0400

    Reuse existing definition_file_paths from factory_bot_helper
    Note:
    
    By default, Factory bot sets definition_file_paths to directories to search for factories:
    https://github.com/thoughtbot/factory_bot/blob/8446cb6c5b39ea046d8ba180197aabc66adf62ed/lib/factory_bot/find_definitions.rb#L10
    
    Cypress on rails SmartFactoryWrapper is expecting files to be a pattern or a file that when evaluated with Dir[xxx],
    will return files, not a directory:
    https://github.com/shakacode/cypress-playwright-on-rails/blob/abb505c0691c29d5f2a57c2ba28aedbfd43d079e/lib/cypress_on_rails/smart_factory_wrapper.rb#L88
    
    Therefore, to reuse the existing definition_file_paths, we must convert the directories to glob pattern matches.

--- cypress/e2e/e2e_helper.rb
+++ cypress/e2e/e2e_helper.rb
@@ -28,12 +28,15 @@ factory = CypressOnRails::SimpleRailsFactory
 factory = FactoryBot if defined?(FactoryBot)
 factory = FactoryGirl if defined?(FactoryGirl)
 
+# TODO: By default, Factory bot sets definition_file_paths to directories to search for factories:
+# https://github.com/thoughtbot/factory_bot/blob/8446cb6c5b39ea046d8ba180197aabc66adf62ed/lib/factory_bot/find_definitions.rb#L10
+# Cypress on rails SmartFactoryWrapper is expecting files to be a pattern or a file that when evaluated with Dir[xxx],
+# will return files, not a directory:
+# https://github.com/shakacode/cypress-playwright-on-rails/blob/abb505c0691c29d5f2a57c2ba28aedbfd43d079e/lib/cypress_on_rails/smart_factory_wrapper.rb#L88
+# Therefore, to reuse the existing definition_file_paths, we must convert the directories to glob pattern matches.
+require Rails.root.join('spec/support/factory_bot_helper')
 CypressOnRails::SmartFactoryWrapper.configure(
   always_reload: false,
   factory: factory,
-  files: [
-    Rails.root.join('spec', 'factories.rb'),
-    Rails.root.join('spec', 'factories', '**', '*.rb'),
-    *Vmdb::Plugins.collect {|p| p.root.join("spec/factories", "**", "*.rb")}
-  ]
+  files: FactoryBot.definition_file_paths.flat_map { |p| p.directory? ? p.join("**/*.rb") : p }
 )
