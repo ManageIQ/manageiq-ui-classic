88e730efea7cfa599451207a289046582c21c4a3
Elsa Mary <elsa.mary@ibm.com>
2025-10-09 17:24:34 +0530

    Updates on date-time picker components

--- app/javascript/components/date-time-picker/index.jsx
+++ app/javascript/components/date-time-picker/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import {
   DatePicker,
   DatePickerInput,
@@ -12,44 +12,41 @@ import { getCurrentDate, getCurrentTimeAndPeriod } from '../service-dialog-form/
 const CustomDateTimePicker = (field) => {
   const { initialData, onChange } = field;
 
-  const [date, setDate] = useState(initialData.date || getCurrentDate);
+  const [date, setDate] = useState(() => initialData.date || getCurrentDate());
   const [time, setTime] = useState(() => initialData.time || getCurrentTimeAndPeriod().time);
   const [isValid, setIsValid] = useState(true);
   const [period, setPeriod] = useState(() => initialData.period || getCurrentTimeAndPeriod().period);
 
-  const combinedDateTime = () => {
-    const dateTime = `${date} ${time} ${period}`;
-    return dateTime;
-  };
-
   const handleDateChange = (newDate) => {
-    if (newDate.length > 0) {
+    if (newDate && newDate.length) {
       const formattedDate = new Intl.DateTimeFormat('en-US', {
         month: '2-digit',
         day: '2-digit',
         year: 'numeric',
       }).format(newDate[0]);
       setDate(formattedDate);
-      onChange({ value: combinedDateTime(), initialData });
+      onChange({ value: `${formattedDate} ${time} ${period}`, initialData });
     }
   };
 
   // Function to validate the time input
   const validateTime = (value) => {
     const timeRegex = /^(0[1-9]|1[0-2]):[0-5][0-9]$/; // Matches 12-hour format hh:mm
-    setIsValid(timeRegex.test(value));
+    return timeRegex.test(value);
   };
 
   const handleTimeChange = (event) => {
     const newTime = event.target.value;
     setTime(newTime);
-    validateTime(newTime);
-    if (isValid) onChange({ value: combinedDateTime(), initialData });
+    const isValidTime = validateTime(newTime);
+    setIsValid(isValidTime)
+    if (isValidTime) onChange({ value: `${date} ${newTime} ${period}`, initialData });
   };
 
   const handlePeriodChange = (event) => {
-    setPeriod(event.target.value);
-    onChange({ value: combinedDateTime(), initialData });
+    const newPeriod = event.target.value;
+    setPeriod(newPeriod);
+    onChange({ value: `${date} ${time} ${newPeriod}`, initialData });
   };
 
   return (
