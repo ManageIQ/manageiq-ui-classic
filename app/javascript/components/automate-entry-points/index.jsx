72a5e39fe76621d5390f7f8dafa4131a37bfeb0e
Gilbert Cherrie <g.cherrie@outlook.com>
2025-10-31 14:30:40 -0400

    Refactor code and some remove unnecessary code

--- app/javascript/components/automate-entry-points/index.jsx
+++ app/javascript/components/automate-entry-points/index.jsx
@@ -177,7 +177,7 @@ const AutomateEntryPoints = ({
 
   return !isLoading && (
     <Modal
-      modalHeading="Select Entry Point Instance"
+      modalHeading={__('Select Entry Point Instance')}
       open={showModal}
       primaryButtonText={__('OK')}
       secondaryButtonText={__('Cancel')}
