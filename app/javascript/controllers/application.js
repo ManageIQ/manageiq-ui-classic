47e707ee4a384c4371acefeddf4e17f97115f7d2
Jason Frey <fryguy9@gmail.com>
2025-12-23 12:49:26 -0500

    Set stimulus debug in development mode

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,7 +3,7 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
+application.debug = (process.env.NODE_ENV === 'development')
 window.Stimulus   = application
 
 export { application }
