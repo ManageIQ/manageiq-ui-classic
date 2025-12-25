188ebd49d665238ef13e9a8ee9351e3ed370c3b4
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2025-12-25 23:05:46 +0530

    Carbon 11 migration: no-records-found component

--- app/javascript/components/no-records-found.jsx
+++ app/javascript/components/no-records-found.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Search32 } from '@carbon/icons-react';
+import { Search } from '@carbon/react/icons';
 
 const NoRecordsFound = () => (
   <div className="no-records-found">
-    <div className="icon"><Search32 /></div>
+    <div className="icon"><Search size={32} /></div>
     <div className="label">{ __('No records found') }</div>
   </div>
 );
