992b3ca0dbc29b7113b981a304f5c1f317c663dc
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-01-22 14:19:07 +0530

    Carbon 11 migration: evacuate, flavor, live-migrate, pxe-servers, region forms

--- app/javascript/components/flavor-form/index.jsx
+++ app/javascript/components/flavor-form/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'carbon-components-react';
+import { Grid, Column } from '@carbon/react';
 import MiqFormRenderer from '@@ddf';
 import miqRedirectBack from '../../helpers/miq-redirect-back';
 import createSchema from './flavor-form.schema';
@@ -21,14 +21,16 @@ const FlavorForm = ({ redirect }) => {
 
   return (
     <Grid>
-      <MiqFormRenderer
-        schema={createSchema(emsId, setState)}
-        onSubmit={onSubmit}
-        onCancel={onCancel}
-        buttonsLabels={{
-          submitLabel: __('Add'),
-        }}
-      />
+      <Column sm={4} md={8} lg={16}>
+        <MiqFormRenderer
+          schema={createSchema(emsId, setState)}
+          onSubmit={onSubmit}
+          onCancel={onCancel}
+          buttonsLabels={{
+            submitLabel: __('Add'),
+          }}
+        />
+      </Column>
     </Grid>
   );
 };
