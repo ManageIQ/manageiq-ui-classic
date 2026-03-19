ea31b2463766860efe05824ce982fc4991a1794f
Gilbert Cherrie <g.cherrie@outlook.com>
2026-03-19 12:31:00 -0400

    Fix url regex validation

--- app/javascript/components/workflow-repository-form/helpers.jsx
+++ app/javascript/components/workflow-repository-form/helpers.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable max-len */
 
 // NOTE: ddfUrlRegex is the regex output of https://github.com/data-driven-forms/react-forms/blob/3199d495361313616901f52ba2da6a21cb3d3a50/packages/react-form-renderer/src/validators/url-validator.js#L60-L80
-const ddfUrlRegex = /^((([-a-zA-Z0-9%._+~#=])@)(([-a-zA-Z0-9%._+~#=])(?:(?:).)*))|((?:\S+(?::\S*)?@)?)(?:((?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0))|(((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}|::1|1::)|((?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))|(localhost))((?::\d{2,5})?)((?:[/][^\s?#]*)?)((?:[?][^\s#]*)?)((?:[#]\S*)?)$/i;
-// sshRegex is essentially the same as ddfUrlRegex but we've removed the protocol and modified PATH to have a colon instead of a slash
-const sshRegex = /^(?:\S+(?::\S*)?@)?(?:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}|::1|1::|(?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?)|localhost)(?::\d{2,5})?(?:[:][^\s?#]*)?(?:[?][^\s#]*)?(?:[#]\S*)?$/i;
+// Updated to require http:// or https:// protocol at the start of the string
+const ddfUrlRegex = /^https?:\/\/(?:(?:\S+(?::\S*)?@)?(?:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0))|(?:(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4})*::(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4})*|(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4}){7}|::1|1::)|(?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?)|localhost)(?::\d{2,5})?(?:[/][^\s?#]*)?(?:[?][^\s#]*)?(?:[#]\S*)?$/i;
+// sshRegex matches SSH URLs with user@host:path format (requires @ or : to distinguish from plain domains)
+const sshRegex = /^(?:(?:\S+@)(?:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)|(?:(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4})*::(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4})*|(?:[0-9A-Fa-f]{1,4})(?::[0-9A-Fa-f]{1,4}){7}|::1|1::)|(?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?)|localhost):(?:[^\s?#]+))$/i;
 
 export const customUrlValidator = (urlString) => {
   // TODO: not repeat the ddf url validation code (or have it hardcoded (?))
@@ -14,5 +15,11 @@ export const customUrlValidator = (urlString) => {
   if (urlString && (urlString.match(sshRegex) || urlString.match(ddfUrlRegex))) {
     isValid = true;
   }
+
+  // In development mode, allow file:// URLs for local testing
+  if (!isValid && urlString && process.env.NODE_ENV === 'development' && urlString.startsWith('file://')) {
+    isValid = true;
+  }
+
   return isValid;
 };
