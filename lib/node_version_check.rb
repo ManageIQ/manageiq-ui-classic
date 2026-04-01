9477a277ab9dc6b0d6565ae18cf84d192ebd63bb
Elsa Mary <elsa.mary@ibm.com>
2026-04-01 15:39:21 +0530

    Add disable comment to fix Rails/Exit warning

--- lib/node_version_check.rb
+++ lib/node_version_check.rb
@@ -5,9 +5,11 @@ module NodeVersionCheck
   def self.verify!
     gem_root = defined?(APP_PATH) ? APP_PATH : File.expand_path("..", __dir__)
     version_check_script = File.join(gem_root, "scripts/check-node-version.js")
-    
+
     unless system("node", version_check_script)
+      # rubocop:disable Rails/Exit
       exit 1
+      # rubocop:enable Rails/Exit
     end
   end
 end
