870c0fe74b43ab758a33aa16c1c5dfe67a03b304
Elsa Mary <elsa.mary@ibm.com>
2026-04-01 15:55:36 +0530

    Update the helper to return a boolean (to fix the RuboCop warning)
    Let the caller (bin scripts) handle the exit.

--- lib/node_version_check.rb
+++ lib/node_version_check.rb
@@ -6,6 +6,6 @@ module NodeVersionCheck
     gem_root = defined?(APP_PATH) ? APP_PATH : File.expand_path("..", __dir__)
     version_check_script = File.join(gem_root, "scripts/check-node-version.js")
 
-    abort unless system("node", version_check_script)
+    system("node", version_check_script)
   end
 end
