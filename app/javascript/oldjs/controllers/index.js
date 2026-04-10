4896cdad78b6a77c397928a1a1d77d029bf8ea0a
Gilbert Cherrie <g.cherrie@outlook.com>
2026-04-10 15:03:21 -0400

    Delete container dashboard tech debt

--- app/javascript/oldjs/controllers/index.js
+++ app/javascript/oldjs/controllers/index.js
@@ -1,8 +1,5 @@
 require('./buttons/button_group_controller.js');
 require('./catalog/catalog_item_form_controller.js');
-require('./container_dashboard/card/card-module.js');
-require('./container_dashboard/util.js');
-require('./container_dashboard/util/dashboard-utils-factory.js');
 require('./dialog_editor/dialog_editor_controller.js');
 require('./dialog_user/dialog_user_controller.js');
 require('./dialog_user/dialog_user_reconfigure_controller.js');
