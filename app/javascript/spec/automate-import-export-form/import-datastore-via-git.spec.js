2a42ca5246d398a01d8642da45a76fd3a47d1210
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-04-20 20:30:37 +0530

    RTL migration: async-creds, miq-action, chart-card, automation-simulation, automate-datastore & c-and-u-collections forms

--- app/javascript/spec/automate-import-export-form/import-datastore-via-git.spec.js
+++ app/javascript/spec/automate-import-export-form/import-datastore-via-git.spec.js
@@ -1,56 +1,58 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
-import { act } from 'react-dom/test-utils';
-import { shallow } from 'enzyme';
 
 import ImportDatastoreViaGit from '../../components/automate-import-export-form/import-datastore-via-git';
-import '../helpers/addFlash';
-import { mount } from '../helpers/mountForm';
+import { renderWithRedux } from '../helpers/mountForm';
 
 describe('Import datastore via git component', () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
   it('should render correctly', () => {
-    const wrapper = shallow(<ImportDatastoreViaGit />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = renderWithRedux(<ImportDatastoreViaGit />);
+    expect(container).toMatchSnapshot();
   });
 
   it('should have submit button disabled when url is not valid', () => {
-    const wrapper = mount(<ImportDatastoreViaGit />);
-    const button = wrapper.find('button');
-    expect(button.props().disabled).toBe(true);
+    renderWithRedux(<ImportDatastoreViaGit />);
+    const button = screen.getByRole('button', { name: /save/i });
+    expect(button).toBeDisabled();
   });
 
-  it('should have submit button always disabled', () => {
-    const wrapper = mount(<ImportDatastoreViaGit disableSubmit />);
-    wrapper.find('input[name="git_url"]').simulate('change', { target: { value: 'http://' } });
-    const button = wrapper.find('button');
-    expect(button.props().disabled).toBe(true);
+  it('should have submit button always disabled', async() => {
+    const user = userEvent.setup();
+    renderWithRedux(<ImportDatastoreViaGit disableSubmit />);
+
+    const input = screen.getByRole('textbox', { name: /git url/i });
+    await user.type(input, 'http://');
+
+    const button = screen.getByRole('button', { name: /save/i });
+    expect(button).toBeDisabled();
   });
 
-  it('should call api correct endpoint after submit', async(done) => {
+  it('should call api correct endpoint after submit', async() => {
     const flashSpy = jest.spyOn(window, 'add_flash');
     fetchMock.postOnce('/miq_ae_tools/retrieve_git_datastore', [{ message: 'Foo', level: 'Bar' }]);
 
-    let wrapper;
-    await act(async() => {
-      wrapper = mount(<ImportDatastoreViaGit />);
+    const user = userEvent.setup();
+    renderWithRedux(<ImportDatastoreViaGit />);
+
+    const input = screen.getByRole('textbox', { name: /git url/i });
+    await user.type(input, 'http://');
+
+    await user.click(screen.getByRole('button', { name: /save/i }));
+
+    await waitFor(() => {
+      expect(fetchMock.called('/miq_ae_tools/retrieve_git_datastore')).toBe(true);
     });
-    wrapper.update();
 
-    await act(async() => {
-      wrapper.find('input[name="git_url"]').simulate('change', { target: { value: 'http://' } });
-      wrapper.find('form').simulate('submit');
-    });
-
-    const expectedCall = expect.arrayContaining([
-      '/miq_ae_tools/retrieve_git_datastore',
-      expect.objectContaining({
-        body: '{"git_url":"http://","git_verify_ssl":true}',
-      }),
-    ]);
-
-    expect(fetchMock.lastCall()).toEqual(expectedCall);
+    const lastCall = fetchMock.lastCall();
+    expect(lastCall[0]).toBe('/miq_ae_tools/retrieve_git_datastore');
+    expect(lastCall[1].body).toBe('{"git_url":"http://","git_verify_ssl":true}');
     expect(flashSpy).toHaveBeenCalledWith('Foo', 'Bar');
-    done();
   });
 });
