aa59570bba52d1b23a596bbc66145c6ba0673edf
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-04-28 15:47:38 +0530

    RTL migration: cloud-tenant, cloud-volume-backup, attach-detach-cloud-volume, datastore, embedded-terraform-creds & embedded-terraform-repository forms

--- app/javascript/spec/cloud-tenant-form/cloud-tenant-form.spec.js
+++ app/javascript/spec/cloud-tenant-form/cloud-tenant-form.spec.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
 import fetchMock from 'fetch-mock';
-import { shallow } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { screen, waitFor } from '@testing-library/react';
 import CloudTenantForm from '../../components/cloud-tenant-form';
-import { mount } from '../helpers/mountForm';
+import { renderWithRedux } from '../helpers/mountForm';
 
-require('../helpers/miqSparkle.js');
-require('../helpers/miqAjaxButton.js');
+import '../helpers/miqAjaxButton';
 
 describe('Cloud tenant form component', () => {
-  let emsChoices;
   let submitSpy;
   beforeEach(() => {
-    emsChoices = {
-      'Label 1': '1',
-      'Label 2': '2',
-      'Label 3': '3',
-      'Label 4': '4',
-    };
     submitSpy = jest.spyOn(window, 'miqAjaxButton');
   });
 
@@ -29,18 +19,25 @@ describe('Cloud tenant form component', () => {
   });
 
   it('should render adding form variant', () => {
-    const wrapper = shallow(<CloudTenantForm />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = renderWithRedux(<CloudTenantForm />);
+    expect(container).toMatchSnapshot();
   });
 
-  it('should render editing form variant', async(done) => {
-    fetchMock.getOnce('/api/cloud_tenants/1', { name: 'foo' });
-    let wrapper;
-    await act(async() => {
-      wrapper = mount(<CloudTenantForm recordId="1" />);
+  it('should render editing form variant', async() => {
+    fetchMock.get('/api/cloud_tenants/1', { name: 'foo' });
+
+    const { container } = renderWithRedux(<CloudTenantForm recordId="1" />);
+
+    // Wait for the API call to complete
+    await waitFor(() => {
+      expect(fetchMock.called('/api/cloud_tenants/1')).toBe(true);
     });
-    expect(fetchMock.called('/api/cloud_tenants/1')).toBe(true);
-    expect(toJson(wrapper)).toMatchSnapshot();
-    done();
+
+    // Wait for form to be fully rendered
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
+    });
+
+    expect(container).toMatchSnapshot();
   });
 });
