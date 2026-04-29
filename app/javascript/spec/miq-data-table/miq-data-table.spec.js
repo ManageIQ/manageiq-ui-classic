d2217ae6c0289afd120d20249fc4b7eea44d5643
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-04-29 14:40:32 +0530

    RTL migration: miq-data-table, miq-policy-set, tag, tag-view, value-modifier & value-selector

--- app/javascript/spec/miq-data-table/miq-data-table.spec.js
+++ app/javascript/spec/miq-data-table/miq-data-table.spec.js
@@ -1,43 +1,50 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import MiqDataTable from '../../components/miq-data-table';
 import {
-  simpleData, containerNodesData, hostData, catalogData, timeProfileReportsData, chargebackRateData, complexTableData
+  simpleData,
+  containerNodesData,
+  hostData,
+  catalogData,
+  timeProfileReportsData,
+  chargebackRateData,
+  complexTableData,
 } from './data';
 
 describe('Data table component', () => {
   it('should render a simple data table', () => {
     const onSelect = jest.fn();
     const { miqHeaders, miqRows } = simpleData;
-    const wrapper = shallow(<MiqDataTable
-      headers={miqHeaders}
-      rows={miqRows}
-      onCellClick={onSelect}
-      mode="list"
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable
+        headers={miqHeaders}
+        rows={miqRows}
+        onCellClick={onSelect}
+        mode="list"
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a simple non-clickable data table', () => {
     const { miqHeaders, miqRows } = timeProfileReportsData();
-    const wrapper = shallow(<MiqDataTable
-      headers={miqHeaders}
-      rows={miqRows}
-      mode="list"
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable headers={miqHeaders} rows={miqRows} mode="list" />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render strings without double escaping strings', () => {
-    const onClick = jest.fn();
     const { miqHeaders, miqRows } = complexTableData();
-    const wrapper = shallow(<MiqDataTable
-      headers={miqHeaders}
-      rows={miqRows}
-      mode="list"
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable headers={miqHeaders} rows={miqRows} mode="list" />
+    );
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a simple data tabe with pagination and sorting', () => {
@@ -52,18 +59,22 @@ describe('Data table component', () => {
       page: 1,
       onPageChange: jest.fn(),
     };
-    const wrapper = shallow(<MiqDataTable
-      rows={miqRows}
-      headers={miqHeaders}
-      onCellClick={onSelect}
-      sortable
-      rowCheckBox
-      showPagination
-      pageOptions={pageOptions}
-      mode="list"
-      gridChecks={[]}
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable
+        rows={miqRows}
+        headers={miqHeaders}
+        onCellClick={onSelect}
+        sortable
+        rowCheckBox
+        showPagination
+        pageOptions={pageOptions}
+        mode="list"
+        gridChecks={[]}
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a static-gtl-view data table with checkboxes and icon', () => {
@@ -78,18 +89,22 @@ describe('Data table component', () => {
       page: 1,
       onPageChange: jest.fn(),
     };
-    const wrapper = shallow(<MiqDataTable
-      rows={miqRows}
-      headers={miqHeaders}
-      onCellClick={onSelect}
-      sortable
-      rowCheckBox
-      showPagination
-      pageOptions={pageOptions}
-      mode="static-gtl-view"
-      gridChecks={[]}
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable
+        rows={miqRows}
+        headers={miqHeaders}
+        onCellClick={onSelect}
+        sortable
+        rowCheckBox
+        showPagination
+        pageOptions={pageOptions}
+        mode="static-gtl-view"
+        gridChecks={[]}
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a static-gtl-view data table with checkboxes, images and icon', () => {
@@ -104,18 +119,22 @@ describe('Data table component', () => {
       page: 1,
       onPageChange: jest.fn(),
     };
-    const wrapper = shallow(<MiqDataTable
-      rows={miqRows}
-      headers={miqHeaders}
-      onCellClick={onSelect}
-      sortable
-      rowCheckBox
-      showPagination
-      pageOptions={pageOptions}
-      mode="static-gtl-view"
-      gridChecks={[]}
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable
+        rows={miqRows}
+        headers={miqHeaders}
+        onCellClick={onSelect}
+        sortable
+        rowCheckBox
+        showPagination
+        pageOptions={pageOptions}
+        mode="static-gtl-view"
+        gridChecks={[]}
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a static-gtl-view data table with icons and buttons', () => {
@@ -130,27 +149,31 @@ describe('Data table component', () => {
       page: 1,
       onPageChange: jest.fn(),
     };
-    const wrapper = shallow(<MiqDataTable
-      rows={miqRows}
-      headers={miqHeaders}
-      onCellClick={onSelect}
-      sortable
-      rowCheckBox
-      showPagination
-      pageOptions={pageOptions}
-      mode="static-gtl-view"
-      gridChecks={[]}
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable
+        rows={miqRows}
+        headers={miqHeaders}
+        onCellClick={onSelect}
+        sortable
+        rowCheckBox
+        showPagination
+        pageOptions={pageOptions}
+        mode="static-gtl-view"
+        gridChecks={[]}
+      />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render a data table with cells which have multiple rows or single row', () => {
     const { headers, rows } = chargebackRateData;
-    const wrapper = shallow(<MiqDataTable
-      headers={headers}
-      rows={rows}
-      mode="chargeback-rate"
-    />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { container } = render(
+      <MiqDataTable headers={headers} rows={rows} mode="chargeback-rate" />
+    );
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
