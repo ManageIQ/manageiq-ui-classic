65691855edface6540e854b8addb7664b86c1569
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-04-29 14:36:28 +0530

    RTL migration: miq-condition, category-modifier, tag-category, tag-modifier, tag-selector & tagging-with-reducer

--- app/javascript/tagging/tests/category.modifier.test.js
+++ app/javascript/tagging/tests/category.modifier.test.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import CategoryModifier from '../components/InnerComponents/CategoryModifier';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
-const selectedTagCategory = { label: 'animal', id: 1 };
-const onTagCategoryChange = jest.fn();
-const tagCategories = [
-  { label: 'Name', id: 1 },
-  { label: 'Number', id: 2 },
-  { label: 'Animal', id: 3 },
-  { label: 'Food', id: 4 }
-];
+describe('Category modifier Component', () => {
+  const selectedTagCategory = { label: 'animal', id: 1 };
+  const onTagCategoryChange = jest.fn();
+  const tagCategories = [
+    { label: 'Name', id: 1 },
+    { label: 'Number', id: 2 },
+    { label: 'Animal', id: 3 },
+    { label: 'Food', id: 4 },
+  ];
 
-describe('TagCategory Component', () => {
-  it('match snapshot', () => {
-    const component = shallow(
+  it('should match snapshot', () => {
+    const { container } = render(
       <CategoryModifier
         selectedTagCategory={selectedTagCategory}
         onTagCategoryChange={onTagCategoryChange}
@@ -22,7 +21,6 @@ describe('TagCategory Component', () => {
       />
     );
 
-    const tree = toJson(component);
-    expect(tree).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
