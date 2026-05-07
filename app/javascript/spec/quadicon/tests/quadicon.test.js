2db042db521905746ad4afeaf89c7ba1a6ed33e8
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-05-07 22:31:54 +0530

    RTL migration: miq-custom-tab, notification-message, prov-grid, protocol-selector, quadicon & remove-catalog-item-modal

--- app/javascript/spec/quadicon/tests/quadicon.test.js
+++ app/javascript/spec/quadicon/tests/quadicon.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 
 import { Quadicon } from '../../../components/quadicon';
 import {
-  quads, numbers, piecharts, hosts, vms, containerproviders,
+  quads,
+  numbers,
+  piecharts,
+  hosts,
+  vms,
+  containerproviders,
 } from '../data/quadicons';
 
 /* eslint no-bitwise: ["error", { "allow": ["^", ">>>"] }], react/jsx-closing-tag-location: ["off"] */
@@ -15,15 +19,26 @@ const hObj = (obj) => {
   for (let i = 0; i < s.length; i += 1) {
     h = Math.imul(h ^ s.charCodeAt(i), 951274213);
   }
-  return (h ^ h >>> 9) >>> 0;
+  return (h ^ (h >>> 9)) >>> 0;
 };
 
 describe('Quadicon', () => {
   it('renders just fine...', () => {
-    const all = [...quads, ...numbers, ...piecharts, ...hosts, ...vms, ...containerproviders];
-    const qs = mount(<>
-      { all.map((quad) => <Quadicon key={hObj(quad)} data={quad} />)}
-    </>);
-    expect(toJson(qs)).toMatchSnapshot();
+    const all = [
+      ...quads,
+      ...numbers,
+      ...piecharts,
+      ...hosts,
+      ...vms,
+      ...containerproviders,
+    ];
+    const { container } = render(
+      <>
+        {all.map((quad) => (
+          <Quadicon key={hObj(quad)} data={quad} />
+        ))}
+      </>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
