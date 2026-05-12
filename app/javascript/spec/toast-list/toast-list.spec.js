8b7caf99991bb6582e28195f4e48c49ef9d9b4fa
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-05-12 18:32:38 +0530

    RTL migration: select, timeline-chart, toast-list, tree-view selector, vm-snapshot-tree & widget-wrapper

--- app/javascript/spec/toast-list/toast-list.spec.js
+++ app/javascript/spec/toast-list/toast-list.spec.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
-import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -23,48 +22,64 @@ describe('Toast list tests', () => {
   };
   const mockStore = configureStore([thunk]);
 
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
   it('should render correctly with notifications', () => {
     const store = mockStore({ ...initialState });
-    const wrapper = mount(
+    const { container } = render(
       <Provider store={store}>
         <ToastList />
-      </Provider>,
+      </Provider>
     );
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it('should not render without notifications', () => {
     const store = mockStore({
       ...initialState,
-      notificationReducer: { ...initialState.notificationReducer, notifications: [], toastNotifications: [] },
+      notificationReducer: {
+        ...initialState.notificationReducer,
+        notifications: [],
+        toastNotifications: [],
+      },
     });
-    const wrapper = mount(
+    const { container } = render(
       <Provider store={store}>
         <ToastList />
-      </Provider>,
+      </Provider>
     );
-    expect(wrapper.find('div.toast-notifications-list-pf')).toHaveLength(0);
+    expect(
+      container.querySelector('div.toast-notifications-list-pf')
+    ).not.toBeInTheDocument();
   });
 
-  it('should dispatch markNotificationRead after click on close button', async(done) => {
+  it('should dispatch markNotificationRead after click on close button', async() => {
+    const user = userEvent.setup();
     fetchMock.postOnce('/api/notifications/', {
       action: 'mark_all_seen',
       resources: [{ id: '10000000003624' }],
     });
     const store = mockStore(initialState);
-    const wrapper = mount(
+    const { container } = render(
       <Provider store={store}>
         <ToastList />
-      </Provider>,
+      </Provider>
     );
-    await act(async() => {
-      wrapper.find('svg.cds--toast-notification__close-icon').first().simulate('click');
+
+    const closeButton = container.querySelector(
+      'svg.cds--toast-notification__close-icon'
+    );
+    await user.click(closeButton);
+
+    await waitFor(() => {
+      const expectedPayload = {
+        payload: '10000000003624',
+        type: MARK_NOTIFICATION_READ,
+      };
+      expect(store.getActions()).toEqual([expectedPayload]);
     });
-    const expectedPayload = {
-      payload: '10000000003624',
-      type: MARK_NOTIFICATION_READ,
-    };
-    expect(store.getActions()).toEqual([expectedPayload]);
-    done();
   });
 });
