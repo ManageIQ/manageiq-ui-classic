d0d242f1d41e5cb6030f935530934f7d56f5fe82
Elsa Mary <elsa.mary@ibm.com>
2026-05-14 12:22:36 +0530

    Add inline JSDoc comments for the custom component

--- app/javascript/components/select-with-icons/index.jsx
+++ app/javascript/components/select-with-icons/index.jsx
@@ -3,6 +3,49 @@ import PropTypes from 'prop-types';
 import { Dropdown } from '@carbon/react';
 import { useFieldApi } from '@@ddf';
 
+/**
+ * SelectWithIcons - A dropdown component that displays icons alongside option labels
+ *
+ * This component extends Carbon's Dropdown to render custom icons for each option.
+ * It integrates with Data Driven Forms (DDF) and is designed for use in form schemas.
+ *
+ * @component
+ * @example
+ * // Usage in a Data Driven Forms schema:
+ * {
+ *   component: 'select-with-icons',
+ *   name: 'datatype',
+ *   label: 'Data Type',
+ *   options: [
+ *     { label: 'String', value: 'string', 'data-icon': 'fa fa-font' },
+ *     { label: 'Integer', value: 'integer', 'data-icon': 'fa fa-hashtag' },
+ *     { label: 'Boolean', value: 'boolean', 'data-icon': 'fa fa-check-square' }
+ *   ],
+ *   placeholder: '<Choose>',
+ *   includeEmpty: true,
+ *   isRequired: false
+ * }
+ *
+ * @example
+ * // Options can also be provided in array format (from backend):
+ * // [['Display Text', 'value', { 'data-icon': 'fa fa-icon' }], ...]
+ * // These are transformed to the object format by transformSelectOptions helper
+ *
+ * @param {Object} props - Component props
+ * @param {string} [props.label] - Label text displayed above the dropdown
+ * @param {Array<Object>} [props.options=[]] - Array of option objects
+ * @param {string} props.options[].label - Display text for the option
+ * @param {string|number} props.options[].value - Value for the option
+ * @param {string} [props.options[].'data-icon'] - CSS class for the icon (e.g., 'fa fa-icon')
+ * @param {string} [props.placeholder='<Choose>'] - Placeholder text shown when no selection
+ * @param {boolean} [props.includeEmpty=false] - Whether to include a placeholder option
+ * @param {boolean} [props.isRequired=false] - Whether the field is required
+ * @param {string} [props.id] - HTML id attribute (defaults to name if not provided)
+ * @param {string} [props.name] - Field name for form submission
+ *
+ * @returns {React.Component} A Carbon Dropdown with icon support
+ *
+ */
 const SelectWithIcons = ({
   label,
   options,
