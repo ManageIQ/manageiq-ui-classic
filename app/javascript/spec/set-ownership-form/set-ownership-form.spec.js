2f1659277e65cba722d4ac2d7690af5881253755
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-05-14 15:34:10 +0530

    RTL migration: copy-catalog, edit-service, set-ownership, settings-replication, vm-server-relationship & workers forms

--- app/javascript/spec/set-ownership-form/set-ownership-form.spec.js
+++ app/javascript/spec/set-ownership-form/set-ownership-form.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
 import SetOwnershipForm from '../../components/set-ownership-form';
 import createSchema from '../../components/set-ownership-form/ownership-form.schema';
 import '../helpers/miqAjaxButton';
 import '../helpers/miqSparkle';
-import '../helpers/addFlash';
-import { mount } from '../helpers/mountForm';
+import { renderWithRedux } from '../helpers/mountForm';
 
 describe('Set ownership form component', () => {
   let initialProps;
@@ -28,84 +28,180 @@ describe('Set ownership form component', () => {
   });
 
   it('should correctly map group and owner options', () => {
-    const groupOptions = [{ label: 'Foo', value: '1', key: 'key_1' }, { label: 'Bar', value: '2', key: 'key_2' }];
-    const ownerOptions = [{ label: 'Baz', value: '3', key: 'key_1' }, { label: 'Qux', value: '4', key: 'key_2' }];
+    const groupOptions = [
+      { label: 'Foo', value: '1', key: 'key_1' },
+      { label: 'Bar', value: '2', key: 'key_2' },
+    ];
+    const ownerOptions = [
+      { label: 'Baz', value: '3', key: 'key_1' },
+      { label: 'Qux', value: '4', key: 'key_2' },
+    ];
 
     const expectedResult = [
       expect.objectContaining({
-        options: [{
-          value: '3',
-          label: 'Baz',
-          key: 'key_1',
-        }, {
-          value: '4',
-          label: 'Qux',
-          key: 'key_2',
-        }],
+        options: [
+          {
+            value: '3',
+            label: 'Baz',
+            key: 'key_1',
+          },
+          {
+            value: '4',
+            label: 'Qux',
+            key: 'key_2',
+          },
+        ],
       }),
       expect.objectContaining({
-        options: [{
-          value: '1',
-          label: 'Foo',
-          key: 'key_1',
-        }, {
-          value: '2',
-          label: 'Bar',
-          key: 'key_2',
-        }],
+        options: [
+          {
+            value: '1',
+            label: 'Foo',
+            key: 'key_1',
+          },
+          {
+            value: '2',
+            label: 'Bar',
+            key: 'key_2',
+          },
+        ],
       }),
     ];
     const { fields } = createSchema(ownerOptions, groupOptions);
     expect(fields).toEqual(expectedResult);
   });
 
-  it('should request initialForm values after mount', async(done) => {
+  it('should request initialForm values after mount', async() => {
     fetchMock
-      .getOnce('/api/vms/123456?expand=resources&attributes=evm_owner_id,miq_group_id',
-        { evm_owner_id: 'a', miq_group_id: 'z2' })
-      .getOnce('/api/users?expand=resources&attributes=id,name&sort_by=name&sort_order=ascending',
-        { resources: [{ name: 'f', id: 'a' }, { name: 's', id: 'z' }] })
-      .getOnce('/api/groups?expand=resources&attributes=id,description&sort_by=description&sort_order=ascending',
-        { resources: [{ description: 'f2', id: 'a2' }, { description: 's2', id: 'z2' }] })
+      .getOnce(
+        '/api/vms/123456?expand=resources&attributes=evm_owner_id,miq_group_id',
+        { evm_owner_id: 'a', miq_group_id: 'z2' }
+      )
+      .getOnce(
+        '/api/users?expand=resources&attributes=id,name&sort_by=name&sort_order=ascending',
+        {
+          resources: [
+            { name: 'f', id: 'a' },
+            { name: 's', id: 'z' },
+          ],
+        }
+      )
+      .getOnce(
+        '/api/groups?expand=resources&attributes=id,description&sort_by=description&sort_order=ascending',
+        {
+          resources: [
+            { description: 'f2', id: 'a2' },
+            { description: 's2', id: 'z2' },
+          ],
+        }
+      )
       .getOnce('/api/tenant_groups/z2', {});
+    const { container } = renderWithRedux(
+      <SetOwnershipForm {...initialProps} />
+    );
 
-    let wrapper;
-    await act(async() => {
-      wrapper = mount(<SetOwnershipForm {...initialProps} />);
+    await waitFor(() => {
+      expect(fetchMock.calls().length).toBe(4);
+      const wrapper = container.querySelector('#set-ownership-form');
+      expect(wrapper).toBeInTheDocument();
+      expect(wrapper.querySelector('form')).toBeInTheDocument();
+      expect(
+        screen.getByRole('combobox', { name: /Select an Owner/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole('combobox', { name: /Select a Group/i })
+      ).toBeInTheDocument();
     });
-
-    expect(wrapper.contains(<SetOwnershipForm {...initialProps} />)).toBeTruthy();
-    done();
   });
 
-  it('should send correct data on save', async(done) => {
+  it('should send correct data on save', async() => {
+    const user = userEvent.setup();
     fetchMock
-      .getOnce('/api/vms/123456?expand=resources&attributes=evm_owner_id,miq_group_id',
-        { evm_owner_id: 'a', miq_group_id: 'z2' })
-      .getOnce('/api/users?expand=resources&attributes=id,name&sort_by=name&sort_order=ascending',
-        { resources: [{ name: 'f', id: 'a' }, { name: 's', id: 'z' }] })
-      .getOnce('/api/groups?expand=resources&attributes=id,description&sort_by=description&sort_order=ascending',
-        { resources: [{ description: 'f2', id: 'a2' }, { description: 's2', id: 'z2' }] })
+      .getOnce(
+        '/api/vms/123456?expand=resources&attributes=evm_owner_id,miq_group_id',
+        { evm_owner_id: 'a', miq_group_id: 'z2' }
+      )
+      .getOnce(
+        '/api/users?expand=resources&attributes=id,name&sort_by=name&sort_order=ascending',
+        {
+          resources: [
+            { name: 'f', id: 'a' },
+            { name: 's', id: 'z' },
+          ],
+        }
+      )
+      .getOnce(
+        '/api/groups?expand=resources&attributes=id,description&sort_by=description&sort_order=ascending',
+        {
+          resources: [
+            { description: 'f2', id: 'a2' },
+            { description: 's2', id: 'z2' },
+          ],
+        }
+      )
       .getOnce('/api/tenant_groups/z2', {})
       .postOnce('/api/vms', {});
+    const { container } = renderWithRedux(
+      <SetOwnershipForm {...initialProps} />
+    );
 
-    let wrapper;
-    await act(async() => {
-      wrapper = mount(<SetOwnershipForm {...initialProps} />);
+    await waitFor(() => {
+      expect(container.querySelector('form')).toBeInTheDocument();
+      expect(
+        screen.getByRole('combobox', { name: /Select an Owner/i })
+      ).toBeInTheDocument();
     });
-    const form = wrapper.find('form');
-    form.find('select[name="user"]').simulate('change', 'z');
-    wrapper.update();
-    await act(async() => {
-      wrapper.find('form').simulate('submit');
+    const userSelect = screen.getByRole('combobox', {
+      name: /Select an Owner/i,
     });
-    expect(submitSpy).toHaveBeenCalledWith('/vms/ownership_update/?button=save', { objectIds: ['123456'] });
-    done();
+    await user.selectOptions(userSelect, 'z');
+    const submitButton = container.querySelector('button.cds--btn--primary');
+    await user.click(submitButton);
+    await waitFor(() => {
+      expect(submitSpy).toHaveBeenCalledWith(
+        '/vms/ownership_update/?button=save',
+        { objectIds: ['123456'] }
+      );
+    });
   });
 
-  it('should send correct data on cancel', () => {
-    const wrapper = mount(<SetOwnershipForm {...initialProps} />);
-    wrapper.find('button.cds--btn--secondary').last().simulate('click');
-    expect(submitSpy).toHaveBeenCalledWith('/vms/ownership_update/?button=cancel&objectIds=123456');
+  it('should send correct data on cancel', async() => {
+    const user = userEvent.setup();
+    fetchMock
+      .getOnce(
+        '/api/vms/123456?expand=resources&attributes=evm_owner_id,miq_group_id',
+        { evm_owner_id: 'a', miq_group_id: 'z2' }
+      )
+      .getOnce(
+        '/api/users?expand=resources&attributes=id,name&sort_by=name&sort_order=ascending',
+        {
+          resources: [
+            { name: 'f', id: 'a' },
+            { name: 's', id: 'z' },
+          ],
+        }
+      )
+      .getOnce(
+        '/api/groups?expand=resources&attributes=id,description&sort_by=description&sort_order=ascending',
+        {
+          resources: [
+            { description: 'f2', id: 'a2' },
+            { description: 's2', id: 'z2' },
+          ],
+        }
+      )
+      .getOnce('/api/tenant_groups/z2', {});
+    renderWithRedux(<SetOwnershipForm {...initialProps} />);
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole('combobox', { name: /Select an Owner/i })
+      ).toBeInTheDocument();
+    });
+    const cancelButton = screen.getByRole('button', { name: /Cancel/i });
+    await user.click(cancelButton);
+    expect(submitSpy).toHaveBeenCalledWith(
+      '/vms/ownership_update/?button=cancel&objectIds=123456'
+    );
   });
 });
--- app/javascript/spec/settings-replication-form/settings-replication-form.spec.js
+++ app/javascript/spec/settings-replication-form/settings-replication-form.spec.js
@@ -1,117 +1,131 @@
 import React from 'react';
 import fetchMock from 'fetch-mock';
-import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRedux } from '../helpers/mountForm';
 import SettingsReplicationForm from '../../components/settings-replication-form';
 
 describe('SettingsReplicationForm', () => {
-  const mockStore = configureStore();
-  const store = mockStore({});
-  let wrapper;
-
   const replicationMockData = {
     replication_type: 'none',
     subscriptions: [],
   };
 
-  // Helper function to mount component with proper setup
-  const mountComponent = async(props = {}) => {
-    let component;
-    await act(async() => {
-      component = mount(
-        <Provider store={store}>
-          <SettingsReplicationForm
-            pglogicalReplicationFormId="new"
-            {...props}
-          />
-        </Provider>
-      );
-    });
-    component.update();
-    return component;
-  };
-
   // Helper function to change replication type
-  const changeReplicationType = async(wrapper, value) => {
-    const selectElement = wrapper.find('select[name="replication_type"]').getDOMNode();
-    selectElement.value = value;
-
-    await act(async() => {
-      wrapper.find('select[name="replication_type"]').simulate('change', {
-        target: selectElement,
-      });
+  const changeReplicationType = async(container, user, value) => {
+    await waitFor(() => {
+      expect(
+        container.querySelector('select[name="replication_type"]')
+      ).toBeInTheDocument();
     });
-    wrapper.update();
+
+    const select = container.querySelector('select[name="replication_type"]');
+    await user.selectOptions(select, value);
   };
 
   beforeEach(() => {
     // Mock API call before component renders
-    fetchMock.get('/ops/pglogical_subscriptions_form_fields/new', replicationMockData);
+    fetchMock.get(
+      '/ops/pglogical_subscriptions_form_fields/new',
+      replicationMockData
+    );
   });
 
   afterEach(() => {
-    // Cleanup
-    if (wrapper) {
-      wrapper.unmount();
-      wrapper = null;
-    }
     fetchMock.reset();
     fetchMock.restore();
   });
 
   describe('Initial Rendering', () => {
     it('should render without crashing', async() => {
-      wrapper = await mountComponent();
-      expect(wrapper.find(SettingsReplicationForm).exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(container.querySelector('form')).toBeInTheDocument();
+      });
     });
 
     it('should render the replication type dropdown', async() => {
-      wrapper = await mountComponent();
-      const dropdown = wrapper.find('select[name="replication_type"]');
-
-      expect(dropdown.exists()).toBe(true);
-      expect(dropdown.length).toBe(1);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(
+          container.querySelector('select[name="replication_type"]')
+        ).toBeInTheDocument();
+      });
+      const dropdown = container.querySelector(
+        'select[name="replication_type"]'
+      );
+      expect(dropdown).toBeInTheDocument();
     });
 
     it('should not show subscription section initially', async() => {
-      wrapper = await mountComponent();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
 
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(false);
+      await waitFor(() => {
+        expect(container.querySelector('form')).toBeInTheDocument();
+      });
+
+      expect(
+        container.querySelector('div#subscriptions_section')
+      ).not.toBeInTheDocument();
     });
   });
 
   describe('Replication Type Selection', () => {
     it('should render the subscription section when replication type is global', async() => {
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'global');
-
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(true);
+      const user = userEvent.setup();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'global');
+      await waitFor(() => {
+        expect(
+          container.querySelector('div#subscriptions_section')
+        ).toBeInTheDocument();
+      });
     });
 
     it('should not render the subscription section when replication type is remote', async() => {
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'remote');
-
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(false);
+      const user = userEvent.setup();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'remote');
+      await waitFor(() => {
+        expect(
+          container.querySelector('div#subscriptions_section')
+        ).not.toBeInTheDocument();
+      });
     });
 
     it('should not render the subscription section when replication type is none', async() => {
-      wrapper = await mountComponent();
-
-      // Ensure type is none (default)
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(false);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(container.querySelector('form')).toBeInTheDocument();
+      });
+      expect(
+        container.querySelector('div#subscriptions_section')
+      ).not.toBeInTheDocument();
     });
 
     it('should display Add Subscription button when replication type is global', async() => {
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'global');
-
-      const addButton = wrapper.find('button')
-        .filterWhere((node) => node.text() === 'Add Subscription');
-
-      expect(addButton.exists()).toBe(true);
+      const user = userEvent.setup();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'global');
+      await waitFor(() => {
+        expect(
+          screen.getByRole('button', { name: 'Add Subscription' })
+        ).toBeInTheDocument();
+      });
     });
   });
 
@@ -119,163 +133,181 @@ describe('SettingsReplicationForm', () => {
     it('should fetch data on mount when pglogicalReplicationFormId is provided', async() => {
       const mockData = {
         replication_type: 'global',
-        subscriptions: [{
-          dbname: 'testdb',
-          host: 'localhost',
-          user: 'testuser',
-          password: 'testpass',
-          port: '5432',
-        }],
+        subscriptions: [
+          {
+            dbname: 'testdb',
+            host: 'localhost',
+            user: 'testuser',
+            password: 'testpass',
+            port: '5432',
+          },
+        ],
       };
-
       fetchMock.reset();
       fetchMock.get('/ops/pglogical_subscriptions_form_fields/123', mockData);
 
-      await act(async() => {
-        wrapper = mount(
-          <Provider store={store}>
-            <SettingsReplicationForm pglogicalReplicationFormId="123" />
-          </Provider>
-        );
-      });
+      renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="123" />
+      );
 
-      // Wait for async operations
-      await act(async() => {
-        await new Promise((resolve) => setTimeout(resolve, 100));
+      await waitFor(() => {
+        expect(
+          fetchMock.called('/ops/pglogical_subscriptions_form_fields/123')
+        ).toBe(true);
       });
-      wrapper.update();
-
-      expect(fetchMock.called('/ops/pglogical_subscriptions_form_fields/123')).toBe(true);
     });
   });
 
   describe('User Interactions', () => {
     it('should maintain state when switching between replication types', async() => {
-      wrapper = await mountComponent();
-
+      const user = userEvent.setup();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
       // Switch to global
-      await changeReplicationType(wrapper, 'global');
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(true);
-
+      await changeReplicationType(container, user, 'global');
+      await waitFor(() => {
+        expect(
+          container.querySelector('div#subscriptions_section')
+        ).toBeInTheDocument();
+      });
       // Switch to remote
-      await changeReplicationType(wrapper, 'remote');
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(false);
-
+      await changeReplicationType(container, user, 'remote');
+      await waitFor(() => {
+        expect(
+          container.querySelector('div#subscriptions_section')
+        ).not.toBeInTheDocument();
+      });
       // Switch back to global
-      await changeReplicationType(wrapper, 'global');
-      expect(wrapper.find('div#subscriptions_section').exists()).toBe(true);
+      await changeReplicationType(container, user, 'global');
+      await waitFor(() => {
+        expect(
+          container.querySelector('div#subscriptions_section')
+        ).toBeInTheDocument();
+      });
     });
   });
 
   describe('Accessibility', () => {
     it('should have proper form structure', async() => {
-      wrapper = await mountComponent();
-
-      // Should have form elements
-      const form = wrapper.find('form');
-      expect(form.exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(container.querySelector('form')).toBeInTheDocument();
+      });
     });
 
     it('should have labeled form fields', async() => {
-      wrapper = await mountComponent();
-      const dropdown = wrapper.find('select[name="replication_type"]');
-
-      // Should have associated label or be properly identified
-      expect(dropdown.exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(
+          container.querySelector('select[name="replication_type"]')
+        ).toBeInTheDocument();
+      });
+      const dropdown = container.querySelector(
+        'select[name="replication_type"]'
+      );
+      expect(dropdown).toBeInTheDocument();
     });
 
     it('should render form elements in correct order', async() => {
-      wrapper = await mountComponent();
-
-      // Replication type dropdown should be present
-      const dropdown = wrapper.find('select[name="replication_type"]');
-      expect(dropdown.exists()).toBe(true);
-
-      // Form should have submit button
-      const submitButton = wrapper.find('button[type="submit"]');
-      expect(submitButton.exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(
+          container.querySelector('select[name="replication_type"]')
+        ).toBeInTheDocument();
+      });
+      const dropdown = container.querySelector(
+        'select[name="replication_type"]'
+      );
+      expect(dropdown).toBeInTheDocument();
+      const submitButton = container.querySelector('button[type="submit"]');
+      expect(submitButton).toBeInTheDocument();
     });
   });
 
   describe('Save Functionality', () => {
     it('should have a save button', async() => {
-      wrapper = await mountComponent();
-
-      // Find save button
-      const saveButton = wrapper.find('button[type="submit"]');
-      expect(saveButton.exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(
+          container.querySelector('button[type="submit"]')
+        ).toBeInTheDocument();
+      });
+      const saveButton = container.querySelector('button[type="submit"]');
+      expect(saveButton).toBeInTheDocument();
     });
 
     it('should call save API when form is submitted', async() => {
+      const user = userEvent.setup();
       fetchMock.post('/ops/pglogical_save_subscriptions/new?button=save', {
         message: 'Settings saved successfully',
       });
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'remote');
 
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'remote');
+      const form = container.querySelector('form');
+      await user.click(container.querySelector('button[type="submit"]'));
 
-      // Find and submit the form
-      const form = wrapper.find('form').first();
-
-      await act(async() => {
-        form.simulate('submit', { preventDefault: () => {} });
-      });
-
-      // Wait for async operations
-      await act(async() => {
-        await new Promise((resolve) => setTimeout(resolve, 200));
-      });
-
-      // Verify form submission mechanism exists
-      expect(form.exists()).toBe(true);
+      expect(form).toBeInTheDocument();
     });
 
     it('should prevent save when global type has no subscriptions', async() => {
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'global');
-
-      // Try to save without adding subscriptions
-      const saveButton = wrapper.find('button[type="submit"]').first();
-
-      await act(async() => {
-        saveButton.simulate('click');
+      const user = userEvent.setup();
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'global');
+      await waitFor(() => {
+        expect(
+          container.querySelector('button[type="submit"]')
+        ).toBeInTheDocument();
       });
-
-      // Should not call save API
-      expect(fetchMock.called('/ops/pglogical_save_subscriptions/new?button=save')).toBe(false);
+      const saveButton = container.querySelector('button[type="submit"]');
+      await user.click(saveButton);
+      expect(
+        fetchMock.called('/ops/pglogical_save_subscriptions/new?button=save')
+      ).toBe(false);
     });
   });
 
   describe('Error Handling', () => {
     it('should handle API error on save gracefully', async() => {
+      const user = userEvent.setup();
       fetchMock.post('/ops/pglogical_save_subscriptions/new?button=save', 500);
-
-      wrapper = await mountComponent();
-      await changeReplicationType(wrapper, 'remote');
-
-      // Try to save
-      const form = wrapper.find('form').first();
-
-      await act(async() => {
-        form.simulate('submit', { preventDefault: () => {} });
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await changeReplicationType(container, user, 'remote');
+      const form = container.querySelector('form');
+      await user.click(container.querySelector('button[type="submit"]'));
+      await waitFor(() => {
+        expect(form).toBeInTheDocument();
       });
-
-      // Wait for error handling
-      await act(async() => {
-        await new Promise((resolve) => setTimeout(resolve, 200));
-      });
-
-      // Component should still be mounted and functional
-      expect(wrapper.find(SettingsReplicationForm).exists()).toBe(true);
     });
 
     it('should have proper error boundaries in place', async() => {
-      wrapper = await mountComponent();
-
-      // Component should render without errors
-      expect(wrapper.find(SettingsReplicationForm).exists()).toBe(true);
-      expect(wrapper.find('select[name="replication_type"]').exists()).toBe(true);
-      expect(wrapper.find('button[type="submit"]').exists()).toBe(true);
+      const { container } = renderWithRedux(
+        <SettingsReplicationForm pglogicalReplicationFormId="new" />
+      );
+      await waitFor(() => {
+        expect(container.querySelector('form')).toBeInTheDocument();
+      });
+      expect(
+        container.querySelector('select[name="replication_type"]')
+      ).toBeInTheDocument();
+      expect(
+        container.querySelector('button[type="submit"]')
+      ).toBeInTheDocument();
     });
   });
 });
