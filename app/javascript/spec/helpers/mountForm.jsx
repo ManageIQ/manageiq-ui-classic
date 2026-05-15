4a8942cb48a253715944b72e521c38a3676ed86b
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-05-15 10:31:54 +0530

    Remove Enzyme render helpers

--- app/javascript/spec/helpers/mountForm.jsx
+++ app/javascript/spec/helpers/mountForm.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { mount as enzymeMount, shallow as enzymeShallow } from 'enzyme';
 import { render as rtlRender } from '@testing-library/react';
-
-// Enzyme helpers (legacy - for backward compatibility)
-export const mount = children => enzymeMount(
-  <Provider store={ManageIQ.redux.store}>
-    { children }
-  </Provider>,
-);
-
-export const shallow = children => enzymeShallow(
-  <Provider store={ManageIQ.redux.store}>
-    { children }
-  </Provider>,
-);
 
 // React Testing Library helper
 export const renderWithRedux = (component) => {
