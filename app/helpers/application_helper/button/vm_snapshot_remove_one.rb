f4114dab5662f5818a06cd2ee6e975a9665327df
Adam Grare <adam@grare.com>
2026-05-19 16:37:59 -0400

    Merge pull request #10040 from GilbertCherrie/fix-snapshot-delete-button-enable
    Fix the snapshot page delete toolbar button
    
    (cherry picked from commit cc4c2a56dfda710584b6470e4859032c54005493)

--- app/helpers/application_helper/button/vm_snapshot_remove_one.rb
+++ app/helpers/application_helper/button/vm_snapshot_remove_one.rb
@@ -3,7 +3,6 @@ class ApplicationHelper::Button::VmSnapshotRemoveOne < ApplicationHelper::Button
 
   def disabled?
     # TODO: move ovirt's @active special case into supports mechanism.
-    @error_message = @record.try(:remove_snapshot_denied_message, @active)
     @error_message ||= unless @record.supports?(:remove_snapshot)
                          @record.unsupported_reason(:remove_snapshot)
                        end
