bb0059fe2618d737f5c2c069b2e69d881faf44cd
Gilbert Cherrie <g.cherrie@outlook.com>
2026-06-12 15:12:32 -0400

    Fix rubocop errors

--- app/controllers/application_controller/report_downloads.rb
+++ app/controllers/application_controller/report_downloads.rb
@@ -121,14 +121,14 @@ module ApplicationController::ReportDownloads
     result = MiqReportResult.for_user(current_user).find(params[:id])
 
     @options = report_print_options(result.report, result) # used by the layouts/print
-    
+
     # Prepare data for React component
     @report_data = {
-      :headers => result.report.headers,
+      :headers   => result.report.headers,
       :col_order => result.report.col_order,
-      :title => result.report.title
+      :title     => result.report.title
     }
-    
+
     # Parse HTML rows into structured data
     @table_data = result.table.data.map do |row|
       row_hash = {}
@@ -142,10 +142,10 @@ module ApplicationController::ReportDownloads
       :template => 'layouts/print/report',
       :layout   => 'layouts/print',
       :locals   => {
-        :report => result.report,
-        :data   => result.html_rows.join,
+        :report      => result.report,
+        :data        => result.html_rows.join,
         :report_data => @report_data,
-        :table_data => @table_data
+        :table_data  => @table_data
       }
     )
   end
