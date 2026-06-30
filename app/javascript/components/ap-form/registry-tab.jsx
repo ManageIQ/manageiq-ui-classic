8824a868b8e0fde5b16b5af27a1f86d38e19fa61
ohhAndy <andy.hujze@gmail.com>
2026-06-30 10:39:03 -0400

    use ddf for modal forms, and move css

--- app/javascript/components/ap-form/registry-tab.jsx
+++ app/javascript/components/ap-form/registry-tab.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
 import { useFieldApi, useFormApi } from '@@ddf';
-import {
-  Button, Modal, TextInput,
-} from '@carbon/react';
+import { Button } from '@carbon/react';
 import { Add } from '@carbon/react/icons';
 import MiqDataTable from '../miq-data-table';
 
@@ -12,70 +9,7 @@ const RegistryTab = (props) => {
   } = useFieldApi(props);
   const formOptions = useFormApi();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalValues, setModalValues] = useState({ regKey: '', regValue: '' });
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [errors, setErrors] = useState({});
-
-  const handleOpenModal = () => {
-    setModalValues({ regKey: '', regValue: '' });
-    setEditingIndex(null);
-    setErrors({});
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setModalValues({ regKey: '', regValue: '' });
-    setEditingIndex(null);
-    setErrors({});
-  };
-
-  const validateForm = () => {
-    const newErrors = {};
-    if (!modalValues.regKey || modalValues.regKey.trim() === '') {
-      newErrors.regKey = __('Registry Key is required');
-    }
-    if (!modalValues.regValue || modalValues.regValue.trim() === '') {
-      newErrors.regValue = __('Registry Value is required');
-    }
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
-
-  const handleModalSubmit = () => {
-    if (!validateForm()) {
-      return;
-    }
-
-    const entryData = {
-      depth: 0,
-      hive: 'HKLM',
-      key: modalValues.regKey,
-      value: modalValues.regValue,
-    };
-
-    let newValue;
-    if (editingIndex !== null) {
-      newValue = [...value];
-      newValue[editingIndex] = entryData;
-    } else {
-      newValue = [...value, entryData];
-    }
-
-    formOptions.change(name, newValue);
-    handleCloseModal();
-  };
-
-  const handleEditClick = (index) => {
-    const entry = value[index];
-    setModalValues({
-      regKey: entry.key,
-      regValue: entry.value,
-    });
-    setEditingIndex(index);
-    setIsModalOpen(true);
-  };
+  const { onOpenModal, onEditClick } = props;
 
   const handleDelete = (index) => {
     const newValue = value.filter((_, i) => i !== index);
@@ -115,14 +49,15 @@ const RegistryTab = (props) => {
 
   return (
     <div className="ap-form-registry">
-      <div style={{ marginBottom: '1rem' }}>
+      <h3>{__('Registry Entry')}</h3>
+      <div className="ap-form-tab-toolbar">
         <Button
           kind="primary"
           size="sm"
           renderIcon={Add}
-          onClick={handleOpenModal}
+          onClick={onOpenModal}
         >
-          {__('Add')}
+          {__('Add Registry Entry')}
         </Button>
       </div>
       {rows.length > 0 && (
@@ -140,7 +75,7 @@ const RegistryTab = (props) => {
                 if (keyCell && valueCell) {
                   const index = value.findIndex((e) => e.key === keyCell.value && e.value === valueCell.value);
                   if (index !== -1) {
-                    handleEditClick(index);
+                    onEditClick(index, value[index]);
                   }
                 }
               }
@@ -149,49 +84,6 @@ const RegistryTab = (props) => {
           mode="ap-form-registry"
         />
       )}
-      <Modal
-        open={isModalOpen}
-        modalHeading={editingIndex !== null ? __('Edit Registry Entry') : __('Add Registry Entry')}
-        primaryButtonText={editingIndex !== null ? __('Update') : __('Add')}
-        secondaryButtonText={__('Cancel')}
-        onRequestSubmit={handleModalSubmit}
-        onRequestClose={handleCloseModal}
-        size="sm"
-      >
-        <div className="ap-modal-form" style={{ marginBottom: '1rem' }}>
-          <TextInput
-            id="regKey"
-            labelText={__('Registry Key')}
-            placeholder={__('Enter registry key')}
-            value={modalValues.regKey}
-            onChange={(e) => {
-              setModalValues({ ...modalValues, regKey: e.target.value });
-              if (errors.regKey) {
-                setErrors({ ...errors, regKey: '' });
-              }
-            }}
-            invalid={!!errors.regKey}
-            invalidText={errors.regKey}
-            required
-            style={{ marginBottom: '1rem' }}
-          />
-          <TextInput
-            id="regValue"
-            labelText={__('Registry Value')}
-            placeholder={__('Enter registry value')}
-            value={modalValues.regValue}
-            onChange={(e) => {
-              setModalValues({ ...modalValues, regValue: e.target.value });
-              if (errors.regValue) {
-                setErrors({ ...errors, regValue: '' });
-              }
-            }}
-            invalid={!!errors.regValue}
-            invalidText={errors.regValue}
-            required
-          />
-        </div>
-      </Modal>
     </div>
   );
 };
