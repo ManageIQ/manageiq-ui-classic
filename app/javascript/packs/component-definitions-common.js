8a7b54a572a949da97f92aa0d3fe36af6288e989
ishaaq.sr <ishaaq.sr@xaasio.com>
2026-07-06 14:20:44 +0530

    Add interactive pod terminal UI

--- app/javascript/packs/component-definitions-common.js
+++ app/javascript/packs/component-definitions-common.js
@@ -37,6 +37,7 @@ import CloudVolumeBackupForm from '../components/cloud-volume-backup-form';
 import CloudVolumeForm from '../components/cloud-volume-form';
 import ConditionForm from '../components/condition-form';
 import ContainerDashboardCards from '../components/ems_container_dashboard';
+import ContainerGroupTerminal from "../components/container-group-terminal"
 import ContainerProjects from '../components/container-projects';
 import CopyCatalogForm from '../components/copy-catalog-form/copy-catalog-form';
 import CopyDashboardForm from '../components/copy-dashboard-form/copy-dashboard-form';
@@ -237,6 +238,7 @@ ManageIQ.component.addReact('CloudVolumeBackupForm', CloudVolumeBackupForm);
 ManageIQ.component.addReact('CloudVolumeForm', CloudVolumeForm);
 ManageIQ.component.addReact('ConditionForm', ConditionForm);
 ManageIQ.component.addReact('ContainerDashboardCards', ContainerDashboardCards);
+ManageIQ.component.addReact('ContainerGroupTerminal', ContainerGroupTerminal);
 ManageIQ.component.addReact('ContainerProjects', ContainerProjects);
 ManageIQ.component.addReact('CopyCatalogForm', CopyCatalogForm);
 ManageIQ.component.addReact('CopyDashboardForm', CopyDashboardForm);
