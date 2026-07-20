f3211e64235f37929e38ff7d7a8982bb314a9dc8
Gilbert Cherrie <g.cherrie@outlook.com>
2026-07-20 13:28:55 -0400

    Combine print and non print page

--- config/routes.rb
+++ config/routes.rb
@@ -2886,7 +2886,6 @@ Rails.application.routes.draw do
         remove_security_group
         retire
         right_size
-        right_size_data
         show
         show_list
       ],
@@ -2929,7 +2928,6 @@ Rails.application.routes.draw do
         perf_chart_chooser
         protect
         retire
-        right_size_data
         right_size_print
         show
         tagging_edit
@@ -3023,7 +3021,6 @@ Rails.application.routes.draw do
         explorer
         filesystem_download
         reconfigure_form_fields
-        right_size_data
         right_size_print
         launch_html5_console
         launch_vmrc_console
