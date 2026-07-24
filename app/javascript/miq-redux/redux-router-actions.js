464e36bc1ced407ae5b7f38035a8d4983f9d93e0
Gilbert Cherrie <g.cherrie@outlook.com>
2026-07-24 09:59:10 -0400

    Fix jest specs

--- app/javascript/miq-redux/redux-router-actions.js
+++ app/javascript/miq-redux/redux-router-actions.js
@@ -4,8 +4,8 @@ const createReduxRoutingActions = ({ dispatch }) => ({
   push: (where) => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'push', args: [where] } }),
   replace: (where) => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'replace', args: [where] } }),
   go: (howMany) => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'go', args: [howMany] } }),
-  goBack: () => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'back', args: [] } }),
-  goForward: () => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'forward', args: [] } }),
+  goBack: () => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'go', args: [-1] } }),
+  goForward: () => dispatch({ type: '@@router/NAVIGATE', payload: { method: 'go', args: [1] } }),
 });
 
 export default createReduxRoutingActions;
