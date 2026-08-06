7f3eec732a15bfaa0f213821d1d7759877d561d1
ishaaq-48 <ishaaq.sr@xaasio.com>
2026-08-06 18:16:08 +0530

    reverting to old js

--- app/javascript/packs/remote_consoles_kubevirt_vnc.js
+++ app/javascript/packs/remote_consoles_kubevirt_vnc.js
@@ -1,1 +1,53 @@
-require('./remote_consoles_vnc.js');
+window.$ = window.jQuery = require('jquery');
+const RFB = require('@novnc/novnc').default;
+
+require('../oldjs/i18n.js');
+require('../oldjs/remote_console.js');
+
+$(function() {
+  const host = window.location.hostname;
+  const encrypt = window.location.protocol === 'https:';
+  let port = encrypt ? 443 : 80;
+
+  if (window.location.port) {
+    port = window.location.port;
+  }
+
+  const scheme = encrypt ? 'wss' : 'ws';
+
+  const params = new URLSearchParams(window.location.search);
+
+  const secret = params.get('secret');
+  const dataUrl = params.get('url');
+
+  const url = new URL(dataUrl, `${scheme}://${host}:${port}`).href;
+
+  const rfb = new RFB(
+    document.getElementById('remote-console'),
+    url,
+    {
+      shared: true,
+      credentials: {password: secret}
+    }
+  );
+
+  rfb.addEventListener('connect', () => {
+    $('#connection-status')
+      .removeClass('label-danger label-warning')
+      .addClass('label-success')
+      .text(__('Connected'));
+  });
+
+  rfb.addEventListener('disconnect', (e) => {
+    $('#connection-status')
+      .removeClass('label-success label-warning')
+      .addClass('label-danger')
+      .text(__('Disconnected'));
+
+    console.error('Disconnect:', e.detail.clean ? 'Clean' : 'Unclean');
+  });
+
+  $('#ctrlaltdel').click(() => {
+    rfb.sendCtrlAltDel();
+  });
+});
