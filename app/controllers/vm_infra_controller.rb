94cf3475f571e03027a4901f075d7233a6c6fb23
Gilbert Cherrie <g.cherrie@outlook.com>
2026-08-24 13:30:11 -0400

    Refactor common code to helper function

--- app/controllers/vm_infra_controller.rb
+++ app/controllers/vm_infra_controller.rb
@@ -42,16 +42,18 @@ class VmInfraController < ApplicationController
   end
 
   def prefix_by_nodetype(nodetype)
-    case TreeBuilder.get_model_for_prefix(nodetype).underscore
+    model = TreeBuilder.get_model_for_prefix(nodetype)
+    return nil if model.nil?
+
+    case model.underscore
     when "miq_template" then "templates"
     when "vm"           then "vms"
     end
   end
 
   def set_elements_and_redirect_unauthorized_user
-    params[:id] = normalize_vm_node_id(params[:id])
-    @nodetype, _id = parse_nodetype_and_id(params[:id])
-    prefix = prefix_by_nodetype(@nodetype)
+    prefix = setup_node_and_prefix
+    return true if prefix.nil?
 
     # Position in tree that matches selected record
     if role_allows?(:feature => "vandt_accord")
