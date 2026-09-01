dfa6dcf11494460b77a334063682d48c03a67d5c
Gilbert Cherrie <g.cherrie@outlook.com>
2026-09-01 12:16:30 -0400

    Fix broken translation

--- app/javascript/components/utilization/utilization-report.tsx
+++ app/javascript/components/utilization/utilization-report.tsx
@@ -123,12 +123,10 @@ const UtilizationReport = ({
 
       <hr />
       <p>
-        {sprintf(
-          __('* Information shown is based on available trend data from %s to %s in the %s time zone.'),
-          trendStart,
-          trendEnd,
-          timezone,
-        )}
+        {__('* Information shown is based on available trend data from %{start_time} to %{end_time} in the %{timezone} time zone.')
+          .replace('%{start_time}', trendStart)
+          .replace('%{end_time}', trendEnd)
+          .replace('%{timezone}', timezone)}
       </p>
     </>
   );
