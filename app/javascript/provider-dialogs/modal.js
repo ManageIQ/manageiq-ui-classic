40df0aa6b45e298d3c69ce6b38172ee2e625f3cd
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-09-10 15:09:41 +0530

    Forward onClose to modal body component
    Allows onClose to be invoked from other events or side effects as needed

--- app/javascript/provider-dialogs/modal.js
+++ app/javascript/provider-dialogs/modal.js
@@ -62,7 +62,7 @@ function modal(title, Inner, closed, removeId) {
           {/* <Modal.Title>{title}</Modal.Title> */}
         </ModalHeader>
         <ModalBody>
-          <Inner />
+          <Inner onClose={closed} />
           <div id={/* see closeModal */ removeId} />
         </ModalBody>
         <ModalFooter>
