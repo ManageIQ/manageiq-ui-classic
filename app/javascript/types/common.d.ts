c22b10740b5951d65b5b0274424c895e2d98d4c1
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-09-11 14:43:14 +0530

    Define type for wait_for_task

--- app/javascript/types/common.d.ts
+++ app/javascript/types/common.d.ts
@@ -16,3 +16,48 @@ export type DataType =
  * Shorthand for objects with string keys and DataType values
  */
 export type GenericDataType = Record<string, DataType>;
+
+type MiqTaskState =
+  | 'Initialized'
+  | 'Queued'
+  | 'Active'
+  | 'Finished';
+
+type MiqTaskStatus =
+  | 'Ok'
+  | 'Warn'
+  | 'Error'
+  | 'Timeout'
+  | 'Expired'
+  | 'Unknown';
+
+type MiqTaskAction = {
+  name: string;
+  method: 'post' | 'get' | 'patch' | 'put' | 'delete';
+  href: string;
+};
+
+/**
+ * ManageIQ task response from API.wait_for_task
+ */
+export type MiqTaskResponse = {
+  href: string;
+  id: string;
+  id_value: number;
+  name: string;
+  state: MiqTaskState;
+  status: MiqTaskStatus;
+  message: string;
+  userid: string;
+  created_on: string;
+  updated_on: string;
+  started_on: string | null;
+  pct_complete: number | null;
+  context_data: GenericDataType | null;
+  results: GenericDataType | null;
+  miq_server_id: string;
+  identifier: string | null;
+  zone: string | null;
+  task_results: GenericDataType | null;
+  actions: MiqTaskAction[];
+};
