a1cd572d0d30a9b10d95c33353ede76a1d522e35
Gilbert Cherrie <g.cherrie@outlook.com>
2026-09-15 11:34:25 -0400

    Fix missing firmware feature

--- app/presenters/menu/default_menu.rb
+++ app/presenters/menu/default_menu.rb
@@ -103,14 +103,14 @@ module Menu
 
       def physical_infrastructure_menu_section
         Menu::Section.new(:phy, N_("Physical Infrastructure"), nil, [
-          Menu::Item.new('physical_infra_overview', N_('Overview'),  'physical_infra_overview', {:feature => 'physical_infra_overview'},               '/physical_infra_overview/show'),
-          Menu::Item.new('ems_physical_infra',      N_('Providers'), 'ems_physical_infra',      {:feature => 'ems_physical_infra_show_list'},          '/ems_physical_infra/show_list'),
-          Menu::Item.new('physical_chassis',        N_('Chassis'),   'physical_chassis',        {:feature => 'physical_chassis_show_list'},            '/physical_chassis/show_list'),
-          Menu::Item.new('physical_rack',           N_('Racks'),     'physical_rack',           {:feature => 'physical_rack_show_list'},               '/physical_rack/show_list'),
-          Menu::Item.new('physical_server',         N_('Servers'),   'physical_server',         {:feature => 'physical_server_show_list'},             '/physical_server/show_list'),
-          Menu::Item.new('physical_storage',        N_('Storages'),  'physical_storage',        {:feature => 'physical_storage_show_list'},            '/physical_storage/show_list'),
-          Menu::Item.new('physical_switch',         N_('Switches'),  'physical_switch',         {:feature => 'physical_switch_show_list'},             '/physical_switch/show_list'),
-          Menu::Item.new('firmware_registry',       N_('Firmware Registry'), 'firmware',        {:feature => 'firmware', :any => true},                '/firmware_registry/show_list'),
+          Menu::Item.new('physical_infra_overview', N_('Overview'),  'physical_infra_overview',   {:feature => 'physical_infra_overview'},      '/physical_infra_overview/show'),
+          Menu::Item.new('ems_physical_infra',      N_('Providers'), 'ems_physical_infra',        {:feature => 'ems_physical_infra_show_list'}, '/ems_physical_infra/show_list'),
+          Menu::Item.new('physical_chassis',        N_('Chassis'),   'physical_chassis',          {:feature => 'physical_chassis_show_list'},   '/physical_chassis/show_list'),
+          Menu::Item.new('physical_rack',           N_('Racks'),     'physical_rack',             {:feature => 'physical_rack_show_list'},      '/physical_rack/show_list'),
+          Menu::Item.new('physical_server',         N_('Servers'),   'physical_server',           {:feature => 'physical_server_show_list'},    '/physical_server/show_list'),
+          Menu::Item.new('physical_storage',        N_('Storages'),  'physical_storage',          {:feature => 'physical_storage_show_list'},   '/physical_storage/show_list'),
+          Menu::Item.new('physical_switch',         N_('Switches'),  'physical_switch',           {:feature => 'physical_switch_show_list'},    '/physical_switch/show_list'),
+          Menu::Item.new('firmware_registry',       N_('Firmware Registry'), 'firmware_registry', {:feature => 'firmware_registry', :any => true}, '/firmware_registry/show_list'),
         ])
       end
 
